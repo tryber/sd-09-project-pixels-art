c6f152e0100b4c6305f7198116246099e556e425
Jose Daniel Arreaza <jdaniell.93@gmail.com>
2020-12-08 17:46:22 -0300

    black color seleted class

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const colorBlack = document.querySelectorAll('.color')[0];
+colorBlack.classList.add('selected')
 
 
 
@@ -11,17 +12,29 @@ function selectedColor() {
                 case 'color':
                     allColors[i].classList.add('selected')
                     break;
-            
-                default:
-                    allColors[i].className = 'color';
+
+                case 'color selected':
+                    allColors[i].classList.remove('selected')
                     break;
+            
             }
         })
     }
 }
 
+// function unseletedColor() {
+//     let allColors = document.querySelectorAll('.color');
+//     for (let i = 0; i < allColors.length; i++) {
+//       allColors[i].addEventListener('click', (event) => {
+//             console.log(event.target)
+//         })
+    
+//     }
+// }
 
+let allColors = document.querySelectorAll('.color');
 
 
 
 selectedColor();
+// unseletedColor();
