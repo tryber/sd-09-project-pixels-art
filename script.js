7322b094be253ed049cb9efeedda36abe2a59391
GabrielLennox <meunomeelennox@gmail.com>
2020-12-08 19:21:45 -0300

    ajustes do code climate

--- script.js
+++ script.js
@@ -2,19 +2,19 @@ const colors = document.getElementsByClassName('color');
 const pixels = document.getElementsByClassName('pixel');
 
 for (let index = 0; index < colors.length; index += 1) {
-    let colorSelector = colors[index];
-    colorSelector.addEventListener('click', function(event) {
-        const selected = document.querySelector('.selected');
-        selected.className = 'color';
-        event.target.className = 'color selected';
-    })
+  const colorSelector = colors[index];
+  colorSelector.addEventListener('click', function (event) {
+    const selected = document.querySelector('.selected');
+    selected.className = 'color';
+    event.target.className = 'color selected';
+  });
 }
 
 for (let index = 0; index < pixels.length; index += 1) {
-    let pixelSelector = pixels[index];
-    pixelSelector.addEventListener('click', function(event) {        
-        const selected = document.querySelector('.selected');
-        let selectedColor = window.getComputedStyle(selected);
-        event.target.style.backgroundColor = selectedColor.getPropertyValue('background-color');
-    })
+  const pixelSelector = pixels[index];
+  pixelSelector.addEventListener('click', function (event) {
+    const selected = document.querySelector('.selected');
+    const selectedColor = window.getComputedStyle(selected);
+    event.target.style.backgroundColor = selectedColor.getPropertyValue('background-color');
+  });
 }
