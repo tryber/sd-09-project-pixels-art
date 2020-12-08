bc93d2fbd9751cd68330717932b91a64e035f923
Cristiano Lopes Duarte <tkdcris@gmail.com>
2020-12-08 16:50:20 -0300

    Matrix criada dinamicamente em js

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-let div_colorPalette = document.querySelector('#div_colorPalette');
-let rows = 5;
-let columns = 5;
-function createDivsRowsColumns(rows, columns) {
-  for (let i = 0; i < rows; i += 1) {
-    for (let j = 0; j < columns; j += 1) {
+window.onload = function(){
 
+let div_colorPalette = document.querySelector('#div_colorPalette');
+let div_matriz = document.querySelector('#div_matriz');
+let entrada = 5;
+// let columns = 5;
+function createDivsRowsColumns(entrada) {
+  for (let i = 1; i <= entrada; i += 1) {
+    for (let j = 1; j <= entrada; j += 1) {
+      let div = document.createElement('div');
+      div.id = `${i}x${j}`
+      div_matriz.appendChild(div);
     }
   }
 }
 
-createDivsRowsColumns(rows, columns)
+createDivsRowsColumns(entrada)
 
-console.log(div_colorPalette)
+console.log(div_colorPalette);
+console.log(div_matriz
+  );
+
+
+}
