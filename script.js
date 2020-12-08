c2262401063e8031ccf332429bb03191984d5181
Thiago Granville <thiagogranville@gmail.com>
2020-12-08 18:43:39 -0300

    Correções para o code climate

--- script.js
+++ script.js
@@ -1,71 +1,71 @@
 function createColorPalette() {
-    const colorPalette = document.querySelector('#color-palette');
-    const backgroundColors = ['black', 'red', 'green', 'blue'];
-    const totalNumberOfColors = 4;
-    for (let index = 0; index < totalNumberOfColors; index += 1) {
-      const color = document.createElement('div');
-      color.className = 'color';
-      if (index === 0) {
-        color.classList.add('selected');
-      }
-      color.style.backgroundColor = backgroundColors[index];
-      colorPalette.appendChild(color);
+  const colorPalette = document.querySelector('#color-palette');
+  const backgroundColors = ['black', 'red', 'green', 'blue'];
+  const totalNumberOfColors = 4;
+  for (let index = 0; index < totalNumberOfColors; index += 1) {
+    const color = document.createElement('div');
+    color.className = 'color';
+    if (index === 0) {
+      color.classList.add('selected');
+    }
+    color.style.backgroundColor = backgroundColors[index];
+    colorPalette.appendChild(color);
+  }
+}
+
+createColorPalette();
+
+function createPixelBoard() {
+  const pixelBoardLines = document.querySelectorAll('.pixel-board-line');
+
+  for (let line = 0; line < pixelBoardLines.length; line += 1) {
+    for (let column = 0; column < pixelBoardLines.length; column += 1) {
+      const pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      pixelBoardLines[line].appendChild(pixel);
     }
   }
+}
 
-  createColorPalette();
+createPixelBoard();
 
-  function createPixelBoard() {
-    const pixelBoardLines = document.querySelectorAll('.pixel-board-line');
+function selectColor() {
+  const colorsPalette = document.querySelector('#color-palette');
+  const colors = document.querySelectorAll('.color');
 
-    for (let line = 0; line < pixelBoardLines.length; line += 1) {
-      for (let column = 0; column < pixelBoardLines.length; column += 1) {
-        const pixel = document.createElement('div');
-        pixel.className = 'pixel';
-        pixelBoardLines[line].appendChild(pixel);
+  colorsPalette.addEventListener('click', function (event) {
+    colors.forEach(function (color) {
+      if (event.target.id !== 'color-palette') {
+        color.classList.remove('selected');
+        event.target.classList.add('selected');
       }
-    }
-  }
-
-  createPixelBoard();
-
-  function selectColor() {
-    const colorsPalette = document.querySelector('#color-palette');
-    const colors = document.querySelectorAll('.color');
-
-    colorsPalette.addEventListener('click', function (event) {
-      colors.forEach(function (color) {
-        if (event.target.id !== 'color-palette') {
-          color.classList.remove('selected');
-          event.target.classList.add('selected');
-        }
-      });
     });
-  }
+  });
+}
 
-  selectColor();
+selectColor();
 
-  function paintSelectedPixel() {
-    const pixelBoard = document.querySelector('#pixel-board');
+function paintSelectedPixel() {
+  const pixelBoard = document.querySelector('#pixel-board');
 
-    pixelBoard.addEventListener('click', function (event) {
-      const selectedColor = document.querySelector('.selected');
-      const color = selectedColor.style.backgroundColor;
-      event.target.style.backgroundColor = color;
+  pixelBoard.addEventListener('click', function (event) {
+    const selectedColor = document.querySelector('.selected');
+    const color = selectedColor.style.backgroundColor;
+    event.target.style.backgroundColor = color;
+  });
+}
+
+paintSelectedPixel();
+
+function clearBoard() {
+  const clearButtom = document.querySelector('#clear-board');
+
+  clearButtom.addEventListener('click', function () {
+    const pixels = document.querySelectorAll('.pixel');
+    pixels.forEach(function (pixel) {
+      pixel.style.backgroundColor = 'white';
     });
-  }
+  });
+}
 
-  paintSelectedPixel();
-
-  function clearBoard() {
-    const clearButtom = document.querySelector('#clear-board');
-
-    clearButtom.addEventListener('click', function () {
-      const pixels = document.querySelectorAll('.pixel');
-      pixels.forEach(function (pixel) {
-        pixel.style.backgroundColor = 'white';
-      })
-    })
-  }
-
-  clearBoard();
+clearBoard();
