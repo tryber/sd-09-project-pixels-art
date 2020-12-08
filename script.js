2058efe73219d4e3d0f6f7a2b488033cd0d5cbcd
Daniela Muller <danielamuller20@gmail.com>
2020-12-08 17:44:23 -0300

    Adição do quadro de pixel

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-window.onload = function() {
-  const reedColors = document.querySelectorAll('#color-palette>div');
+const reedColors = document.querySelectorAll('#color-palette>div');
+const boardOfPixel = document.querySelector('#pixel-board');
 
-  function coloPalette() {
-  reedColors[0].style.backgroundColor = 'black';
-  reedColors[1].style.backgroundColor = 'red';
-  reedColors[2].style.backgroundColor = 'green';
-  reedColors[3].style.backgroundColor = 'blue';
+function coloPalette() {
+    reedColors[0].style.backgroundColor = 'black';
+    reedColors[1].style.backgroundColor = 'red';
+    reedColors[2].style.backgroundColor = 'green';
+    reedColors[3].style.backgroundColor = 'blue';
+}
+coloPalette();
+
+function GeneratePixelBoard() {
+  for (let index = 0; index < 25; index += 1) {
+  const divPixels = document.createElement('div');
+  divPixels.className = 'pixel';
+  boardOfPixel.appendChild(divPixels)
   }
 }
-
+GeneratePixelBoard()
