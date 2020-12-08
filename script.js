ca5e989d1c3afdeb651d1ddc49dc75f9ccfb7ce2
MarcioFujita <hanker78@gmail.com>
2020-12-08 20:20:48 -0300

    Requisito 7.V4

--- script.js
+++ script.js
@@ -1,47 +1,45 @@
-window.onload = function () {
   const chngClr = document.querySelectorAll('.color');
   const paletteRed = document.querySelector('.red');
   function changeColorToRed() {
     for (let index = 0; index < chngClr.length; index += 1) {
-        if (index !== 1) {
+      if (index !== 1) {
           chngClr[index].classList.remove('selected');
-        } else {
+      } else {
           paletteRed.classList.add('selected');
         }
-      }
+    }
   }
   const paletteBlue = document.querySelector('.blue');
   function changeColorToBlue() {
     for (let index = 0; index < chngClr.length; index += 1) {
-        if (index !== 2) {
+      if (index !== 2) {
           chngClr[index].classList.remove('selected');
-        } else {
+      } else {
           paletteBlue.classList.add('selected');
-        }
       }
+    }
   }
   const paletteGreen = document.querySelector('.green');
   function changeColorToGreen() {
     for (let index = 0; index < chngClr.length; index += 1) {
-        if (index !== 3) {
+      if (index !== 3) {
           chngClr[index].classList.remove('selected');
-        } else {
+      } else {
           paletteGreen.classList.add('selected');
-        }
       }
+    }
   }
   const paletteBlack = document.querySelector('.black');
   function changeColorToBlack() {
     for (let index = 0; index < chngClr.length; index += 1) {
-        if (index !== 0) {
+      if (index !== 0) {
           chngClr[index].classList.remove('selected');
-        } else {
+      } else {
           paletteBlack.classList.add('selected');
-        }
       }
+    }
   }
   paletteRed.addEventListener('click', changeColorToRed);
   paletteBlue.addEventListener('click', changeColorToBlue);
   paletteGreen.addEventListener('click', changeColorToGreen);
   paletteBlack.addEventListener('click', changeColorToBlack);
-}
