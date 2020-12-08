72d89eac4637486e8881b5bd26f2a6622de8b724
Khyrki <khyrkimitsunobi@gmail.com>
2020-12-08 16:12:20 -0300

    corrige bug

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function fillColorPalette() {
   const divColor = document.getElementsByClassName('color');
+  divColor[0].style.backgroundColor = 'black';
   for (let index = 1; index < divColor.length; index += 1) {
     const randoRgbNumber = Math.ceil(Math.random() * 255);
     const randoRgbNumber1 = Math.ceil(Math.random() * 255);
     const randoRgbNumber2 = Math.ceil(Math.random() * 255);
     const rgb = 'rgb( '+ randoRgbNumber + ' , ' + randoRgbNumber1 + ' , ' + randoRgbNumber2 + ' )';
     divColor[index].style.backgroundColor = rgb;
-    divColor[0].style.backgroundColor = 'black';
   }
 }
 fillColorPalette();
