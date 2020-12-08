d6c134be5b4dccb80d4362d3d115fb15663f48ac
Rafael Geronimo <rafaelgeronimo@outlook.com>
2020-12-08 17:54:27 -0300

    Requisito 6 - Ao carregar a página, a cor preta da paleta já deve estar selecionada para pintar os pixels.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,38 @@
 window.onload = function() {
   createColor();
+  drawPixelBoard(5);
 }
 function createColor() {
-  let colors = document.querySelectorAll('.color');
-  for (let index = 1; index < colors.length; index += 1) {
+  let colorBox = document.querySelectorAll('.color');
+  for (let index = 1; index < colorBox.length; index += 1) {
     let randomColor = [];
     let rgbColors = 3;
     for (let index = 0; index < rgbColors; index += 1) {
       randomColor.push(Math.floor(Math.random()*254));
     }
-    colors[index].style.backgroundColor = 'rgb('+randomColor+')';
+    colorBox[index].style.backgroundColor = 'rgb('+randomColor+')';
+  }
+  colorBox[0].classList.add('selected');
+}
+
+function drawPixelBoard(lines) {
+  let pixelBoard = document.querySelector('#pixel-board');
+  for (let line = 1; line <= lines; line += 1) {
+    let lineId = 'line'+line;
+    let newLine = document.createElement('tr');
+    newLine.setAttribute('id', lineId);
+    pixelBoard.appendChild(newLine);
+    drawColumns(line, 5);
+  }
+}
+
+function drawColumns(lineId, columns) {
+  let line = document.querySelector('#line'+lineId);
+  for (let lineColumn = 1; lineColumn <= columns; lineColumn += 1) {
+    //let pixelId = 'pixel'+ lineId +'.'+lineColumn;
+    let newPixel = document.createElement('td');
+    //newPixel.setAttribute('id', pixelId);
+    newPixel.setAttribute('class', 'pixel');
+    line.appendChild(newPixel);
   }
 }
