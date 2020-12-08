730bcb18d0a240e38d76457260e1c4cb77f212b6
Khyrki <khyrkimitsunobi@gmail.com>
2020-12-08 15:46:20 -0300

    Adiciona logica que preeche cada div .color com uma cor diferente

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const body = document.querySelector('body');
-const createHeder = document.createElement('header');
-body.appendChild(createHeder);
-const header = document.querySelector('header');
-header.id = 'Header';
-header.appendChild(document.createElement('h1'));
-const h1 = document.querySelector('h1');
-h1.id = 'title';
-h1.innerText = 'Paleta de Cores';
+function fillColorPalette () {
+  const divColor = document.getElementsByClassName('color');
+  for (let index = 0; index < divColor.length; index += 1) {
+    let randoRgbNumber = Math.ceil(Math.random() * 255);
+    let randoRgbNumber1 = Math.ceil(Math.random() * 255);
+    let randoRgbNumber2 = Math.ceil(Math.random() * 255);
+    let rgb = 'rgb( '+ randoRgbNumber + ' , ' + randoRgbNumber1 + ' , ' + randoRgbNumber2 + ' )';
+    divColor[index].style.backgroundColor = rgb;
+    console.log(rgb)
+  }
+}
+fillColorPalette ();
