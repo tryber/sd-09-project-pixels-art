b14b8b14f1bdd6489961aa3ee13ceb74e878856c
Layo Kaminski <layokaminski@gmail.com>
2020-12-08 15:11:09 -0300

    Requisito 2

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    function colorPalette () {
+        let color = document.querySelectorAll('.color')
+        color[0].style.backgroundColor = 'black'
+        color[1].style.backgroundColor = 'steelblue'
+        color[2].style.backgroundColor = 'red'
+        color[3].style.backgroundColor = 'yellow'
+    }
+    colorPalette();
+}
