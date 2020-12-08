a6cfb3b522553cfd503214258dbac5eb52a0822d
Thiago Granville <thiagogranville@gmail.com>
2020-12-08 16:27:55 -0300

    Correções para o code climate no requisito 4

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 function createPixelBoard() {
   const pixelBoardLines = document.querySelectorAll(".pixel-board-line");
 
-  for (line of pixelBoardLines) {
-    for (let index = 0; index < pixelBoardLines.length; index += 1) {
+  for (let line = 0; line < pixelBoardLines.length; line += 1) {
+    for (let column = 0; column < pixelBoardLines.length; column += 1) {
       const pixel = document.createElement('div');
       pixel.className = 'pixel';
-      line.appendChild(pixel);
-      console.log(index);
+      pixelBoardLines[line].appendChild(pixel);
     }
   }
 }
