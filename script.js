1abd616ff36799dcbcab3df9949c4082fb580c42
Ana Luiza Machado Salgado <analuizamsalgado@gmail.com>
2020-12-08 17:36:27 -0300

    Adiciona requisito 7

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
-let pixelBoard = document.getElementsByClassName('pixel');
-    pixelBoard.addEventListener('click', selectColor);
+// let pixelBoard = document.getElementsByClassName('pixel');
+//     pixelBoard.addEventListener('click', selectColor);
 
 function selectColor() {
-    let selectedList = document.getElementsByClassName('selected');
+    let selectedColor = document.getElementsByClassName('color selected');
     let colors = document.getElementsByClassName('color')
 
-    colors.addEventListener('click', function(event) {
-        if (selectedList.length === 1) {
-            for (let index = 0; index < selectedList.length; index += 1) {
-                selectedList[index].className = 'color';
+    for (let index = 0; index < colors.length; index += 1) {
+        colors[index].addEventListener('click', function(event) {
+
+            for (let index = 0; index < selectedColor.length; index +=1) {
+                selectedColor[index].className = 'color';
             }
 
             event.target.className = 'color selected';
             console.log(event.target.className);
-        } else {
-            event.target.className = 'color'
-            console.log(event.target.className);
-        }
-    })
-
-
-
+        })
+    }
 }
 
 selectColor();
