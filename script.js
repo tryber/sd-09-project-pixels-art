968445b2654b79d854c93f38b6a06709005c9b66
Cristiano Lopes Duarte <tkdcris@gmail.com>
2020-12-08 18:19:36 -0300

    pixel-board ajustado

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = function() {
   let div_colorPalette = document.querySelector('#color-palette');
-  let div_matriz = document.querySelector('#div_matriz');
+  let div_matriz = document.querySelector('#pixel-board');
   let entrada = 5;
   div_matriz.style.width = (entrada*40);
   function createDivsRowsColumns(entrada) {
@@ -8,7 +8,7 @@ window.onload = function() {
       for (let j = 1; j <= entrada; j += 1) {
         let div = document.createElement('div');
         div.id = `${i}x${j}`
-        div.className = 'divs_matriz';
+        div.className = 'pixel';
         div_matriz.appendChild(div);
       }
     }
