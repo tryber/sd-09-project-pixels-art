75c2cab4d073462efad120ad800f1dde8117c7ed
Johne Alves <johne_af@hotmail.com>
2020-12-08 18:02:32 -0300

    adiciona requisito 7

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-window.onload = function () {
-    let pixelBlack = document.getElementById('black')
-    pixelBlack.className += " selected"
+
+function changeSelected () {
+  let paletteColors = document.getElementById('color-palette')
+  paletteColors.addEventListener('click', function () {
+    let palettes = document.querySelectorAll('.color');
+      for (let index = 0; index < palettes.length; index += 1) {
+        palettes[index].className = 'color'
+      }
+    event.target.className = "color selected"
+  })
 }
+changeSelected()
