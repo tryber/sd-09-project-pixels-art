460567030115a906ad55306e8e76749fb3364946
danielfasanaro <danielfasanaro@gmail.com>
2020-12-08 17:26:22 -0300

    2nd Requirement - Made color pallets

---
+++ script.js
@@ -0,0 +1,24 @@
+function drawColorPallets() {
+  let pallets = document.querySelectorAll('#color-palette .color');
+  let colors = ['black', 'red', 'green', 'blue'];
+  for (let index = 0; index < pallets.length; index += 1) {
+    pallets[index].style.backgroundColor = colors[index];
+    pallets[index].style.border = '1px black solid';
+    pallets[index].style.display = 'inline-block';
+    pallets[index].style.width = '40px';
+    pallets[index].style.height = '40px';
+  }
+
+  pallets[0].style.backgroundColor = 'black';
+  pallets[1].style.backgroundColor = 'red';
+  pallets[2].style.backgroundColor = 'green';
+  pallets[3].style.backgroundColor = 'blue';
+}
+
+function randomRGB () {
+  Math.random
+}
+
+window.onload = function() {
+  drawColorPallets();
+}
