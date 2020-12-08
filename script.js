c4522b608467301d6af09da453c880735d3eb0f7
Ana Luiza Machado Salgado <analuizamsalgado@gmail.com>
2020-12-08 18:06:43 -0300

    Adiciona requisito 8

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-// let pixelBoard = document.getElementsByClassName('pixel');
-//     pixelBoard.addEventListener('click', selectColor);
-
 function selectColor() {
-    let selectedColor = document.getElementsByClassName('color selected');
-    let colors = document.getElementsByClassName('color')
+    const selectedColors = document.getElementsByClassName('color selected');
+    const colors = document.getElementsByClassName('color');
 
-    for (let index = 0; index < colors.length; index += 1) {
-        colors[index].addEventListener('click', function(event) {
+  for (let index = 0; index < colors.length; index += 1) {
+    colors[index].addEventListener ('click', function (event) {
+      for (let index2 = 0; index2 < selectedColors.length; index2 += 1) {
+        selectedColors[index].className = 'color';
+      }
 
-            for (let index = 0; index < selectedColor.length; index +=1) {
-                selectedColor[index].className = 'color';
-            }
+      event.target.className = 'color selected';
+      console.log(event.target.id);
 
-            event.target.className = 'color selected';
-            console.log(event.target.className);
-        })
-    }
+    });
+  }
 }
 
 selectColor();
+
+
+function colorPixel() {
+  let pixels = document.getElementsByClassName('pixel');
+  let selectedColor = document.querySelector('.color.selected');
+  let bgColor = selectedColor.backgroundColor;
+
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].addEventListener('click', function(event) {
+
+      event.target.backgroundColor = bgColor;
+
+    });
+  }
+}
+
+colorPixel();
