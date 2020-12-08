6a571956f734860e2c791431e919a3e008084386
MZTBTG <mztbtg@hotmail.com>
2020-12-08 20:54:26 -0300

    Corrigindo erros no CodeClimate

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 window.onload = function start() {
-    pixelBoard(5)
-}
-
-function pixelBoard(matrix) {
-    for (index = 0; index < matrix; index += 1) {
-        let pixelsLine = document.createElement('div');
-        pixelsLine.className = 'pixel-line'
-        document.getElementById('pixel-board').appendChild(pixelsLine);
-        for (index2 = 0; index2 < matrix; index2 += 1) {
-            let pixelsColumn = document.createElement('div');
-            pixelsColumn.className = 'pixel';
-            document.querySelectorAll('.pixel-line')[index].appendChild(pixelsColumn);
-        }
+  function pixelBoard(matrix) {
+    for (let index = 0; index < matrix; index += 1) {
+      const pixelsLine = document.createElement('div');
+      pixelsLine.className = 'pixel-line';
+      document.getElementById('pixel-board').appendChild(pixelsLine);
+      for (let index2 = 0; index2 < matrix; index2 += 1) {
+        const pixelsColumn = document.createElement('div');
+        pixelsColumn.className = 'pixel';
+        document.querySelectorAll('.pixel-line')[index].appendChild(pixelsColumn);
+      }
     }
+  }
+
+pixelBoard(5);
 }
