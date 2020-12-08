66ba8362aca94e9592d42e030a543189cc0eba0f
Layo Kaminski <layokaminski@gmail.com>
2020-12-08 18:13:21 -0300

    Requisito 4 e 5

--- script.js
+++ script.js
@@ -9,19 +9,23 @@ window.onload = function() {
     colorPalette();
     
     let pixelBoard = document.querySelector('#pixel-board')
-    
-    function createdPixelLine() {
-        for (let index = 0; index < 25; index += 1) {
+
+    function createdPixelLine(line) {
+        for (let index = 0; index < 5; index += 1) {
             let createdElement = document.createElement('div')
-            if (index < 5) {
-            createdElement.classList = 'pixel line'
-            } else {
-                createdElement.classList = 'pixel column'
-            }
-            pixelBoard.appendChild(createdElement)
+            createdElement.className = 'pixel'
+            line.appendChild(createdElement)
+        }
+    }
+
+        function createdPixelColumn() {
+            for (let index = 0; index < 5; index += 1) {
+                let line = document.createElement('div')
+                line.className = 'line'
+                pixelBoard.appendChild(line)
+                createdPixelLine(line)
             }
         }
-        createdPixelLine();
-        
+        createdPixelColumn();
 }
     
