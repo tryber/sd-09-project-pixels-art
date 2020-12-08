cb5ea599e34bd7ba689110ded4af675cebf7a5b4
Carine Becker <caarine.m@gmail.com>
2020-12-08 17:47:25 -0300

    req 5

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-
-function createPixelsBoard() {
-  const pixelBoard = document.querySelector('#pixel-board');
-  const pixelColumn = 5;
-
-  for (let indexLine = 0; indexLine < pixelColumn; indexLine += 1) {
-    const pixelLine = document.createElement('div');
-    pixelLine.classList.add('pixel-line');
-
-    for (let indexColumn = 0; indexColumn < pixelColumn; indexColumn += 1) {
-      const pixel = document.createElement('div');
-      pixel.classList.add('pixel');
-      pixelLine.appendChild(pixel);
+window.onload = function(){
+  function createPixelsBoard() {
+    const pixelBoard = document.querySelector('#pixel-board');
+    const pixelColumn = 5;
+    
+    for (let indexLine = 0; indexLine < pixelColumn; indexLine += 1) {
+      const pixelLine = document.createElement('div');
+      pixelLine.classList.add('pixel-line');
+      
+      for (let indexColumn = 0; indexColumn < pixelColumn; indexColumn += 1) {
+        const pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+        pixelLine.appendChild(pixel);
+      }
+      pixelBoard.appendChild(pixelLine);
     }
-    pixelBoard.appendChild(pixelLine);
   }
+  createPixelsBoard();
 }
-createPixelsBoard();
