8ae468fcbf54043184212360d98a57a756fe48a4
marcusWittho <witthoeft.mcs@hotmail.com>
2020-12-08 17:52:47 -0300

    Pixel-Art, resolução do requisito 4

--- script.js
+++ script.js
@@ -18,3 +18,30 @@ function colorizePalette() {
   }
 }
 colorizePalette();
+
+function createPixelsLine() {
+  let pixelsRows = document.createElement('tr');
+  pixelsRows.style.width = '40px';
+  pixelBoard.appendChild(pixelsRows);
+}
+
+function createPixelsColumns(line = 0) {
+  let lines = document.querySelectorAll('tr');
+  let pixelsColumn = document.createElement('td');
+  pixelsColumn.className = 'pixel';
+  pixelsColumn.style.height = '40px';
+  pixelsColumn.style.border = '1px solid gray';
+  lines[line].appendChild(pixelsColumn);
+}
+
+function createBoard(width = 5) {
+  pixelBoard.style.width = `${width * 40}px`;
+
+  for (let rows = 0; rows < width; rows += 1) {
+    createPixelsLine();
+    for (let column = 0; column < width; column += 1) {
+      createPixelsColumns(rows);
+    }
+  }
+}
+createBoard(5);
