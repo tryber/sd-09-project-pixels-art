98a58d5a4d368795024d495f9ba8470ca4456982
Tiago Yoneda <xyonedax@gmail.com>
2020-12-08 20:03:10 -0300

    adicionando algumas funcoes

--- script.js
+++ script.js
@@ -1,6 +1,40 @@
 let baseDoQuadro = 5;
 sessionStorage.selectedColor = 'black';
 
+const colorArray = ['#2a9d8f','#f4a261','#c5c3c6','#46494c','#1985a1',
+  '#cb997e','#ddbea9','#e5989b','#b5838d','#e63946','#a8dadc','#457b9d',
+  '#457b9d','#f4f1de','#e07a5f','#81b29a','#f2cc8f','#ffd6a5','#9bf6ff',
+  '#ffc6ff','#d00000','#ffba08','#00b4d8','#90e0ef','#caf0f8','#fca311',
+  '#06d6a0','#073b4c','#f77f00','#fcbf49','#b7e4c7','#8ecae6','#ffddd2',
+  '#ff006e','#3a86ff','#8338ec','#48cae4','#ea9ab2','#b3dee2','#4ecdc4',
+  '#ffe66d','#d8572a','#edb183','#251605','#0d21a1','#e18ad4','#f2545b',
+  '#890620','#890620','#01baef'];
+
+function pickStarterColors(array) {
+  let starterColors = [];
+  let ok;
+  let num;
+  for (let idx = 0; idx < 3 ; idx += 1) {
+    ok = false;
+    while (!ok) {
+      num = Math.floor(Math.random()*array.length);
+      if (!starterColors.includes(array[num])) {
+        starterColors.push(array[num]);
+        ok = true;
+      }
+    }
+  }
+  return (starterColors);
+}
+
+function setStarterColors(){
+  let starterColors = pickStarterColors(colorArray);
+  let colors = document.querySelectorAll('.color');
+  for (let idx = 1; idx < colors.length; idx += 1){
+    colors[idx].style.backgroundColor = starterColors[idx-1];
+    colors[idx].innerText = starterColors[idx-1];
+  }
+}
 
 function clearBoard() {
   let allPixels = document.querySelectorAll('.pixel');
@@ -38,7 +72,7 @@ for (let idx = 0; idx < minhasLinhas.length; idx += 1) {
 
 const colorButtons = document.querySelectorAll('.color');
 
-function removeColorButtonMarks () {
+function removeColorButtonMarks() {
   for (let idx = 0; idx < colorButtons.length; idx += 1) {
     if (colorButtons[idx].classList.contains('selected')) {
       colorButtons[idx].classList.toggle('selected');
@@ -54,19 +88,19 @@ function selectColor(self) {
 }
 
 for (let idx = 0; idx < colorButtons.length; idx += 1) {
-  colorButtons[idx].addEventListener('click', function(target) {
+  colorButtons[idx].addEventListener('click', function (target) {
     selectColor(target);
   });
 }
 
-function colorPixel(self){
+function colorPixel(self) {
   self.target.style.backgroundColor = sessionStorage.selectedColor;
 }
 
 const myPixels = document.querySelectorAll('.pixel');
 
 for (let idx = 0; idx < myPixels.length; idx += 1) {
-  myPixels[idx].addEventListener('click', function(target) {
+  myPixels[idx].addEventListener('click', function (target) {
     colorPixel(target);
   });
 }
