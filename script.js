d4afc9e938503d8e0836be8a36754b66cdb2e9b8
Felipe Rocha <felipe.sr27@gmail.com>
2020-12-08 16:54:13 -0300

    Ajustes reportados pelo codeclimate dos Requisitos 2 e 3.

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
 window.onload = function () {
-  //const paletteColors = document.querySelectorAll('.color');
-  const paletteColor = document.querySelector('#color-palette')
+  const paletteColor = document.querySelector('#color-palette');
   const colorsArray = ['black', 'blue', 'green', 'red'];
 
   function createDivs() {
-    let div = document.createElement('div');
+    const div = document.createElement('div');
     return div;
-  };
-
-  function createColorsPalette(palette) {
-    let palleteArray = [];
-    for (let index = 0; index < 4; index += 1){
-      let paletteSquare = palette.appendChild(createDivs());
-      paletteSquare.className = 'color';
-      palleteArray.push(paletteSquare);
-    }
-    addColorSquare(palleteArray, colorsArray);
-  };
+  }
 
   function addColorSquare(palette, colors) {
     for (let index = 0; index < palette.length; index += 1) {
       palette[index].style.backgroundColor = colors[index];
     }
-  };
+  }
+
+  function createColorsPalette(palette) {
+    const palleteArray = [];
+    for (let index = 0; index < 4; index += 1) {
+      const paletteSquare = palette.appendChild(createDivs());
+      paletteSquare.className = 'color';
+      palleteArray.push(paletteSquare);
+    }
+    addColorSquare(palleteArray, colorsArray);
+  }
 
   createColorsPalette(paletteColor);
-  //addColorSquare(paletteColors, colorsArray);
 
 };
