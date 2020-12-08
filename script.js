447fb2d1175b2588f1fc2a67a73c31ea51e4c8b3
Lucas Silva <martssilvapro@gmail.com>
2020-12-08 16:47:46 -0300

    js iniciado

---
+++ script.js
@@ -0,0 +1,15 @@
+// Seleciona a cor preta ao abrir o app
+function colorBlackSelectedOnload() {
+  let black = document.getElementById('black')
+
+  return black
+}
+console.log(colorBlackSelectedOnload())
+
+// Função que pinta os quadrados
+function paintSquare() {
+
+}
+
+// 1 - faço a função que vai selecionar a cor preta no carregamento da página
+// 2 - crio a função que vai pintar os quadrados
