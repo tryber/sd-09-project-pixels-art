bc5159c19b2e5296d481cd6fb2282582545a142e
Danrlley Rodrigues <danrlley.c.a.rodrigues@gmail.com>
2020-12-08 19:04:20 -0300

    Requisito 6 Corrigido 3

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 window.onload(document.getElementsByClassName('color1')[0].classList.add('selected'))
-var colorSelected = getElementsByClassName('selected')
 
 
+function changeSelected () {
+    if (document.querySelector('.color2').addEventListener('onclick')) {
+        document.querySelector('.selected').classList.remove('selected');
+        document.querySelector('color2').classList.add('selected')
+    }
+}
+
+changeSelected();
