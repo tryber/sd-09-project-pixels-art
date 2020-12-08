8478b58a51e76312cbc3a2f0b0e4687c38843d32
Ana Luiza Machado Salgado <analuizamsalgado@gmail.com>
2020-12-08 19:12:32 -0300

    Adiciona requisito 9

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-window.onload = function() {
-  selectColor();
-  colorPixel();
-}
-
 function selectColor() {
   const selectedColors = document.getElementsByClassName('color selected');
   const colors = document.getElementsByClassName('color');
@@ -10,10 +5,10 @@ function selectColor() {
   for (let index = 0; index < colors.length; index += 1) {
     colors[index].addEventListener('click', function(event) {
       for (let index2 = 0; index2 < selectedColors.length; index2 += 1) {
-        selectedColors[index2].className = 'color';
+        selectedColors[index2].classList.remove('selected');
       }
 
-      event.target.className = 'color selected';
+      event.target.classList.add('selected')
       console.log(event.target.id);
     });
   }
@@ -23,7 +18,7 @@ function colorPixel() {
   const pixels = document.getElementsByClassName('pixel');
 
   for (let index = 0; index < pixels.length; index += 1) {
-    pixels[index].addEventListener ('click', function (event) {
+    pixels[index].addEventListener('click', function(event) {
       const selectedColor = window.getComputedStyle(document.querySelector('.color.selected'));
       const bgColor = selectedColor.backgroundColor;
       event.target.style.backgroundColor = bgColor;
@@ -31,3 +26,27 @@ function colorPixel() {
   }
 }
 
+function createBtn() {
+  const btnSection = document.querySelector('#button-section');
+  const clearBtn = document.createElement('button');
+  clearBtn.id = 'clear-board';
+  clearBtn.innerText = 'Limpar';
+  btnSection.appendChild(clearBtn);
+}
+
+function clearBoard() {
+  const clearBtn = document.querySelector('#clear-board');
+  clearBtn.addEventListener('click', function() {
+    const pixels = document.getElementsByClassName('pixel');
+    for (let index = 0; index < pixels.length; index += 1) {
+      pixels[index].style.backgroundColor = 'white';
+    }
+  })
+}
+
+window.onload = function () {
+  selectColor();
+  colorPixel();
+  createBtn();
+  clearBoard();
+}
