8b62c3c69cb9c15b9b5b588048637d629c2d89e2
Pablo Fernandes <pabloprates@gmail.com>
2020-12-08 17:15:06 -0300

    Change color is working

--- script.js
+++ script.js
@@ -2,9 +2,30 @@ window.onload = function () {
     let currentColorSelected = 'black';
     let colorPalette = document.getElementById('color-palette');
     colorPalette.addEventListener('click', setColorToPaint);
+    let pixelBoard = document.getElementById('pixel-board');
+    pixelBoard.addEventListener('click', changePixelColor); 
 
     function setColorToPaint (evt) {
-        let colorSelected = evt.target.id;
-        currentColorSelected = colorSelected;
+        let colorSelectedElementId = evt.target.id;
+        chageClassSelectedElement(colorSelectedElementId);
+        currentColorSelected = colorSelectedElementId;       
+    }
+
+    function changePixelColor (evt) {
+        if (evt.target.className === 'pixel') {
+            let clickedElement = evt.target;
+            clickedElement.style.backgroundColor = currentColorSelected;
+        }
+    }   
+
+    function chageClassSelectedElement (elementId) {
+        let element = document.getElementById(elementId);
+        element.classList.add('selected');
+        removePreviousSelectedElementClass(currentColorSelected);
+    }
+
+    function removePreviousSelectedElementClass (elementId) {
+        let element = document.getElementById(elementId);
+        element.classList.toggle('selected');
     }
 }
