77adf569e33058e7f8ec1858a70fa1b0a695ef13
Carine Becker <caarine.m@gmail.com>
2020-12-08 18:24:52 -0300

    fixed req 5

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 window.onload = function () {
-  function createPixelsBoard() {
-    const pixelBoard = document.querySelector('#pixel-board');
-    const pixelColumn = 5;
-
-    for (let indexLine = 0; indexLine < pixelColumn; indexLine += 1) {
-      const pixelLine = document.createElement('div');
-      pixelLine.classList.add('pixel-line');
-
-      for (let indexColumn = 0; indexColumn < pixelColumn; indexColumn += 1) {
-        const pixel = document.createElement('div');
-        pixel.classList.add('pixel');
-        pixelLine.appendChild(pixel);
-      }
-      pixelBoard.appendChild(pixelLine);
-    }
-  }
   createPixelsBoard();
-};
+}
+
+function createPixelsBoard() {
+  const pixelBoard = document.querySelector('#pixel-board');
+  const pixelColumn = 5;
+
+  for (let indexLine = 0; indexLine < pixelColumn; indexLine += 1) {
+    const pixelLine = document.createElement('div');
+    pixelLine.classList.add('pixel-line');
+
+    for (let indexColumn = 0; indexColumn < pixelColumn; indexColumn += 1) {
+      const pixel = document.createElement('div');
+      pixel.classList.add('pixel');
+      pixelLine.appendChild(pixel);
+    }
+    pixelBoard.appendChild(pixelLine);
+  }
+}
