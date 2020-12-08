de983b3c9b4f106204d279fcee1a1b1ab431694a
ViniciusChagas <viniciushrcs@gmail.com>
2020-12-08 17:15:25 -0300

    requisitos 4 e 5

---
+++ script.js
@@ -0,0 +1,33 @@
+window.onload = teste (5, 5)
+
+    function teste (width, height){
+    let numberOfLines = width;
+    let numberOfColumns = height;
+    let getPixelBoard = document.getElementById('pixel-board');
+    
+    fillPixelBoard()
+
+    //cria 1 pixel
+    function createPixel (){
+        let pixel = document.createElement('div');
+        pixel.className = 'pixel';
+        pixel.style.background = 'white'
+        return pixel 
+    }
+
+    // acrescentar o número de linhas no pixelboard
+    function fillPixelBoard(){
+        for (let index = 0; index < numberOfLines; index += 1){
+            let lines = document.createElement('div');
+            lines.className = 'lines';
+            //acrescentar o número de colunas nas linhas 
+            for(let index = 0; index < numberOfColumns; index += 1){
+                let pixel = createPixel();
+                lines.appendChild(pixel);
+            };
+            getPixelBoard.appendChild(lines);
+        }
+    }
+
+
+}
