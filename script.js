dedf475838b036b5db3a2533640e46fcad1ef926
Pedro Henrique <pedro.876.ph@gmail.com>
2020-12-08 16:57:59 -0300

    requisito 4 talvez esteja certo

---
+++ script.js
@@ -0,0 +1,15 @@
+let pixelBoard = document.querySelector('.pixel-board');
+let pxColumn = document.querySelector('.pixel-column');
+function pixelLine(){
+    let line;
+    for(let lineIndex = 0; lineIndex < 5; lineIndex += 1) {
+        let lineContainer = document.createElement('div');
+        for(let columnIndex = 0; columnIndex < 5; columnIndex += 1) {
+            let pxColumn = document.createElement('div');
+            pxColumn.className = 'color';
+            lineContainer.appendChild(pxColumn);
+        }
+        pixelBoard.appendChild(lineContainer);
+    }
+}
+pixelLine();
