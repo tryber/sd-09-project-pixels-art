8ab43dc0d371b37b8ad19e521ad2df79d85072db
Daniela Muller <danielamuller20@gmail.com>
2020-12-08 15:19:54 -0300

    Primeira parte do projeto

---
+++ script.js
@@ -0,0 +1,11 @@
+let reedColors = document.querySelectorAll('#color-palette>div');
+// for (let index = 0; index <reedColors.length; index += 1) {
+// }
+
+reedColors[0].style.backgroundColor = 'black';
+reedColors[1].style.backgroundColor = 'red';
+reedColors[2].style.backgroundColor = 'green';
+reedColors[3].style.backgroundColor = 'blue';
+
+
+
