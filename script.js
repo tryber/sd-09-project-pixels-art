1626f8cab0104146c6a81c244a1ab003186357f2
Thiago Carreira Vallim <thiagocvallim@gmail.com>
2020-12-08 17:39:21 -0400

    Adiciona exercicio 10 e 11

--- script.js
+++ script.js
@@ -12,22 +12,61 @@ const palette2 = document.querySelector('#color2');
 const palette3 = document.querySelector('#color3');
 const palette4 = document.querySelector('#color4');
 const btnErase = document.querySelector('#clear-board');
+const numberSquare = document.querySelector('#board-size');
+const generateSquare = document.querySelector('#generate-board');
+let atualNumber = 5;
+
+function isInside(size) {
+  let rightSize = size;
+  if (size < 5) {
+    rightSize = 5;
+  } else if (size > 50) {
+    rightSize = 50;
+  }
+  return rightSize;
+}
+
+function makeSquare() {
+  let widthSquare = parseInt(numberSquare.value);
+  if (widthSquare == NaN) {
+    alert('Board inválido!');
+  } else {
+    widthSquare = isInside(widthSquare);
+  }
+  return widthSquare;
+}
 
 function makeLinePixels(line) {
-  for (let index = 0; index < 5; index += 1) {
+  let numberOfSquare = makeSquare();
+  for (let index = 0; index < numberOfSquare; index += 1) {
     const pixel = document.createElement('div');
     pixel.className = 'pixel';
     line.appendChild(pixel);
   }
+  atualNumber = makeSquare();
+}
+
+function reposeSquare() {
+  const pixelBoard = document.querySelector('#pixel-board');
+  let childrenPixel = document.querySelectorAll('.linePixel');
+  for (let index = 0; index < atualNumber; index += 1){
+    pixelBoard.removeChild(childrenPixel[index]);
+  }
+  makePixels();
 }
 
 function makePixels() {
-  for (let index = 0; index < 5; index += 1) {
+  let numberOfSquare = makeSquare();
+  for (let index = 0; index < numberOfSquare; index += 1) {
     const pixelLine = document.createElement('div');
     makeLinePixels(pixelLine);
     pixelLine.className = 'linePixel';
     pixelBoard.appendChild(pixelLine);
   }
+}
+
+function setNumbersOfSquares() {
+  generateSquare.addEventListener('click', reposeSquare);
 }
 
 function makePixelColorized(event) {
@@ -53,7 +92,7 @@ function setSelected() {
 
 function eraseAll() {
   const pixel = document.querySelectorAll('.pixel');
-  for(let index = 0; index < pixel.length; index += 1) {
+  for (let index = 0; index < pixel.length; index += 1) {
     pixel[index].style.backgroundColor = 'white';
   }
 }
@@ -62,7 +101,9 @@ function eraseScrean() {
   btnErase.addEventListener('click', eraseAll);
 }
 
+
 makePixels();
 colorizePixels();
 setSelected();
 eraseScrean();
+setNumbersOfSquares();
