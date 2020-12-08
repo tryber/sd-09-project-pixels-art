5fc3652085d22697917f72a69e904fdf9f36b379
danielfasanaro <danielfasanaro@gmail.com>
2020-12-08 18:36:32 -0300

    4th Requiriment - Fixed pixelBoard

--- script.js
+++ script.js
@@ -15,12 +15,12 @@ function drawPixelBoard(boardSize) {
   pixelBoard.style.display = 'table';
   for (let index = 0; index < boardSize; index += 1) {
     const pixelLine = document.createElement('div');
-    pixelLine.style.display = 'table-row'
+    pixelLine.style.display = 'table-row';
     for (let indexSquares = 0; indexSquares < boardSize; indexSquares += 1) {
       const pixelSquare = document.createElement('div');
       pixelSquare.className = 'pixel';
       pixelSquare.style.backgroundColor = 'white';
-      pixelSquare.style.display = 'table.cell'
+      pixelSquare.style.display = 'table-cell';
       pixelLine.appendChild(pixelSquare);
     }
     pixelBoard.appendChild(pixelLine);
@@ -30,4 +30,4 @@ function drawPixelBoard(boardSize) {
 window.onload = function () {
   drawColorPallets();
   drawPixelBoard(5);
-}
+};
