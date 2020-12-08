61cd5798717d726a82e78637d14bc0d8c7e9979d
Cristiano Lopes Duarte <tkdcris@gmail.com>
2020-12-08 17:32:08 -0300

    Paleta de cor criada

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 window.onload = function() {
-  let div_colorPalette = document.querySelector('#div_colorPalette');
+  let div_colorPalette = document.querySelector('#color-palette');
   let div_matriz = document.querySelector('#div_matriz');
   let entrada = 5;
   div_matriz.style.width = (entrada*40);
-  // let columns = 5;
   function createDivsRowsColumns(entrada) {
     for (let i = 1; i <= entrada; i += 1) {
       for (let j = 1; j <= entrada; j += 1) {
