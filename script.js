dbcb6018d5f36626231bbc010b794f3a36a71752
Qu4k3r <neves0007@gmail.com>
2020-12-08 17:37:51 -0300

    Resolvidos problemas do codeclimate

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 // Step 2 & 3
 function setPalletColors() {
-  const color1 = document.querySelector('.color')
-  const color2 = color1.nextElementSibling
-  const color3 = color2.nextElementSibling
-  const color4 = document.querySelector('#color-palette').lastElementChild
+  const color1 = document.querySelector('.color');
+  const color2 = color1.nextElementSibling;
+  const color3 = color2.nextElementSibling;
+  const color4 = document.querySelector('#color-palette').lastElementChild;
 
-  color1.style.background = 'black'
-  color2.style.background = 'darkorange'
-  color3.style.background = 'darkviolet'
-  color4.style.background = 'deepskyblue' 
+  color1.style.background = 'black';
+  color2.style.background = 'darkorange';
+  color3.style.background = 'darkviolet';
+  color4.style.background = 'deepskyblue';
 }
 
-setPalletColors()
+setPalletColors();
 
 // Step 4
 function createPixel(className) {
-  const pixel = document.createElement('div')
-  pixel.className = className
-  return pixel
+  const pixel = document.createElement('div');
+  pixel.className = className;
+  return pixel;
 }
 
 function createPixelBoard(idName) {
-  const pixelBoard = document.createElement('div')
-  pixelBoard.id = idName
-  return document.body.appendChild(pixelBoard)
+  const pixelBoard = document.createElement('div');
+  pixelBoard.id = idName;
+  return document.body.appendChild(pixelBoard);
 }
 
-createPixelBoard('pixel-board')
+createPixelBoard('pixel-board');
 
 function fillPixelBoard() {
-  const pixelBoard = document.querySelector('#pixel-board')
-  const maxNumberOfPixels = 25
+  const pixelBoard = document.querySelector('#pixel-board');
+  const maxNumberOfPixels = 25;
   for (let pixelIndex = 0; pixelIndex < maxNumberOfPixels; pixelIndex += 1) {
-    pixelBoard.appendChild(createPixel('pixel')) 
+    pixelBoard.appendChild(createPixel('pixel'));
   }
 }
 
-fillPixelBoard()
+fillPixelBoard();
