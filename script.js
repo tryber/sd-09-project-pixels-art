e7edb5bef56a305808d22d784ec751d2ca46ea49
Matheus Monteiro <monteiromsh@gmail.com>
2020-12-08 19:23:52 -0300

    Sexto commit

--- script.js
+++ script.js
@@ -2,54 +2,94 @@ window.onload = firstSelectedFun;
 
 //Quando a página carregar, executa os comandos abaixo:
 function firstSelectedFun() {
-  document.querySelector(".black").classList.add("selected");
+  document.querySelector('.black').classList.add('selected');
   clickSelect();
+  paintPixel();
 }
 
-//Recupera caminho das cores e adiciona classe "selected"
+//Recupera caminho das cores e adiciona classe 'selected'
 function clickSelect() {
-  let localRed = document.querySelector(".red");
-  let localBlue = document.querySelector(".blue");
-  let localGreen = document.querySelector(".green");
-  let localBlack = document.querySelector(".black");
+  let localRed = document.querySelector('.red');
+  let localBlue = document.querySelector('.blue');
+  let localGreen = document.querySelector('.green');
+  let localBlack = document.querySelector('.black');
 
-  localRed.addEventListener("click", function () {
-    localRed.classList.add("selected");
+  localRed.addEventListener('click', function () {
+    localRed.classList.add('selected');
 
-    if (localBlack.classList.contains ("selected") || (localBlue.classList.contains ("selected") || (localGreen.classList.contains ("selected")))){
-        localBlack.classList.remove('selected')
-        localBlue.classList.remove('selected')
-        localGreen.classList.remove('selected')
+    if (
+      localBlack.classList.contains('selected') ||
+      localBlue.classList.contains('selected') ||
+      localGreen.classList.contains('selected')
+    ) {
+      localBlack.classList.remove('selected');
+      localBlue.classList.remove('selected');
+      localGreen.classList.remove('selected');
     }
   });
 
-  localBlue.addEventListener("click", function () {
-    localBlue.classList.add("selected");
+  localBlue.addEventListener('click', function () {
+    localBlue.classList.add('selected');
 
-    if (localBlack.classList.contains ("selected") || (localRed.classList.contains ("selected") || (localGreen.classList.contains ("selected")))){
-        localBlack.classList.remove('selected')
-        localRed.classList.remove('selected')
-        localGreen.classList.remove('selected')
+    if (
+      localBlack.classList.contains('selected') ||
+      localRed.classList.contains('selected') ||
+      localGreen.classList.contains('selected')
+    ) {
+      localBlack.classList.remove('selected');
+      localRed.classList.remove('selected');
+      localGreen.classList.remove('selected');
     }
   });
 
-  localGreen.addEventListener("click", function () {
-    localGreen.classList.add("selected");
+  localGreen.addEventListener('click', function () {
+    localGreen.classList.add('selected');
 
-    if (localBlack.classList.contains ("selected") || (localBlue.classList.contains ("selected") || (localRed.classList.contains ("selected")))){
-        localBlack.classList.remove('selected')
-        localBlue.classList.remove('selected')
-        localRed.classList.remove('selected')
+    if (
+      localBlack.classList.contains('selected') ||
+      localBlue.classList.contains('selected') ||
+      localRed.classList.contains('selected')
+    ) {
+      localBlack.classList.remove('selected');
+      localBlue.classList.remove('selected');
+      localRed.classList.remove('selected');
     }
   });
 
-  localBlack.addEventListener("click", function () {
-    localBlack.classList.add("selected");
+  localBlack.addEventListener('click', function () {
+    localBlack.classList.add('selected');
 
-    if (localRed.classList.contains ("selected") || (localBlue.classList.contains ("selected") || (localGreen.classList.contains ("selected")))){
-        localRed.classList.remove('selected')
-        localBlue.classList.remove('selected')
-        localGreen.classList.remove('selected')
+    if (
+      localRed.classList.contains('selected') ||
+      localBlue.classList.contains('selected') ||
+      localGreen.classList.contains('selected')
+    ) {
+      localRed.classList.remove('selected');
+      localBlue.classList.remove('selected');
+      localGreen.classList.remove('selected');
     }
   });
+}
+
+function paintPixel(){
+    let localWhite = document.querySelectorAll('.pixel')
+    let localRed = document.querySelector('.red');
+    let localBlue = document.querySelector('.blue');
+    let localGreen = document.querySelector('.green');
+    let localBlack = document.querySelector('.black');
+    
+    for (let index = 0; index < localWhite.length; index += 1 ){
+        localWhite[index].addEventListener('click', function(){
+            let colorSelected = document.querySelector('.selected')
+            if (colorSelected === localBlack){
+                localWhite[index].style.backgroundColor = 'black'
+            } else if (colorSelected === localRed){
+                localWhite[index].style.backgroundColor = 'red'
+            } else if (colorSelected === localBlue){
+                localWhite[index].style.backgroundColor = 'blue'
+            } else if (colorSelected === localGreen){
+                localWhite[index].style.backgroundColor = 'green'
+            }
+        })
+    }
 }
