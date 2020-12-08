040e76ae49825c703a936e6f6fbe3635cebea346
Felipe Rocha <felipe.sr27@gmail.com>
2020-12-08 16:45:42 -0300

    Ajuste dos Requisitos 2 e 3.

--- script.js
+++ script.js
@@ -1,13 +1,30 @@
-window.onload = function() {
-  let paletteColors = document.querySelectorAll('.color');
-  let colorsArray = ['black', 'blue', 'green', 'red'];
+window.onload = function () {
+  //const paletteColors = document.querySelectorAll('.color');
+  const paletteColor = document.querySelector('#color-palette')
+  const colorsArray = ['black', 'blue', 'green', 'red'];
 
-  addColorsPalette(paletteColors, colorsArray);
+  function createDivs() {
+    let div = document.createElement('div');
+    return div;
+  };
 
-  function addColorsPalette(paletteColors, colorsArray) {
-    for (let index = 0; index < paletteColors.length; index += 1) {
-      paletteColors[index].style.backgroundColor = colorsArray[index];
+  function createColorsPalette(palette) {
+    let palleteArray = [];
+    for (let index = 0; index < 4; index += 1){
+      let paletteSquare = palette.appendChild(createDivs());
+      paletteSquare.className = 'color';
+      palleteArray.push(paletteSquare);
     }
-  }
+    addColorSquare(palleteArray, colorsArray);
+  };
 
-}
+  function addColorSquare(palette, colors) {
+    for (let index = 0; index < palette.length; index += 1) {
+      palette[index].style.backgroundColor = colors[index];
+    }
+  };
+
+  createColorsPalette(paletteColor);
+  //addColorSquare(paletteColors, colorsArray);
+
+};
