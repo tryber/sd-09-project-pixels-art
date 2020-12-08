90efa94e4e471fba29485bef7adc820b6882b88f
Pablo Fernandes <pabloprates@gmail.com>
2020-12-08 16:28:13 -0300

    Defining initial selected color as black again

---
+++ script.js
@@ -0,0 +1,7 @@
+let colorPalette = document.getElementById('color-palette');
+colorPalette.addEventListener('click', setColorToPaint);
+
+function setColorToPaint (evt) {
+    let colorSelected = evt.target.style.backgroundColor;
+    
+}
