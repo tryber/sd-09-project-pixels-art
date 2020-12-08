ca623a7bc2dbd1d022fbabc293372b6c0dfe6ab4
Luiz Henrique <lhfmworker@gmail.com>
2020-12-08 19:37:44 -0300

    pixel ja pode ser pintado

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let board = document.querySelector('#pixel-board');
+let colorsTable = document.querySelector('#color-palette');
+let pixels = document.querySelector('#pixel-board');
+var color = '';
 
-function rowGenerator(){
+function rowGenerator() {
     for (let i=0; i<5; i+=1){
         let pxl = document.createElement('div');
         pxl.className = 'row';
@@ -9,7 +12,7 @@ function rowGenerator(){
 }
 rowGenerator();
 
-function pxlGenerator(){
+function pxlGenerator() {
     let rowBoard = document.querySelectorAll('.row');
     for (let i=0; i<5; i+=1){
         for (let j=0; j<5 ; j+=1){
@@ -20,3 +23,15 @@ function pxlGenerator(){
     }
 }
 pxlGenerator();
+
+let colorSaved = colorsTable.addEventListener('click', catchColor);
+function catchColor (){
+    console.log(event.srcElement.id);
+    color = event.srcElement.id;
+}
+pixels.addEventListener('click', function (event){
+    console.log(event);
+    let paintPxl = event.target;
+    paintPxl.style.backgroundColor = colorSaved;
+    event.target.style.backgroundColor = color;
+})
