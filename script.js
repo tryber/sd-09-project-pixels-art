d46b300f25a1f4b35e6ffab4125c6c2c72e680d6
Tiago Yoneda <xyonedax@gmail.com>
2020-12-08 16:41:14 -0300

    board feita

---
+++ script.js
@@ -0,0 +1,21 @@
+let baseDoQuadro = 5;
+
+for (let idx = 0; idx < baseDoQuadro; idx += 1){
+    let newLine = document.createElement('div');
+    newLine.className = 'linha';
+    document.querySelector('#pixel-board').appendChild(newLine);
+}
+
+let minhasLinhas = document.querySelectorAll('.linha')
+for (let item of minhasLinhas){
+    item.style.height = '40px';
+    item.style.width = baseDoQuadro*40+'px';
+}
+
+for ( let item of minhasLinhas){
+    for ( let idx = 0; idx < baseDoQuadro ; idx+= 1){
+        let newPixel = document.createElement('div');
+        newPixel.className = 'pixel';
+        item.appendChild(newPixel);
+    }
+}
