bd8d8a1ebca19a74e30a804f40f305bddd8cda88
Tiago de Almeida Paz <tiagopaz.dev@gmail.com>
2020-12-08 17:17:30 -0300

    requisito 9 completo

--- script.js
+++ script.js
@@ -9,3 +9,15 @@ function makeLinePixels() {
 }
 
 makeLinePixels();
+
+function clearAllPixels() {
+  for (let index = 0; index < 25; index += 1) {
+    const getPixels = document.querySelectorAll('.pixel')[index];
+    getPixels.style.backgroundColor = 'white';
+  }
+}
+
+let clearButton = document.getElementById('clear-board');
+clearButton.addEventListener('click',clearAllPixels);
+
+
