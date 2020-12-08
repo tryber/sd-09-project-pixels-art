4e2ee74ce535c3dc28375332245b31d503d33ff6
Danrlley Rodrigues <danrlley.c.a.rodrigues@gmail.com>
2020-12-08 18:32:32 -0300

    Requisito 6 Concluido

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+function selectOnLoad () {
+    let onLoadVar = document.getElementsByClassName('color1')[0];
+    console.log(onLoadVar);
+    onLoadVar = classList.add('selected')
+}
+window.onload(selectOnLoad)
+
+window.onload = function () {
+    selectOnLoad
+}
+
 window.onload(
     document.getElementsByClassName('color1')[0].classList.add('selected')
 )
