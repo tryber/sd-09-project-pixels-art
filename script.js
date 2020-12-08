32f97cfe22840ecdf0029aff5ab3cb1e8086709e
Layo Kaminski <layokaminski@gmail.com>
2020-12-08 16:25:10 -0300

    Reestruturando requisito 4 e requisito 5

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    function colorPalette () {
+    function colorPalette() {
         let color = document.querySelectorAll('.color')
         color[0].style.backgroundColor = 'black'
         color[1].style.backgroundColor = 'steelblue'
@@ -7,4 +7,26 @@ window.onload = function() {
         color[3].style.backgroundColor = 'yellow'
     }
     colorPalette();
+
+    function createdPixelLine() {
+        let pixelBoard = document.querySelector('#pixel-board')
+        for (let index = 0; index < 5; index += 1) {
+            let createdElement = document.createElement('div')
+            pixelBoard.appendChild(createdElement)
+            createdElement.className = 'pixel line'
+            }
+        }
+        createdPixelLine()
+
+        function createdPixelColumn() {
+            let pixelBoard = document.querySelectorAll('.pixel.line')
+            for (let index = 0; index < pixelBoard.length; index += 1) {
+                for (let indexTwo = 0; indexTwo < 5; indexTwo += 1) {
+                let createdElement = document.createElement('div')
+                pixelBoard[index].appendChild(createdElement)
+                createdElement.className = 'pixel column'
+                }
+                }
+            }
+            createdPixelColumn()
 }
