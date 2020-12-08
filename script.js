321f343e81c460ba4f843a80a283bd08dcd89030
Cristiano Lopes Duarte <tkdcris@gmail.com>
2020-12-08 17:20:43 -0300

    Ajustes code climate

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
-window.onload = function(){
-
-let div_colorPalette = document.querySelector('#div_colorPalette');
-let div_matriz = document.querySelector('#div_matriz');
-let entrada = 5;
-// let columns = 5;
-function createDivsRowsColumns(entrada) {
-  for (let i = 1; i <= entrada; i += 1) {
-    for (let j = 1; j <= entrada; j += 1) {
-      let div = document.createElement('div');
-      div.id = `${i}x${j}`
-      div_matriz.appendChild(div);
+window.onload = function() {
+  let div_colorPalette = document.querySelector('#div_colorPalette');
+  let div_matriz = document.querySelector('#div_matriz');
+  let entrada = 5;
+  div_matriz.style.width = (entrada*40);
+  // let columns = 5;
+  function createDivsRowsColumns(entrada) {
+    for (let i = 1; i <= entrada; i += 1) {
+      for (let j = 1; j <= entrada; j += 1) {
+        let div = document.createElement('div');
+        div.id = `${i}x${j}`
+        div_matriz.appendChild(div);
+      }
     }
   }
-}
-
-createDivsRowsColumns(entrada)
-
-console.log(div_colorPalette);
-console.log(div_matriz
-  );
-
-
+  createDivsRowsColumns(entrada)
+  console.log(div_colorPalette);
+  console.log(div_matriz);
 }
