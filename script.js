848d360bcf3aa4ebb9b2cdd25db795703104194f
Carine Becker <caarine.m@gmail.com>
2020-12-08 17:52:48 -0300

    fixed code climate issues

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-window.onload = function(){
+window.onload = function () {
   function createPixelsBoard() {
     const pixelBoard = document.querySelector('#pixel-board');
     const pixelColumn = 5;
-    
+
     for (let indexLine = 0; indexLine < pixelColumn; indexLine += 1) {
       const pixelLine = document.createElement('div');
       pixelLine.classList.add('pixel-line');
-      
+
       for (let indexColumn = 0; indexColumn < pixelColumn; indexColumn += 1) {
         const pixel = document.createElement('div');
         pixel.classList.add('pixel');
@@ -16,4 +16,4 @@ window.onload = function(){
     }
   }
   createPixelsBoard();
-}
+};
