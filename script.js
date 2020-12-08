10d66facbf77291be68665c60fa578e4bb89c5fa
Nathália <nathaliaveneziano@icloud.com>
2020-12-08 19:20:32 -0300

    Requisitos 06

--- script.js
+++ script.js
@@ -24,3 +24,8 @@ function createPixelBoard() {
 }
 
 createPixelBoard();
+
+window.onload = function() {
+  let colorSelected = document.querySelector('#color-palette .selected').classList[2];
+  console.log(colorSelected);
+}
