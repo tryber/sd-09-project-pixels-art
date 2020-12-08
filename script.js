9a6e1c2bfd61104e666f14633f466f14c488a1f6
lcgoandete <lcgoandete@hotmail.com>
2020-12-08 17:21:21 -0300

    tarefa 4/3 - 🚀

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ function createPixelBoard() {
   for (let linha = 0; linha < pixelBoardSize; linha += 1) {
     const pixel = document.createElement('div');
     pixel.className = 'pixel';
+    pixel.style.backgroundColor = 'white';
     pixelBoard.appendChild(pixel);
   }
 }
