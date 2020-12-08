4eac69155a0d8bc5333b3d0f70159f3f3e74f68e
Thiago Granville <thiagogranville@gmail.com>
2020-12-08 17:24:05 -0300

    Requisito 7: Implementa função para selecionar cor na paleta de cores

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-window.onload = function() {
-function createPixelBoard() {
-  const pixelBoardLines = document.querySelectorAll('.pixel-board-line');
+window.onload = function () {
+  function createPixelBoard() {
+    const pixelBoardLines = document.querySelectorAll('.pixel-board-line');
 
-  for (let line = 0; line < pixelBoardLines.length; line += 1) {
-    for (let column = 0; column < pixelBoardLines.length; column += 1) {
-      const pixel = document.createElement('div');
-      pixel.className = 'pixel';
-      pixelBoardLines[line].appendChild(pixel);
+    for (let line = 0; line < pixelBoardLines.length; line += 1) {
+      for (let column = 0; column < pixelBoardLines.length; column += 1) {
+        const pixel = document.createElement('div');
+        pixel.className = 'pixel';
+        pixelBoardLines[line].appendChild(pixel);
+      }
     }
   }
-}
 
-createPixelBoard();
-}
+  createPixelBoard();
+
+  function selectColor() {
+    const colorsPalette = document.querySelector('#color-palette');
+    const colors = document.querySelectorAll('.color');
+
+    colorsPalette.addEventListener('click', function (event) {
+      colors.forEach(function (color) {
+        if (event.target.id != 'color-palette') {
+          color.classList.remove('selected');
+          event.target.classList.add('selected');
+        }
+      });
+    });
+  }
+
+  selectColor();
+};
