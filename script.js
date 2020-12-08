4887ccfeee0c1293bd28e866669f406ffaeef9e9
SantosDiv <diogenesfelipe.s@outlook.com>
2020-12-08 16:38:36 -0300

    Resolvendo problemas do codeclimate - 1

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
-window.onload = function (){
-  randomColorsPalette();
-}
-
-//Validate color to not be white
-function verifyColorWrite (sumNumbersColors, rgbArray) {
-  let colorWhiteNumber = 765;
-  if(sumNumbersColors === colorWhiteNumber) {
+// Validate color to not be white
+function verifyColorWrite(sumNumbersColors, rgbArray) {
+  const colorWhiteNumber = 765;
+  if (sumNumbersColors === colorWhiteNumber) {
     rgbArray.pop();
     rgbArray.push(Math.random() * 255);
   }
   return rgbArray;
 }
 
-//Function of random RGB numbers
+// Function of random RGB numbers
 function randomRgbNumberColor() {
-  let numberColorsRgb = 3;
+  const numberColorsRgb = 3;
   let rgbArray = [];
-  let sumNumbersColors = 0;
+  const sumNumbersColors = 0;
   for (let index = 0; index < numberColorsRgb; index += 1) {
     rgbArray.push(Math.random() * 255);
     sumNumbersColors += rgbArray[index];
@@ -25,11 +21,20 @@ function randomRgbNumberColor() {
   return rgbArray;
 }
 
-//Put color random on the elements
+// Put color random on the elements
 function randomColorsPalette() {
-  let pixelColorSquare = document.querySelectorAll('.color');
+  const pixelColorSquare = document.querySelectorAll('.color');
   for (let index = 1; index < pixelColorSquare.length; index += 1) {
-    let colorNumber = randomRgbNumberColor();
+    const colorNumber = randomRgbNumberColor();
     pixelColorSquare[index].style.backgroundColor = `rgb(${colorNumber[0]}, ${colorNumber[1]}, ${colorNumber[2]})`;
   }
+}
+
+function createPixelElement() {
+  let pixelBoardElement = document.querySelector('#pixel-board');
+  console.log(pixelBoardElement);
+}
+
+window.onload = function () {
+    randomColorsPalette();
 }
