12c05a7d8f5567ce214ade89355d292649c331f2
Raphael Rêgo <raphael.mlrego@gmail.com>
2020-12-08 17:43:12 -0300

    adicionando quadro de pixels

---
+++ script.js
@@ -0,0 +1,5 @@
+for (let index = 0; index < 25; index += 1) {
+    let div = document.createElement('div');
+    div.className = 'pixel';
+    document.querySelector('#pixel-board').appendChild(div);
+}
