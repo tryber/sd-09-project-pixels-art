9c78cedbee82fd118c57ad0b4f208a71c268b73f
MarcioFujita <hanker78@gmail.com>
2020-12-08 20:16:25 -0300

    Requisito 7.V3

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
 window.onload = function () {
-  let chngClr = document.querySelectorAll('.color');
-  let paletteRed = document.querySelector('.red');
-  function changeColorToRed (){
-      for (let index = 0; index < chngClr.length; index += 1) {
+  const chngClr = document.querySelectorAll('.color');
+  const paletteRed = document.querySelector('.red');
+  function changeColorToRed() {
+    for (let index = 0; index < chngClr.length; index += 1) {
         if (index !== 1) {
-            chngClr[index].classList.remove('selected');
+          chngClr[index].classList.remove('selected');
         } else {
-            paletteRed.classList.add('selected');
+          paletteRed.classList.add('selected');
         }
       }
   }
-  let paletteBlue = document.querySelector('.blue');
-  function changeColorToBlue () {
-      for (let index = 0; index < chngClr.length; index += 1) {
+  const paletteBlue = document.querySelector('.blue');
+  function changeColorToBlue() {
+    for (let index = 0; index < chngClr.length; index += 1) {
         if (index !== 2) {
-            chngClr[index].classList.remove('selected');
+          chngClr[index].classList.remove('selected');
         } else {
-            paletteBlue.classList.add('selected');
+          paletteBlue.classList.add('selected');
         }
       }
   }
-  let paletteGreen = document.querySelector('.green');
-  function changeColorToGreen (){
-      for (let index = 0; index < chngClr.length; index += 1) {
+  const paletteGreen = document.querySelector('.green');
+  function changeColorToGreen() {
+    for (let index = 0; index < chngClr.length; index += 1) {
         if (index !== 3) {
-            chngClr[index].classList.remove('selected');
+          chngClr[index].classList.remove('selected');
         } else {
-            paletteGreen.classList.add('selected');
+          paletteGreen.classList.add('selected');
         }
       }
   }
-  let paletteBlack = document.querySelector('.black');
-  function changeColorToBlack (){
-      for (let index = 0; index < chngClr.length; index += 1) {
+  const paletteBlack = document.querySelector('.black');
+  function changeColorToBlack() {
+    for (let index = 0; index < chngClr.length; index += 1) {
         if (index !== 0) {
-            chngClr[index].classList.remove('selected');
+          chngClr[index].classList.remove('selected');
         } else {
-            paletteBlack.classList.add('selected');
+          paletteBlack.classList.add('selected');
         }
       }
   }
