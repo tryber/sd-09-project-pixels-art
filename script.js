589ba87ae3ee4403100c4be0d27111b87576b1a7
Carine Becker <caarine.m@gmail.com>
2020-12-08 17:32:53 -0300

    fixed code climate issues

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 
 function createPixelsBoard() {
-	const pixelBoard = document.querySelector('#pixel-board');
-	const pixelColumn = 5;
+  const pixelBoard = document.querySelector('#pixel-board');
+  const pixelColumn = 5;
 
-	for (let indexLine = 0; indexLine < pixelColumn; indexLine += 1) {
-		const pixelLine = document.createElement('div');
-		pixelLine.classList.add('pixel-line');
+  for (let indexLine = 0; indexLine < pixelColumn; indexLine += 1) {
+    const pixelLine = document.createElement('div');
+    pixelLine.classList.add('pixel-line');
 
-		for (let indexColumn = 0; indexColumn < pixelColumn; indexColumn += 1) {
-			const pixel = document.createElement('div');
-			pixel.classList.add('pixel');
-			pixelLine.appendChild(pixel);
-		}
-		pixelBoard.appendChild(pixelLine);
-	}
+    for (let indexColumn = 0; indexColumn < pixelColumn; indexColumn += 1) {
+      const pixel = document.createElement('div');
+      pixel.classList.add('pixel');
+      pixelLine.appendChild(pixel);
+    }
+    pixelBoard.appendChild(pixelLine);
+  }
 }
 createPixelsBoard();
