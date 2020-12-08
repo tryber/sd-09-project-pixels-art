8035f49c35ac0ee2c3bbf66d8c93450eee2b49f1
Khyrki <khyrkimitsunobi@gmail.com>
2020-12-08 15:51:01 -0300

    Code climate

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-function fillColorPalette () {
+function fillColorPalette() {
   const divColor = document.getElementsByClassName('color');
   for (let index = 0; index < divColor.length; index += 1) {
-    let randoRgbNumber = Math.ceil(Math.random() * 255);
-    let randoRgbNumber1 = Math.ceil(Math.random() * 255);
-    let randoRgbNumber2 = Math.ceil(Math.random() * 255);
-    let rgb = 'rgb( '+ randoRgbNumber + ' , ' + randoRgbNumber1 + ' , ' + randoRgbNumber2 + ' )';
+    const randoRgbNumber = Math.ceil(Math.random() * 255);
+    const randoRgbNumber1 = Math.ceil(Math.random() * 255);
+    const randoRgbNumber2 = Math.ceil(Math.random() * 255);
+    const rgb = 'rgb( '+ randoRgbNumber + ' , ' + randoRgbNumber1 + ' , ' + randoRgbNumber2 + ' )';
     divColor[index].style.backgroundColor = rgb;
-    console.log(rgb)
   }
 }
-fillColorPalette ();
+fillColorPalette();
