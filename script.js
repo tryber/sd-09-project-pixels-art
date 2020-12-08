039ec7ad933dd8ed1f6d872e048d977b7102d8ac
Tiago de Almeida Paz <tiagopaz.dev@gmail.com>
2020-12-08 16:19:43 -0300

    requisito 6 e 7 completos

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-let pixelBoard = document.getElementById('pixel-board');
-console.log(pixelBoard);
+const pixelBoard = document.getElementById('pixel-board');
 
 function makeLinePixels() {
   for (let index = 0; index < 25; index += 1) {
-    let pixels = document.createElement('div');
+    const pixels = document.createElement('div');
     pixels.className = 'pixel';
     pixelBoard.appendChild(pixels);
-    console.log(index);
   }
 }
-makeLinePixels();
 
+makeLinePixels();
