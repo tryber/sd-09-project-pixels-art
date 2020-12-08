9222fd857f0b07af865fb73b415b1803975cd3ef
Luciano_Lodi <lrlodi@gmail.com>
2020-12-08 19:19:36 -0300

    Correçoes de CC-60 issues

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 
 window.onload = function () {
-    let pixelRow = document.getElementById("pixel-board");
-    let canvasHeight = 5;
-    let canvasWidth = 5;
+	let pixelRow = document.getElementById('pixel-board');
+	let canvasHeight = 5;
+	let canvasWidth = 5;
 
-    function createRows(){
-        for(let i = 0; i < canvasHeight; i += 1) {
-            let row = document.createElement('div');
-            for(let j = 0; j < canvasWidth; j += 1) {
-                let pixel = document.createElement('div');
-                pixel.className = 'pixel';
-                row.appendChild(pixel);
-            }
-            pixelRow.appendChild(row);
-        }
-    }
-    createRows();
+	function createRows() {
+			for(let i = 0; i < canvasHeight; i += 1) {
+					let row = document.createElement('div');
+					row.className = 'line';
+					for(let j = 0; j < canvasWidth; j += 1) {
+							let pixel = document.createElement('div');
+							pixel.className = 'pixel';
+							row.appendChild(pixel);
+					}
+					pixelRow.appendChild(row);
+			}
+	}
+	createRows();
 }
