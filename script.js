760c240132b9bbd9f5adad564c9978957947afe9
ggaldino95 <ggaldino95@gmail.com>
2020-12-08 16:40:13 -0300

    [Task 3] Structuring palette to be generated dinamically

---
+++ script.js
@@ -0,0 +1,25 @@
+function generatePalette() {
+  const palette = document.getElementById('color-palette');
+  const colorPaletteArray = ['green', 'blue', 'red', 'yellow', 'purple', 'orange', 'teal', 'pink', 'gray'];
+  const paletteSize = 4;
+
+  // [Task 3] First color is always BLACK
+  const firstColor = document.createElement('div');
+  firstColor.className = 'color';
+  firstColor.style.backgroundColor = 'black';
+  palette.appendChild(firstColor);
+
+  // Remaining colors
+  for (let i = 1; i < paletteSize; i += 1) {
+    const color = document.createElement('div');
+    color.className = 'color';
+    color.style.backgroundColor = colorPaletteArray[i];
+    palette.appendChild(color);
+  }
+  palette.addEventListener('click', function () {
+  });
+}
+
+window.onload = function () {
+  generatePalette();
+};
