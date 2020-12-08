557acd48b47c3ad15d3a70c863edb6d44c0c3c06
Tiago Yoneda <xyonedax@gmail.com>
2020-12-08 17:49:54 -0300

    simplificando as funcoes for

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 let baseDoQuadro = 5;
 
 for (let idx = 0; idx < baseDoQuadro; idx += 1) {
-  let newLine = document.createElement('div');
+  const newLine = document.createElement('div');
   newLine.className = 'linha';
   document.querySelector('#pixel-board').appendChild(newLine);
 }
 
 let minhasLinhas = document.querySelectorAll('.linha');
-for (let item of minhasLinhas) {
-  item.style.height = '40px';
-  item.style.width = `${baseDoQuadro * 40}px`;
+for (let idx = 0; idx < minhasLinhas.length ; idx += 1 ) {
+  minhasLinhas[idx].style.height = '40px';
+  minhasLinhas[idx].style.width = `${baseDoQuadro * 40}px`;
 }
 
-for (let item of minhasLinhas) {
-  for (let idx = 0; idx < baseDoQuadro; idx += 1) {
-    let newPixel = document.createElement('div');
+for (let idx = 0; idx < minhasLinhas.length; idx += 1) {
+  for (let idx2 = 0; idx2 < baseDoQuadro; idx2 += 1) {
+    const newPixel = document.createElement('div');
     newPixel.style.backgroundColor = 'white';
     newPixel.className = 'pixel';
-    item.appendChild(newPixel);
+    minhasLinhas[idx].appendChild(newPixel);
   }
 }
