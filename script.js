fbf0f89c0dbbf31fc387696aa937a3d332d203aa
Tiago Yoneda <xyonedax@gmail.com>
2020-12-08 17:25:45 -0300

    arrumando a tabela

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ for (let idx = 0; idx < baseDoQuadro; idx += 1) {
 let minhasLinhas = document.querySelectorAll('.linha');
 for (let item of minhasLinhas) {
   item.style.height = '40px';
-  item.style.width = `${baseDoQuadro*42}` + 'px';
+  item.style.width = `${baseDoQuadro*40}` + 'px';
 }
 
 for ( let item of minhasLinhas){
