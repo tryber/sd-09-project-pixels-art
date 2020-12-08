fb6d0122575408b0c47c76e8880133a5f7d0dbbf
Rafael Azevedo <rafaelazevedo321@gmail.com>
2020-12-08 19:31:04 -0300

    feito requisito 7 e 8

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('para o codeclimate nao xiar');
+window.onload = function() {
+    let colorPalette = document.querySelector('#color-palette');
+    colorPalette.addEventListener('click', function(event) {
+        let colors = document.querySelectorAll('.color')
+        for(let index in colors) {
+            if (event.target === colors[index]) {
+                let selected = document.querySelector('.selected');
+                selected.classList.remove('selected');
+                event.target.classList.add('selected');
+            }
+        }
+    });
+    
+    let pixelBoard = document.querySelector('#pixel-board');
+    pixelBoard.addEventListener('click', function(event) {
+        if (event.target.className === 'pixel') {
+            let color = document.querySelector('.selected');
+            event.target.style.backgroundColor = color.classList[1];
+        }
+    });
+}
