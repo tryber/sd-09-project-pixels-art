e7dc2f0c1a9215c1d64b3dc6af3a4b06ceacf970
Pedro Henrique <pedroenrique.400@gmail.com>
2020-12-08 20:13:14 -0300

    Faz o requisito 4

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-let colors = document.querySelectorAll('.color');
-let arrayOfColors = ['black','yellow','green','red']
+const colors = document.querySelectorAll('.color');
+const arrayOfColors = ['black', 'yellow', 'green', 'red'];
 for (let index = 0; index < colors.length; index += 1) {
-    colors[index].style.backgroundColor = arrayOfColors[index];
+  colors[index].style.backgroundColor = arrayOfColors[index];
+}
+
+let pixels = document.querySelectorAll('td');
+
+for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].className = 'pixel'; 
 }
