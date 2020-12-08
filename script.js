6e43773f677fe4e464d0759bb882896c419e00d6
Cristiano Lopes Duarte <tkdcris@gmail.com>
2020-12-08 15:56:41 -0300

    Criei o scopo da funçao createDivsRowsColumns

---
+++ script.js
@@ -0,0 +1,14 @@
+let div_colorPalette = document.querySelector('#div_colorPalette');
+let rows = 5;
+let columns = 5;
+function createDivsRowsColumns(rows, columns) {
+  for (let i = 0; i < rows; i += 1) {
+    for (let j = 0; j < columns; j += 1) {
+
+    }
+  }
+}
+
+createDivsRowsColumns(rows, columns)
+
+console.log(div_colorPalette)
