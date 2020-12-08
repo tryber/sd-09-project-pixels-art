e4b25091466ba9c0d2abfe0e659e64a952736898
Khyrki <khyrkimitsunobi@gmail.com>
2020-12-08 16:02:57 -0300

    adiciona requisito 3

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function fillColorPalette() {
   const divColor = document.getElementsByClassName('color');
-  for (let index = 0; index < divColor.length; index += 1) {
+  for (let index = 1; index < divColor.length; index += 1) {
     const randoRgbNumber = Math.ceil(Math.random() * 255);
     const randoRgbNumber1 = Math.ceil(Math.random() * 255);
     const randoRgbNumber2 = Math.ceil(Math.random() * 255);
     const rgb = 'rgb( '+ randoRgbNumber + ' , ' + randoRgbNumber1 + ' , ' + randoRgbNumber2 + ' )';
     divColor[index].style.backgroundColor = rgb;
+    divColor[0].style.backgroundColor = 'black';
   }
 }
 fillColorPalette();
