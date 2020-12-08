d9b7b7a6d63f6324790fbf9cb545d2e75812220c
Matheus Schreiner <matheusrubisch@gmail.com>
2020-12-08 16:57:01 -0300

    Segundo requisito

---
+++ script.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+  paletteColor();
+}
+
+function paletteColor () {
+  const palette = document.querySelectorAll('.color');
+  palette[0].style.backgroundColor = 'black';
+  palette[1].style.backgroundColor = 'blue';
+  palette[2].style.backgroundColor = 'red';
+  palette[3].style.backgroundColor = 'yellow';
+}
