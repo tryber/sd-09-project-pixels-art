59aac542a749c10af6fa5ca29eeb55deedb9c5d9
Khyrki <khyrkimitsunobi@gmail.com>
2020-12-08 14:56:18 -0300

    Altera let para const nas linhas 1,2,4 e 7 no script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let body = document.querySelector('body');
-let createHeder = document.createElement('header');
+const body = document.querySelector('body');
+const createHeder = document.createElement('header');
 body.appendChild(createHeder);
-let header = document.querySelector('header');
+const header = document.querySelector('header');
 header.id = 'Header';
 header.appendChild(document.createElement('h1'));
-let h1 = document.querySelector('h1');
+const h1 = document.querySelector('h1');
 h1.id = 'title';
 h1.innerText = 'Paleta de Cores';
