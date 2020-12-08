3c1140a009f68e0c47aaf8a4c602614d7e7645f8
Rafael Pufal <rafael.pufal23@gmail.com>
2020-12-08 17:40:27 -0300

    adicionando requisitos 5 e 6

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+window.onload = function () {
+    const colorSelected = document.querySelector('.black');
+    colorSelected.classList.add('selected')
+}
+
 function grid(){
     const gridBase = document.querySelector("#pixel-board");
     const gridN = 5;
@@ -5,6 +10,7 @@ function grid(){
     for (let index = 0; index < gridN; index++){
         const rowPixel = document.createElement("div");
         gridBase.appendChild(rowPixel)
+        rowPixel.className += "row-pixel"
         for (let nIndex = 0; nIndex < gridN; nIndex++){
             const pixelCell =document.createElement("div");
             rowPixel.appendChild(pixelCell)
