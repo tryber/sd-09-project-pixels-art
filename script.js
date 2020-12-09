f2dad09463a97c40e46f152cc814becc69ba692a
devfelipesantiago <felipesantiagolds@gmail.com>
2020-12-09 10:51:33 -0300

    Dimensao dos pixels

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
 window.onload = function () {
   createEventColorBox();
-  clear()
-}
+  clear();
+
+};
 
 function createEventColorBox() {
-  let box = document.querySelectorAll('.pixel');
-  let color = document.querySelectorAll('#color-palette .color');
+  let box = document.querySelectorAll(".pixel");
+  let color = document.querySelectorAll("#color-palette .color");
 
   for (let index = 0; index < color.length; index += 1) {
-    color[index].addEventListener('click', function(event){
-      let cor = event.target.id;
-
+    color[index].addEventListener("click", function (event) {
+      cor = event.target.id;
       for (let i = 0; i < box.length; i += 1) {
-        box[i].addEventListener('click', function(event) {
-        event.target.style.backgroundColor = cor;
-      });
+        box[i].addEventListener("click", function (event) {
+          event.target.style.backgroundColor = cor;
+        });
       }
     });
+  }
+
+  function classSelected() {
+    let newClassName = document.querySelector("#" + cor);
+    console.log(cor);
+    //for (let index = 0; index < color.length; index++) {
+    if (cor === "red") {
+      newClassName.className = "color selected";
+      console.log(newClassName);
+    }
+    //}
   }
 }
 
 function clear() {
-  let button = document.querySelector('#clear-board')
-  let pixel = document.querySelectorAll('div .pixel')
+  let button = document.querySelector("#clear-board");
+  let pixel = document.querySelectorAll("div .pixel");
 
-  button.addEventListener('click', colored)
-  function colored(){
+  button.addEventListener("click", colored);
+  function colored() {
     for (let index = 0; index < pixel.length; index += 1) {
-      pixel[index].style.backgroundColor = 'white'
+      pixel[index].style.backgroundColor = "white";
     }
   }
 }
