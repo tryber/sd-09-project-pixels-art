f79464d293df1f0e6936c0efa2ee35fd5ed13a26
Cristiano Lopes Duarte <tkdcris@gmail.com>
2020-12-09 01:02:09 -0300

    Paleta e pincel funcionando

--- script.js
+++ script.js
@@ -1,24 +1,59 @@
 window.onload = function() {
   let div_colorPalette = document.querySelector('#color-palette');
   let div_matriz = document.querySelector('#pixel-board');
+  let paletteColors = ['black', 'red', 'green', 'blue'];
+  let pincelColor = 'black';
   let entrada = 5;
-  div_matriz.style.width = (entrada*40);
+  // div_matriz.style.width = (entrada*40);
   function createDivsRowsColumns(entrada) {
     for (let i = 1; i <= entrada; i += 1) {
       for (let j = 1; j <= entrada; j += 1) {
         let div = document.createElement('div');
-        div.id = `${i}x${j}`
+        div.id = `${i}x${j}`;
         div.className = 'pixel';
-        div_matriz.appendChild(div);
         div.style.backgroundColor = 'white';
+        div_matriz.appendChild(div);
       }
     }
   }
   createDivsRowsColumns(entrada)
+  // console.log(div_colorPalette);
+  let qntColors = paletteColors.length;
+  function createPalettes(qntColors) {
+    for (let i = 1; i <= qntColors; i += 1) {
+      let div_palette = document.createElement('div');
+      div_palette.id = `div${i}`;
+      div_palette.className = 'color';
+      div_palette.style.backgroundColor = paletteColors[i - 1];
+      div_colorPalette.appendChild(div_palette);
+    }
+  }
+  createPalettes(qntColors);
+
   console.log(div_colorPalette);
   console.log(div_matriz);
+
+  div_matriz.addEventListener('click', colorize);
+
+  function colorize(event) {
+    let targ = event.target;
+    targ.style.backgroundColor = pincelColor;
+    // console.log(targ.style.backgroundColor);
+  }
+
+  div_colorPalette.addEventListener('click', colorCapture);
+
+  function colorCapture(event) {
+    let capturedColor = event.target;
+    let color = capturedColor.style.backgroundColor;
+    pincelColor = color;
+    // pincelColor.style.backgroundColor = capturedColor.style.backgroundColor;
+    console.log(color);
+  }
+
+
 }
 
-// function listener(object) {
-//   object.;
-// }
+// div.addEventListener('click', colorize(i,j));
+
+
