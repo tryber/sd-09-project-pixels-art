31f8b3fbcf1e6e5a5e75e6d526123555c3d9aac6
rach-vp <picanco.raquel@gmail.com>
2020-12-08 21:18:04 -0400

    Implementa limpeza do quadro ao clicar no botão

--- script.js
+++ script.js
@@ -13,22 +13,44 @@ function changeColor(target, pixelColor) {
   }
 }
 
-const pixels = document.querySelectorAll('.pixel');
-for (let index = 0; index < pixels.length; index += 1) {
-  let pixelClicked = pixels[index];
-  pixelClicked.addEventListener('click', function (event) {
-    const selectedColor = document.querySelector('.selected');
-    changeColor(event.target, window.getComputedStyle(selectedColor, null).getPropertyValue('background-color')); 
+function paintPixel () {
+  const pixels = document.querySelectorAll('.pixel');
+  for (let index = 0; index < pixels.length; index += 1) {
+    let pixelClicked = pixels[index];
+    pixelClicked.addEventListener('click', function (event) {
+      const selectedColor = document.querySelector('.selected');
+      changeColor(event.target, window.getComputedStyle(selectedColor, null).getPropertyValue('background-color')); 
   });
-}
+}}
+
+paintPixel();
 
 // Selecionando a cor
-const colorPalette = document.querySelectorAll('.color');
-for (let index = 0; index < colorPalette.length; index += 1) {
-  colorPalette[index].addEventListener('click', function () {
-    const selectedColor = document.querySelector('.selected');
-    selectedColor.className = selectedColor.className.replace(' selected', '');
-    colorPalette[index].className += ' selected';
-    console.log(`Color ${index + 1} selected.`);
-  });
+function selectColor() {
+  const colorPalette = document.querySelectorAll('.color');
+  for (let index = 0; index < colorPalette.length; index += 1) {
+    colorPalette[index].addEventListener('click', function () {
+      const selectedColor = document.querySelector('.selected');
+      selectedColor.className = selectedColor.className.replace(' selected', '');
+      colorPalette[index].className += ' selected';
+      console.log(`Color ${index + 1} selected.`);
+    });
+  }
 }
+
+selectColor();
+
+// Limpar pixels
+
+function clearBoard() {
+  const clearPixels = document.querySelector('#clear-board');
+  clearPixels.addEventListener('click', function () {
+    const pixels = document.querySelectorAll('.pixel');
+    for (let index = 0; index < pixels.length; index += 1) {
+      pixels[index].style.backgroundColor = 'white';
+    }
+    console.log('Board successfully cleared.')
+});
+}
+
+clearBoard();
