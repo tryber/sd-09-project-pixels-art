8ddeed020c2a7e8e62fac2c7c0f90918a92e8948
Gabriel Castro <kastro.gabriel17@gmail.com>
2020-12-09 16:29:33 -0400

    requisitos 8,9: finalizados.

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const blackColor = document.querySelector('#black');
 const redColor = document.querySelector('#red');
 const cyanColor = document.querySelector('#cyan');
 const yellowColor = document.querySelector('#yellow');
+let bgColor = 'black'
+
 
 function palletColor() {
   blackColor.style.backgroundColor = 'black';
@@ -14,21 +16,44 @@ function selectedClass(event) {
   const classSelected = document.querySelector('.selected');
   classSelected.className = 'color';
   event.target.className = 'color selected';
+  bgColor = event.target.style.backgroundColor;
+  console.log(bgColor)
 }
+
 function clickColor() {
   const squade = document.querySelector('#color-palette');
   squade.addEventListener('click', selectedClass);
 }
 
-function clearbutton() {
-  const buttonClear = document.createElement('button');
-  const pixelBoard = document.querySelector('#button-container');
-  buttonClear.innerText = 'Clear';
-  pixelBoard.appendChild(buttonClear);
+function paintSquare() {
+  const pixelBoard = document.querySelector('#pixel-board');
+  pixelBoard.addEventListener('click', changeBackgroundColor);
 }
+
+function changeBackgroundColor(event) {
+  event.target.style.backgroundColor = bgColor;
+}
+
+function button() {
+  const button = document.createElement('button');
+  const buttoncontainer = document.querySelector('#button-container');
+  button.id = 'clear-board';
+  button.innerText = 'Clear';
+  button.addEventListener('click', clearPixel)
+  buttoncontainer.appendChild(button);
+}
+
+function clearPixel() {
+  const pixels = document.querySelectorAll('.pixel');
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].style.backgroundColor = 'white';
+  }
+}
+
 
 window.onload = function () {
   palletColor();
   clickColor();
-  clearbutton();
+  paintSquare();
+  button();
 };
