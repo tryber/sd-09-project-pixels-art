e383ae9e6008e24dbe8ca84e7b1f770ab559cc2d
Gabriel Castro <kastro.gabriel17@gmail.com>
2020-12-09 01:10:20 -0400

    requisito 7: adcionando a classe selected a selecionada

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-window.onload = function () {
-  clearbutton();
-};
+let blackColor = document.querySelector("#black");
+let redColor = document.querySelector("#red");
+let cyanColor = document.querySelector("#cyan");
+let yellowColor = document.querySelector("#yellow");
+
+function palletColor() {
+  blackColor.style.backgroundColor = "black";
+  redColor.style.backgroundColor = "red";
+  cyanColor.style.backgroundColor = "cyan";
+  yellowColor.style.backgroundColor = "yellow";
+}
+
+function clickColor() {
+  let squadeColor = document.querySelector("#color-palette");
+  squadeColor.addEventListener("click", selectedClass);
+}
+function selectedClass(event) {
+  let classSelected = document.querySelector(".selected");
+  classSelected.className = "color";
+  event.target.className = "color selected";
+}
 
 function clearbutton() {
   let buttonClear = document.createElement("button");
@@ -8,3 +26,9 @@ function clearbutton() {
   buttonClear.innerText = "Clear";
   pixelBoard.appendChild(buttonClear);
 }
+
+window.onload = function () {
+  palletColor();
+  clickColor();
+  clearbutton();
+};
