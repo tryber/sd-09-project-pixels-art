7c682e399e78781fc28a7123c27b206f7fa697d7
Pamela Anderson Sales <pamelasalesr@gmail.com>
2020-12-09 17:25:37 -0300

    Add exercise number 6

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 window.onload = function() {
+  let selectBlackColor = document.getElementsByClassName('black');
+  let selectPinkColor = document.getElementsByClassName('pink');
+  let selectChampagneColor = document.getElementsByClassName('champagne');
+  let selectTealColor = document.getElementsByClassName('teal');
 
+  selectBlackColor[0].className += ' selected';
 }
