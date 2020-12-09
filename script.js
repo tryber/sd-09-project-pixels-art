29be20c3bf3023240ec08f590c6fb7f396bec6f0
Vinicius Palenske <viniciuspalenske@gmail.com>
2020-12-09 15:06:23 -0300

    correção de identação e espaços desnecessários

--- script.js
+++ script.js
@@ -1,54 +1,55 @@
 function paletteGenerator(numOfPale) {
+	let paletteColors = document.body.querySelector('#color-palette');
+	paletteColors.style.width = `${numOfPale * 43}px`;
   for (let index = 0; index < numOfPale; index += 1) {
-  	const makeDiv = document.createElement('div');
+		const makeDiv = document.createElement('div');
     makeDiv.className = 'color';
-    document.body.querySelector('#color-palette').appendChild(makeDiv);
+    paletteColors.appendChild(makeDiv);
 	}
 }
 function putColor(numColors) {
 	let colorsPalette = ['rgb(0 , 0 , 0)'];
 	function rgbGenerator() {
-  	let rgb = [0, 0, 0];
-    for (let index = 0; index < 3; index += 1) {
-    	rgb[index] = (Math.floor(Math.random() * 255 + 1));
-      if (index === 2) {
-      	let colorValue = rgb.reduce((total, currentElement) => total + currentElement);
-        if (colorValue > 150 && colorValue < 700) {
-        	index += 1;
-        } else {
+		const rgb = [0, 0, 0];
+		for (let index = 0; index < 3; index += 1) {
+			rgb[index] = (Math.random() * 255 + 1);
+			if (index === 2) {
+				let colorValue = rgb.reduce((total, currentElement) => total + currentElement);
+				if (colorValue > 150 && colorValue < 700) {
+					index += 1;
+				} else {
 					index = 0;
-        }
+				}
 			}
 		}
-		return `rgb(${rgb[2]} , ${rgb[1]} , ${rgb[0]})`;
-  }
+	return `rgb(${rgb[0]} , ${rgb[1]} , ${rgb[2]})`;
+	}
   function repeatTest(cores) {
-  	cores.sort();
-    let repeat = false;
-    for (let i = 0; i < cores.length; i += 1) {
-    	if (cores[i] === cores[i + 1]) {
-      	repeat = true;
-      }
-    }
-    return repeat;
+		let repeat = false;
+		for (let i = 0; i < cores.length; i += 1) {
+			if (cores[i] === cores[i + 1]) {
+			repeat = true;
+			}
+		}
+  return repeat;
   }
   for (let index = 1; index < numColors; index += 1) {
-  	colorsPalette[index] = rgbGenerator();
-    if (index === (numColors - 1)) {
-    	if (repeatTest(colorsPalette) === false) {
-      	index += 1;
-      } else {
-      	index = 0;
-    	}
+		colorsPalette[index] = rgbGenerator();
+		if (index === (numColors - 1)) {
+			if (repeatTest(colorsPalette) === false) {
+				index += 1;
+			} else {
+				index = 0;
+			}
     }
   }
   return colorsPalette;
 }
 
 function setColors(numColors) {
-  let boxNoColor = document.querySelectorAll('div .color');
-  for (let index = 0; index < boxNoColor.length; index += 1) {
-  	document.querySelectorAll('div .color')[index].style.backgroundColor = putColor(numColors)[index];
+	let boxNoColor = document.querySelectorAll('div .color');
+	for (let index = 0; index < boxNoColor.length; index += 1) {
+		document.querySelectorAll('div .color')[index].style.backgroundColor = putColor(numColors)[index];
   }
 }
 
@@ -56,14 +57,14 @@ function whiteFrames(line, column) {
 	let panel = document.body.querySelector('#pixel-board');
   panel.style.width = `${column * 42}px`;
   for (let lIndex = 0; lIndex < line; lIndex += 1) {
-  	for (let cIndex = 0; cIndex < column; cIndex += 1) {
-    	let emptyBox = document.createElement('div');
-      emptyBox.className = 'pixel';
+		for (let cIndex = 0; cIndex < column; cIndex += 1) {
+			let emptyBox = document.createElement('div');
+			emptyBox.className = 'pixel';
       panel.appendChild(emptyBox);
     }
   }
 }
 
 paletteGenerator(4);
-setColors(4);
+setColors(100);
 whiteFrames(5, 5);
