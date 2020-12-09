e0d7d08d03a58d0920a784f0d8e2a341b5edc75b
Carine Becker <caarine.m@gmail.com>
2020-12-09 18:59:24 -0300

    req 8 gloria a deux!

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 window.onload = function () {
+  createPalette();
+  colorsOfThePalette();
   createPixelsBoard();
   selectColor();
+  paintThePixel();
+}
+
+function createPalette() {
+  const colorPalette = document.getElementById('color-palette')
+  const numberOfColors = 4;
+
+  for (let index = 0; index < numberOfColors; index += 1) {
+    const color = document.createElement('div');
+    color.classList.add('color');
+    colorPalette.appendChild(color);
+  }
+  document.querySelector('.color').classList.add('selected')
+}
+
+function colorsOfThePalette() {
+  let paletteElements = document.getElementsByClassName('color');
+  paletteElements[0].style.backgroundColor = 'black';
+  paletteElements[1].style.backgroundColor = 'rgb(196, 77, 88)';
+  paletteElements[2].style.backgroundColor = 'rgb(199, 244, 100)';
+  paletteElements[3].style.backgroundColor = 'rgb(78, 170, 198)';
 }
 
 function createPixelsBoard() {
@@ -15,20 +38,34 @@ function createPixelsBoard() {
       const pixel = document.createElement('div');
       pixel.classList.add('pixel');
       pixelLine.appendChild(pixel);
-      // pixel.addEventListener('click', paintPixel(pixel))
-    }
+
+    };
     pixelBoard.appendChild(pixelLine);
   }
+
 }
 
 function selectColor() {
   let selectColor = document.querySelectorAll('.color')
-  for (index = 0; index < selectColor.length; index += 1) {
-    selectColor[index].addEventListener('click', function() {
-      for (othersIndex = 0; othersIndex < index; othersIndex += 1) {
+  for (let index = 0; index < selectColor.length; index += 1) {
+    selectColor[index].addEventListener('click', function () {
+      for (let othersIndex = 0; othersIndex < index; othersIndex += 1) {
         selectColor[othersIndex].classList.remove('selected')
       }
       this.classList.add('selected')
+    })
+  }
+}
+
+function paintThePixel() {
+  debugger;
+  const pixels = document.querySelectorAll('.pixel')
+
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].addEventListener('click', function () {
+      const selectedElement = document.querySelector('.color.selected');
+      const currentColor = window.getComputedStyle(selectedElement).getPropertyValue('background-color')
+      this.style.backgroundColor = currentColor;
     })
   }
 }
