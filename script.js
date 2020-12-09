caff78a659c41723c0909ec63a753513896e3ae9
Cristiano Lopes Duarte <tkdcris@gmail.com>
2020-12-09 17:24:28 -0300

    Cores randomicas implementadas

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 window.onload = function() {
   let div_colorPalette = document.querySelector('#color-palette');
   let div_matriz = document.querySelector('#pixel-board');
-  let paletteColors = ['black', 'red', 'green', 'blue'];
+  let rgbRandom = randomColorRGB()
+  let paletteColors = ['black', rgbRandom[0] , rgbRandom[1] , rgbRandom[2]];
   let pincelColor = 'black';
+
+  function randomColorRGB() {
+    let rgbArray = [];
+    for (let rgb = 0; rgb < 3; rgb += 1) {
+      let rgbNum = [];
+      for (let i = 0; i < 3; i += 1) {
+        rgbNum.push(Math.ceil(Math.random() * 256));
+      }
+      rgbArray.push([`rgb(${rgbNum[0]} , ${rgbNum[1]} , ${rgbNum[2]})`]);
+    }
+    return rgbArray;
+  }
 
   function createDivsRowsColumns(entrada) {
     let div_matriz = document.querySelector('#pixel-board');
     while (div_matriz.firstChild) {
       div_matriz.removeChild(div_matriz.lastChild);
     }
+
     let heighWidth = entrada * '42';
     div_matriz.style.width = heighWidth+'px';
     div_matriz.style.height = heighWidth+'px';
@@ -23,7 +37,6 @@ window.onload = function() {
     }
   }
   createDivsRowsColumns(5);
-
 
   let qntColors = paletteColors.length;
   function createPalettes(qntColors) {
@@ -48,6 +61,7 @@ window.onload = function() {
     let targ = event.target;
     targ.style.backgroundColor = pincelColor;
   }
+
   function white() {
     let clearDivs = div_matriz.children;
     for (let i = 0; i < clearDivs.length; i += 1) {
@@ -79,5 +93,4 @@ window.onload = function() {
     }
     createDivsRowsColumns(num);
   }
-
 }
