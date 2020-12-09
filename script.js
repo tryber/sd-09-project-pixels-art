680afc2488e5a451a4f4a207f679b43d058c4aa4
devfelipesantiago <felipesantiagolds@gmail.com>
2020-12-08 22:05:17 -0300

    Limpando com o botao limpar

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 window.onload = function () {
+  createEventColorBox();
+  clear()
+}
 
+function createEventColorBox() {
   let box = document.querySelector('#pixel-board');
-  let color = document.querySelector('#black');
+  let color = document.querySelectorAll('.color');
 
   function boxColor(event) {
-    event.target.style.backgroundColor = 'black';
+    event.target.style.backgroundColor = 'gray';
   }
   box.addEventListener('click', boxColor);
+}
+
+function clear() {
+  let button = document.querySelector('#clear-board')
+  console.log(button);
+  let pixel = document.querySelectorAll('div .pixel')
+
+  button.addEventListener('click', colored)
+  function colored(){
+    for (let index = 0; index < pixel.length; index++) {
+      pixel[index].style.backgroundColor = 'white'
+    }
+  }
 }
