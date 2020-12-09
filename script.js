2fab932f99e6aef8dfa68116077bd920466c2203
Danrlley Rodrigues <danrlley.c.a.rodrigues@gmail.com>
2020-12-09 15:41:15 -0300

    Teste 1

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-window.onload(document.getElementsByClassName('color1')[0].classList.add('selected'))
+// window.onload(document.getElementsByClassName('color1')[0].classList.add('selected'))
 
+// document.querySelector('.color.color2').addEventListener('onclick', function(){
+//     document.querySelector('.color.selected').classList.remove('selected');
+//     document.querySelector('.color2').classList.add('selected')
+// });
 
-function changeSelected () {
-    if (document.querySelector('.color2').addEventListener('onclick')) {
-        document.querySelector('.selected').classList.remove('selected');
-        document.querySelector('color2').classList.add('selected')
-    }
-}
+// function changeSelected () {
+//     if (document.querySelector('.color2').addEventListener = 'onclick') {
+//         document.querySelector('.selected').classList.remove('selected');
+//         document.querySelector('color2').classList.add('selected')
+//     }
+// }
 
-changeSelected();
+// changeSelected();
