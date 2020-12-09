216a49eeddaf08a6678e12348a8415d10b8881be
Johne Alves <johne_af@hotmail.com>
2020-12-09 08:36:57 -0300

    adiciona bonus do projeto

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+window.onload = function() {
+  let pixelBoard = document.querySelector('#pixel-board');
+  for (index = 0; index < 25; index += 1 ) {
+    let pixel = document.createElement("div");
+    pixel.className = 'pixel';
+    pixelBoard.appendChild(pixel);
+  }  
+}
 
 function changeSelected() {
   const paletteColors = document.getElementById('color-palette');
@@ -18,7 +26,6 @@ function paintPixel() {
     let selectedColor = document.querySelector('.selected')
     let colorPaint = selectedColor.id
     event.target.style.background = colorPaint;
-    
   });
 }
 paintPixel()
@@ -33,3 +40,45 @@ function clearBoard() {
   });
 }
 clearBoard()
+
+function sizeBoard() {
+  let pixelBoard = document.querySelector('#pixel-board');
+  while (pixelBoard.firstChild) {
+  pixelBoard.removeChild(pixelBoard.firstChild);
+  }
+  defineNumber()
+}
+
+function defineNumber() {
+  let number = document.getElementById('board-size').value;
+  if (number === '' || number < 0) {
+    alert('Board inválido!');
+    number = 5;
+  } else if (number < 5) {
+    alert('Valor menor que 5, considerar 5 como padrão');
+    number = 5;
+  } else if (number > 50) {
+    alert('Valor menor que 5, considerar 50 como padrão');
+    number = 50;
+  }
+  newBoard(number)
+}
+
+function newBoard(number) {
+  const numberTotal = number * number
+  const pixelBoard = document.querySelector('#pixel-board');
+  let area = number * 42
+  pixelBoard.style.width = `${area}px`
+  for (index = 0; index < numberTotal; index += 1 ) {
+    let pixel = document.createElement("div");
+    pixel.className = 'pixel';
+    pixel.style.background = 'white';
+    pixelBoard.appendChild(pixel);
+  }  
+}
+
+function generatorBoard() {
+  let generate = document.querySelector('#generate-board')
+  generate.addEventListener('click', sizeBoard)
+}
+generatorBoard()
