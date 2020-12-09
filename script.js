48c3a01db74ec326ef9498990d7d93218c4cdbf9
rach-vp <picanco.raquel@gmail.com>
2020-12-08 20:57:32 -0400

    Implementa coleta de cores e pintura (req 7 e 8)

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 window.onload = function () {
   const blackPixelPalette = document.querySelector('.black');
   blackPixelPalette.className += ' selected';
-  console.log('Black color selected');
+  console.log('Color 1 selected');
 };
 
 // Pintando o pixel
@@ -18,14 +18,17 @@ for (let index = 0; index < pixels.length; index += 1) {
   let pixelClicked = pixels[index];
   pixelClicked.addEventListener('click', function (event) {
     const selectedColor = document.querySelector('.selected');
-    changeColor(event.target, window.getComputedStyle(selectedColor,null).getPropertyValue('background-color')); 
+    changeColor(event.target, window.getComputedStyle(selectedColor, null).getPropertyValue('background-color')); 
   });
 }
 
-// const colorPalette = document.querySelectorAll('.color');
-// for (let index = 0; index < colorPalette; index += 1) {
-//   colorPalette[index].addEventListener('click', function () {
-//     const selectedColors = document.querySelectorAll('.selected');
-
-//   });
-// }
+// Selecionando a cor
+const colorPalette = document.querySelectorAll('.color');
+for (let index = 0; index < colorPalette.length; index += 1) {
+  colorPalette[index].addEventListener('click', function () {
+    const selectedColor = document.querySelector('.selected');
+    selectedColor.className = selectedColor.className.replace(' selected', '');
+    colorPalette[index].className += ' selected';
+    console.log(`Color ${index + 1} selected.`);
+  });
+}
