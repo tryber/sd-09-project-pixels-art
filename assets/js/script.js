62ccc66d9fded4760a13b9a9355e2ec101170a42
Rafael Geronimo <rafaelgeronimo@outlook.com>
2020-12-09 13:22:19 -0300

    Some code climate errors repaired

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,60 +1,52 @@
-window.onload = function () {
-  createColor();
-  drawPixelBoard(5);
-  paintPixel();
-}
-
-
-let pixelBoard = document.querySelector('#pixel-board');
-let selectedColor = 'rgb(0 , 0 , 0)'
-let colorBox = document.getElementsByClassName('color');
+const pixelBoard = document.querySelector('#pixel-board');
+let selectedColor = 'rgb(0 , 0 , 0)';
+const colorBox = document.getElementsByClassName('color');
 let clearButton = document.querySelector('#clear-board');
 clearButton.addEventListener('click', clearCanvas);
 
 // Função para criar a paleta de cores selecionáveis
 function createColor() {
-  let colorBox = document.querySelectorAll('.color');
+  //let colorBox = document.querySelectorAll('.color');
   for (let index = 0; index < colorBox.length; index += 1) {
-    let randomColor = []
-    let rgbColors = 3
+    const randomColor = [];
+    const rgbColors = 3;
     for (let rgbIndex = 0; rgbIndex < rgbColors; rgbIndex += 1) {
-      randomColor.push(Math.floor(Math.random() * 254))
+      randomColor.push(Math.floor(Math.random() * 254));
     }
-    colorBox[index].style.backgroundColor = 'rgb(' + randomColor + ')';
+    colorBox[index].style.backgroundColor = `rgb(${randomColor})`;
   }
   colorBox[0].style.backgroundColor = 'rgb(0 , 0 , 0)';
   colorBox[0].classList.add('selected');
 }
 
-// Função para desenhar o quadro de pixel conforme a quantidade de linhas desejadas
-function drawPixelBoard(lines) {
-  let pixelBoard = document.querySelector('#pixel-board');
-  for (let line = 1; line <= lines; line += 1) {
-    let lineId = 'line' + line
-    let newLine = document.createElement('tr')
-    newLine.setAttribute('id', lineId)
-    pixelBoard.appendChild(newLine)
-    drawColumns(line, lines)
+function drawColumns(lineId, columns) {
+  const line = document.querySelector(`#line${lineId}`);
+  for (let lineColumn = 1; lineColumn <= columns; lineColumn += 1) {
+    const newPixel = document.createElement('td');
+    newPixel.setAttribute('class', 'pixel');
+    newPixel.style.backgroundColor = 'white';
+    line.appendChild(newPixel);
   }
 }
 
-function drawColumns(lineId, columns) {
-  let line = document.querySelector('#line' + lineId);
-  for (let lineColumn = 1; lineColumn <= columns; lineColumn += 1) {
-    let newPixel = document.createElement('td')
-    newPixel.setAttribute('class', 'pixel')
-    newPixel.style.backgroundColor = 'white'
-    line.appendChild(newPixel)
+// Função para desenhar o quadro de pixel conforme a quantidade de linhas desejadas
+function drawPixelBoard(lines) {
+  for (let line = 1; line <= lines; line += 1) {
+    const lineId = `line${line}`;
+    const newLine = document.createElement('tr');
+    newLine.setAttribute('id', lineId);
+    pixelBoard.appendChild(newLine);
+    drawColumns(line, lines);
   }
 }
 
 for (let index = 0; index < colorBox.length; index += 1) {
   colorBox[index].onclick = function () {
-    colorBox[index].classList.add('selected')
+    colorBox[index].classList.add('selected');
     selectedColor = colorBox[index].style.backgroundColor;
     for (let box = 0; box < colorBox.length; box += 1) {
       if (colorBox[index] !== colorBox[box] && colorBox[box].classList.contains('selected')) {
-        colorBox[box].classList.remove('selected')
+        colorBox[box].classList.remove('selected');
       }
     }
   }
@@ -63,9 +55,9 @@ for (let index = 0; index < colorBox.length; index += 1) {
 // função para pintar o pixel clicado com a cor selecionada
 function paintPixel() {
   for (let index = 0; index < pixelBoard.childElementCount; index += 1) {
-    let boardLine = pixelBoard.children[index];
+    const boardLine = pixelBoard.children[index];
     for (let pixel = 0; pixel < boardLine.children.length; pixel += 1) {
-      let paintPixel = pixelBoard.children[index].children[pixel]
+      const paintPixel = pixelBoard.children[index].children[pixel];
       paintPixel.onclick = function() {
         paintPixel.style.backgroundColor = selectedColor;
       }
@@ -75,10 +67,16 @@ function paintPixel() {
 
 function clearCanvas() {
   for (let index = 0; index < pixelBoard.childElementCount; index += 1) {
-    let boardLine = pixelBoard.children[index];
+    const boardLine = pixelBoard.children[index];
     for (let pixel = 0; pixel < boardLine.children.length; pixel += 1) {
-      let paintPixel = pixelBoard.children[index].children[pixel]
+      const paintPixel = pixelBoard.children[index].children[pixel];
       paintPixel.style.backgroundColor = 'white';
     }
   }
+}
+
+window.onload = function () {
+  createColor();
+  drawPixelBoard(5);
+  paintPixel();
 }
