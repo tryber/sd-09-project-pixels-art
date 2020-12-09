90396756c8728da7202d5fb22f25c1ee52e8d5d8
Carine Becker <caarine.m@gmail.com>
2020-12-09 10:23:08 -0300

    req 7

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 window.onload = function () {
   createPixelsBoard();
+  selectColor();
 }
 
 function createPixelsBoard() {
@@ -14,7 +15,20 @@ function createPixelsBoard() {
       const pixel = document.createElement('div');
       pixel.classList.add('pixel');
       pixelLine.appendChild(pixel);
+      // pixel.addEventListener('click', paintPixel(pixel))
     }
     pixelBoard.appendChild(pixelLine);
+  }
+}
+
+function selectColor() {
+  let selectColor = document.querySelectorAll('.color')
+  for (index = 0; index < selectColor.length; index += 1) {
+    selectColor[index].addEventListener('click', function() {
+      for (othersIndex = 0; othersIndex < index; othersIndex += 1) {
+        selectColor[othersIndex].classList.remove('selected')
+      }
+      this.classList.add('selected')
+    })
   }
 }
