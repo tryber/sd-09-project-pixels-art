0bff8d8df531f8e5fde15d3c9e4bfc7eefa8921f
LeoMajevski <leonardo.majevski@gmail.com>
2020-12-09 19:43:38 -0300

    Mandatory requirement #02

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-asa
+let colorPalette = document.createElement('div');
+colorPalette.setAttribute('id', 'color-palette');
+
+let mainBody = document.getElementsByTagName('main')[0];
+mainBody.appendChild(colorPalette);
+
+let arrayOfColors = ['black', 'red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+
+for (let index in arrayOfColors) {
+  let colorDiv = document.createElement('div');
+  // colorDiv.setAttribute('class', 'color');
+  colorDiv.setAttribute('class', 'color ' + arrayOfColors[index]);
+  colorPalette.appendChild(colorDiv);
+}
