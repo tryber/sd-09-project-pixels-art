821da16ed151af86faec2f1ddc46eedfa451de26
caabeatriz <contatocarolinebs@gmail.com>
2020-12-08 23:38:13 -0300

    CodeClimateFix: Ajust spacing

--- script.js
+++ script.js
@@ -5,39 +5,41 @@ document.querySelectorAll('.color').forEach(item => {
         verifiedClass()
         item.classList.add("selected")
         color = item.id
-})})
+    })
+})
 
-function verifiedClass(){
+function verifiedClass() {
     let element = document.querySelector('.selected')
     element.classList.remove("selected")
 }
 
-    document.querySelectorAll('.pixel').forEach(itemPixel => {
-        itemPixel.addEventListener('click', event => {
-        
-            event.preventDefault()
-            itemPixel.style.backgroundColor = 'black';
-            if(color == 'black'){
-                itemPixel.style.backgroundColor = 'black';
-        
-            } else if(color == 'red'){
-                itemPixel.style.backgroundColor = 'red';
-        
-            }
-            else if(color == 'yellow'){
-                itemPixel.style.backgroundColor = 'yellow';
-        
-            }else if(color == 'purple'){
-                itemPixel.style.backgroundColor = 'purple';
-        
-            }
-        
-        })})
+document.querySelectorAll('.pixel').forEach(itemPixel => {
+    itemPixel.addEventListener('click', event => {
 
-        button.addEventListener('click', eventButton => {
-            eventButton.preventDefault()
-            document.querySelectorAll('.pixel').forEach(itemPixel => {
-                itemPixel.style.backgroundColor = 'white'
-                    
-            })
-        })
+        event.preventDefault()
+        itemPixel.style.backgroundColor = 'black';
+        if (color == 'black') {
+            itemPixel.style.backgroundColor = 'black';
+
+        } else if (color == 'red') {
+            itemPixel.style.backgroundColor = 'red';
+
+        }
+        else if (color == 'yellow') {
+            itemPixel.style.backgroundColor = 'yellow';
+
+        } else if (color == 'purple') {
+            itemPixel.style.backgroundColor = 'purple';
+
+        }
+
+    })
+})
+
+button.addEventListener('click', eventButton => {
+    eventButton.preventDefault()
+    document.querySelectorAll('.pixel').forEach(itemPixel => {
+        itemPixel.style.backgroundColor = 'white'
+
+    })
+})
