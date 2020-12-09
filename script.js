1f50b88fca6b30a7be8388669b845a7944171b4e
LeoMajevski <leonardo.majevski@gmail.com>
2020-12-09 20:53:26 -0300

    Creating elements only with javascript

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
+let title = document.createElement('h1');
+title.setAttribute('id', 'title');
+title.innerText = 'Paleta de Cores';
+
 let colorPalette = document.createElement('div');
 colorPalette.setAttribute('id', 'color-palette');
 
 let headerBody = document.getElementsByTagName('header')[0];
+headerBody.appendChild(title);
 headerBody.appendChild(colorPalette);
 
 let arrayOfColors = ['black', 'red', 'green', 'blue'];
-
 
 for (let index in arrayOfColors) {
   let colorDiv = document.createElement('div');
@@ -13,4 +17,7 @@ for (let index in arrayOfColors) {
   colorPalette.appendChild(colorDiv);
 }
 
-let pixelsBoard
+let table = document.createElement('table');
+let tr = document.createElement('tr');
+let td = document.createElement('td');
+tr.appendChild(td);
