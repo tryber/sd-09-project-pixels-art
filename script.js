70562aa8809b25d21df3be906059428d2b06faf1
Vinicius Palenske <viniciuspalenske@gmail.com>
2020-12-09 04:31:32 -0300

    criação de funções para: gerar rgb / testar cor igual / colorir paleta / gerar quadro branco

--- script.js
+++ script.js
@@ -1,44 +1,65 @@
-function randColor() {
-    let setOfColors = [];
-    setOfColors[0] = 'rgb(0 , 0 , 0)';
-    for(let index = 1; index < 4;) {
-        let r = Math.floor(Math.random() * 255 + 1);
-        let g = Math.floor(Math.random() * 255 + 1);
-        let b = Math.floor(Math.random() * 255 + 1);
-        if ((r+g+b) < 720 && (r+g+b) > 75) {
-            setOfColors[index] = `rgb(${r} , ${g} , ${b})`;
-            if (setOfColors[index] !== (setOfColors[index-1] && setOfColors[index-2] && setOfColors[index-3])) {
-                index += 1;
-            }
-        }
-    }
-    return setOfColors;
-}
+paletteGenerator();
+setColors();
+paintingFrame();
 
-function paletteGen() {
-    for(let index = 0; index < 4; index += 1) {
+function paletteGenerator() {
+    for (let index = 0; index < 4; index += 1) {
         let makeDiv = document.createElement('div');
         makeDiv.className = 'color';
         document.body.querySelector('#color-palette').appendChild(makeDiv);
     }
 }
 
-function setColors(){
-    let boxNoColor = document.querySelectorAll('div .color');
-    for (let index = 0; index < boxNoColor.length; index += 1){
-        document.querySelectorAll('div .color')[index].style.backgroundColor = randColor()[index];
+function putColor() {
+    let numberOfColors = 4;
+    let colorsOfPalette = [];
+    colorsOfPalette.push('rgb(0 , 0 , 0)');
+    for (let index = 1; index < numberOfColors; index += 1) {
+        colorsOfPalette[index] = rgbGenerator();
+        if(index == (numberOfColors-1)) {
+            if (repeatTest(colorsOfPalette) === false) {
+                return colorsOfPalette;
+            }
+        }
+    }
+    function rgbGenerator() {
+        let rgb = [0,0,0];
+        for (let index = 0; index < 3; index += 1) {
+            rgb[index] = (Math.floor(Math.random() * 255 + 1));
+            if (index === 2) {
+                let colorValue = rgb.reduce((total, currentElement) => total + currentElement);
+                if (colorValue > 150 && colorValue < 700) { // Verifica se a cor é é muito clara ou muito escura
+                    return `rgb(${rgb[2]} , ${rgb[1]} , ${rgb[0]})`;
+                } else {
+                    index = 0;
+                }
+            }
+        }
+    }
+    function repeatTest(cores) {
+        cores.sort();
+        let repeat = false;
+        for (let i = 0; i < cores.length; i += 1) {
+            if (cores[i] === cores[i+1]) {
+                repeat = true;
+            }
+        }
+        return repeat;
     }
 }
 
-function paintingFrame(){
-    for(let index = 0; index < 25; index +=1) {
+function setColors() {
+    let boxNoColor = document.querySelectorAll('div .color');
+    for (let index = 0; index < boxNoColor.length; index += 1) {
+        document.querySelectorAll('div .color')[index].style.backgroundColor = putColor()[index];
+    }
+}
+
+function paintingFrame() {
+    for (let index = 0; index < 25; index +=1) {
         let emptyBox = document.createElement('div');
         emptyBox.className = 'pixel';
         document.body.querySelector('#pixel-board').appendChild(emptyBox);
     }
     
 }
-
-paletteGen();
-setColors();
-paintingFrame();
