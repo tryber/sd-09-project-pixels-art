31251ce65d4da6656423e2979d4b30e352f51e6f
MarcosAlbuquerque <mpdsa20@gmail.com>
2020-12-09 14:59:07 -0300

    melhorias no codigo, inclusao do botao limpar, ajustes do codeclimate

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
-const paletaDeCores = document.getElementById('color-palette')
-const paletaDePreenchimento = document.getElementById('pixel-board')
-let corPreenchida = document.getElementsByClassName("color")
-let corDePintura = document.getElementsByClassName("pixel")
-let corDorQuadrado = ""
+const paletaDeCores = document.getElementById('color-palette');
+const paletaDePreenchimento = document.getElementById('pixel-board');
+const resetaPaleta = document.getElementById('clear-board');
+const corPreenchida = document.getElementsByClassName('color')
+const corDePintura = document.getElementsByClassName('pixel')
+let corDorQuadrado = 'black';
 
 paletaDeCores.addEventListener('click', (event) => {
   for (let i = 0; i <= corPreenchida.length; i += 1) {
-      if (corPreenchida[i] === event.target) {
-          // console.log("evento Selecionado")
-          corPreenchida[i].classList.add('selected')
-          corDorQuadrado = corPreenchida[i].classList[1]
-      } else if (corPreenchida[i] !== event.target) {
-          try {
-              corPreenchida[i].classList.remove('selected');
-          } catch (e) {
-              return e
-              // console.log("Foi removido seleção anterior")
-}}}})
+    if (corPreenchida[i] === event.target) {
+      // console.log("evento Selecionado")
+      corPreenchida[i].classList.add('selected');
+      corDorQuadrado = corPreenchida[i].classList[1];
+    } else if (corPreenchida[i] !== event.target) {
+      try {
+        corPreenchida[i].classList.remove('selected');
+      } catch (e) {
+        return e;
+        // console.log("Foi removido seleção anterior")
+    }
+   }
+  }
+ }
+);
 
 paletaDePreenchimento.addEventListener('click', (event) => {
   for (let i = 0; i <= corDePintura.length; i += 1) {
     if (corDePintura[i] === event.target) {
       try {
-      corDePintura[i].className = "pixel"
+      corDePintura[i].className = 'pixel';
       corDePintura[i].classList.add(corDorQuadrado);
       } catch (e) {
-      return e
-}}}})
+      return e;
+    }
+   }
+  }
+ }
+)
+
+resetaPaleta.addEventListener('click', (event) => {
+  for (let i = 0; i < corDePintura.length; i += 1) {
+    corDePintura[i].className = 'pixel';
+    console.log("Cores limpas")
+  }
+})
