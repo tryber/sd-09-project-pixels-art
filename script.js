2a77baa7d4bab50420cc01c6444a9c6ab82c877d
devfelipesantiago <felipesantiagolds@gmail.com>
2020-12-09 19:15:22 -0300

    New Table

--- script.js
+++ script.js
@@ -1,34 +1,34 @@
 function createEventColorBox() {
-  let box = document.querySelector("#pixel-board");
+  let box = document.querySelector('#pixel-board');
 
   box.addEventListener('click', (event) => {
     let selectedColor = document.querySelector('.selected');
-    let cor = window.getComputedStyle(selectedColor)
-    event.target.style.backgroundColor = cor.getPropertyValue('background-color');
-
-
-  })
+    let cor = window.getComputedStyle(selectedColor);
+    event.target.style.backgroundColor = cor.getPropertyValue(
+      'background-color'
+    );
+  });
 }
 
 function clear() {
-  let button = document.querySelector("#clear-board");
-  let pixel = document.querySelectorAll("div .pixel");
+  let button = document.querySelector('#clear-board');
+  let pixel = document.querySelectorAll('div .pixel');
 
-  button.addEventListener("click", colored);
+  button.addEventListener('click', colored);
   function colored() {
     for (let index = 0; index < pixel.length; index += 1) {
-      pixel[index].style.backgroundColor = "white";
+      pixel[index].style.backgroundColor = 'white';
     }
   }
 }
 
 function classSelected() {
-  let color = document.querySelectorAll('.color')
-  let colorP = document.querySelector('#color-palette')
+  let color = document.querySelectorAll('.color');
+  let colorP = document.querySelector('#color-palette');
 
   colorP.addEventListener('click', (event) => {
     for (let index = 0; index < color.length; index++) {
-      if (event.target.id != 'color-palette'){
+      if (event.target.id != 'color-palette') {
         color[index].classList.remove('selected');
         event.target.classList.add('selected');
       }
@@ -36,20 +36,8 @@ function classSelected() {
   });
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
 window.onload = function () {
-  classSelected()
+  classSelected();
   createEventColorBox();
   clear();
 };
