5e241cbdffe08aba2acd7574adc4470da939efcd
Diego Vini <analistamm@gmail.com>
2020-12-08 22:47:01 -0300

    Ajustes de code climate

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-window.onload = function(){
-    let rowColumnTotal = 5;
-    let divPixelBoard = document.getElementById('pixel-board')
+window.onload = function () {
+  let rowColumnTotal = 5;
+  const divPixelBoard = document.getElementById('pixel-board');
     
-    createDivPixel();
+  createDivPixel();
 
-    function createDiv(className){
-        let divBox = document.createElement('div');
-        divBox.className = className;
-        return divBox;
+  function createDiv(className) {
+    let divBox = document.createElement('div');
+    divBox.className = className;
+    return divBox;
     };
 
-    function createDivPixel(){
-        for(let indexDivPixel = 0; indexDivPixel < rowColumnTotal; indexDivPixel += 1){
-            for(indexDivBoxPixel = 0; indexDivBoxPixel< rowColumnTotal; indexDivBoxPixel += 1){
-                let createBox = createDiv('pixel');
-                let pixelFrame = divPixelBoard.appendChild(createBox);
-            };
-        };
-    };
-  };
+  function createDivPixel() {
+    for (let indexDivPixel = 0; indexDivPixel < rowColumnTotal; indexDivPixel += 1) {
+      for (let indexDivBoxPixel = 0; indexDivBoxPixel< rowColumnTotal; indexDivBoxPixel += 1) {
+        let createBox = createDiv('pixel');
+          const pixelFrame = divPixelBoard.appendChild(createBox);
+            }
+        }
+    }
+  }
