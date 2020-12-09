477c67596dafe896a94c8ecbe0ffa46709e99311
Poeta <pedrotrybe@gmail.com>
2020-12-09 17:45:02 -0300

    JS update

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 window.onload = function() {
-  let selectColor1 = document.getElementsByClassName('.background-item1');
-  let selectColor2 = document.getElementsByClassName('.background-item2');
-  let selectColor3 = document.getElementsByClassName('.background-item3');
-  let selectColor4 = document.getElementsByClassName('.background-item4');
-
+  let selectColor1 = document.getElementsByClassName('background-item1');
+  let selectColor2 = document.getElementsByClassName('background-item2');
+  let selectColor3 = document.getElementsByClassName('background-item3');
+  let selectColor4 = document.getElementsByClassName('background-item4');
   selectColor1[0].className += ' selected';
 }
