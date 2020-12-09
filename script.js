043ba8b88ad1724892143a3a1d0b2af918ba4373
Rafael Azevedo <rafaelazevedo321@gmail.com>
2020-12-09 09:52:31 -0300

    corrigindo codeclimate

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-window.onload = function() {
-    let btn = document.querySelector('#clear-board');
-    btn.addEventListener('click', function() {
-        let pixels = document.querySelectorAll('.pixel');
-        for (let index = 0; index < pixels.length; index += 1) {
-             pixels[index].style.backgroundColor = 'white';
-        }
-    });
+window.onload = function () {
+  const btn = document.querySelector('#clear-board');
+  btn.addEventListener('click', function() {
+    const pixels = document.querySelectorAll('.pixel');
+    for (let index = 0; index < pixels.length; index += 1) {
+      pixels[index].style.backgroundColor = 'white';
+    }
+  });
 
-    let colorPalette = document.querySelector('#color-palette');
-    colorPalette.addEventListener('click', function(event) {
-        let colors = document.querySelectorAll('.color')
-        for(let index in colors) {
-            if (event.target === colors[index]) {
-                let selected = document.querySelector('.selected');
-                selected.classList.remove('selected');
-                event.target.classList.add('selected');
-            }
-        }
-    });
-    
-    let pixelBoard = document.querySelector('#pixel-board');
-    pixelBoard.addEventListener('click', function(event) {
-        if (event.target.className === 'pixel') {
-            let color = document.querySelector('.selected');
-            event.target.style.backgroundColor = color.classList[1];
-        }
-    });
-}
+  const colorPalette = document.querySelector('#color-palette');
+  colorPalette.addEventListener('click', function (event) {
+  const colors = document.querySelectorAll('.color');
+  for (let index in colors) {
+    if (event.target === colors[index]) {
+      let selected = document.querySelector('.selected');
+      selected.classList.remove('selected');
+      event.target.classList.add('selected');
+    }
+  }
+  });
+
+  const pixelBoard = document.querySelector('#pixel-board');
+  pixelBoard.addEventListener('click', function (event) {
+    if (event.target.className === 'pixel') {
+      const color = document.querySelector('.selected');
+      event.target.style.backgroundColor = color.classList[1];
+    }
+  });
+};
