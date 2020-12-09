51e6690a5cb7d525320647376a17e97bb163ff6a
Danrlley Rodrigues <danrlley.c.a.rodrigues@gmail.com>
2020-12-09 16:22:04 -0300

    Requisito 7 Concluido

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-// window.onload(document.getElementsByClassName('color1')[0].classList.add('selected'))
+function loadSelected (){
+    document.getElementsByClassName('color1')[0].classList.add('selected')
+}
 
-// document.querySelector('.color.color2').addEventListener('onclick', function(){
-//     document.querySelector('.color.selected').classList.remove('selected');
-//     document.querySelector('.color2').classList.add('selected')
-// });
+window.onload(loadSelected());
 
-// function changeSelected () {
-//     if (document.querySelector('.color2').addEventListener = 'onclick') {
-//         document.querySelector('.selected').classList.remove('selected');
-//         document.querySelector('color2').classList.add('selected')
-//     }
-// }
-
-// changeSelected();
+function changeSelected() {
+    let selectedColor = document.getElementsByClassName('color');
+    for (i = 0; i < selectedColor.length; i++) {
+        selectedColor[i].addEventListener('click', function() {
+            for (i2 = 0; i2 < i; i2++) {
+                selectedColor[i2].classList.remove('selected')
+            }
+            this.classList.add('selected')
+        })
+    }
+}
+changeSelected();
