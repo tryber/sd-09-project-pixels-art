62bf3c1f838ac184e3e88f72a7ed2c5c31d5cc77
Luciano_Lodi <lrlodi@gmail.com>
2020-12-09 17:49:16 -0300

    Requisito 7. Altera a cor selecionada

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 window.onload = function () {
 	let pixelRow = document.getElementById('pixel-board');
 	let canvasHeight = 5;
-	let canvasWidth = 5;
+  let canvasWidth = 5;   
+  let bgColor = 'black';
+  let chosenColor = document.querySelector('.selected').style.backgroundColor;
 
 	function createRows() {
     for(let i = 0; i < canvasHeight; i += 1) {
@@ -11,10 +13,34 @@ window.onload = function () {
       for(let j = 0; j < canvasWidth; j += 1) {
         let pixel = document.createElement('div');
         pixel.className = 'pixel';
+        pixel.addEventListener('click', paintPixel);
         row.appendChild(pixel);
       }
       pixelRow.appendChild(row);
     }
   }
-	createRows();
+  createRows();
+  startPalette();
+  
+  function paintPixel() {
+    console.log(event.target);
+    event.target.style.backgroundColor = chosenColor;
+  }
+
+  function startPalette() {
+    let colorOptions = document.querySelectorAll('.color');
+    for(let i = 0; i < colorOptions.length; i += 1){
+      colorOptions[i].addEventListener('click', selectedColor)
+    }    
+  }
+
+  function selectedColor() {
+    let actualColor = event.target;
+    if(actualColor.className = 'color') {
+      let lastSelectedColor = document.querySelector('.selected');
+      lastSelectedColor.classList.remove('selected');
+      actualColor.classList.add('selected');
+    }
+  }
+
 }
