bd7b515fbc9a213fafed2ed0bb2a0771871d0330
Pedro Henrique <pedro.876.ph@gmail.com>
2020-12-09 12:09:55 -0300

    ajustes ate o requisito 4

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function pxBoard(){
     let line;
     for(let lineIndex = 0; lineIndex < 5; lineIndex += 1) {
         let lineContainer = document.createElement('div');
+        lineContainer.className = 'pixel-line';
         for(let columnIndex = 0; columnIndex < 5; columnIndex += 1) {
             let pxColumn = document.createElement('div');
             pxColumn.className = 'pixel';
