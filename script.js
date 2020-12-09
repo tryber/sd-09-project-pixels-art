4007fb47c8e8baa4ebb443468d0c993f010a54a2
Giovani L D <giovani.diccini@hotmail.com>
2020-12-09 15:47:41 -0300

    functions removeColorSelction and selectColor added

--- script.js
+++ script.js
@@ -2,3 +2,27 @@ window.onload = function() {
     let blackSquare = document.querySelector('.color');
     blackSquare.className = 'color selected';
 }
+
+function removeColorSelection(){
+    let colorPalette = document.querySelector('#color-palette');
+    colorPalette.addEventListener('click', function(event){
+        let colorSelected = document.querySelector('.selected');
+        colorSelected.className = 'color';
+    });
+}
+
+removeColorSelection()
+
+function selectColor(){
+    let colorSelected = document.querySelector('.color', '.selected');
+    colorSelected.className = 'color';
+    let colorPalette = document.querySelector('#color-palette');
+    colorPalette.addEventListener('click', function(event){
+        let eventClass = event.target.className;
+        if (eventClass !== ''){
+            event.target.className = 'color selected';
+        }        
+    });
+};
+
+selectColor();
