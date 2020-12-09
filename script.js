7bfaad94610e334774339d37fa0ad01d28c2a473
Nathália <nathaliaveneziano@icloud.com>
2020-12-09 12:07:14 -0300

    Requisitos 09 - Alteraçoes

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Variáveis Globais
 const colorPalette = document.querySelector('#color-palette');
 const pixelBoard = document.querySelector('#pixel-board');
+const colors = ['black', 'blue', 'pink', 'purple'];
 let colorSelected = '';
 
 // Funções
 function createColorPalette(blocks) {
-  const colors = ['black', 'blue', 'pink', 'purple'];
   colorSelected = '';
 
   for (let index = 0; index < colors.length; index += 1) {
@@ -46,6 +46,12 @@ function createPixelBoard() {
   }
 }
 
+function removeColorClasses(pixelElement) {
+  for (let indexColor = 0; indexColor < colors.length; indexColor += 1) {
+    pixelElement.classList.remove(colors[indexColor]);
+  }
+}
+
 createPixelBoard();
 
 // Estrutura
@@ -68,7 +74,17 @@ colorPalette.addEventListener('click', function (event) {
 pixelBoard.addEventListener('click', function (event) {
   const classes = event.target;
 
+  removeColorClasses(classes);
+
   if (classes.className !== 'line-pixel') {
     classes.classList.add(colorSelected);
+  }
+});
+
+const clearButton = document.querySelector('#clear-board');
+const pixelDiv = document.querySelectorAll('#pixel-board .pixel');
+clearButton.addEventListener('click', function () {
+  for (let index = 0; index < pixelDiv.length; index += 1) {
+    removeColorClasses(pixelDiv[index]);
   }
 });
