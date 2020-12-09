c992fadacaf2a1fade04d9b118c20379f98f7c77
fernandacajueiro <fernandacajueiromachado@gmail.com>
2020-12-09 17:31:20 -0300

    Adiciona novo requisito

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 window.onload = function() {
-  
+  let selectColor1 = document.getElementsByClassName('item1');
+  let selectColor2 = document.getElementsByClassName('item2');
+  let selectColor3 = document.getElementsByClassName('item3');
+  let selectColor4 = document.getElementsByClassName('item4');
+
+  selectColor1[0].className += ' selected';
 }
+
+console.log(function);
