c7cf323e3d61ebecab8b729ba7297c578c39954e
PeryclesF <perycles_reis@hotmail.com>
2020-12-09 19:03:31 -0300

    quarto requisitos do projeto erro v05

--- script.js
+++ script.js
@@ -29,17 +29,15 @@
   // criando uma caixa para linha
   function createBoxLine(pixelBoard) {
     let line = document.createElement('div');
-    line.className = 'pixel'
+    
     searchPixel.appendChild(line);
-
+    
     for (let index = 0; index < 5; index++) {
       let box = document.createElement('div');
-      box.className = 'line';
+      box.className = 'pixel';
       line.appendChild(box);
       box.addEventListener('click', paintPixel);
     }
-    return line;
-
   }
 
   // pinta as caixas da paleta
