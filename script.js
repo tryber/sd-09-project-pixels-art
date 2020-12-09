1b4158a893293e47cb9bacaf20e8374e212c51ad
ViniciusChagas <viniciushrcs@gmail.com>
2020-12-08 22:11:20 -0300

    variadas mudanças

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-window.onload = teste (5, 5)
+window.onload = matrix (5, 5), paintPallete(), firstColor ()
 
-    function teste (width, height){
+function matrix (width, height){
+    
     let numberOfLines = width;
     let numberOfColumns = height;
     let getPixelBoard = document.getElementById('pixel-board');
@@ -16,58 +17,77 @@ window.onload = teste (5, 5)
         pixel.style.background = 'white';
         return pixel
     }
-
     // acrescentar o número de linhas no pixelboard
     function fillPixelBoard(){
+
         for (let index = 0; index < numberOfLines; index += 1){
             let lines = document.createElement('div');
             lines.className = 'lines';
-            //acrescentar o número de colunas nas linhas 
-            for(let index = 0; index < numberOfColumns; index += 1){
-                let pixel = createPixel();
-                lines.appendChild(pixel);
-            };
             getPixelBoard.appendChild(lines);
-        }
-    }
 
+
+            for(let index = 0; index < numberOfColumns; index += 1){
+            let pixel = createPixel()
+            let columns = document.createElement('div');
+            columns.className = 'columns';
+            columns.appendChild(pixel)
+            lines.appendChild(columns);
+            };
+        }
+
+    }
+}
+
+function paintPallete(){
+    let getBlack = document.querySelector('.color.black')
+    let getRed = document.querySelector('.color.red')
+    let getGreen = document.querySelector('.color.green')
+    let getBlue = document.querySelector('.color.blue')
+
+    getBlack.style.backgroundColor = 'black'
+    getRed.style.backgroundColor = 'red'
+    getGreen.style.backgroundColor = 'green'
+    getBlue.style.backgroundColor = 'blue'
 
 }
 
-addEventListener(window.onload, selectedColor('black')) ;
-
-function selectedColor(colorName){
-    let getColorsPalette = document.querySelectorAll('.color');
-    for (let index in getColorsPalette){
-        if (String(getColorsPalette[index].className).includes(colorName)){
-            getColorsPalette[index].className += 'selected'
+function firstColor(){
+    let getColorsPaletteElements = document.querySelectorAll('.color');
+    for (let index = 0; index < getColorsPaletteElements.length; index += 1){
+         let palleteColor = getColorsPaletteElements[index].style.backgroundColor;
+            if(palleteColor=== 'black'){
+                getColorsPaletteElements[index].className += 'selected'
         }
     }
+}
+    
 
-}    
+let getColorsPaletteDiv = document.querySelector('#color-palette');
 
-let getColorsPalette = document.getElementById('color-palette');
-
-getColorsPalette.addEventListener('click', recebeclick)
+getColorsPaletteDiv.addEventListener('click', recebeclick)
 
 function recebeclick(eventOrigin){
 
     if (eventOrigin.target.id !== 'color-palette'){
-    let clickedColorClassNameString = String(eventOrigin.target.className);
-    let clickedColorClassName = eventOrigin.target.className;
-    let getColorsPaletteElements = document.querySelectorAll('.color');
 
-    if (clickedColorClassNameString.includes('selected')){
-        let newClass = String(clickedColorClassName.replace('selected', ''));
-        eventOrigin.target.className = newClass
-    } else {        
-        for (let index in getColorsPaletteElements){
-            if (String(getColorsPaletteElements[index].className).includes('selected')){
-            let newClass = String(getColorsPaletteElements[index].className.replace('selected', ''));
-            getColorsPaletteElements[index].className = newClass 
+        let clickedColorClassNameString = String(eventOrigin.target.className);
+        let clickedColorClassName = eventOrigin.target.className;
+        let clickedColor = eventOrigin.target.style.backgroundColor
+        let getColorsPaletteElements = document.querySelectorAll('.color');
+
+        if (clickedColorClassNameString.includes('selected')){
+            let newClass = String(clickedColorClassName.replace('selected', ''));
+            eventOrigin.target.className = newClass
+        } else {        
+            for (let index = 0; index < getColorsPaletteElements.length; index += 1){
+                if (String(getColorsPaletteElements[index].className).includes('selected')){
+                    let newClass = String(getColorsPaletteElements[index].className.replace('selected', ''));
+                    getColorsPaletteElements[index].className = newClass 
+                    }
             }
-        }
-        eventOrigin.target.className += 'selected'
-        }
+            eventOrigin.target.className += 'selected'
+            }
+        
     } 
+    return eventOrigin.target.style.backgroundColor
 }
