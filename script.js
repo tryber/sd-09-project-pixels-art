f61d10d4338cde63e8b356afdeba276a0bad64eb
RodrigoNeoEx <draja.acc@gmail.com>
2020-12-09 12:38:30 -0300

    working on color buttom

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 window.onload = function() {
-  let squareBase = 5;
-  let numberOfLines = squareBase
-  let squaresLeft = numberOfLines;
-  let squaresRight = numberOfLines;
+  let squareBase = 4;  
+
   let pixels = document.querySelectorAll(".pixels");
+
+    function selecionaCor (eventClick) {  
+      console.log(eventClick.target);    
+    }
+      let clickBlack = document.getElementById("black");
+      let clickRed = document.getElementById("red");
+      let clickBlue = document.getElementById("blue");
+      let clickPink = document.getElementById("pink");
+      clickBlack.addEventListener("click", selecionaCor);
+      clickRed.addEventListener('click', selecionaCor);
+      clickBlue.addEventListener('click', selecionaCor);
+      clickPink.addEventListener('click', selecionaCor);
   
   fillSquare(pixels);
   function fillSquare(pixels) {
     for(let index = 0; index < pixels.length; index += 1) {
-      fillLine(pixels[index]);
-      squaresRight += 1;
-      squaresLeft += 1;
+      fillLine(pixels[index]);    
     }
   }
 
@@ -21,10 +29,15 @@ window.onload = function() {
   }
 
   function fillLine(divPixels) {
-    for (let lineColumn = 1; lineColumn <= squareBase; lineColumn += 1) {
+    for (let lineColumn = 0; lineColumn <= squareBase; lineColumn += 1) {
       let box = createBox("box");
       divPixels.appendChild(box);
     }
   }
 }
 
+
+
+
+
+  
