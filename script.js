a636e1329987a70da0c0f5fa09817692c091c298
Daniela Muller <danielamuller20@gmail.com>
2020-12-09 08:25:40 -0300

    Construção da matriz de pizals

--- script.js
+++ script.js
@@ -2,18 +2,18 @@ const reedColors = document.querySelectorAll('#color-palette>div');
 const boardOfPixel = document.querySelector('#pixel-board');
 
 function coloPalette() {
-    reedColors[0].style.backgroundColor = 'black';
-    reedColors[1].style.backgroundColor = 'red';
-    reedColors[2].style.backgroundColor = 'green';
-    reedColors[3].style.backgroundColor = 'blue';
+  reedColors[0].style.backgroundColor = 'black';
+  reedColors[1].style.backgroundColor = 'red';
+  reedColors[2].style.backgroundColor = 'green';
+  reedColors[3].style.backgroundColor = 'blue';
 }
 coloPalette();
 
 function GeneratePixelBoard() {
   for (let index = 0; index < 25; index += 1) {
-  const divPixels = document.createElement('div');
-  divPixels.className = 'pixel';
-  boardOfPixel.appendChild(divPixels)
+    const divPixels = document.createElement('div');
+    divPixels.className = 'pixel';
+    boardOfPixel.appendChild(divPixels);
   }
 }
-GeneratePixelBoard()
+GeneratePixelBoard();
