18f97a7875275ebc534f693a9967f50b47f9f0ba
Vinicius Palenske <viniciuspalenske@gmail.com>
2020-12-09 04:56:11 -0300

    correção de identação/ funções chamadas antes de ser declarada

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-paletteGenerator();
-setColors();
-paintingFrame();
-
 function paletteGenerator() {
     for (let index = 0; index < 4; index += 1) {
-        let makeDiv = document.createElement('div');
+        const makeDiv = document.createElement('div');
         makeDiv.className = 'color';
         document.body.querySelector('#color-palette').appendChild(makeDiv);
     }
@@ -16,31 +12,37 @@ function putColor() {
     colorsOfPalette.push('rgb(0 , 0 , 0)');
     for (let index = 1; index < numberOfColors; index += 1) {
         colorsOfPalette[index] = rgbGenerator();
-        if(index == (numberOfColors-1)) {
+        if (index === (numberOfColors - 1)) {
             if (repeatTest(colorsOfPalette) === false) {
-                return colorsOfPalette;
+                index += 1;
+            } else {
+                index = 0;
             }
         }
     }
+    return colorsOfPalette;
+
     function rgbGenerator() {
-        let rgb = [0,0,0];
+        let rgb = [0, 0, 0];
         for (let index = 0; index < 3; index += 1) {
             rgb[index] = (Math.floor(Math.random() * 255 + 1));
             if (index === 2) {
                 let colorValue = rgb.reduce((total, currentElement) => total + currentElement);
                 if (colorValue > 150 && colorValue < 700) { // Verifica se a cor é é muito clara ou muito escura
-                    return `rgb(${rgb[2]} , ${rgb[1]} , ${rgb[0]})`;
+                    index += 1;
                 } else {
                     index = 0;
                 }
             }
         }
+        return `rgb(${rgb[2]} , ${rgb[1]} , ${rgb[0]})`;
     }
+
     function repeatTest(cores) {
         cores.sort();
         let repeat = false;
         for (let i = 0; i < cores.length; i += 1) {
-            if (cores[i] === cores[i+1]) {
+            if (cores[i] === cores[i + 1]) {
                 repeat = true;
             }
         }
@@ -56,10 +58,13 @@ function setColors() {
 }
 
 function paintingFrame() {
-    for (let index = 0; index < 25; index +=1) {
+    for (let index = 0; index < 25; index += 1) {
         let emptyBox = document.createElement('div');
         emptyBox.className = 'pixel';
         document.body.querySelector('#pixel-board').appendChild(emptyBox);
     }
-    
 }
+
+paletteGenerator();
+setColors();
+paintingFrame();
