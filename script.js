4cec4e378f5e65a28bf4c353d3e32b7182abe7a4
Ana Luiza Machado Salgado <analuizamsalgado@gmail.com>
2020-12-09 01:50:54 -0300

    Adciona desafio 10

--- script.js
+++ script.js
@@ -3,13 +3,12 @@ function selectColor() {
   const colors = document.getElementsByClassName('color');
 
   for (let index = 0; index < colors.length; index += 1) {
-    colors[index].addEventListener('click', function(event) {
+    colors[index].addEventListener ('click', function(event) {
       for (let index2 = 0; index2 < selectedColors.length; index2 += 1) {
         selectedColors[index2].classList.remove('selected');
       }
 
-      event.target.classList.add('selected')
-      console.log(event.target.id);
+      event.target.classList.add('selected');
     });
   }
 }
@@ -18,7 +17,7 @@ function colorPixel() {
   const pixels = document.getElementsByClassName('pixel');
 
   for (let index = 0; index < pixels.length; index += 1) {
-    pixels[index].addEventListener('click', function(event) {
+    pixels[index].addEventListener('click', function (event) {
       const selectedColor = window.getComputedStyle(document.querySelector('.color.selected'));
       const bgColor = selectedColor.backgroundColor;
       event.target.style.backgroundColor = bgColor;
@@ -41,6 +40,59 @@ function clearBoard() {
     for (let index = 0; index < pixels.length; index += 1) {
       pixels[index].style.backgroundColor = 'white';
     }
+  });
+}
+
+function createInput() {
+  const btnSection = document.querySelector('#button-section');
+  const input = document.createElement('input');
+  input.min = '1';
+  input.max = '50';
+  input.id = 'board-size';
+  input.type = 'number';
+  btnSection.appendChild(input);
+}
+
+function createGenBoardBtn() {
+  const btnSection = document.querySelector('#button-section');
+  const genBoardBtn = document.createElement('button');
+  genBoardBtn.id = 'generate-board';
+  genBoardBtn.innerText = 'VQV';
+  btnSection.appendChild(genBoardBtn);
+}
+
+function generateBoard() {
+  const inputValue = document.querySelector('#board-size').value;
+  const pixelBoardDiv = document.querySelector('#pixel-board');
+  for (let index = 0; index < inputValue; index +=1) {
+    const boardLine = document.createElement('div');
+    boardLine.className = 'pixel-line';
+    boardLine.style.display = 'table-row';
+    pixelBoardDiv.appendChild(boardLine);
+  }
+  for (let index = 0; index < inputValue; index +=1) {
+    const pixelLine = document.querySelectorAll('.pixel-line');
+    for (let index = 0; index < pixelLine.length; index += 1) {
+      const boardColumn = document.createElement('div');
+      boardColumn.className = 'pixel';
+      boardColumn.style.display = 'table-cell';
+      pixelLine[index].appendChild(boardColumn);
+    }
+  }
+}
+
+function genBoardBtn() {
+  const button = document.getElementById('generate-board');
+  button.addEventListener('click', function() {
+    const inputValue = document.querySelector('#board-size').value;
+    if (!inputValue) {
+      alert('Board inválido!')
+    } else {
+      const pixelBoardDiv = document.querySelector('#pixel-board');
+      pixelBoardDiv.querySelectorAll('*').forEach(n => n.remove());
+      generateBoard();
+      colorPixel();
+    }
   })
 }
 
@@ -49,4 +101,8 @@ window.onload = function () {
   colorPixel();
   createBtn();
   clearBoard();
-}
+  createInput();
+  createGenBoardBtn();
+  generateBoard();
+  genBoardBtn();
+};
