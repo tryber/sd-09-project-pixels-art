6a1396d3961e6dcfb5523f9e457d18dfd2155a81
Nathália <nathaliaveneziano@icloud.com>
2020-12-09 17:31:49 -0300

    Requisitos 10

--- script.js
+++ script.js
@@ -3,60 +3,67 @@ const colorPalette = document.querySelector('#color-palette');
 const pixelBoard = document.querySelector('#pixel-board');
 const colors = ['black', 'blue', 'pink', 'purple'];
 let colorSelected = '';
+let size = 5;
 
-// Funções
+// Criando Elementos via JavaScript
+function addElementHTML(element, classes, parentID) {
+  const elementHTML = document.createElement(element);
+
+  for (let index = 0; index < classes.length; index += 1) {
+    elementHTML.classList.add(classes[index]);
+  }
+  
+  parentID.appendChild(elementHTML);
+  return elementHTML;
+}
+
+// Criando Paleta de Cores
 function createColorPalette(blocks) {
   colorSelected = '';
 
   for (let index = 0; index < colors.length; index += 1) {
-    const colorDiv = document.createElement('div');
-    colorDiv.classList.add('block', 'color', colors[index]);
+    const colorDiv = addElementHTML('div', ['block', 'color', colors[index]], blocks);
 
     if (index === 0) {
       colorSelected = colors[index];
       colorDiv.classList.add('selected');
     }
-
-    blocks.appendChild(colorDiv);
   }
 
   return colorSelected;
 }
 
-function createColumnPixel(repeatColumn, insertPixel) {
-  for (let index = 0; index < repeatColumn; index += 1) {
-    const columnPixel = document.createElement('div');
-    columnPixel.className = 'pixel block';
-    insertPixel.appendChild(columnPixel);
-  }
-}
-
-function createLinePixel(repeatLine, repeatColumn) {
-  for (let index = 0; index < repeatLine; index += 1) {
-    const linePixel = document.createElement('div');
-    linePixel.className = 'line-pixel';
-    pixelBoard.appendChild(linePixel);
-    createColumnPixel(repeatColumn, linePixel);
-  }
-}
-
-function createPixelBoard() {
-  for (let index = 0; index < 5; index += 5) {
-    createLinePixel(5, 5);
-  }
-}
-
+// Removendo Classe de Cores
 function removeColorClasses(pixelElement) {
   for (let indexColor = 0; indexColor < colors.length; indexColor += 1) {
     pixelElement.classList.remove(colors[indexColor]);
   }
 }
 
-createPixelBoard();
+// Criando Tabela de Pixel
+function createPixelBoard(sizeGrid) {
+  for (let index = 0; index < sizeGrid; index += 1) {
+    const linePixel = addElementHTML('div', ['line-pixel'], pixelBoard);
+
+    for (let index = 0; index < sizeGrid; index += 1) {
+      addElementHTML('div', ['pixel', 'block'], linePixel);
+    }
+  }
+}
+
+// Removendo Tabela
+function removePixelBoard() {
+  const qtdChildNodes = document.querySelectorAll('.line-pixel');
+  for (let indexChild = 0; indexChild < qtdChildNodes.length; indexChild += 1) {
+    pixelBoard.removeChild(qtdChildNodes[indexChild]);
+  }
+}
 
 // Estrutura
+createPixelBoard(size);
 colorSelected = createColorPalette(colorPalette);
 
+// AddEventListeners
 colorPalette.addEventListener('click', function (event) {
   const divSelect = event.target;
   const colorDiv = divSelect.classList[2];
@@ -86,5 +93,24 @@ const pixelDiv = document.querySelectorAll('#pixel-board .pixel');
 clearButton.addEventListener('click', function () {
   for (let index = 0; index < pixelDiv.length; index += 1) {
     removeColorClasses(pixelDiv[index]);
+  }
+});
+
+const inputBoardSize = document.querySelector('#board-size');
+let valueInput = 0;
+inputBoardSize.addEventListener('input', function (event) {
+  valueInput = event.target.value;
+});
+
+const buttonGenerateBoard = document.querySelector('#generate-board');
+buttonGenerateBoard.addEventListener('click', function () {
+  console.log(valueInput);
+  if (valueInput < 0) {
+    alert('Digite valores acima de 0.');
+  } else if (valueInput === '') {
+    alert('Board inválido!');
+  } else {
+    removePixelBoard();
+    createPixelBoard(valueInput);
   }
 });
