85ce239cfa27338845b2d23f917732bc1b425af9
Poeta <pedrotrybe@gmail.com>
2020-12-09 17:25:52 -0300

    Iniciando JS

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 window.onload = function() {
-    let backgoundPixelColor = "white"
+  let selectColor1 = document.getElementsByClassName('.background-item1');
+  let selectColor2 = document.getElementsByClassName('.background-item2');
+  let selectColor3 = document.getElementsByClassName('.background-item3');
+  let selectColor4 = document.getElementsByClassName('.background-item4');
+
+selectColor1[0].className += ' selected';
 }
