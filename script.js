5d0feefaf066b4ce22294ba0bde8e9dc871bff45
Luiz Henrique <lhfmworker@gmail.com>
2020-12-09 11:45:16 -0300

    cor preta inicial e classe selected incorporada

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let board = document.querySelector('#pixel-board');
 let colorsTable = document.querySelector('#color-palette');
 let pixels = document.querySelector('#pixel-board');
-var color = '';
+var selected = document.querySelector('.selected');
+var color = selected.style.backgroundColor;
+window.onload = console.log(color);
+//selected.classList = 'color selected';
 
+//Codigo abaixo gera o quadro de pixels
 function rowGenerator() {
     for (let i=0; i<5; i+=1){
         let pxl = document.createElement('div');
@@ -24,14 +28,24 @@ function pxlGenerator() {
 }
 pxlGenerator();
 
+//Codigo a seguir executa o evento do click para escolher a cor e colorir
 let colorSaved = colorsTable.addEventListener('click', catchColor);
 function catchColor (){
     console.log(event.srcElement.id);
+    console.log(color);
     color = event.srcElement.id;
+    event.srcElement.className.pull = 'color selected';
+    colorsTable.nextElementSibling.nameClass = 'color'
+
 }
+var color = 'black';
 pixels.addEventListener('click', function (event){
-    console.log(event);
-    let paintPxl = event.target;
-    paintPxl.style.backgroundColor = colorSaved;
-    event.target.style.backgroundColor = color;
+    console.log(color);
+    //let paintPxl = event.target;
+    //paintPxl.style.backgroundColor = colorSaved;
+    if (color !== 'black'){
+        event.target.style.backgroundColor = color;
+    } else {
+        event.target.style.backgroundColor = 'black';
+    }
 })
