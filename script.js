186f60a276a75ed9b8049a2f2f80d20e7f051871
MZTBTG <mztbtg@hotmail.com>
2020-12-09 00:55:33 -0300

    Meio perdido

--- script.js
+++ script.js
@@ -12,5 +12,15 @@ window.onload = function start() {
     }
   }
 
+  let color = 'rgb(0 , 0 , 0)';
+  let color1 = document.getElementsByClassName('color')[0];
+  let color2 = document.getElementsByClassName('color')[1];
+  let color3 = document.getElementsByClassName('color')[2];
+  let color4 = document.getElementsByClassName('color')[3];
+  color1.addEventListener('click', setColor);
+  color2.addEventListener('click', setColor);
+  color3.addEventListener('click', setColor);
+  color4.addEventListener('click', setColor);
+
   pixelBoard(5);
 };
