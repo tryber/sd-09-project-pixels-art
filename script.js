09c80c6b43889cd5f7942fd5ddc44c38dc7f3cf4
fernandacajueiro <fernandacajueiromachado@gmail.com>
2020-12-10 18:44:02 -0300

    Adiciona todas as alterações dos arquivos index, style e script

--- script.js
+++ script.js
@@ -1,8 +1,85 @@
-window.onload = function() {
-  let selectColor1 = document.getElementsByClassName('item1');
-  let selectColor2 = document.getElementsByClassName('item2');
-  let selectColor3 = document.getElementsByClassName('item3');
-  let selectColor4 = document.getElementsByClassName('item4');
+window.onload = function () {
+  createPalette ();
+  colorizePalette ();
+  createPixelBoard ();
+  selectedElement ();
+  colorPixel ();
+  clearBoard ();
+  paletteRandomizer ();
+}
 
-  selectColor1[0].className += ' selected';
+function createPalette () {
+  const colorPalette = document.getElementById('color-palette');
+  const numOfColors = 4;
+  for (let index = 0; index < numOfColors; index += 1) {
+    const color = document.createElement('div');
+    color.classList.add('color');
+    colorPalette.appendChild(color);
+  }
+  document.querySelector('.color').classList.add('selected');
+}
+
+function colorizePalette () {
+  const paletteColor = document.querySelectorAll('.color');
+  paletteColor[0].style.backgroundColor = 'black';
+  paletteColor[1].style.backgroundColor = `rgb(${randomNumbers()}, ${randomNumbers()}, ${randomNumbers()})`;
+  paletteColor[2].style.backgroundColor = `rgb(${randomNumbers()}, ${randomNumbers()}, ${randomNumbers()})`;
+  paletteColor[3].style.backgroundColor = `rgb(${randomNumbers()}, ${randomNumbers()}, ${randomNumbers()})`;
+}
+
+function createPixelBoard () {
+  const pixelBoard = document.getElementById('pixel-board');
+  const numOfColumns = 5;
+  for (let row = 0; row < numOfColumns; row += 1) {
+    const pixelRow = document.createElement('div');
+    pixelRow.classList.add('pixel-row');
+    for (let column = 0; column < numOfColumns; column += 1) {
+      const pixel = document.createElement('div');
+      pixel.classList.add('pixel');
+      pixelRow.appendChild(pixel);
+    }
+    pixelBoard.appendChild(pixelRow);
+  }
+}
+
+function selectedElement () {
+  const colors = document.getElementsByClassName('color');
+  for (index = 0; index < colors.length; index += 1) {
+    colors[index].addEventListener('click', function () {
+      for (let index2 = 0; index2 < colors.length; index2 += 1) {
+        colors[index2].classList.remove('selected');
+      }
+      this.classList.add('selected');
+    })
+  }
+}
+
+function colorPixel () {
+  const pixels = document.getElementsByClassName('pixel');
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].addEventListener('click', function () {
+      const elementSelected = document.querySelector('.color.selected');
+      const elementSelectedColor = window.getComputedStyle(elementSelected).getPropertyValue('background-color');
+      this.style.backgroundColor = elementSelectedColor;
+    })
+  }
+}
+
+function clearBoard () {
+  const pixels = document.getElementsByClassName('pixel');
+  const button = document.getElementById('clear-board');
+  button.addEventListener('click', function () {
+    for (let index = 0; index < pixels.length; index +=1) {
+      pixels[index].style.backgroundColor = 'rgb(255, 255, 255)';
+    }
+  })
+}
+
+function paletteRandomizer () {
+  const button = document.querySelector('#randomize-colors');
+  button.addEventListener('click', colorizePalette);
+}
+
+function randomNumbers () {
+  return Math.round(Math.random()*255)
 }
