8dc8824adea825ba9eb2ef3b7d1722bb72c0f067
Igor Marcos Riegel <igor.riegel@gmail.com>
2020-12-10 04:10:21 -0300

    Refatorado o painel de pixels de uma tabela de divs feitas a mao para uma tabela feita dinamicamente via javascrip.

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// Arquivo de JS do projeto pixels-art
+window.onload = function () {
+  const myPixelBoard = document.querySelector('#pixel-board');
+  let numberOfLinesAndColumns = 5
+  
+//inicio das funções de renderização do pixel-board  
+  insertLine(numberOfLinesAndColumns);
+  // Função inspirada na função de construção de DIV do prof Lucas(Gordinho) na pagina de algoritmo do triangulo.
+  function createDiv(nameOfTheClass) {
+    let myDiv = document.createElement('div');
+    myDiv.className = nameOfTheClass;
+    return myDiv;
+  }
+  function insertLine(lineOfTheTable) {
+    for (let line = 0; line < lineOfTheTable; line += 1) {
+      myPixelBoard.appendChild(createDiv('line'));
+      insertPixel(numberOfLinesAndColumns, line);
+    }
+  }
+  function insertPixel(columnOfTheTable, lineOfTheLoop) {
+    for (let index = 0; index < columnOfTheTable; index += 1) {
+      let lineOfPixelBoard = document.querySelectorAll('.line')[lineOfTheLoop];
+      lineOfPixelBoard.appendChild(createDiv('pixel'));
+    }
+  }
+//fim das funções de renderização do pixel-board
+}
