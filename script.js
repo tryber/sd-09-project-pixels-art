d0e5f0bec0078cd83bc398c17706bcdca4a22136
devfelipesantiago <felipesantiagolds@gmail.com>
2020-12-10 16:31:18 -0300

    Code Climate

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
 function createEventColorBox() {
-  let box = document.querySelector('#pixel-board');
+  const box = document.querySelector('#pixel-board');
 
   box.addEventListener('click', (event) => {
-    let selectedColor = document.querySelector('.selected');
+    const selectedColor = document.querySelector('.selected');
     let cor = window.getComputedStyle(selectedColor);
-    event.target.style.backgroundColor = cor.getPropertyValue(
-      'background-color'
-    );
+    event.target.style.backgroundColor = cor.getPropertyValue('background-color');
   });
 }
 
 function clear() {
-  let button = document.querySelector('#clear-board');
-  let pixel = document.querySelectorAll('.pixel');
-  console.log(pixel);
+  const button = document.querySelector('#clear-board');
+  const pixel = document.querySelectorAll('.pixel');
 
-  button.addEventListener('click', clearBox);
   function clearBox() {
     for (let index = 0; index < pixel.length; index += 1) {
       pixel[index].style.backgroundColor = 'white';
     }
   }
+  button.addEventListener('click', clearBox);
 }
 
 function classSelected() {
-  let color = document.querySelectorAll('.color');
-  let colorP = document.querySelector('#color-palette');
+  const color = document.querySelectorAll('.color');
+  const colorP = document.querySelector('#color-palette');
 
   colorP.addEventListener('click', (event) => {
-    for (let index = 0; index < color.length; index++) {
-      if (event.target.id != 'color-palette') {
+    for (let index = 0; index < color.length; index += 1) {
+      if (event.target.id !== 'color-palette') {
         color[index].classList.remove('selected');
         event.target.classList.add('selected');
       }
@@ -38,13 +35,13 @@ function classSelected() {
 }
 
 function pixelBox() {
-  let table = document.querySelector('#pixel-board');
+  const table = document.querySelector('#pixel-board');
 
   for (let index = 0; index < 5; index += 1) {
-    let row = document.createElement('div');
-    table.appendChild(row)
+    const row = document.createElement('div');
+    table.appendChild(row);
     for (let j = 0; j < 5; j += 1) {
-      let cell = document.createElement('div');
+      const cell = document.createElement('div');
       table.appendChild(cell).classList = 'pixel td';
     }
   }
