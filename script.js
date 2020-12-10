d53d9c7e41b10e2ac69af50968ce3334987250dd
Vinicius Palenske <viniciuspalenske@gmail.com>
2020-12-09 22:36:59 -0300

    removendo todas as #id do style.css / atributos declarados via JS

--- script.js
+++ script.js
@@ -1,63 +1,67 @@
-function paletteGenerator(numOfPale) {
-  const paletteColors = document.body.querySelector('#color-palette');
-  paletteColors.style.width = `${numOfPale * 43}px`;
-  for (let index = 0; index < numOfPale; index += 1) {
+function paletteGenerator(numColors) {
+  const paletteColors = document.getElementById('color-palette');
+  paletteColors.style.maxWidth = '258px';
+  paletteColors.style.width = `${numColors * 43}px`;
+  for (let index = 0; index < numColors; index += 1) {
     const makeDiv = document.createElement('div');
     makeDiv.className = 'color';
     paletteColors.appendChild(makeDiv);
   }
-}
-
-function putColor(numColors) {
-  const colorsPalette = ['rgb(0 , 0 , 0)'];
-  function rgbGenerator() {
-    const rgb = [0, 0, 0];
-    for (let index = 0; index < 3; index += 1) {
-      rgb[index] = (Math.random() * 255 + 1);
-      if (index === 2) {
-        const colorValue = rgb.reduce((total, currentElement) => total + currentElement);
-        if (colorValue > 150 && colorValue < 700) {
+  function setColor() {
+    const colorsPalette = ['rgb(0 , 0 , 0)'];
+    function rgbGenerator() {
+      const rgb = [];
+      for (let index = 0; index < 3; index += 1) {
+        rgb[index] = (Math.random() * 255 + 1);
+        if (index === 2) {
+          const colorValue = rgb.reduce((total, currentElement) => total + currentElement);
+          if (colorValue > 150 && colorValue < 700) {
+            index += 1;
+          } else {
+            index = 0;
+          }
+        }
+      }
+      return `rgb(${rgb[0]} , ${rgb[1]} , ${rgb[2]})`;
+    }
+    function repeatTest(colored) {
+      let repeat = false;
+      for (let i = 0; i < colored.length; i += 1) {
+        if (colored[i] === colored[i + 1]) {
+          repeat = true;
+        }
+      }
+      return repeat;
+    }
+    for (let index = 1; index < numColors; index += 1) {
+      colorsPalette[index] = rgbGenerator();
+      if (index === (numColors - 1)) {
+        if (repeatTest(colorsPalette) === false) {
           index += 1;
         } else {
           index = 0;
         }
       }
     }
-    return `rgb(${rgb[0]} , ${rgb[1]} , ${rgb[2]})`;
+    return colorsPalette;
   }
-  function repeatTest(cores) {
-    let repeat = false;
-    for (let i = 0; i < cores.length; i += 1) {
-      if (cores[i] === cores[i + 1]) {
-        repeat = true;
-      }
-    }
-    return repeat;
-  }
-  for (let index = 1; index < numColors; index += 1) {
-    colorsPalette[index] = rgbGenerator();
-    if (index === (numColors - 1)) {
-      if (repeatTest(colorsPalette) === false) {
-        index += 1;
-      } else {
-        index = 0;
-      }
+  function defineColors() {
+    const boxNoColor = document.querySelectorAll('div .color');
+    for (let index = 0; index < boxNoColor.length; index += 1) {
+      document.querySelectorAll('div .color')[index].style.backgroundColor = setColor(numColors)[index];
     }
   }
-  return colorsPalette;
-}
-
-function setColors(numColors) {
-  const boxNoColor = document.querySelectorAll('div .color');
-  for (let index = 0; index < boxNoColor.length; index += 1) {
-    document.querySelectorAll('div .color')[index].style.backgroundColor = putColor(numColors)[index];
-  }
+  defineColors();
 }
 
 function whiteFrames(line, column) {
   const panel = document.body.querySelector('#pixel-board');
   panel.style.width = `${column * 40}px`;
   panel.style.height = `${column * 40}px`;
+  panel.style.display = 'inline-block';
+  panel.style.marginTop = '10px';
+  panel.style.maxHeight = '630px';
+  panel.style.maxWidth = '630px';
   for (let lIndex = 0; lIndex < line; lIndex += 1) {
     for (let cIndex = 0; cIndex < column; cIndex += 1) {
       const emptyBox = document.createElement('div');
@@ -85,13 +89,9 @@ function coloring() {
   emptyFrame.addEventListener('click', coloringFrame);
 }
 
-// function clearFrames() {}
-
 window.onload = function () {
-  sessionStorage.setItem('thisColor', 'rgb(0 , 0 , 0)');
   const maxColors = 4;
   paletteGenerator(maxColors),
-  setColors(maxColors),
   whiteFrames(5, 5),
   selectColor(),
   coloring();
