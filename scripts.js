414d3ac28ddf8f6432c322342acb1d5b9a8180e1
Ederson S <edersonrodrigo31@gmail.com>
2020-12-10 14:13:44 -0300

    decimo-segundo requisito

--- scripts.js
+++ scripts.js
@@ -1,28 +1,42 @@
-window.onload = function (){
-  addEventListener('click', changeSelectedColor)
+window.onload = function () {
+  let button = document.querySelector('#clear-board')
+  let colors = document.querySelectorAll('.color')
 
-  function changeSelectedColor(event){
+
+  function randomnumber(min, max){
+  min = Math.ceil(0);
+  max = Math.floor(255);
+  return Math.floor(Math.random() * (max - min + 1)) + min
+  }
+
+  for(let index = 0; index < colors.length; index += 1){
+    colors[index].style.backgroundColor = `rgb(${randomnumber(0, 255)}, ${randomnumber(0, 255)}, ${randomnumber(0, 255)})`
+  }
+
+  console.log(colors)
+
+  addEventListener('click', changeSelectedColor)
+  button.addEventListener('click', cleartable)
+
+  function changeSelectedColor(event) {
     let oldselectedColor = document.querySelector('.selected')
-    let paintColor = oldselectedColor.classList[1]
+    let paintColor = oldselectedColor.style.backgroundColor
+
     let newselectedColor = event.target
 
-    if(newselectedColor.classList.contains('color')){
-    oldselectedColor.classList.remove('selected')
-    newselectedColor.className += ' selected'
+    if (newselectedColor.classList.contains('color')) {
+      oldselectedColor.classList.remove('selected')
+      newselectedColor.className += ' selected'
     }
-    else if(newselectedColor.classList.contains('pixel')){
+    else if (newselectedColor.classList.contains('pixel')) {
       newselectedColor.style.backgroundColor = `${paintColor}`
     }
   }
 
-  let button = document.querySelector('#clear-board')
-
-  button.addEventListener('click', cleartable)
-
-  function cleartable(){
+  function cleartable() {
     let pixels = document.querySelectorAll('.pixel')
 
-    for(let index = 0; index < pixels.length; index++){
+    for (let index = 0; index < pixels.length; index++) {
       pixels[index].style.backgroundColor = "white";
     }
   }
