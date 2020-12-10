46d53e5eded146cbb9b3ea8f7d25cda0a9a98195
Felipe Rocha <felipe.sr27@gmail.com>
2020-12-10 11:53:15 -0300

    Correcao de problemas apontados pelo code climate.

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-//This function create new divs elements.
+// This function create new divs elements.
 function createDivs() {
   const div = document.createElement('div');
   return div;
 }
 
-//This function gerantes random rgb colors.
+// This function gerantes random rgb colors.
 function generateRandomColors() {
   const rgbNumberArray = [];
   let color = '';
@@ -18,7 +18,7 @@ function generateRandomColors() {
   return color;
 }
 
-//This function add background-color to squares(divs) in color-palette section.
+// This function add background-color to squares(divs) in color-palette section.
 function addColorSquare(palette) {
   for (let index = 0; index < palette.length; index += 1) {
     if (index === 0) {
@@ -30,7 +30,7 @@ function addColorSquare(palette) {
   }
 }
 
-//this function create the squares(divs) in 'color-palette' section.
+// This function create the squares(divs) in 'color-palette' section.
 function createColorsPalette(palette, amount) {
   const palleteArray = [];
   for (let index = 0; index < amount; index += 1) {
@@ -41,14 +41,14 @@ function createColorsPalette(palette, amount) {
   addColorSquare(palleteArray);
 }
 
-//this function create the pixel board.
+// This function create the pixel board.
 function createPixelBoard(board, amount) {
-  let column = Math.sqrt(amount);
-  let line = Math.sqrt(amount);
+  const column = Math.sqrt(amount);
+  const line = Math.sqrt(amount);
   for (let index = 0; index < column; index += 1) {
     const squarecolumn = board.appendChild(createDivs());
     squarecolumn.className = 'table-row';
-    for (let index = 0; index < line; index += 1) {
+    for (let index2 = 0; index2 < line; index2 += 1) {
       const squareLine = squarecolumn.appendChild(createDivs());
       squareLine.className = 'pixel';
       squareLine.style.backgroundColor = 'rgb(255, 255, 255)';
@@ -56,27 +56,27 @@ function createPixelBoard(board, amount) {
   }
 }
 
-//this function delete the pixel board.
+// This function delete the pixel board.
 function deletePixelBoard(board) {
   while (board.firstChild) {
     board.removeChild(board.lastChild);
   }
 }
 
-//this function remove class 'selected' from divs in 'color-palette' section.
+// This function remove class 'selected' from divs in 'color-palette' section.
 function deselectColor() {
   const currentColor = document.querySelector('.selected');
   currentColor.className = 'color';
 }
 
-//this function add class 'selected' to divs in 'color-palette' section.
+// This function add class 'selected' to divs in 'color-palette' section.
 function selectColor(event) {
   const selectedColor = event.target;
   deselectColor();
   selectedColor.className += ' selected';
 }
 
-//this function add the same background-color of palette with class selected in pixel board.
+// This function add the same background-color of palette with class selected in pixel board.
 function fillSquare(event) {
   const selectedSquare = event.target;
   if (selectedSquare !== document.querySelector('#pixel-board')) {
@@ -85,7 +85,7 @@ function fillSquare(event) {
   }
 }
 
-//This function create a new element button.
+// This function create a new element button.
 function createButton(section) {
   const newButton = document.createElement('button');
   newButton.type = 'button';
@@ -93,14 +93,14 @@ function createButton(section) {
   return newButton;
 }
 
-//This function create the clear button.
+// This function create the clear button.
 function createClearButton(section) {
   const clearButton = createButton(section);
   clearButton.innerText = 'Limpar';
   clearButton.id = 'clear-board';
 }
 
-//This function create the elements form and childs.
+// This function create the elements form and childs.
 function createForm(section) {
   const newForm = document.createElement('form');
   const newInput = document.createElement('input');
@@ -109,7 +109,7 @@ function createForm(section) {
   return newForm;
 }
 
-//This function add background-color white to pixel board.
+// This function add background-color white to pixel board.
 function clearBoard() {
   const squares = document.querySelectorAll('.pixel');
   for (let index = 0; index < squares.length; index += 1) {
@@ -117,7 +117,7 @@ function clearBoard() {
   }
 }
 
-//This function add the elements form/input/buttom where the user insert the size of pixel board.
+// This function add the elements form/input/buttom where the user insert the size of pixel board.
 function addSizeForm(section) {
   const form = createForm(section);
   const inputButton = createButton(form);
@@ -132,7 +132,7 @@ function addSizeForm(section) {
   inputButton.type = 'button';
 }
 
-//This function update pixel board size with a new size.
+// This function update pixel board size with a new size.
 function updateBoardSize() {
   const board = document.querySelector('#pixel-board');
   const inputBoardSize = document.querySelector('#board-size');
@@ -147,35 +147,24 @@ function updateBoardSize() {
   createPixelBoard(board, (inputBoardSize.value * inputBoardSize.value));
 }
 
-//This function prevent submit of a input in form.
+// This function prevent submit of a input in form.
 function preventSubmit(event) {
   event.preventDefault();
 }
 
-
-window.onload = function() {
-
+window.onload = function () {
   const paletteColor = document.querySelector('#color-palette');
   const pixelBoard = document.querySelector('#pixel-board');
   const amountColors = 4;
   const amountSquares = 25;
   const buttonSection = document.querySelector('#form-section');
-  
   createColorsPalette(paletteColor, amountColors);
-
   createPixelBoard(pixelBoard, amountSquares);
-
   createClearButton(buttonSection);
-
   addSizeForm(buttonSection);
-  
   paletteColor.addEventListener('click', selectColor);
-
   pixelBoard.addEventListener('click', fillSquare);
-
   document.querySelector('#generate-board').addEventListener('click', updateBoardSize);
-
   document.querySelector('#clear-board').addEventListener('click', clearBoard);
-
-  document.querySelector('#board-form').addEventListener('submit', preventSubmit)
+  document.querySelector('#board-form').addEventListener('submit', preventSubmit);
 };
