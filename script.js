f8d84a39b2c77e342f522c812600ee1645b359be
MZTBTG <mztbtg@hotmail.com>
2020-12-10 19:07:06 -0300

    Corrigindo erros de cognição

--- script.js
+++ script.js
@@ -1,77 +1,70 @@
-window.onload = function start() {
-  const color = 'rgb(0 , 0 , 0)';
+const color = 'rgb(0 , 0 , 0)';
 
-  function aleatoryColor() {
-    let newColor = 'rgb(';
-    newColor += `${Math.round(Math.random() * 255)} , `;
-    newColor += `${Math.round(Math.random() * 255)} , `;
-    newColor += `${Math.round(Math.random() * 255)})`;
-    return newColor;
-  }
+function aleatoryColor() {
+  let newColor = 'rgb(';
+  newColor += `${Math.round(Math.random() * 255)} , `;
+  newColor += `${Math.round(Math.random() * 255)} , `;
+  newColor += `${Math.round(Math.random() * 255)})`;
+  return newColor;
+}
 
-  function createPalette(numberOfColors) {
-    for (let index = 0; index < numberOfColors; index += 1) {
-      const paletteColor = document.createElement('div');
-      paletteColor.className = 'color';
-      document.getElementsByClassName('color-palette')[0].appendChild(paletteColor);
-      const allPaletColors = document.querySelectorAll('.color');
-      if (index === 0) {
-        allPaletColors[0].style.backgroundColor = color;
-        allPaletColors[0].className += ' selected';
-      } else {
-        allPaletColors[index].style.backgroundColor = aleatoryColor();
-      }
+function createPalette(numberOfColors) {
+  for (let index = 0; index < numberOfColors; index += 1) {
+    const paletteColor = document.createElement('div');
+    paletteColor.className = 'color';
+    document.getElementsByClassName('color-palette')[0].appendChild(paletteColor);
+    const allPaletColors = document.querySelectorAll('.color');
+    if (index === 0) {
+      allPaletColors[0].style.backgroundColor = color;
+      allPaletColors[0].className += ' selected';
+    } else {
+      allPaletColors[index].style.backgroundColor = aleatoryColor();
     }
   }
+}
 
-  createPalette(4);
+function createButton() {
+  const button = document.createElement('button');
+  button.innerText = 'Limpar';
+  button.className = 'clear';
+  button.id = 'clear-board';
+  document.querySelector('.button-place').appendChild(button);
+}
 
-  function createButton() {
-    const button = document.createElement('button');
-    button.innerText = 'Limpar';
-    button.className = 'clear';
-    button.id = 'clear-board';
-    document.querySelector('.button-place').appendChild(button);
-  }
-
-  createButton();
-
-  function pixelBoard(matrix) {
-    for (let index = 0; index < matrix; index += 1) {
-      const pixelsLine = document.createElement('div');
-      pixelsLine.className = 'pixel-line';
-      document.getElementsByClassName('pixel-board-inside')[0].appendChild(pixelsLine);
-      for (let index2 = 0; index2 < matrix; index2 += 1) {
-        const pixelsColumn = document.createElement('div');
-        pixelsColumn.className = 'pixel';
-        document.querySelectorAll('.pixel-line')[index].appendChild(pixelsColumn);
-      }
+function pixelBoard(matrix) {
+  for (let index = 0; index < matrix; index += 1) {
+    const pixelsLine = document.createElement('div');
+    pixelsLine.className = 'pixel-line';
+    document.getElementsByClassName('pixel-board-inside')[0].appendChild(pixelsLine);
+    for (let index2 = 0; index2 < matrix; index2 += 1) {
+      const pixelsColumn = document.createElement('div');
+      pixelsColumn.className = 'pixel';
+      document.querySelectorAll('.pixel-line')[index].appendChild(pixelsColumn);
     }
   }
+}
 
-  pixelBoard(5);
+function paint(coloring) {
+  const selectedColor = document.querySelector('.selected').style.backgroundColor;
+  coloring.target.style.backgroundColor = selectedColor;
+}
 
-  function paint(coloring) {
-    const selectedColor = document.querySelector('.selected').style.backgroundColor;
-    coloring.target.style.backgroundColor = selectedColor;
-  }
-
+function theColors() {
   const colors = document.querySelectorAll('.color');
-
   function changeColor(event) {
     for (let index = 0; index < colors.length; index += 1) {
       colors[index].className = 'color';
     }
     event.target.className = 'color selected';
   }
-
   document.querySelectorAll('.color').forEach((item) => {
     item.addEventListener('click', changeColor);
   });
+};
 
+function clearning() {
   const onclickPixelBoard = document.getElementsByClassName('pixel-board-inside')[0];
   onclickPixelBoard.addEventListener('click', paint);
-
   const clear = document.querySelectorAll('.pixel');
   function cleaner() {
     for (let index = 0; index < clear.length; index += 1) {
@@ -79,4 +72,12 @@ window.onload = function start() {
     }
   }
   document.getElementsByClassName('clear')[0].addEventListener('click', cleaner);
+};
+
+window.onload = function start() {
+  createPalette(4);
+  createButton();
+  pixelBoard(5);
+  theColors();
+  clearning();
 };
