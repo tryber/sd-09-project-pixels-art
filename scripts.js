899e58dba33ad6bebfcdf7b456f1169147bc4bab
Ederson S <edersonrodrigo31@gmail.com>
2020-12-10 15:13:19 -0300

    decimo e decimo-primeiro requisito

--- scripts.js
+++ scripts.js
@@ -1,20 +1,59 @@
 window.onload = function () {
   let button = document.querySelector('#clear-board')
   let colors = document.querySelectorAll('.color')
+  let buttonborder = document.querySelector('#generate-board')
+  let border = 9;
+
+  let inputborder = document.querySelector('#board-size')
 
 
-  function randomnumber(min, max){
-  min = Math.ceil(0);
-  max = Math.floor(255);
-  return Math.floor(Math.random() * (max - min + 1)) + min
+  function randomnumber(min, max) {
+    min = Math.ceil(0);
+    max = Math.floor(255);
+    return Math.floor(Math.random() * (max - min + 1)) + min
   }
 
-  for(let index = 1; index < colors.length; index += 1){
+  for (let index = 1; index < colors.length; index += 1) {
     colors[index].style.backgroundColor = `rgb(${randomnumber(0, 255)}, ${randomnumber(0, 255)}, ${randomnumber(0, 255)})`
   }
 
   colors[0].style.backgroundColor = 'rgb(0, 0, 0)'
-  console.log(colors)
+
+
+  buttonborder.addEventListener('click', changeBorder)
+
+  let table = document.querySelector('#tbody-board')
+
+  function changeBorder() {
+    if(inputborder.value < 5){
+      inputborder.value = 5
+    }
+    else if(inputborder.value > 50){
+      inputborder.value = 50
+    }
+    while (table.firstChild){
+      table.removeChild(table.firstChild)
+    }
+    for(let index = 0; index < inputborder.value; index += 1){
+      let newcollumn = createnewline()
+      for(let position = 0; position < inputborder.value; position += 1){
+        let newline = createnewcollumn('pixel')
+        newcollumn.appendChild(newline)
+      }
+      table.appendChild(newcollumn)
+    }
+  }
+
+  function createnewline() {
+    let collumn = document.createElement('tr')
+    return collumn
+  }
+
+  function createnewcollumn(className){
+    let line = document.createElement('th')
+    line.className = className
+    return line
+  }
 
   addEventListener('click', changeSelectedColor)
   button.addEventListener('click', cleartable)
