7d1b5add372d42f12b4490d431ac3a597bbe70c0
RodrigoNeoEx <draja.acc@gmail.com>
2020-12-10 16:39:23 -0300

    remake entire project

--- script.js
+++ script.js
@@ -1,43 +1,27 @@
-window.onload = function() {
-  let squareBase = 4;
-  let line = document.querySelectorAll(".line");
-  
+window.onload = function(){
 
-    function selecionaCor () { 
-      
-    }
-    let clickBlack = document.getElementById("black");
-    let clickRed = document.getElementById("red");
-    let clickBlue = document.getElementById("blue");
-    let clickPink = document.getElementById("pink");
-    clickBlack.addEventListener("click", selecionaCor);
-    clickRed.addEventListener('click', selecionaCor);
-    clickBlue.addEventListener('click', selecionaCor);
-    clickPink.addEventListener('click', selecionaCor);
+
+  createPalete()
+  createBoardLine()
+  createBoardColum ()
+  }
   
-  fillSquare(line);
-  function fillSquare(line) {
-    for(let index = 0; index < line.length; index += 1) {
-      fillLine(line[index]);
+  
+  function createPalete () {
+    let palete = [1,2,3,4];
+     for (let index of palete) {
+      let squareColor = document.createElement('div');
+      squareColor.className = 'color';
+      document.getElementById('color-palette').appendChild(squareColor)
+      let colorSelector = document.querySelectorAll(".color")[0].classList.add('color-selected');
     }
   }
-
-  function createBox(className) {
-    let pixel = document.createElement("div");
-    pixel.className = className;
-    return pixel;
-  }
-
-  function fillLine(divLine) {
-    for (let lineColumn = 0; lineColumn <= squareBase; lineColumn += 1) {
-      let pixel = createBox("pixel");
-      divLine.appendChild(pixel);
+  
+  function createBoardLine () {
+    let base = [1,2,3,4,5,1,2,3,4,5,1,2,3,4,5,1,,1,2,3,4,2,3,4,5]
+    for (let index of base) {
+      let pixel = document.createElement('div');
+      pixel.className = 'pixel';
+      document.getElementById('pixel-board').appendChild(pixel)
     }
   }
-}
-
-
-
-
-
-  
