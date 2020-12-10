948bffb357a5792b514d2082ff29776699bd4c50
fernandacajueiro <fernandacajueiromachado@gmail.com>
2020-12-10 18:49:31 -0300

    Adiciona resolução issues do Code Climate

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 window.onload = function () {
-  createPalette ();
-  colorizePalette ();
-  createPixelBoard ();
-  selectedElement ();
-  colorPixel ();
-  clearBoard ();
-  paletteRandomizer ();
-}
+  createPalette();
+  colorizePalette();
+  createPixelBoard();
+  selectedElement();
+  colorPixel();
+  clearBoard();
+  paletteRandomizer();
+};
 
-function createPalette () {
+function createPalette() {
   const colorPalette = document.getElementById('color-palette');
   const numOfColors = 4;
   for (let index = 0; index < numOfColors; index += 1) {
@@ -19,7 +19,11 @@ function createPalette () {
   document.querySelector('.color').classList.add('selected');
 }
 
-function colorizePalette () {
+function randomNumbers() {
+  return Math.round(Math.random() * 255);
+}
+
+function colorizePalette() {
   const paletteColor = document.querySelectorAll('.color');
   paletteColor[0].style.backgroundColor = 'black';
   paletteColor[1].style.backgroundColor = `rgb(${randomNumbers()}, ${randomNumbers()}, ${randomNumbers()})`;
@@ -27,7 +31,7 @@ function colorizePalette () {
   paletteColor[3].style.backgroundColor = `rgb(${randomNumbers()}, ${randomNumbers()}, ${randomNumbers()})`;
 }
 
-function createPixelBoard () {
+function createPixelBoard() {
   const pixelBoard = document.getElementById('pixel-board');
   const numOfColumns = 5;
   for (let row = 0; row < numOfColumns; row += 1) {
@@ -42,9 +46,9 @@ function createPixelBoard () {
   }
 }
 
-function selectedElement () {
+function selectedElement() {
   const colors = document.getElementsByClassName('color');
-  for (index = 0; index < colors.length; index += 1) {
+  for (let index = 0; index < colors.length; index += 1) {
     colors[index].addEventListener('click', function () {
       for (let index2 = 0; index2 < colors.length; index2 += 1) {
         colors[index2].classList.remove('selected');
@@ -54,32 +58,28 @@ function selectedElement () {
   }
 }
 
-function colorPixel () {
+function colorPixel() {
   const pixels = document.getElementsByClassName('pixel');
   for (let index = 0; index < pixels.length; index += 1) {
     pixels[index].addEventListener('click', function () {
       const elementSelected = document.querySelector('.color.selected');
       const elementSelectedColor = window.getComputedStyle(elementSelected).getPropertyValue('background-color');
       this.style.backgroundColor = elementSelectedColor;
-    })
+    });
   }
 }
 
-function clearBoard () {
+function clearBoard() {
   const pixels = document.getElementsByClassName('pixel');
   const button = document.getElementById('clear-board');
   button.addEventListener('click', function () {
-    for (let index = 0; index < pixels.length; index +=1) {
+    for (let index = 0; index < pixels.length; index += 1) {
       pixels[index].style.backgroundColor = 'rgb(255, 255, 255)';
     }
-  })
+  });
 }
 
-function paletteRandomizer () {
+function paletteRandomizer() {
   const button = document.querySelector('#randomize-colors');
   button.addEventListener('click', colorizePalette);
-}
-
-function randomNumbers () {
-  return Math.round(Math.random()*255)
 }
