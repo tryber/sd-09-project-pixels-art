d3b6045acec58a5aa9d8596222aac43a1179a3c1
Felipe Rocha <felipe.sr27@gmail.com>
2020-12-10 11:03:20 -0300

    Correcao da funcao que atualiza o novo tamanho do pixel board: Requisito 10.

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-const paletteColor = document.querySelector('#color-palette');
-const pixelBoard = document.querySelector('#pixel-board');
-const buttonSection = document.querySelector('#button-section');
+//This function create new divs elements.
 function createDivs() {
   const div = document.createElement('div');
   return div;
 }
 
+//This function gerantes random rgb colors.
 function generateRandomColors() {
   const rgbNumberArray = [];
   let color = '';
@@ -19,6 +18,7 @@ function generateRandomColors() {
   return color;
 }
 
+//This function add background-color to squares(divs) in color-palette section.
 function addColorSquare(palette) {
   for (let index = 0; index < palette.length; index += 1) {
     if (index === 0) {
@@ -30,6 +30,7 @@ function addColorSquare(palette) {
   }
 }
 
+//this function create the squares(divs) in 'color-palette' section.
 function createColorsPalette(palette, amount) {
   const palleteArray = [];
   for (let index = 0; index < amount; index += 1) {
@@ -40,15 +41,13 @@ function createColorsPalette(palette, amount) {
   addColorSquare(palleteArray);
 }
 
-const amountColors = 4;
-createColorsPalette(paletteColor, amountColors);
-
-function createSquares(board, amount) {
+//this function create the pixel board.
+function createPixelBoard(board, amount) {
   let column = Math.sqrt(amount);
   let line = Math.sqrt(amount);
   for (let index = 0; index < column; index += 1) {
     const squarecolumn = board.appendChild(createDivs());
-    squarecolumn.style.backgroundColor = 'rgb(255, 255, 255)';
+    squarecolumn.className = 'table-row';
     for (let index = 0; index < line; index += 1) {
       const squareLine = squarecolumn.appendChild(createDivs());
       squareLine.className = 'pixel';
@@ -57,38 +56,27 @@ function createSquares(board, amount) {
   }
 }
 
-function removeSquares(board) {
+//this function delete the pixel board.
+function deletePixelBoard(board) {
   while (board.firstChild) {
     board.removeChild(board.lastChild);
   }
 }
 
-function loadBoard(board, amount) {
-  if (sessionStorage.getItem('board-size') === null) {
-    createSquares(board, amount);
-  } else {
-    const newBoardSize = sessionStorage.getItem('board-size');
-    removeSquares(board);
-    createSquares(board, newBoardSize);
-  }
-}
-
-const amountSquares = 25;
-loadBoard(pixelBoard, amountSquares);
-
+//this function remove class 'selected' from divs in 'color-palette' section.
 function deselectColor() {
   const currentColor = document.querySelector('.selected');
   currentColor.className = 'color';
 }
 
+//this function add class 'selected' to divs in 'color-palette' section.
 function selectColor(event) {
   const selectedColor = event.target;
   deselectColor();
   selectedColor.className += ' selected';
 }
 
-paletteColor.addEventListener('click', selectColor);
-
+//this function add the same background-color of palette with class selected in pixel board.
 function fillSquare(event) {
   const selectedSquare = event.target;
   if (selectedSquare !== document.querySelector('#pixel-board')) {
@@ -97,8 +85,7 @@ function fillSquare(event) {
   }
 }
 
-pixelBoard.addEventListener('click', fillSquare);
-
+//This function create a new element button.
 function createButton(section) {
   const newButton = document.createElement('button');
   newButton.type = 'button';
@@ -106,24 +93,23 @@ function createButton(section) {
   return newButton;
 }
 
+//This function create the clear button.
 function createClearButton(section) {
   const clearButton = createButton(section);
   clearButton.innerText = 'Limpar';
   clearButton.id = 'clear-board';
 }
 
-createClearButton(buttonSection);
-
+//This function create the elements form and childs.
 function createForm(section) {
   const newForm = document.createElement('form');
-  const newLabel = document.createElement('label');
   const newInput = document.createElement('input');
   section.appendChild(newForm);
-  newForm.appendChild(newLabel);
   newForm.appendChild(newInput);
   return newForm;
 }
 
+//This function add background-color white to pixel board.
 function clearBoard() {
   const squares = document.querySelectorAll('.pixel');
   for (let index = 0; index < squares.length; index += 1) {
@@ -131,27 +117,24 @@ function clearBoard() {
   }
 }
 
-const buttonClearBoard = document.querySelector('#clear-board');
-buttonClearBoard.addEventListener('click', clearBoard);
-
-function createInputSizeBoard(section) {
-  const inputForm = createForm(section);
-  const inputButton = createButton(inputForm);
-  const formLabel = inputForm.firstChild;
-  const formInput = formLabel.nextSibling;
-  formLabel.for = 'board-size';
+//This function add the elements form/input/buttom where the user insert the size of pixel board.
+function addSizeForm(section) {
+  const form = createForm(section);
+  const inputButton = createButton(form);
+  const formInput = form.firstChild;
+  form.id = 'board-form'
   formInput.id = 'board-size';
   formInput.type = 'number';
   formInput.min = 1;
   formInput.max = 50;
   inputButton.innerText = 'VQV';
   inputButton.id = 'generate-board';
-  inputButton.type = 'input';
+  inputButton.type = 'button';
 }
 
-createInputSizeBoard(buttonSection);
-
-function storeBoardSize() {
+//This function update pixel board size with a new size.
+function updateBoardSize() {
+  const board = document.querySelector('#pixel-board');
   const inputBoardSize = document.querySelector('#board-size');
   if (inputBoardSize.value === '') {
     return alert('Board inválido!');
@@ -160,8 +143,39 @@ function storeBoardSize() {
   } else if (inputBoardSize.value > 50) {
     inputBoardSize.value = 50;
   }
-  sessionStorage.setItem('board-size', (inputBoardSize.value * inputBoardSize.value));
+  deletePixelBoard(board);
+  createPixelBoard(board, (inputBoardSize.value * inputBoardSize.value));
 }
 
-const buttonGenerateBoard = document.querySelector('#generate-board');
-buttonGenerateBoard.addEventListener('click', storeBoardSize);
+//This function prevent submit of a input in form.
+function preventSubmit(event) {
+  event.preventDefault();
+}
+
+
+window.onload = function() {
+
+  const paletteColor = document.querySelector('#color-palette');
+  const pixelBoard = document.querySelector('#pixel-board');
+  const amountColors = 4;
+  const amountSquares = 25;
+  const buttonSection = document.querySelector('#form-section');
+  
+  createColorsPalette(paletteColor, amountColors);
+
+  createPixelBoard(pixelBoard, amountSquares);
+
+  createClearButton(buttonSection);
+
+  addSizeForm(buttonSection);
+  
+  paletteColor.addEventListener('click', selectColor);
+
+  pixelBoard.addEventListener('click', fillSquare);
+
+  document.querySelector('#generate-board').addEventListener('click', updateBoardSize);
+
+  document.querySelector('#clear-board').addEventListener('click', clearBoard);
+
+  document.querySelector('#board-form').addEventListener('submit', preventSubmit)
+};
