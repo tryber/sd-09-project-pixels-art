cbf55ac0151f3f4f85bf4d86933cbf7f5dfc8a95
Poeta <pedrotrybe@gmail.com>
2020-12-10 18:06:07 -0300

    criando as funcoes

---
+++ rascunho.js
@@ -0,0 +1,48 @@
+// window.onload = function() {
+//   let paletteColors = document.getElementsByClassName('color')
+//   let selectColor1 = document.getElementById('background-item1');
+//   let selectColor2 = document.getElementById('background-item2');
+//   let selectColor3 = document.getElementById('background-item3');
+//   let selectColor4 = document.getElementById('background-item4');
+
+//   selectColor1[0].className += ' selected';
+
+//   for (let index = 0; index < paletteColors.lenght; index += 1) {
+//     click.addEventListener("click", removeClassSelected);
+//     click.addEventListener("click", addClassSelected);
+//   }
+
+//   function removeClassSelected() {
+//     document.getElementById('elemento').classList.remove('selected');
+//   }
+
+//   function addClassSelected () {
+//     document.getElementById('elemento').classList.add('selected');
+//   }
+
+//   function changeColor1() {
+//     selectColor1.style.backgroundColor = "rgb(0, 0, 0)";
+//   }
+
+//   function changeColor2() {
+//     selectColor2[0].style.backgroundColor = "rgb(89, 89, 89)";
+//   }
+
+//   function changeColor3() {
+//     selectColor3[0].style.backgroundColor = "rgb(115, 115, 115)";
+//   }
+
+//   function changeColor4() {
+//     selectColor4[0].style.backgroundColor = "rgb(217, 217, 217)";
+//   }
+//   let eventClickColorSet1 = background-item1.addEventListener("click", changeColor1);
+//   let eventClickColorSet2 = background-item2.addEventListener("click", changeColor2);
+//   let eventClickColorSet3 = background-item3.addEventListener("click", changeColor3);
+//   let eventClickColorSet4 = background-item4.addEventListener("click", changeColor4);
+// }
+
+// // add() — Adiciona uma classe
+// // remove() — Remove uma classe
+// // contains() — Retorna um booleano indicando se o elemento contém ou não a classe indicada.
+// // toggle() — Adiciona ou remove a classe indicada, dependendo se ela existe ou não no elemento.
+// // event.target ()
