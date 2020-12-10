8b84c770e87008a8c1d13bc5bef20f4ee4ccd5b3
Diego Vini <analistamm@gmail.com>
2020-12-09 23:25:09 -0300

    Criando função que adiciona cor ao pixel

--- script.js
+++ script.js
@@ -1,35 +1,52 @@
-window.onload = function () {
-  const rowColumnTotal = 5;
-  const divPixelBoard = document.getElementById('pixel-board');
-  const selectColorPalette = document.getElementsByClassName('color')
-  const searchSelected = document.getElementsByClassName('selected')   
 
-  function createDiv(className) {
+   function createDiv(className) {
     const divBox = document.createElement('div');
     divBox.className = className;
     return divBox;
   }
 
   function createDivPixel() {
+    const rowColumnTotal = 5; 
+    const divPixelBoard = document.getElementById('pixel-board'); 
     for (let indexDivPixel = 0; indexDivPixel < rowColumnTotal; indexDivPixel += 1) {
       for (let indexDivBoxPixel = 0; indexDivBoxPixel < rowColumnTotal; indexDivBoxPixel += 1) {
         const createBox = createDiv('pixel');
-        const pixelFrame = divPixelBoard.appendChild(createBox);
+        divPixelBoard.appendChild(createBox);
       }
     }
   }
-  createDivPixel();
+  
 
-  function cycleColorPalet(){
-    for(let indexColor = 0; indexColor < selectColorPalette.length; indexColor += 1){
-        selectColorPalette[indexColor].addEventListener('click', function(origin){
-            for (let indexColor2 = 0; indexColor2 < searchSelected.length; indexColor += 1){
-                searchSelected[indexColor2].classList.remove('selected')
-            }                   
-            origin.target.classList.add('selected');  
+  function cycleColorPalet() {
+    const selectColorPalette = document.getElementsByClassName('color');
+    const searchSelected = document.getElementsByClassName('selected');
+    for (let indexColor = 0; indexColor < selectColorPalette.length; indexColor += 1){
+      selectColorPalette[indexColor].addEventListener('click', function(origin){
+        for (let indexColor2 = 0; indexColor2 < searchSelected.length; indexColor += 1){
+          searchSelected[indexColor2].classList.remove('selected')
+        };
+          origin.target.classList.add('selected');
+          
         });
         
     }
   }
-  cycleColorPalet();
+  
+
+  function addColors(){
+      let clickPixel = document.getElementsByClassName('pixel');
+      for (let indexClick = 0; indexClick < clickPixel.length; indexClick += 1){
+          clickPixel[indexClick].addEventListener('click', function(event){
+            const returnColor = window.getComputedStyle(document.querySelector('.color.selected'));
+            const colorPix = returnColor.backgroundColor;
+            event.target.style.backgroundColor = colorPix;
+          });
+      }
+  }
+  
+
+  window.onload = function () {
+    createDivPixel();
+    cycleColorPalet();
+    addColors()
 };
