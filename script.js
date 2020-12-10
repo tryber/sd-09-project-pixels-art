ba248cfd64c990da2e3dac2089abdc605d28637a
Igor Marcos Riegel <igor.riegel@gmail.com>
2020-12-10 04:37:56 -0300

    Fix dos apontamentos do CodeClimate relacionados ao push anterior.

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 window.onload = function () {
   const myPixelBoard = document.querySelector('#pixel-board');
-  let numberOfLinesAndColumns = 5
-  
-//inicio das funções de renderização do pixel-board  
-  insertLine(numberOfLinesAndColumns);
-  // Função inspirada na função de construção de DIV do prof Lucas(Gordinho) na pagina de algoritmo do triangulo.
-  function createDiv(nameOfTheClass) {
-    let myDiv = document.createElement('div');
-    myDiv.className = nameOfTheClass;
-    return myDiv;
-  }
-  function insertLine(lineOfTheTable) {
-    for (let line = 0; line < lineOfTheTable; line += 1) {
-      myPixelBoard.appendChild(createDiv('line'));
-      insertPixel(numberOfLinesAndColumns, line);
-    }
-  }
-  function insertPixel(columnOfTheTable, lineOfTheLoop) {
-    for (let index = 0; index < columnOfTheTable; index += 1) {
-      let lineOfPixelBoard = document.querySelectorAll('.line')[lineOfTheLoop];
-      lineOfPixelBoard.appendChild(createDiv('pixel'));
-    }
-  }
-//fim das funções de renderização do pixel-board
+  const numberOfLinesAndColumns = 5;
+// inicio das funções de renderização do pixel-board
+// Função inspirada na função de construção de DIV do prof Lucas na pagina de alg do triangulo.
+function createDiv(nameOfTheClass) {
+  const myDiv = document.createElement('div');
+  myDiv.className = nameOfTheClass;
+  return myDiv;
 }
+function insertPixel(columnOfTheTable, lineOfTheLoop) {
+  for (let index = 0; index < columnOfTheTable; index += 1) {
+    const lineOfPixelBoard = document.querySelectorAll('.line')[lineOfTheLoop];
+    lineOfPixelBoard.appendChild(createDiv('pixel'));
+  }
+}
+function insertLine(lineOfTheTable) {
+  for (let line = 0; line < lineOfTheTable; line += 1) {
+    myPixelBoard.appendChild(createDiv('line'));
+    insertPixel(numberOfLinesAndColumns, line);
+  }
+}
+insertLine(numberOfLinesAndColumns);
+// fim das funções de renderização do pixel-board
+};
