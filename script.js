fa18d493c912fe379fbe59f1980680c65316cc2a
Matheus Schreiner <matheusrubisch@gmail.com>
2020-12-10 18:50:43 -0300

    Atualizando para formatação do PC

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-window.onload = function () {
-  paletteColor();
-}
-
-function paletteColor () {
-  const palette = document.querySelectorAll('.color');
-  palette[0].style.backgroundColor = 'black';
-  palette[1].style.backgroundColor = 'blue';
-  palette[2].style.backgroundColor = 'red';
-  palette[3].style.backgroundColor = 'yellow';
-}
+const palette = document.querySelectorAll('.color');
+palette[0].style.backgroundColor = 'black';
+palette[1].style.backgroundColor = 'blue';
+palette[2].style.backgroundColor = 'red';
+palette[3].style.backgroundColor = 'yellow';
