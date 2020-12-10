d374555e56c225983d7b045a96cdb68dfc4e92a0
Kamilavrhcp <kamilavrhcp@hotmail.com>
2020-12-10 17:09:05 -0300

    Questao 9

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
+//colorir
 let colorPalette = document.getElementById("color-palette")
 let pixelBoard = document.getElementById("table")
 let blackEvent = document.querySelector('.selected').style.backgroundColor
+//botão + apagar/ pintar de branco
+let clickWhite = document.querySelector('#clear-board')
+let whiteColor = document.querySelector('.whiteColor')
+
+
 
 pixelBoard.addEventListener("click", function(event){
   console.log(event.target)
+
   event.target.style.backgroundColor = blackEvent
+  console.log(blackEvent)
+  
 })
 
 colorPalette.addEventListener("click", function(event){
   console.log(event.target)
-  blackEvent = event.target.style.backgroundColor  
+  blackEvent = event.target.style.backgroundColor   
 })
 
-/*8 - Ao clicar em um pixel dentro do quadro após selecionar uma cor na paleta, o pixel deve ser preenchido com esta cor.
-As seguintes verificações serão feitas:
-Ao carregar a página deve ser possível pintar os pixels de preto;
+clickWhite.addEventListener("click", function(event){
+  let pixels = document.querySelectorAll('.pixel')
+  for( let index =0; index < pixels.length; index += 1){
+    pixels[index].style.backgroundColor = 'white';
+  }
 
-Após selecionar uma outra cor na paleta, deve ser possível pintar os pixels com essa cor;
+})
 
-Somente o pixel que foi clicado deverá ser preenchido com a cor selecionada, sem influenciar na cor dos demais pixels. */
+
+
