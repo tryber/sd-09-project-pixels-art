de37e8c6cf20a329d3fdff5bdad1049d00c79dc5
Daniela Muller <danielamuller20@gmail.com>
2020-12-10 14:39:58 -0300

    Inicializando a página com a cor preta selecionada

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+window.onload = function() {
+blackColor();
+}
 const reedColors = document.querySelectorAll('#color-palette>div');
 const boardOfPixel = document.querySelector('#pixel-board');
+const divPixels = document.createElement('div');
 
 function coloPalette() {
-  reedColors[0].style.backgroundColor = 'black';
   reedColors[1].style.backgroundColor = 'red';
   reedColors[2].style.backgroundColor = 'green';
   reedColors[3].style.backgroundColor = 'blue';
@@ -16,4 +19,35 @@ function GeneratePixelBoard() {
     boardOfPixel.appendChild(divPixels);
   }
 }
-GeneratePixelBoard();
+  GeneratePixelBoard();
+
+function getBlackColor() {
+  let blackColor = document.querySelector('.selected');
+  let black = 'black';
+  blackColor.style.backgroundColor = black;
+  localStorage.setItem('backgroundColor', black)
+}
+getBlackColor();
+
+function getDivColor() {
+  divPixels.addEventListener('click', function() {
+    for (let index = 0; index < divPixels.length; index += 1) {
+      divPixels[index].style.backgroundColor = 'black';
+    }
+  });
+}
+
+function blackColor() {
+  let selected = document.querySelector('#color-palette .selected');
+  let pixelBoard = document.querySelectorAll('.pixel');
+  let color = selected.style.backgroundColor;
+
+  for (let index = 0; index < pixelBoard.length; index += 1) {
+    let colorPixel = pixelBoard[index]
+    colorPixel.addEventListener('click', function(event) {
+      if(selected.classList.contains('selected')) {
+        event.target.style.backgroundColor = color;
+      }
+    });
+  }
+}
