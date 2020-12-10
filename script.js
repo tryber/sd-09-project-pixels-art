6c1374dec674426211f5064aa9262d4dfbc7c854
Layo Kaminski <layokaminski@gmail.com>
2020-12-10 15:29:03 -0300

    Requisitos do Code Climate

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 window.onload = function () {
-    function colorPalette() {
-        let color = document.querySelectorAll('.color')
-        color[0].style.backgroundColor = 'black'
-        color[1].style.backgroundColor = 'steelblue'
-        color[2].style.backgroundColor = 'red'
-        color[3].style.backgroundColor = 'yellow'
+  function colorPalette() {
+    let color = document.querySelectorAll('.color');
+    color[0].style.backgroundColor = 'black';
+    color[1].style.backgroundColor = 'steelblue';
+    color[2].style.backgroundColor = 'red';
+    color[3].style.backgroundColor = 'yellow';
     }
     colorPalette();
-    
-    let pixelBoard = document.querySelector('#pixel-board')
 
-    function createdPixelLine(line) {
-        for (let index = 0; index < 5; index += 1) {
-            let createdElement = document.createElement('div')
-            createdElement.className = 'pixel'
-            line.appendChild(createdElement)
+  let pixelBoard = document.querySelector('#pixel-board');
+
+  function createdPixelLine(line) {
+    for (let index = 0; index < 5; index += 1) {
+        let createdElement = document.createElement('div')
+        createdElement.className = 'pixel'
+        line.appendChild(createdElement)
         }
-    }
+        }
 
         function createdPixelColumn() {
             for (let index = 0; index < 5; index += 1) {
