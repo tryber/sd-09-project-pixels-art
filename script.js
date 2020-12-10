d16f7a878e41f53a980df1ecdc5fdf69b4a061e5
Henrique Brito Elias <henriquebelias@gmail.com>
2020-12-10 20:33:53 -0300

    Requisito 12

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 const colorPalette = document.querySelector('#color-palette');
 const blackBox = document.querySelector('.black');
-const cyanBox = document.querySelector('.cyan');
-const blueVioletBox = document.querySelector('.blueviolet');
-const greenBox = document.querySelector('.green');
 const pixelBoard = document.querySelector('#pixel-board');
 const clearBoard = document.querySelector('#clear-board');
 const pixelElement = document.querySelectorAll('.pixel');
@@ -24,6 +21,41 @@ function chooseColor() {
 }
 chooseColor();
 
+function generateColor() {
+  const color = `rgb(${Math.floor(Math.random() * 256)} , ${Math.floor(Math.random() * 256)} , ${Math.floor(Math.random() * 256)})`
+  return color;
+}
+
+function createBox() {
+  color = generateColor();
+  const coloredBox = document.createElement('div');
+  coloredBox.style.backgroundColor = color;
+  coloredBox.className = 'color';
+  colorPalette.appendChild(coloredBox);
+}
+createBox();
+createBox();
+createBox();
+
+function changeColor() {
+  let boxOne = document.getElementsByClassName('color')[1];
+  let boxTwo = document.getElementsByClassName('color')[2];
+  let boxThree = document.getElementsByClassName('color')[3];
+  blackBox.style.backgroundColor = 'black';
+  pixelBoard.addEventListener('click', function (event) {
+    if (blackBox.classList.contains('selected')) {
+      event.target.style.backgroundColor = blackBox.style.backgroundColor;
+    } else if (boxOne.classList.contains('selected')) {
+      event.target.style.backgroundColor = boxOne.style.backgroundColor;
+    } else if (boxTwo.classList.contains('selected')) {
+      event.target.style.backgroundColor = boxTwo.style.backgroundColor;
+    } else if (boxThree.classList.contains('selected')) {
+      event.target.style.backgroundColor = boxThree.style.backgroundColor;
+    }
+  });  
+}
+changeColor();
+
 function cleanBoard() {
   clearBoard.addEventListener('click', function () {
     for (let index = 0; index < pixelElement.length; index += 1) {
@@ -33,41 +65,22 @@ function cleanBoard() {
 }
 cleanBoard();
 
-function changeColor() {
-  blackBox.style.backgroundColor = 'black';
-  cyanBox.style.backgroundColor = 'cyan';
-  blueVioletBox.style.backgroundColor = 'blueviolet';
-  greenBox.style.backgroundColor = 'green';
-  pixelBoard.addEventListener('click', function (event) {
-    if (blackBox.classList.contains('selected')) {
-      event.target.style.backgroundColor = blackBox.style.backgroundColor;
-    } else if (cyanBox.classList.contains('selected')) {
-      event.target.style.backgroundColor = cyanBox.style.backgroundColor;
-    } else if (blueVioletBox.classList.contains('selected')) {
-      event.target.style.backgroundColor = blueVioletBox.style.backgroundColor;
-    } else if (greenBox.classList.contains('selected')) {
-      event.target.style.backgroundColor = greenBox.style.backgroundColor;
-    }
-  });
-}
-changeColor();
-
 function createBoard() {
   generateBoardBt.addEventListener('click', function () {
     pixelBoard.innerHTML = '';
     if (boardSize.value === '') {
-      alert('Board inválido!')
+      alert('Board inválido!');
     } else if (boardSize.value > 50) {
       boardSize.value = 50;
     } else if (boardSize.value < 5) {
       boardSize.value = 5;
     }
     for (let index = 0; index < boardSize.value; index += 1) {
-      let tr = document.createElement('div');
+      const tr = document.createElement('div');
       tr.className = 'tr';
       pixelBoard.appendChild(tr);
-      for (let index = 0; index < boardSize.value; index += 1) {
-        let td = document.createElement('div');
+      for (let index2 = 0; index2 < boardSize.value; index2 += 1) {
+        const td = document.createElement('div');
         td.className = 'pixel td';
         tr.appendChild(td);
       }
