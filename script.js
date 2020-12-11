be632e971f5c648def28e27d4e2b6ed5aa7c8363
Marcelo Campos <marcelo.campos.economista@gmail.com>
2020-12-10 23:08:36 -0300

    Adicionando os desafios 10 e 11

--- script.js
+++ script.js
@@ -4,34 +4,55 @@ window.onload = function () {
 };
 
 const getColorPalette = document.getElementById('color-palette');
-const colors = ['black', 'red', 'green', 'blue'];
+const colors = ['black', 'color1', 'color2', 'color3'];
 for (let index = 0; index < colors.length; index += 1) {
   const colorsPalette = document.createElement('div');
   colorsPalette.className = 'color ' + colors[index];
   getColorPalette.appendChild(colorsPalette);
 }
 const getBlackSlotOnPalette = document.querySelector('.black');
-const getRedSlotOnPalette = document.querySelector('.red');
-const getGreenSlotOnPalette = document.querySelector('.green');
-const getBlueSlotOnPalette = document.querySelector('.blue');
+const getRedSlotOnPalette = document.querySelector('.color1');
+const getGreenSlotOnPalette = document.querySelector('.color2');
+const getBlueSlotOnPalette = document.querySelector('.color3');
 getBlackSlotOnPalette.style.backgroundColor = 'black';
 getRedSlotOnPalette.style.backgroundColor = 'red';
 getGreenSlotOnPalette.style.backgroundColor = 'green';
 getBlueSlotOnPalette.style.backgroundColor = 'blue';
 
-function printPixelsFrame() {
+const getPixelBoardContainer = document.querySelector('.pixel-board-container');
+const createPixelBoard = document.createElement('div')
+createPixelBoard.id = 'pixel-board';
+getPixelBoardContainer.appendChild(createPixelBoard);
+
+const getDivButton = document.querySelector('.buttons-container');
+const createInput = document.createElement('input');
+createInput.setAttribute('type', 'number');
+createInput.id = 'board-size';
+getDivButton.appendChild(createInput);
+document.getElementById('board-size').min = '5';
+document.getElementById('board-size').max = '50';
+
+const createInputButton = document.createElement('button');
+createInputButton.id = 'generate-board';
+createInputButton.innerText = 'VQV';
+getDivButton.appendChild(createInputButton);
+
+function printPixelsFrame(n) {
   const getPixelBoard = document.getElementById('pixel-board');
-  for (let line = 0; line < 5; line += 1) {
+  getPixelBoard.innerHTML = '';
+  for (let line = 0; line < n; line += 1) {
     const createDivLine = document.createElement('div');
-    for (let column = 0; column < 5; column += 1) {
+    createDivLine.className = 'fix-pixel';
+    for (let column = 0; column < n; column += 1) {
       const createDivColumns = document.createElement('div');
       createDivColumns.className = 'pixel';
       createDivLine.appendChild(createDivColumns);
     }
     getPixelBoard.appendChild(createDivLine);
   }
+  changePixelColor();
 }
-printPixelsFrame();
+printPixelsFrame(0);
 
 function changeColorSelected() {
   const getColors = document.querySelector('#color-palette');
@@ -64,3 +85,32 @@ function clearPixelsBoard() {
   });
 }
 clearPixelsBoard();
+
+function changeBoardSize() {
+  const getInputField = document.querySelector('#board-size');
+  const getVqvButton = document.querySelector('#generate-board');
+
+  getVqvButton.addEventListener('click', function () {
+    let boardSize = 0;
+    if (getInputField.value === '') {
+      alert('Board inválido!');
+    } else if (getInputField.value < 5 || getInputField.value > 50) {
+      limitBoardSize(getInputField.value);
+    } else {
+      boardSize = getInputField.value;
+      getInputField.value = '';
+      printPixelsFrame(boardSize);
+    }
+  });
+}
+changeBoardSize();
+
+function limitBoardSize(boardSize) {
+  if (boardSize < 5) {
+    boardSize = 5;
+    printPixelsFrame(boardSize);
+  } else {
+    boardSize = 50;
+    printPixelsFrame(boardSize);
+  }
+}
