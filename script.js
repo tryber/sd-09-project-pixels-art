5237dd28820f61c67acd13cebddce2872008c04a
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2020-12-10 22:19:30 -0300

    Iniciando o script

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-//function paintPixel () {}
+let color1 = document.getElementsByClassName('first-color')[0];
+let color2 = document.getElementsByClassName('second-color')[0];
+let color3 = document.getElementsByClassName('third-color')[0];
+let color4 = document.getElementsByClassName('fourth-color')[0];
+
+color1.addEventListener("click",selectingColor)
+color2.addEventListener("click",selectingColor)
+color3.addEventListener("click",selectingColor)
+color4.addEventListener("click",selectingColor)
+
+let selectedColor = selectingColor();
+//color1.classList.add("selected");  
+//console.log(color1);
+let clickButton = document.getElementById('cleaner-button');
+clickButton.addEventListener("click", paintPixel)
+
+function selectingColor () {
+    
+}
+function paintPixel () {    
+    let textToPrint = document.getElementById('teste');
+    textToPrint.innerText = "Hora da verdade. Acho que funcionou!";
+}
+
