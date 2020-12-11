94e531f3dafa8f215c90b0c34feb2c10d845a609
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2020-12-10 22:44:42 -0300

    Atualizaçao script 2

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ let color2 = document.getElementsByClassName('second-color')[0];
 let color3 = document.getElementsByClassName('third-color')[0];
 let color4 = document.getElementsByClassName('fourth-color')[0];
 
-color1.addEventListener("click",selectingColor)
+/*color1.addEventListener("click",selectingColor)
 color2.addEventListener("click",selectingColor)
 color3.addEventListener("click",selectingColor)
-color4.addEventListener("click",selectingColor)
+color4.addEventListener("click",selectingColor)*/
 
 let selectedColor = selectingColor();
 //color1.classList.add("selected");  
 //console.log(color1);
-let clickButton = document.getElementById('cleaner-button');
+let clickButton = document.getElementById('clear-button');
 clickButton.addEventListener("click", paintPixel)
 
 function selectingColor () {
