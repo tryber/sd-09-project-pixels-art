812d43a96c03b702dc49d4b5cfb52658cf600500
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2020-12-10 23:34:44 -0300

    atualizaçao 3 do script

--- script.js
+++ script.js
@@ -8,17 +8,16 @@ color2.addEventListener("click",selectingColor)
 color3.addEventListener("click",selectingColor)
 color4.addEventListener("click",selectingColor)*/
 
-let selectedColor = selectingColor();
+//let selectedColor = selectingColor();
 //color1.classList.add("selected");  
 //console.log(color1);
-let clickButton = document.getElementById('clear-button');
-clickButton.addEventListener("click", paintPixel)
 
-function selectingColor () {
-    
-}
+
+
 function paintPixel () {    
-    let textToPrint = document.getElementById('teste');
-    textToPrint.innerText = "Hora da verdade. Acho que funcionou!";
+  let textToPrint = document.getElementById('teste');
+  textToPrint.innerText = 'Hora da verdade. Acho que funcionou!';
 }
 
+let clickButton = document.getElementById('clear-board');
+clickButton.addEventListener('click', paintPixel)
