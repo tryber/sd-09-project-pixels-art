9b373c940181eadfea5a35e8c2a86ec4fd7bc977
alailsonko <aalailson3@gmail.com>
2020-12-11 00:35:47 -0300

    feat: challenge bonus

--- script.js
+++ script.js
@@ -4,29 +4,58 @@ function main() {
   // verify if started color starts with black
   console.log(document.getElementsByClassName("selected"))
   selectedColor()
-  paintPixels()
+
   clearBoard()
+  createPixelBoard()
+  paintPixels()
   loadMsg()
 }
+
+
 
 function factory() {
   const palleteOptions = document.getElementsByClassName("color")
   const paint = document.querySelector(".selected")
-  const myPixels = document.getElementsByClassName("pixel")
   const clearBoardButton = document.getElementById("clear-board")
+  const myBoard = document.getElementById("pixel-board")
+  const inputPixelsQuantity = document.getElementById("board-size")
+  const buttonAddPixels = document.getElementById("generate-board")
   return {
     MyPallete: palleteOptions,
     Paint: paint,
-    MyPixels: myPixels,
     ClearBoardButton: clearBoardButton,
+    MyBoard: myBoard,
+    InputPixelsQuantity: inputPixelsQuantity,
+    ButtonAddPixels: buttonAddPixels,
   }
 }
+   let myPixels = document.querySelectorAll(".pixel")
+function createPixelBoard() {
+  const makeFactory = factory()
+  const myBoard = makeFactory.MyBoard
+  const buttonAddPixels = makeFactory.ButtonAddPixels
+  const inputPixelsQuantity = makeFactory.InputPixelsQuantity
+  buttonAddPixels.addEventListener('click', function () {
+    const valueSizeBoard = inputPixelsQuantity.value
+     console.log('hello')
+    myBoard.innerHTML = null
+    for( let i = 0; i < valueSizeBoard; i += 1) {
+      for ( let j = 0; j < valueSizeBoard; j += 1){
+        myBoard.innerHTML += '<div class="pixel"></div>'
+      }
+    }
+    document.getElementById("pixel-board").style.width = `calc(42px * ${valueSizeBoard})`
+    document.getElementById("pixel-board").style.height = `calc(40px * ${valueSizeBoard})`
+    myPixels = document.querySelectorAll(".pixel")
+   paintPixels()
+  })
+
+}
+
 function clearBoard() {
    const makeFactory = factory()
    const clearBoardButton = makeFactory.ClearBoardButton
-   const myPixels = makeFactory.MyPixels
    clearBoardButton.addEventListener('click', function(){
-     console.log(myPixels)
      for ( let i = 0; i < myPixels.length; i += 1) {
        myPixels[i].style.backgroundColor = "white"
      }
@@ -34,18 +63,18 @@ function clearBoard() {
 }
 
 function paintPixels() {
-  const makeFactory = factory()
-  const myPixels = makeFactory.MyPixels
+  console.log(myPixels)
   for ( let i = 0; i < myPixels.length; i += 1) {
     myPixels[i].addEventListener("click", function() {
+
       colorizePixel(i)
     })
   }
+
 }
 function colorizePixel(index){
   const makeFactory = factory()
   const paint = makeFactory.Paint
-  const myPixels = makeFactory.MyPixels
   const color = paint.classList[1]
   console.log(myPixels[index])
   myPixels[index].style.backgroundColor = color
