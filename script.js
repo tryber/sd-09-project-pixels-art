c0f051d3c749cd1f1d365cbbad00aa1b4e001f5b
Hudson Carlos <hc88806@gmail.com>
2020-12-12 17:06:16 -0400

    adicionado seletor de cores

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-window.onload = function() {
-    let color = 'black';
-    let selectionColor = document.querySelector('#color-palette');
-    selectionColor.addEventListener('click', function (event) {
-        let colorSelection = event.target.className
-        for (let x = 6; x < colorSelection.length;  x += 1){
-            if (x === 6){
-                color = '';
-            }
-            color += colorSelection[x];
-        }
-    });    
-    let pixels = document.querySelectorAll('.pixel');
-    for (let x = 0; x < pixels.length; x += 1) {
-        pixels[x].addEventListener('click', function (event) {
-            event.target.style.backgroundColor = color; 
-        });  
-    }
-    let button = document.querySelector('#clear-board');
-    button.addEventListener('click', function (event) {
-        for (let x = 0; x < pixels.length; x += 1) {
-        pixels[x].style.backgroundColor ='rgb(255, 255, 255)';
-        }
+window.onload = function () {
+  let cor = document.querySelector('.selected');
+  let selectionColor = document.querySelector('#color-palette');
+  selectionColor.addEventListener('click', function (event) {
+     if (event.target !== selectionColor) {
+        cor.classList.remove('selected');
+        event.target.classList.add('selected'); 
+        cor = document.querySelector('.selected');
+        console.log(selectionColor);
+        console.log(event); 
+      }
     });
+  let pixels = document.querySelectorAll('.pixel');
+  for (let x = 0; x < pixels.length; x += 1) {
+      pixels[x].addEventListener('click', function (event) {
+          event.target.style.backgroundColor = cor.classList[1];
+      });
+  }
+  let button = document.querySelector('#clear-board');
+  button.addEventListener('click', function (event) {
+      for (let x = 0; x < pixels.length; x += 1) {
+      pixels[x].style.backgroundColor ='white';
+      }
+  });
 }
