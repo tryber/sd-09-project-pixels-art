2875f05bbfc108eb1fab90e4a4d289f606b30256
Matheus Mendes <matheus.unilog@gmail.com>
2020-12-11 23:08:14 -0300

    Rework no Projeto

--- script.js
+++ script.js
@@ -1,44 +1,37 @@
-function setColors(){
-    let color1 = document.querySelector('.black')
-    let color2 = document.querySelector('.red')
-    let color3 = document.querySelector('.cyan')
-    let color4 = document.querySelector('.purple')
+//ME ESFORCEI E VOU CONSEGUIR
+addColors();
 
-color1.style.background = 'black';
-color2.style.background = 'red';
-color3.style.background = 'cyan';
-color4.style.background = 'purple';
 
-}
-setColors();
-/*
-let basePixels = document.querySelectorAll(".pixel");;
-let numberOfLines = (basePixels);
-let columnPixels = numberOfLines
-let pixel = document.querySelectorAll(".pixel");
+    function addColors(){
 
-fillPixelBoard(pixel);
+const getColor1 = document.querySelector('#color-palette');
+const getColor2 = document.querySelector('#color-palette');
+const getColor3 = document.querySelector('#color-palette');
+const getColor4 = document.querySelector('#color-palette');
 
-function createPixel(className) {
-    let pixel = document.createElement('div');
-    pixel.className = className;
-    return pixel;
-}
+let colorOne = document.createElement('div')
+let colorTwo = document.createElement('div')
+let colorThree = document.createElement('div')
+let colorFour = document.createElement('div')
 
-function fillPixelBoard (pixel){
-    for (let index = 0; index <= pixel.length; index +=1){
-        fillPixel(pixel[index]);
- }
-}
+colorOne.className = 'color';
+colorOne.innerHTML = 'Preto'
+colorOne.style.background = 'black'
 
-function fillPixel(divPixel){
-    const maxPixels = 8;
-    for(let index = 4; index <= maxPixels; index +=1){
-        if(index < maxPixels){
-        let pixel = createPixel("pixel");
-        divPixel.appendChild(pixel);
-    } else {
-        divPixel.appendChild(createPixel("pixel"));
-  }
- }
-}*/
+colorTwo.className = 'color';
+colorTwo.innerHTML = 'Azul Escuro'
+colorTwo.style.background = 'darkblue'
+
+colorThree.className = 'color';
+colorThree.innerHTML = 'Verde Escuro'
+colorThree.style.background = 'darkgreen'
+
+colorFour.className = 'color';
+colorFour.innerHTML = 'Ciano Escuro'
+colorFour.style.background = 'darkcyan'
+
+getColor1.appendChild(colorOne)
+getColor2.appendChild(colorTwo)
+getColor3.appendChild(colorThree)
+getColor4.appendChild(colorFour)
+};
