96936f6042be796ff65f9f2b564f185e60794b79
RodrigoNeoEx <draja.acc@gmail.com>
2020-12-13 18:31:14 -0300

    'comitao' - functions > choosed color, setPixelBoardColor, classAdd/Remover, listeners

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
-function createPalete () {
-  let palete = [1,2,3,4];
-    for (let index of palete) {
+// creating squares for palette
+function createPalette () {
+  let palette = [1,2,3,4];
+    for (let index of palette) {
     let squareColor = document.createElement('div');
-    
     squareColor.className = 'color';
-    document.getElementById('color-palette').appendChild(squareColor)
-    
+    document.getElementById('color-palette').appendChild(squareColor);
   }
 }
-  
+//creating pixel boart from base to lines and colums
 function createBoardLine () {
-  let base = 5
+  let base = 5;
   for (let line = 0; line < base; line += 1) {
     let div = document.createElement('div');
     for (let colum = 0; colum < base; colum += 1) {
       let pixel = document.createElement('div');
       pixel.className = 'pixel';
-      div.appendChild(pixel);      
+      div.appendChild(pixel);
     }
-    document.getElementById('pixel-board').appendChild(div)
+    document.getElementById('pixel-board').appendChild(div);
   }
 }
-
-function paleteColor() {
-  let pink = document.querySelectorAll('.color')[1];
-  let purple = document.querySelectorAll('.color')[2];
-  let green = document.querySelectorAll('.color')[3];
-  pink.style.backgroundColor = getRandomRgb();
-  purple.style.backgroundColor = getRandomRgb();
-  green.style.backgroundColor = getRandomRgb();
-  if(pink = purple || green || rgb(255, 255, 255) ) {
-    pink = getRandomRgb();
+// setting colors to palette
+function paletteColors() {
+  let color1 = document.querySelectorAll('.color')[1];
+  let color2 = document.querySelectorAll('.color')[2];
+  let color3 = document.querySelectorAll('.color')[3];
+  color1.style.backgroundColor = getRandomRgb();
+  color2.style.backgroundColor = getRandomRgb();
+  color3.style.backgroundColor = getRandomRgb();
+  if(color1 = color2 || color3 || rgb(255 , 255 , 255)) {
+    color1 = getRandomRgb();
   }
-  if(purple = green || rgb(255, 255, 255)){
-  purple = getRandomRgb()
-  }  
+  if(color2 = color3 || rgb(255 , 255 , 255)){
+  color2 = getRandomRgb();
+  }
 }
+// creating random rgb to set on palette
 function getRandomRgb() {
   var num = Math.round(0xffffff * Math.random());
   var r = num >> 8;
@@ -43,11 +43,39 @@ function getRandomRgb() {
   var b = num & 250;
   return 'rgb(' + r + ', ' + g + ', ' + b + ')';
 }
+// class add and remover to setup class and get choosed color
+function choosePaletteColor(event) {
+  document.querySelector('.selected').classList.remove('selected');  
+  event.target.classList.add('selected');
+}
+// created "class mover" to choose colors
+function setChoosedColor() {
+  let classMover = document.querySelector('.selected');
+  let colorSelected = getComputedStyle(classMover);
+  return colorSelected.backgroundColor;
+}
+//open pixel board to acept color by class mover
+function setPixelBoardColor(event) {
+  let pixelColor = event.target;
+  pixelColor.style.backgroundColor = setChoosedColor();
+}
 
-window.onload = function(){
-  createPalete()
-  document.querySelector('#color-palette').firstElementChild.classList.add('selected')
+function listenersOnClick() {  
+  let colorPalette = document.querySelector('#color-palette');
+  colorPalette.addEventListener('click', choosePaletteColor);
+  let pixelBoard = document.querySelector('#pixel-board');
+  pixelBoard.addEventListener('click', setPixelBoardColor);
+}
+
+window.onload = function(){  
+  createPalette()
+  document.querySelector('#color-palette').firstElementChild.classList.add('selected') //aplica class selected ao primeiro elemento
+  document.querySelector('#color-palette').firstElementChild.classList.add('black') // aplica background ao primeiro elemento
   createBoardLine()
   getRandomRgb()
-  paleteColor()
+  paletteColors()
+  setChoosedColor()
+  listenersOnClick() // todos addListener dentro de uma unica função
 }
+
+// criar botão para retornar a classe inicial de todos pixels assim alterando a cor novamente para branco
