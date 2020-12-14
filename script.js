85a405de20c55d62d20a59ac946d08c18884e71d
Pedro Henrique <pedro.876.ph@gmail.com>
2020-12-14 16:12:12 -0300

    code climate 2.0

--- script.js
+++ script.js
@@ -1,61 +1,54 @@
-let pixelBoard = document.querySelector('#pixel-board');
-function pxBoard(){
-    for(let lineIndex = 0; lineIndex < 5; lineIndex += 1) {
-        let lineContainer = document.createElement('div');
-        lineContainer.className = 'pixel-line';
-        for(let columnIndex = 0; columnIndex < 5; columnIndex += 1) {
-            let pxColumn = document.createElement('div');
-            pxColumn.className = 'pixel';
-            lineContainer.appendChild(pxColumn);
-        }
-        pixelBoard.appendChild(lineContainer);
-    }
+const pixelBoard = document.querySelector('#pixel-board');
+function pxBoard() {
+  for(let lineIndex = 0; lineIndex < 5; lineIndex += 1) {
+      let lineContainer = document.createElement('div');
+      lineContainer.className = 'pixel-line';
+      for(let columnIndex = 0; columnIndex < 5; columnIndex += 1) {
+          const pxColumn = document.createElement('div');
+          pxColumn.className = 'pixel';
+          lineContainer.appendChild(pxColumn);
+      }
+      pixelBoard.appendChild(lineContainer);
+  }
 }
 pxBoard();
 
 function colorSelecter(event) {
-    const selected = document.querySelector('.selected');
-    const targetSelected = event.target;
-    if(targetSelected !== selected) {
-        selected.classList.remove('selected');
-        targetSelected.classList.add('selected');
-    }
+  const selected = document.querySelector('.selected');
+  const targetSelected = event.target;
+  if(targetSelected !== selected) {
+      selected.classList.remove('selected');
+      targetSelected.classList.add('selected');
+  }
 }
 
 function getSelecter() {
-    for (let index = 0; index < 4; index += 1) {
-        const paletteList = document.querySelectorAll('.color')[index];
-        paletteList.addEventListener('click', colorSelecter);
-    }
+  for (let index = 0; index < 4; index += 1) {
+      const paletteList = document.querySelectorAll('.color')[index];
+      paletteList.addEventListener('click', colorSelecter);
+  }
 }
 
 getSelecter();
 
 function bgPaint(event) {
-    let classSelector = document.querySelector('.selected');
-    event.target.style.backgroundColor = classSelector.style.backgroundColor;
+  let classSelector = document.querySelector('.selected');
+  event.target.style.backgroundColor = classSelector.style.backgroundColor;
 }
 
+pixelBoard.addEventListener('click', bgPaint);
 
-
-function paintClick() {
-    const pxBoard = document.querySelector('#pixel-board');
-    pxBoard.addEventListener('click', bgPaint);
-}
-
-paintClick();
-
-let cleaner = document.querySelector('#clear-board');
-let pxel = document.querySelectorAll('.pixel');
+const cleaner = document.querySelector('#clear-board');
+const pxel = document.querySelectorAll('.pixel');
 
 function clearboard() {
-    for(let index = 0; index < pxel.length; index += 1) {
-        pxel[index].style.backgroundColor = 'white';
-    }
+  for(let index = 0; index < pxel.length; index += 1) {
+      pxel[index].style.backgroundColor = 'white';
+  }
 }
 
 function btnClick() {
-    cleaner.addEventListener('click', clearboard);
+  cleaner.addEventListener('click', clearboard);
 }
 
 btnClick();
