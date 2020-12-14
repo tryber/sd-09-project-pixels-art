24c7a4ae3c6005690159ab5b8e3cc47692598a7a
Pamela Anderson Sales <pamelasalesr@gmail.com>
2020-12-14 17:14:47 -0300

    Fix some CodeClimate issues

--- script.js
+++ script.js
@@ -9,19 +9,6 @@ function createPalette() {
   document.querySelector('.color').classList.add('selected');
 }
 
-function paintPalette() {
-  const coloredSquare = document.querySelectorAll('.color');
-  coloredSquare[0].style.backgroundColor = 'black';
-  coloredSquare[1].style.backgroundColor = `rgb(${randomNumbers()}, ${randomNumbers()}, ${randomNumbers()})`;
-  coloredSquare[2].style.backgroundColor = `rgb(${randomNumbers()}, ${randomNumbers()}, ${randomNumbers()})`;
-  coloredSquare[3].style.backgroundColor = `rgb(${randomNumbers()}, ${randomNumbers()}, ${randomNumbers()})`;
-}
-
-function myFunction() {
-  var num = document.getElementById('board-size').value;
-  document.getElementById("demo").innerHTML = num;
-}
-
 function createPixelBoard(num) {
   const pixelBoard = document.getElementById('pixel-board');
   let numberOfColumns = num;
@@ -38,21 +25,6 @@ function createPixelBoard(num) {
 }
 createPixelBoard(5);
 
-function newBoard(){
-  let input = getInputValue();
-  if (input < 5) {
-    alert('Numero deve ser maior que 5!');
-    return;
-  }
-  if (document.getElementById('board-size').value.length == 0) {
-    alert('Board inválido!');
-    return;
-  }
-  deleteBoard();
-  createPixelBoard(input);
-  paintPixel();
-}
-
 function getInputValue() {
   let input = parseInt(document.querySelector('input').value);
   if (input < 5 && input > 0){
@@ -65,20 +37,20 @@ function getInputValue() {
 
 function deleteBoard() {
   let lines = document.getElementById('pixel-board').children;
-  let boardSize = lines.length;
+  const boardSize = lines.length;
   for (let i = 0; i < boardSize; i++) {
-    lines[0].remove()
+    lines[0].remove();
   }
 }
 
 function classSelected() {
   const colors = document.getElementsByClassName('color');
-  for (let index = 0; index<colors.length; index += 1) {
-    colors[index].addEventListener('click', function(event){
+  for (let index = 0; index < colors.length; index += 1) {
+    colors[index].addEventListener('click', function(event) {
       for (let index2 = 0; index2 < colors.length; index2 += 1) {
         colors[index2].classList.remove('selected');
       }
-      event.target.classList.add('selected')
+      event.target.classList.add('selected');
     })
   }
 }
@@ -86,31 +58,54 @@ function classSelected() {
 function paintPixel() {
   const pixels = document.getElementsByClassName('pixel');
   for (let index = 0; index < pixels.length; index += 1) {
-    pixels[index].addEventListener('click', function(event){
+    pixels[index].addEventListener('click', function(event) {
       const elementSelected = document.querySelector('.color.selected');
       const elementSelectedBackgroundColor = window.getComputedStyle(elementSelected).getPropertyValue('background-color');
       event.target.style.backgroundColor = elementSelectedBackgroundColor;
-    })
+    });
   }
 }
 
 function clearPixelBoard() {
   const pixels = document.getElementsByClassName('pixel');
   const button = document.getElementById('clear-board');
-  button.addEventListener('click', function(){
-    for (let index = 0; index < pixels.length; index += 1){
+  button.addEventListener('click', function() {
+    for (let index = 0; index < pixels.length; index += 1) {
       pixels[index].style.backgroundColor = 'rgb(255, 255, 255)';
     }
   })
 }
 
 function randomNumbers() {
-  return Math.round(Math.random()*255);
+  return Math.round(Math.random() * 255);
 }
 
 function randomPalette() {
-  const button = document.querySelector('#randomize-color')
+  const button = document.querySelector('#randomize-color');
   button.addEventListener('click', paintPalette);
+}
+
+function paintPalette() {
+  const coloredSquare = document.querySelectorAll('.color');
+  coloredSquare[0].style.backgroundColor = 'black';
+  coloredSquare[1].style.backgroundColor = `rgb(${randomNumbers()}, ${randomNumbers()}, ${randomNumbers()})`;
+  coloredSquare[2].style.backgroundColor = `rgb(${randomNumbers()}, ${randomNumbers()}, ${randomNumbers()})`;
+  coloredSquare[3].style.backgroundColor = `rgb(${randomNumbers()}, ${randomNumbers()}, ${randomNumbers()})`;
+}
+
+function newBoard() {
+  const input = getInputValue();
+  if (input < 5) {
+    alert('Numero deve ser maior que 5!');
+    return;
+  }
+  if (document.getElementById('board-size').value.length === 0) {
+    alert('Board inválido!');
+    return;
+  }
+  deleteBoard();
+  createPixelBoard(input);
+  paintPixel();
 }
 
 window.onload = function() {
