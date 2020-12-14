89006df7474d80bdbb0a27bb21aeec310aeaa902
RodrigoNeoEx <draja.acc@gmail.com>
2020-12-14 18:31:55 -0300

    working on climate

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 // creating squares for palette
-function createPalette () {
-  let palette = [1,2,3,4];
-    for (let index of palette) {
+function createPalette() {
+  let palette = [1, 2, 3, 4];
+  for (const index of palette) {
     let squareColor = document.createElement('div');
     squareColor.className = 'color';
     document.getElementById('color-palette').appendChild(squareColor);
   }
 }
-//creating pixel boart from base to lines and colums
-function createBoardLine () {
+//  creating pixel boart from base to lines and colums
+function createBoardLine() {
   let base = 5;
   for (let line = 0; line < base; line += 1) {
     let div = document.createElement('div');
@@ -20,6 +20,14 @@ function createBoardLine () {
     document.getElementById('pixel-board').appendChild(div);
   }
 }
+// creating random rgb to set on palette
+function getRandomRgb() {
+  let num = Math.round(0xffffff * Math.random());
+  let r = num >> 8;
+  let g = num >> 8 & 250;
+  let b = num & 250;
+  return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+}
 // setting colors to palette without equal colors
 function paletteColors() {
   let color1 = document.querySelectorAll('.color')[1];
@@ -28,41 +36,33 @@ function paletteColors() {
   color1.style.backgroundColor = getRandomRgb();
   color2.style.backgroundColor = getRandomRgb();
   color3.style.backgroundColor = getRandomRgb();
-  if(color1 = color2 || color3 || rgb(255 , 255 , 255)) {
+  if  (color1 = color2 || color3 || rgb(255, 255, 255)) {
     color1 = getRandomRgb();
   }
-  if(color2 = color3 || rgb(255 , 255 , 255)){
-  color2 = getRandomRgb();
+  if (color2 = color3 || rgb(255, 255, 255)) {
+    color2 = getRandomRgb();
   }
-}
-// creating random rgb to set on palette
-function getRandomRgb() {
-  var num = Math.round(0xffffff * Math.random());
-  var r = num >> 8;
-  var g = num >> 8 & 250;
-  var b = num & 250;
-  return 'rgb(' + r + ', ' + g + ', ' + b + ')';
 }
 // class add and remover to setup class and get choosed color
 function choosePaletteColor(event) {
-  document.querySelector('.selected').classList.remove('selected');  
+  document.querySelector('.selected').classList.remove('selected');
   event.target.classList.add('selected');
 }
-//create buttom clear for pixel board color
+//  create buttom clear for pixel board color
 function buttonClear() {
-  let append = document.getElementById('color-palette');  
-  let divButton = document.createElement('div');  
+  let append = document.getElementById('color-palette');
+  let divButton = document.createElement('div');
   append.appendChild(divButton);  
-  let button = document.createElement('button')
+  let button = document.createElement('button');
   button.innerText = 'Limpar';
-  button.id = 'clear-board'
-  append.appendChild(button)
+  button.id = 'clear-board';
+  append.appendChild(button);
 }
 // add function clear to button
 function clear() {
   let clearPixel = document.querySelectorAll('.pixel');
-  for(let index = 0; index < clearPixel.length; index += 1){
-   clearPixel[index].setAttribute("style", "backgroundColor = rgb(255 , 255 , 255)" );
+  for (let index = 0; index < clearPixel.length; index += 1) {
+    clearPixel[index].setAttribute('style', 'backgroundColor = rgb(255 , 255 , 255)' );
   }
 }
 // created "class mover" to choose colors
@@ -71,66 +71,60 @@ function setChoosedColor() {
   let colorSelected = getComputedStyle(classMover);
   return colorSelected.backgroundColor;
 }
-//open pixel board to accept color by class mover
+//  open pixel board to accept color by class mover
 function setPixelBoardColor(event) {
   let pixelColor = event.target;
   pixelColor.style.backgroundColor = setChoosedColor();
 }
 // create and append buttom board size
-function buttonSize () {
+function buttonSize() {
   let append = document.getElementById('color-palette');  
-  let divButton = document.createElement('div');  
-  append.appendChild(divButton);  
-  let button = document.createElement('button')
+  let divButton = document.createElement('div');
+  append.appendChild(divButton);
+  let button = document.createElement('button');
   button.innerText = 'VQV';
   button.id = 'generate-board';
-  append.appendChild(button)  
+  append.appendChild(button);
 }
-
-//create and append buttom choose size
-function chooseSize () {
+// create and append buttom choose size
+function chooseSize() {
   let append = document.getElementById('color-palette');
-  let divButton = document.createElement('div');  
-  append.appendChild(divButton);  
-  let button = document.createElement('button')
+  let divButton = document.createElement('div');
+  append.appendChild(divButton);
+  let button = document.createElement('button');
   button.innerText = '';
   button.id = 'board-size';
-  append.appendChild(button)
+  append.appendChild(button);
 }
-function VQV(){
-  document.getElementById('generate-board')
-  alert('Board inválido!')
+// create function to set limits on size or alert when over extend 
+function VQV() {
+  document.getElementById('generate-board');
+  alert('Board inválido!');
 }
-
 // function to append all listeners
-function listenersOnClick() {  
+function listenersOnClick() {
   let colorPalette = document.querySelector('#color-palette');
   colorPalette.addEventListener('click', choosePaletteColor);
   let pixelBoard = document.querySelector('#pixel-board');
   pixelBoard.addEventListener('click', setPixelBoardColor);
   let buttonClearPixel = document.querySelector('#clear-board');
-  buttonClearPixel.addEventListener('click', clear)
+  buttonClearPixel.addEventListener('click', clear);
   let buttonBoardSize = document.querySelector('#generate-board');
-  buttonBoardSize.addEventListener('click', VQV)
+  buttonBoardSize.addEventListener('click', VQV);
   let buttonChooseSize = document.querySelector('#board-size');
-  buttonChooseSize.addEventListener('click', console.log('aguardando função'))
+  buttonChooseSize.addEventListener('click', console.log('aguardando função'));
 }
-
-
-
-
-
-window.onload = function(){  
-  createPalette()
-  document.querySelector('#color-palette').firstElementChild.classList.add('selected') //aply class selected to first child
-  document.querySelector('#color-palette').firstElementChild.classList.add('black') // aply class black to first child start with bg black
-  createBoardLine()
-  getRandomRgb()
-  paletteColors()
-  setChoosedColor()
-  buttonClear()
-  buttonSize ()
-  chooseSize ()  
-  listenersOnClick()  
+// invoke assigned functions when open page
+window.onload = function() {
+  createPalette();
+  document.querySelector('#color-palette').firstElementChild.classList.add('selected'); // aply class selected to first child
+  document.querySelector('#color-palette').firstElementChild.classList.add('black'); //  aply class black to first child start with bg black
+  createBoardLine();
+  getRandomRgb();
+  paletteColors();
+  setChoosedColor();
+  buttonClear();
+  buttonSize ();
+  chooseSize ();
+  listenersOnClick();
 }
-// criar botão para aumentar a board
