0d8b58655f8d1051c9cc8b758d5274f7bdd696df
Pedro Henrique <pedro.876.ph@gmail.com>
2020-12-14 16:20:21 -0300

    code climate 3.0

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const pixelBoard = document.querySelector('#pixel-board');
 function pxBoard() {
-  for(let lineIndex = 0; lineIndex < 5; lineIndex += 1) {
-      let lineContainer = document.createElement('div');
-      lineContainer.className = 'pixel-line';
-      for(let columnIndex = 0; columnIndex < 5; columnIndex += 1) {
-          const pxColumn = document.createElement('div');
-          pxColumn.className = 'pixel';
-          lineContainer.appendChild(pxColumn);
-      }
-      pixelBoard.appendChild(lineContainer);
+  for (let lineIndex = 0; lineIndex < 5; lineIndex += 1) {
+    const lineContainer = document.createElement('div');
+    lineContainer.className = 'pixel-line';
+    for(let columnIndex = 0; columnIndex < 5; columnIndex += 1) {
+      const pxColumn = document.createElement('div');
+      pxColumn.className = 'pixel';
+      lineContainer.appendChild(pxColumn);
+    }
+    pixelBoard.appendChild(lineContainer);
   }
 }
 pxBoard();
@@ -16,16 +16,16 @@ pxBoard();
 function colorSelecter(event) {
   const selected = document.querySelector('.selected');
   const targetSelected = event.target;
-  if(targetSelected !== selected) {
-      selected.classList.remove('selected');
-      targetSelected.classList.add('selected');
+  if (targetSelected !== selected) {
+    selected.classList.remove('selected');
+    targetSelected.classList.add('selected');
   }
 }
 
 function getSelecter() {
   for (let index = 0; index < 4; index += 1) {
-      const paletteList = document.querySelectorAll('.color')[index];
-      paletteList.addEventListener('click', colorSelecter);
+    const paletteList = document.querySelectorAll('.color')[index];
+    paletteList.addEventListener('click', colorSelecter);
   }
 }
 
@@ -42,8 +42,8 @@ const cleaner = document.querySelector('#clear-board');
 const pxel = document.querySelectorAll('.pixel');
 
 function clearboard() {
-  for(let index = 0; index < pxel.length; index += 1) {
-      pxel[index].style.backgroundColor = 'white';
+  for (let index = 0; index < pxel.length; index += 1) {
+    pxel[index].style.backgroundColor = 'white';
   }
 }
 
