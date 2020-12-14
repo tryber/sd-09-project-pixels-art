0f7046e05fdefd646f89d4b0cc50e7752fe93838
Pamela Anderson Sales <pamelasalesr@gmail.com>
2020-12-14 16:10:03 -0300

    Add max and min board size

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-window.onload = function() {
-  createPalette();
-  paintPalette();
-  createPixelBoard();
-  classSelected();
-  paintPixel();
-  clearPixelBoard();
-  randomPalette();
-}
-
 function createPalette() {
   const colorPalette = document.getElementById('color-palette');
   const numberOfColors = 4;
@@ -27,9 +17,14 @@ function paintPalette() {
   coloredSquare[3].style.backgroundColor = `rgb(${randomNumbers()}, ${randomNumbers()}, ${randomNumbers()})`;
 }
 
-function createPixelBoard() {
+function myFunction() {
+  var num = document.getElementById('board-size').value;
+  document.getElementById("demo").innerHTML = num;
+}
+
+function createPixelBoard(num) {
   const pixelBoard = document.getElementById('pixel-board');
-  const numberOfColumns = 5;
+  let numberOfColumns = num;
   for (let line = 0; line < numberOfColumns; line += 1) {
     const pixelLine = document.createElement('div');
     pixelLine.classList.add('pixel-line');
@@ -39,6 +34,40 @@ function createPixelBoard() {
       pixelLine.appendChild(pixel);
     }
     pixelBoard.appendChild(pixelLine);
+  }
+}
+createPixelBoard(5);
+
+function newBoard(){
+  let input = getInputValue();
+  if (input < 5) {
+    alert('Numero deve ser maior que 5!');
+    return;
+  }
+  if (document.getElementById('board-size').value.length == 0) {
+    alert('Board inválido!');
+    return;
+  }
+  deleteBoard();
+  createPixelBoard(input);
+  paintPixel();
+}
+
+function getInputValue() {
+  let input = parseInt(document.querySelector('input').value);
+  if (input < 5 && input > 0){
+    input = 5;
+  } else if (input > 50) {
+    input = 50;
+  }
+  return input;
+}
+
+function deleteBoard() {
+  let lines = document.getElementById('pixel-board').children;
+  let boardSize = lines.length;
+  for (let i = 0; i < boardSize; i++) {
+    lines[0].remove()
   }
 }
 
@@ -82,4 +111,14 @@ function randomNumbers() {
 function randomPalette() {
   const button = document.querySelector('#randomize-color')
   button.addEventListener('click', paintPalette);
+}
+
+window.onload = function() {
+  createPalette();
+  paintPalette();
+  createPixelBoard();
+  classSelected();
+  paintPixel();
+  clearPixelBoard();
+  randomPalette();
 }
