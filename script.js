ccf6d9c2366da5ef95594091dc9929360a0c2868
RodrigoNeoEx <draja.acc@gmail.com>
2020-12-14 12:07:32 -0300

    add button clear and function to clear all colors from pixel board

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function createBoardLine () {
     document.getElementById('pixel-board').appendChild(div);
   }
 }
-// setting colors to palette
+// setting colors to palette without equal colors
 function paletteColors() {
   let color1 = document.querySelectorAll('.color')[1];
   let color2 = document.querySelectorAll('.color')[2];
@@ -48,34 +48,59 @@ function choosePaletteColor(event) {
   document.querySelector('.selected').classList.remove('selected');  
   event.target.classList.add('selected');
 }
+//create buttom clear for pixel board color
+function buttonClear() {
+  let append = document.getElementById('color-palette');  
+  let divButton = document.createElement('div');  
+  append.appendChild(divButton);  
+  let button = document.createElement('button')
+  button.innerText = 'Clear Board';
+  button.id = 'clear-board'
+  append.appendChild(button)
+}
+// add function clear to button
+function clear() {
+  let clearPixel = document.querySelectorAll('.pixel');
+  for(let index = 0; index < clearPixel.length; index += 1){
+   clearPixel[index].removeAttribute('style');
+  }
+}
 // created "class mover" to choose colors
 function setChoosedColor() {
   let classMover = document.querySelector('.selected');
   let colorSelected = getComputedStyle(classMover);
   return colorSelected.backgroundColor;
 }
-//open pixel board to acept color by class mover
+//open pixel board to accept color by class mover
 function setPixelBoardColor(event) {
   let pixelColor = event.target;
   pixelColor.style.backgroundColor = setChoosedColor();
 }
-
+// function to append all listeners
 function listenersOnClick() {  
   let colorPalette = document.querySelector('#color-palette');
   colorPalette.addEventListener('click', choosePaletteColor);
   let pixelBoard = document.querySelector('#pixel-board');
   pixelBoard.addEventListener('click', setPixelBoardColor);
+  let buttonClearPixel = document.querySelector('#clear-board');
+  buttonClearPixel.addEventListener('click', clear)
 }
 
 window.onload = function(){  
   createPalette()
-  document.querySelector('#color-palette').firstElementChild.classList.add('selected') //aplica class selected ao primeiro elemento
-  document.querySelector('#color-palette').firstElementChild.classList.add('black') // aplica background ao primeiro elemento
+  document.querySelector('#color-palette').firstElementChild.classList.add('selected') //aply class selected to first child
+  document.querySelector('#color-palette').firstElementChild.classList.add('black') // aply class black to first child start with bg black
   createBoardLine()
   getRandomRgb()
   paletteColors()
   setChoosedColor()
-  listenersOnClick() // todos addListener dentro de uma unica função
+  buttonClear()
+  listenersOnClick()  
 }
 
-// criar botão para retornar a classe inicial de todos pixels assim alterando a cor novamente para branco
+// criar botão para aumentar a board
+
+
+
+
+
