e7ed1995d40745ceb25871d241e9e6adeb7b1502
Daniela Muller <danielamuller20@gmail.com>
2020-12-14 07:35:35 -0300

    Adição de botão que limpa os pixels

--- script.js
+++ script.js
@@ -1,53 +1,130 @@
-window.onload = function() {
-blackColor();
-}
 const reedColors = document.querySelectorAll('#color-palette>div');
 const boardOfPixel = document.querySelector('#pixel-board');
 const divPixels = document.createElement('div');
 
+// Classifica as cores da paleta
 function coloPalette() {
   reedColors[1].style.backgroundColor = 'red';
   reedColors[2].style.backgroundColor = 'green';
   reedColors[3].style.backgroundColor = 'blue';
 }
-coloPalette();
 
+// Cria a tabela de pixels
 function GeneratePixelBoard() {
   for (let index = 0; index < 25; index += 1) {
-    const divPixels = document.createElement('div');
-    divPixels.className = 'pixel';
-    boardOfPixel.appendChild(divPixels);
+    const divPixel = document.createElement('div');
+    divPixel.className = 'pixel';
+    boardOfPixel.appendChild(divPixel);
   }
 }
-  GeneratePixelBoard();
 
+// Seta a cor preta no primeiro item da paleta de cores
 function getBlackColor() {
-  let blackColor = document.querySelector('.selected');
-  let black = 'black';
-  blackColor.style.backgroundColor = black;
-  localStorage.setItem('backgroundColor', black)
-}
-getBlackColor();
-
-function getDivColor() {
-  divPixels.addEventListener('click', function() {
-    for (let index = 0; index < divPixels.length; index += 1) {
-      divPixels[index].style.backgroundColor = 'black';
-    }
-  });
+  const colorBlack = document.querySelector('.selected');
+  const black = 'black';
+  colorBlack.style.backgroundColor = black;
+  localStorage.setItem('backgroundColor', black);
 }
 
 function blackColor() {
-  let selected = document.querySelector('#color-palette .selected');
-  let pixelBoard = document.querySelectorAll('.pixel');
-  let color = selected.style.backgroundColor;
+  const selected = document.querySelectorAll('#color-palette .color')[0];
+  const pixelBoard = document.querySelectorAll('.pixel');
+  const color = selected.style.backgroundColor;
 
   for (let index = 0; index < pixelBoard.length; index += 1) {
-    let colorPixel = pixelBoard[index]
-    colorPixel.addEventListener('click', function(event) {
-      if(selected.classList.contains('selected')) {
+    const colorPixel = pixelBoard[index];
+    colorPixel.addEventListener('click', function (event) {
+      if (selected.classList.contains('selected')) {
         event.target.style.backgroundColor = color;
       }
     });
   }
 }
+blackColor();
+
+function colorOne() {
+  const selected = document.querySelectorAll('#color-palette .color')[1];
+  const pixelBoard = document.querySelectorAll('.pixel');
+  const color = selected.style.backgroundColor;
+
+  for (let index = 0; index < pixelBoard.length; index += 1) {
+    const colorPixel = pixelBoard[index];
+    colorPixel.addEventListener('click', function (event) {
+      if (selected.classList.contains('selected')) {
+        event.target.style.backgroundColor = color;
+      }
+    });
+  }
+}
+colorOne();
+
+function colorTwo() {
+  const selected = document.querySelectorAll('#color-palette .color')[2];
+  const pixelBoard = document.querySelectorAll('.pixel');
+  const color = selected.style.backgroundColor;
+
+  for (let index = 0; index < pixelBoard.length; index += 1) {
+    const colorPixel = pixelBoard[index];
+    colorPixel.addEventListener('click', function (event) {
+      if (selected.classList.contains('selected')) {
+        event.target.style.backgroundColor = color;
+      }
+    });
+  }
+}
+colorTwo();
+
+function colorThree() {
+  const selected = document.querySelectorAll('#color-palette .color')[3];
+  const pixelBoard = document.querySelectorAll('.pixel');
+  const color = selected.style.backgroundColor;
+
+  for (let index = 0; index < pixelBoard.length; index += 1) {
+    const colorPixel = pixelBoard[index];
+    colorPixel.addEventListener('click', function (event) {
+      if (selected.classList.contains('selected')) {
+        event.target.style.backgroundColor = color;
+      }
+    });
+  }
+}
+colorThree();
+
+function selectedDesactivate() {
+  let select = document.querySelector('#color-palette');
+  select.addEventListener('click', function(event) {
+      if (event.target.className === 'color') {
+      console.log(event.target.className = 'color selected');
+      } else if (event.target.className === 'color selected'){
+      console.log(event.target.className = 'color');
+      }
+    });
+}
+
+function clearBoard() {
+  const button = document.createElement('button');
+  button.id = 'clear-board';
+  button.innerHTML = 'Limpar quadro de pixel'
+  const buttonsSection = document.querySelector('#buttons');
+  buttonsSection.appendChild(button);
+  const pixelBoard = document.querySelectorAll('.pixel')
+
+  button.addEventListener('click', function () {
+    for (let index = 0; index < pixelBoard.length; index += 1) {
+      let colorsBoard = pixelBoard[index];
+      colorsBoard.style.backgroundColor = 'white';
+    }
+  });
+}
+
+window.onload = function () {
+  GeneratePixelBoard();
+  coloPalette();
+  getBlackColor();
+  blackColor();
+  colorOne();
+  colorTwo();
+  colorThree();
+  selectedDesactivate();
+  clearBoard();
+};
