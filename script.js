5cbd1218fb8b17e6791d501884a3797dc99bb609
Igor Marcos Riegel <igor.riegel@gmail.com>
2020-12-16 14:50:28 -0300

    Fix code climate parte I

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
 window.onload = function () {
   const myPixelBoard = document.querySelector('#pixel-board');
   const numberOfLinesAndColumns = 5;
-  let colorPalette = document.querySelector('#color-palette');
-  let selectedColor = document.getElementsByClassName('selected')[0].innerText;
-  let pixelBoard = document.querySelector('#pixel-board')
   let numberOfPixels = 0;
-
   // inicio das funções de renderização do pixel-board
   function createDiv(nameOfTheClass) {
     const myDiv = document.createElement('div');
     myDiv.className = nameOfTheClass;
     if (nameOfTheClass === 'pixel') {
       numberOfPixels += 1;
-      myDiv.id = `pixel-${numberOfPixels}`
+      myDiv.id = `pixel-${numberOfPixels}`;
     }
     return myDiv;
   }
@@ -31,48 +27,43 @@ window.onload = function () {
   }
   insertLine(numberOfLinesAndColumns);
   // fim das funções de renderização do pixel-board
-
-  // inicio das funções de remoção/inserção da classe selected e armazenamento da cor selecionada 
-  function clearTheSelection() {
-    selectedDiv = document.getElementsByClassName('selected')[0];
-    if (selectedDiv) {
-      selectedDiv.classList.remove('selected');
-    }
-  }
   
-  colorPalette.addEventListener('click', function(event){
-    let generatorOfEvent = event.target;
-    function insertSelectedClassAndReturnColor() {
-      let selectedColoredDiv = generatorOfEvent;
-      selectedColoredDiv.classList.add('selected');
-      selectedColor = selectedColoredDiv.innerText;
-    }
-    if (generatorOfEvent.id !== 'color-palette') {
-      clearTheSelection();
-      insertSelectedClassAndReturnColor()
-    }
-  } )
-  // fim das funções de remoção/inserção da classe selected e armazenamento da cor selecionada 
-
-  // inicio das funções de pintura da pixel-board
-  pixelBoard.addEventListener('click', function(event) {
-    let selectedId = event.target.id;
-    if (selectedId !== 'pixel-board') {
-      let selectedPixel = document.getElementById(selectedId);
-      selectedPixel.style.background = selectedColor;
-    }
-  })
-
-   
-  
-
-
-  // fim das funções de pintura da pixel-board
-
-  // inicio da função que colore randomicamente a paleta de cores
-  // function (){
-  //   document.getElementById("box2").style.color = "rgb(x,x,x)";
-  //   document.getElementById("box2").style.backgroundColor = "rgb(x,x,x)";
-  // }
-  // fim da função que colore randomicamente a paleta de cores
 };
+const colorPalette = document.querySelector('#color-palette');
+const pixelBoard = document.querySelector('#pixel-board');
+let selectedColor = document.getElementsByClassName('selected')[0].innerText;
+// inicio das funções de remoção/inserção da classe selected e armazenamento da cor selecionada
+function clearTheSelection() {
+  const selectedDiv = document.getElementsByClassName('selected')[0];
+  if (selectedDiv) {
+    selectedDiv.classList.remove('selected');
+  }
+}
+colorPalette.addEventListener('click', function (event){
+  const generatorOfEvent = event.target;
+  function insertSelectedClassAndReturnColor() {
+    let selectedColoredDiv = generatorOfEvent;
+    selectedColoredDiv.classList.add('selected');
+    selectedColor = selectedColoredDiv.innerText;
+  }
+  if (generatorOfEvent.id !== 'color-palette') {
+    clearTheSelection();
+    insertSelectedClassAndReturnColor();
+  }
+});
+// fim das funções de remoção/inserção da classe selected e armazenamento da cor selecionada 
+// inicio das funções de pintura da pixel-board
+pixelBoard.addEventListener('click', function (event) {
+  let selectedId = event.target.id;
+  if (selectedId !== 'pixel-board') {
+    let selectedPixel = document.getElementById(selectedId);
+    selectedPixel.style.background = selectedColor;
+  }
+});
+// fim das funções de pintura da pixel-board
+// inicio da função que colore randomicamente a paleta de cores
+// function (){
+//   document.getElementById("box2").style.color = "rgb(x,x,x)";
+//   document.getElementById("box2").style.backgroundColor = "rgb(x,x,x)";
+// }
+// fim da função que colore randomicamente a paleta de cores
