b6ddb0606f23a60b36643162dfbd7d0eb7373002
Gabriel Castro <kastro.gabriel17@gmail.com>
2020-12-16 22:57:16 -0400

    requisito 12:cores da paleta sendo geradas aleatoriamente

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-const blackColor = document.querySelector('#black');
-const redColor = document.querySelector('#red');
-const cyanColor = document.querySelector('#cyan');
-const yellowColor = document.querySelector('#yellow');
+const pixelBoard = document.querySelector('#pixel-board');
+console.log(pixelBoard)
 let bgColor = 'black';
 
+function randomColors() {
+  let rgbColor = 'rgb(';
+  rgbColor += Math.random() * 255;
+  rgbColor += Math.random() * 255;
+  rgbColor += Math.random() * 255;
+  return rgbColor += ')';
+}
+
 function palletColor() {
-  blackColor.style.backgroundColor = 'black';
-  redColor.style.backgroundColor = 'red';
-  cyanColor.style.backgroundColor = 'cyan';
-  yellowColor.style.backgroundColor = 'yellow';
+  document.querySelector('#black').style.backgroundColor = 'black';
+  document.querySelector('#colorRandom1').style.backgroundColor = randomColors();
+  document.querySelector('#colorRandom2').style.backgroundColor = randomColors();
+  document.querySelector('#colorRandom3').style.backgroundColor = randomColors();
+}
+
+function makingSquares() {
+  for (let index = 0; index < 25; index += 1) {
+    const creatSquare = document.createElement('div');
+    creatSquare.classList.add('pixel');
+    pixelBoard.appendChild(creatSquare);
+  }
 }
 
 function selectedClass(event) {
@@ -28,7 +42,6 @@ function changeBackgroundColor(event) {
 }
 
 function paintSquare() {
-  const pixelBoard = document.querySelector('#pixel-board');
   pixelBoard.addEventListener('click', changeBackgroundColor);
 }
 
@@ -50,7 +63,9 @@ function button() {
 
 window.onload = function () {
   palletColor();
+  makingSquares();
   clickColor();
   paintSquare();
   button();
+
 };
