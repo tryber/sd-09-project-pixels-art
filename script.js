9908b766c6e6e3766d0e7f20158556af98cc1e11
Igor Marcos Riegel <igor.riegel@gmail.com>
2020-12-17 03:15:33 -0300

    Correçao atendendo a validaçao do Cypress evaluator

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const myPixelBoard = document.querySelector('#pixel-board');
-let inputBoardSize = document.querySelector('#board-size');
+const inputBoardSize = document.querySelector('#board-size');
 let numberOfLinesAndColumns = inputBoardSize.value;
 let numberOfPixels = 0;
 // inicio das funções de renderização do pixel-board
@@ -9,21 +9,21 @@ function createDiv(nameOfTheClass) {
   if (nameOfTheClass === 'pixel') {
     numberOfPixels += 1;
     myDiv.id = `pixel-${numberOfPixels}`;
-  }
+  };
   return myDiv;
-}
+};
 function insertPixel(columnOfTheTable, lineOfTheLoop) {
   for (let index = 0; index < columnOfTheTable; index += 1) {
     const lineOfPixelBoard = document.querySelectorAll('.line')[lineOfTheLoop];
     lineOfPixelBoard.appendChild(createDiv('pixel'));
-  }
-}
+  };
+};
 function insertLine(lineOfTheTable) {
   for (let line = 0; line < lineOfTheTable; line += 1) {
     myPixelBoard.appendChild(createDiv('line'));
     insertPixel(numberOfLinesAndColumns, line);
-  }
-}
+  };
+};
 window.onload = function () {
   numberOfLinesAndColumns = 5;
   insertLine(numberOfLinesAndColumns);
@@ -36,26 +36,26 @@ function clearTheSelection() {
   const selectedDiv = document.getElementsByClassName('selected')[0];
   if (selectedDiv) {
     selectedDiv.classList.remove('selected');
-  }
-}
+  };
+};
 colorPalette.addEventListener('click', function (event) {
   const generatorOfEvent = event.target;
   function insertSelectedClassAndReturnColor() {
     const selectedColoredDiv = generatorOfEvent;
     selectedColoredDiv.classList.add('selected');
     selectedColor = selectedColoredDiv.innerText;
-  }
+  };
   if (generatorOfEvent.id !== 'color-palette') {
     clearTheSelection();
     insertSelectedClassAndReturnColor();
-  }
+  };
 });
 pixelBoard.addEventListener('click', function (event) {
   const selectedId = event.target.id;
   if (selectedId !== 'pixel-board') {
     const selectedPixel = document.getElementById(selectedId);
     selectedPixel.style.background = selectedColor;
-  }
+  };
 });
 const buttonClearAll = document.querySelector('#clear-board');
 buttonClearAll.addEventListener('click', function () {
@@ -63,26 +63,27 @@ buttonClearAll.addEventListener('click', function () {
   const pixelsBoard = document.getElementsByClassName('pixel');
   for (let element = 0; element < pixelsBoard.length; element += 1) {
     pixelsBoard[element].style.background = whiteColor;
-  }
+  };
 });
 const buttonGenerateCustomPixelBoard = document.querySelector('#generate-board');
 buttonGenerateCustomPixelBoard.addEventListener('click', function () {
-  let linesOfBoard = document.getElementsByClassName('line');
+  const linesOfBoard = document.getElementsByClassName('line');
   numberOfPixels = 0;
   numberOfLinesAndColumns = inputBoardSize.value;
   if (numberOfLinesAndColumns === '') {
     alert('Board inválido!');
-  }
-  if (parseInt(inputBoardSize.min) > numberOfLinesAndColumns) {
+  } else if (5 > numberOfLinesAndColumns) {
     numberOfLinesAndColumns = 5;
     alert('Você escolheu um numero muito baixo!!\nMudamos para 5.');
-  }
-  if (parseInt(inputBoardSize.max) < numberOfLinesAndColumns) {
+  } else if (parseInt(inputBoardSize.max, 10) < numberOfLinesAndColumns) {
     numberOfLinesAndColumns = 50;
     alert('Você escolheu um numero muito alto!!\nMudamos para 50.');
-  }
+  };
   while (linesOfBoard.length > 0) {
+    if (numberOfLinesAndColumns === '') {
+      break
+    };
     linesOfBoard[0].remove();
-  }
+  };
   insertLine(numberOfLinesAndColumns);
-})
+});
