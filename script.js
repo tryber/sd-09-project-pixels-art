1285356422a871aaf333f1d2cbb05dfde9edf360
Deusivan Gomes <gomesdeusivan@gmail.com>
2020-12-18 19:50:01 -0300

    8

--- script.js
+++ script.js
@@ -1,50 +1,27 @@
-const blackBox = document.querySelector('.black');
-const redBox = document.querySelector('.red');
-const blueBox = document.querySelector('.blue');
-const greenBox = document.querySelector('.green');
-const blankBox = document.getElementsByClassName('.pixel');
-const selectedColor = document.querySelector('.selected');
-const paleta = document.getElementsByClassName('color');
-
-// var paletaFilhos = document.getElementsByClassName("color");
-
-// for (let i = 0; i < paletaFilhos.length; i++) {
-//     paletaFilhos[i].addEventListener('click', function(){
-//         if(paletaFilhos[i].classList[2] != "selected") {
-//             var selectedAtual = document.getElementsByClassName("selected");
-//             selectedAtual[0].className = selectedAtual[0].className.replace("selected", "");
-//             this.classList += " selected";
-//         }
-//     })
-    
-// }
-
-
-
+var selectedColor = document.querySelector('.selected');
+const colors = document.getElementsByClassName('color');
+const palette = document.getElementById('color-palette');
+const lista = document.getElementById('pixel-board');
+const itens = lista.getElementsByClassName('pixel')
 
 function transferSelected(originaryEvent) {
-
-    for(let i = 0; i < paleta.length; i += 1) {
-        
-        paleta[i].classList.remove('selected')
-        if (originaryEvent.target === paleta[i] && paleta[i].classList[2] != 'selected') {
+    for(let i = 0; i < colors.length; i += 1) {
+        colors[i].classList.remove('selected')
+        if (originaryEvent.target === colors[i] && colors[i].classList[2] != 'selected') {
             document.getElementsByClassName('color')[i].className += ' selected';
+            selectedColor = document.querySelector('.selected')
         }
     }
 }
 
-blackBox.addEventListener('click', transferSelected)
-redBox.addEventListener('click', transferSelected)
-blueBox.addEventListener('click', transferSelected)
-greenBox.addEventListener('click', transferSelected)
+palette.addEventListener('click', transferSelected)
 
-// function paint() {
-//     blankBox.addEventListener('click', blankBox.style.backgroundColor = 'black')
-// }
+function paint(originaryEvent) {
+    for(i = 0; i < itens.length; i += 1) {
+        if (originaryEvent.target === itens[i]) {
+            itens[i].style.backgroundColor = getComputedStyle(selectedColor).backgroundColor
+        }
+    }
+}
 
-// function changeColor() {
-//     blankBox.style.backgroundColor = 'black';
-// }
-
-// document.addEventListener('load', paint)
-// blankBox.addEventListener('click', )
+lista.addEventListener('click', paint)
