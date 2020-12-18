9011818cb468b94a604362436a0500af708139d0
Marina Alane <marina.alane@outlook.com>
2020-12-18 11:24:44 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
+window.onload = function() {
+  Aocarregar()
+
+const preta = document.getElementsByClassName('preta');
+const azul = document.getElementsByClassName('azul');
+const amarelo = document.getElementsByClassName('amarelo');
+const vermelho = document.getElementsByClassName('vermelho');
+
+
 function Aocarregar() {
 
   const primeiraCorSelecionada = document.getElementsByClassName('color')[0];
    primeiraCorSelecionada.classList.add('selected');
     }
-    window.onload = function() {
-      Aocarregar();
-    }
+
+    
+
+ let selecionaCores = document.querySelectorAll('.color');
+  for (let i = 0; i < selecionaCores.length; i += 1){
+      selecionaCores[i].addEventListener('click', function() {
+      selecionaCores[i].classList.toggle('selected')
+      
+    })
+  }
+
+}
