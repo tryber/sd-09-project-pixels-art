f802f17872ce063868d351d0bcd3b680634c8931
Deusivan Gomes <gomesdeusivan@gmail.com>
2020-12-18 20:20:56 -0300

    9

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 var selectedColor = document.querySelector('.selected');
 const colors = document.getElementsByClassName('color');
 const palette = document.getElementById('color-palette');
-const lista = document.getElementById('pixel-board');
-const itens = lista.getElementsByClassName('pixel')
+const list = document.getElementById('pixel-board');
+const itens = list.getElementsByClassName('pixel')
+const clearButton = document.getElementById('clear-board')
 
 function transferSelected(originaryEvent) {
     for(let i = 0; i < colors.length; i += 1) {
@@ -24,4 +25,11 @@ function paint(originaryEvent) {
     }
 }
 
-lista.addEventListener('click', paint)
+list.addEventListener('click', paint)
+
+function clear() {
+    for(i = 0; i < itens.length; i += 1) {
+        itens[i].style.backgroundColor = 'white'
+    }
+}
+clearButton.addEventListener('click', clear)
