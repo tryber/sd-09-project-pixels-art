31e5b04cc835d967222a3f8a2e1af5b6b7dee405
Marina Alane <marina.alane@outlook.com>
2020-12-18 13:49:25 -0300

    corrigindo requisito 7

--- script.js
+++ script.js
@@ -6,21 +6,19 @@ const azul = document.getElementsByClassName('azul');
 const amarelo = document.getElementsByClassName('amarelo');
 const vermelho = document.getElementsByClassName('vermelho');
 
-
 function Aocarregar() {
-
   const primeiraCorSelecionada = document.getElementsByClassName('color')[0];
    primeiraCorSelecionada.classList.add('selected');
     }
 
     
-
- let selecionaCores = document.querySelectorAll('.color');
-  for (let i = 0; i < selecionaCores.length; i += 1){
-      selecionaCores[i].addEventListener('click', function() {
-      selecionaCores[i].classList.toggle('selected')
-      
+    let corNova = document.querySelectorAll('.color');
+  for (let i = 0; i < corNova.length; i += 1){
+      corNova[i].addEventListener('click', function() {
+        let corAtual= document.querySelector('.selected');
+        corAtual.classList.remove('selected')
+      corNova[i].classList.toggle('selected')
+      console.log(corAtual)
     })
-  }
 
-}
+  }}
