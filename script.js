c00c7efc518d6f87386f2704f3a7acfd263b173b
Daniela Muller <danielamuller20@gmail.com>
2020-12-19 15:51:29 -0300

    Tamanho do pixel definido pelo usuário

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const reedColors = document.querySelectorAll('#color-palette>div');
 const boardOfPixel = document.querySelector('#pixel-board');
+const input = document.createElement('input');
+input.type = 'number';
+input.id = 'board-size';
+input.className = 'board-size';
+const buttonPixel = document.createElement('button');
+buttonPixel.innerHTML = 'VQV';
+buttonPixel.id = 'generate-board';
+buttonPixel.className = 'button';
+const sectionsOfButtons = document.querySelector('#buttons');
+sectionsOfButtons.appendChild(input);
+sectionsOfButtons.appendChild(buttonPixel);
+const buttonGeratePixel = document.querySelector('#generate-board');
+
 
 // Classifica as cores da paleta
 function coloPalette() {
@@ -8,14 +21,39 @@ function coloPalette() {
   reedColors[3].style.backgroundColor = 'blue';
 }
 
+function removeAllChildFromPixelBoard() {
+    const pix = document.querySelector('#pixel-board');
+    while (pix.hasChildNodes()) {
+      pix.removeChild(pix.firstChild);
+    }
+}
+removeAllChildFromPixelBoard();
+
+
+function getInputValue() {
+    let inputPixels = document.querySelector('#board-size').value;
+    inputPixels = parseInt(inputPixels);
+    if (inputPixels.innerHTML < 0 || isNaN(inputPixels)) {
+      alert('Board inválido!');
+    } else if (inputPixels < 5) {
+      inputPixels = 5;
+    } else if (inputPixels > 50) {
+      inputPixels = 50;
+    }
+    return inputPixels;
+}
+
 // Cria a tabela de pixels
-function GeneratePixelBoard() {
-  for (let index = 0; index < 25; index += 1) {
+function GeneratePixelBoard(number) {
+  let pixels = number * number
+  for (let index = 0; index < pixels; index += 1) {
     const divPixel = document.createElement('div');
     divPixel.className = 'pixel';
     boardOfPixel.appendChild(divPixel);
+    boardOfPixel.style.gridTemplateColumns = `repeat(${number}, auto)`;
   }
 }
+GeneratePixelBoard(5);
 
 // Seta a cor preta no primeiro item da paleta de cores
 function getBlackColor() {
@@ -27,7 +65,7 @@ function getBlackColor() {
 
 function blackColor() {
   const selected = document.querySelectorAll('#color-palette .color')[0];
-  const pixelBoard = document.querySelectorAll('.pixel');
+  const pixelBoard = document.querySelectorAll('#pixel-board>.pixel');
   const color = selected.style.backgroundColor;
 
   for (let index = 0; index < pixelBoard.length; index += 1) {
@@ -91,7 +129,6 @@ colorThree();
 
 function selectedActivate() {
   const select = document.querySelector('#color-palette');
-
     select.addEventListener('click', function (event) {
       if (event.target.className === 'color') {
         event.target.className = 'color selected';
@@ -99,23 +136,24 @@ function selectedActivate() {
     });
 }
 
-//SOURCE:
-//https://clubmate.fi/remove-a-class-name-from-multiple-elements-with-pure-javascript
+// SOURCE:
+// https://clubmate.fi/remove-a-class-name-from-multiple-elements-with-pure-javascript
 
-  function selectedDesactivate() {
-    const pix = document.querySelectorAll('.color');
-    const select = document.querySelector('#color-palette');
-    select.addEventListener('click', function () {
-      for (let index = 0; index < pix.length; index += 1) {
-        pix[index].classList.remove('selected');
-      }
-    });
-  }
+function selectedDesactivate() {
+  const pix = document.querySelectorAll('.color');
+  const select = document.querySelector('#color-palette');
+  select.addEventListener('click', function () {
+    for (let index = 0; index < pix.length; index += 1) {
+      pix[index].classList.remove('selected');
+    }
+  });
+}
 
 function clearBoard() {
   const button = document.createElement('button');
   button.id = 'clear-board';
-  button.innerHTML = 'Limpar quadro de pixel';
+  button.className = 'button'
+  button.innerHTML = 'Clear Board';
   const buttonsSection = document.querySelector('#buttons');
   buttonsSection.appendChild(button);
   const pixelBoard = document.querySelectorAll('.pixel');
@@ -127,6 +165,17 @@ function clearBoard() {
     }
   });
 }
+
+function generateUserBoard() {
+  let input = getInputValue();
+  removeAllChildFromPixelBoard();
+  GeneratePixelBoard(input);
+  blackColor();
+  colorOne();
+  colorTwo();
+  colorThree();
+}
+buttonPixel.addEventListener('click', generateUserBoard);
 
 window.onload = function () {
   GeneratePixelBoard();
