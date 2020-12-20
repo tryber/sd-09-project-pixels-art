618fd05071cd04b627e6d1be55f90859a84aaa1e
Jhonatan Arguello <jhonatan.arguello@gmail.com>
2020-12-20 17:51:47 -0300

    Atualizando bordas da paleta de cor T-T.

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // Selecionar Cor
 function colorSelected() {
-  const colorSelected = document.querySelector("#color-palette")
+  const colorOfpalette = document.querySelector('#color-palette')
 
-  colorSelected.addEventListener('click', function(event) {
+  colorOfpalette.addEventListener('click', function(event) {
     // Remover ".selected" anterior
-    let initialColor = document.querySelector(".selected")
-    initialColor.classList.remove("selected")
+    let initialColor = document.querySelector('.selected');
+    initialColor.classList.remove('selected');
 
     // Colocar o ".selected" na cor clicada
-    let newSelected = event.target
-    newSelected.classList.add("selected")
+    let newSelected = event.target;
+    newSelected.classList.add('selected');
 
   })
 }
@@ -18,21 +18,17 @@ colorSelected()
 
 // Pintar Pixel
 function paintPixel() {
-  const color1 = "black"
-  const color2 = "red"
-  const color3 = "green"
-  const color4 = "blue"
+  const color1 = 'black';
+  const color2 = 'red';
+  const color3 = 'green';
+  const color4 = 'blue';
 
-  
-
-  let selectedPixel = document.querySelector("#pixel-board")
+  let selectedPixel = document.querySelector('#pixel-board')
   selectedPixel.addEventListener('click', function(event) {
     let selectedColor = document.querySelector('.selected').classList[1]
-    
+
     let pixel = event.target
     pixel.className = `pixel ${selectedColor}`
   })
 }
 paintPixel()
-
-
