146702550a623b8324ed02eb1d710ff06f6076fa
Marina Alane <marina.alane@outlook.com>
2020-12-19 22:03:39 -0300

    ajustando code climate e corrigindo requisito7

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-function Aocarregar () {
+window.onload = function() {
+	Aocarregar()
+	pintandoPixel()
+
+function Aocarregar() {
   const primeiraCorSelecionada = document.getElementsByClassName('color')[0];
    primeiraCorSelecionada.classList.add('selected');
     }
-    let corNova = document.querySelectorAll('.color');
-  for (let i = 0; i < corNova.length; i += 1){;
-    corNova[i].addEventListener('click', function () {;
-    let corAtual= document.querySelector('.selected');
-    corAtual.classList.remove('selected');
-    corNova[i].classList.toggle('selected');
-    console.log(corAtual);
-    })
-  }
-        function pintandoPixel () {
-let pixel = document.getElementsByClassName('pixel');
-for (let i = 0 ; i < pixel.length; i += 1) {;
-  pixel[i].addEventListener('click', function(event) {;
-  let corSelecionada = window.getComputedStyle(document.querySelector('.selected'));
-  let corBg = corSelecionada.backgroundColor;
-  event.target.style.backgroundColor = corBg;
+    
+    const corNova = document.querySelectorAll('.color');
+  for (let i = 0; i < corNova.length; i += 1){
+      corNova[i].addEventListener('click', function() {
+        const corAtual= document.querySelector('.selected');
+        corAtual.classList.remove('selected')
+      corNova[i].classList.toggle('selected')
+	})
 }
-)}
-}
-
-window.onload = function () {
- Aocarregar ();
- pintandoPixel ();
-  
+	function pintandoPixel (){
+	const pixel = document.getElementsByClassName('pixel');
+	
+	for (let i = 0 ; i < pixel.length; i += 1) {
+		pixel[i].addEventListener('click', function(event) {
+		const corSelecionada = window.getComputedStyle(document.querySelector('.selected'));
+		const corBg = corSelecionada.backgroundColor
+		event.target.style.backgroundColor = corBg
+	}
+	
+		)}
+	}
 }
