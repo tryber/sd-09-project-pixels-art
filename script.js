ec3cae12aa2511cbc99031b3a2f040caf2cdebf9
Marina Alane <marina.alane@outlook.com>
2020-12-19 21:36:48 -0300

    ajusta para o code climate

--- script.js
+++ script.js
@@ -1,35 +1,29 @@
-window.onload = function() {
-  Aocarregar()
-  pintandoPixel()
-
-
-  
-function Aocarregar() {
+function Aocarregar () {
   const primeiraCorSelecionada = document.getElementsByClassName('color')[0];
    primeiraCorSelecionada.classList.add('selected');
     }
-
-    
     let corNova = document.querySelectorAll('.color');
-  for (let i = 0; i < corNova.length; i += 1){
-      corNova[i].addEventListener('click', function() {
-        let corAtual= document.querySelector('.selected');
-        corAtual.classList.remove('selected')
-      corNova[i].classList.toggle('selected')
-      console.log(corAtual)
+  for (let i = 0; i < corNova.length; i += 1){;
+    corNova[i].addEventListener('click', function () {;
+    let corAtual= document.querySelector('.selected');
+    corAtual.classList.remove('selected');
+    corNova[i].classList.toggle('selected');
+    console.log(corAtual);
     })
-
   }
-function pintandoPixel (){
+        function pintandoPixel () {
 let pixel = document.getElementsByClassName('pixel');
-
-for (let i = 0 ; i < pixel.length; i += 1) {
-  pixel[i].addEventListener('click', function(event) {
+for (let i = 0 ; i < pixel.length; i += 1) {;
+  pixel[i].addEventListener('click', function(event) {;
   let corSelecionada = window.getComputedStyle(document.querySelector('.selected'));
-  let corBg = corSelecionada.backgroundColor
-  event.target.style.backgroundColor = corBg
+  let corBg = corSelecionada.backgroundColor;
+  event.target.style.backgroundColor = corBg;
+}
+)}
 }
 
-  )}
-}
+window.onload = function () {
+ Aocarregar ();
+ pintandoPixel ();
+  
 }
