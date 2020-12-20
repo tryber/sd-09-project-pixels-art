f75e741326c6c999250f471c40667197d2afbfef
Andrechiapetta <andreluizchiapetta@gmail.com>
2020-12-20 20:06:53 -0300

    Exercicio 8

--- script.js
+++ script.js
@@ -15,13 +15,13 @@ function selectColor() {
   const colorPalette = document.getElementById('color-palette');
   const currentSelectedPixel = document.getElementsByClassName('selected');
   let selectedColor = currentSelectedPixel[0].id;
-  localStorage.setItem("color", selectedColor);
+  localStorage.setItem('color', selectedColor);
   colorPalette.addEventListener('click', function (selectedPixel) {
     if (selectedPixel.target.className !== 'center') {
       currentSelectedPixel[0].classList.remove('selected');
       selectedPixel.target.classList.add('selected');
       selectedColor = currentSelectedPixel[0].id;
-      localStorage.setItem("color", selectedColor);
+      localStorage.setItem('color', selectedColor);
     }
   });
 }
