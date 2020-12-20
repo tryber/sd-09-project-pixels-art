3f811097d777de4ab430a39e6072bd0220a54955
Andrechiapetta <andreluizchiapetta@gmail.com>
2020-12-20 20:04:04 -0300

    Exercicio 8

--- script.js
+++ script.js
@@ -14,14 +14,14 @@ function generatePixelFrame(size) {
 function selectColor() {
   const colorPalette = document.getElementById('color-palette');
   const currentSelectedPixel = document.getElementsByClassName('selected');
-  var selectedColor = currentSelectedPixel[0].id;
-  localStorage.setItem("color", selectedColor); 
+  let selectedColor = currentSelectedPixel[0].id;
+  localStorage.setItem("color", selectedColor);
   colorPalette.addEventListener('click', function (selectedPixel) {
     if (selectedPixel.target.className !== 'center') {
       currentSelectedPixel[0].classList.remove('selected');
       selectedPixel.target.classList.add('selected');
       selectedColor = currentSelectedPixel[0].id;
-      localStorage.setItem("color", selectedColor);       
+      localStorage.setItem("color", selectedColor);
     }
   });
 }
@@ -30,8 +30,7 @@ function paintPixel() {
   const pixelBoard = document.getElementById('pixel-board');
   pixelBoard.addEventListener('click', function (selectedPixel) {
     if (selectedPixel.target.id !== 'pixel-board') {
-      let color = localStorage.getItem('color');
-      console.log("cor", color);
+      const color = localStorage.getItem('color');
       selectedPixel.target.className = 'pixel';
       selectedPixel.target.classList.add(color);
     }
