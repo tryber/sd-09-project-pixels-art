70459fd431371b0296acc58e6f093c07cdad8883
Fabio Venancio Almeida <fvalmeida.@gmail.com>
2020-12-21 16:50:38 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,5 @@
+
+document.getElementsByClassName('color')[0].style.backgroundColor="black";
+document.getElementsByClassName('color')[1].style.backgroundColor="red";
+document.getElementsByClassName('color')[2].style.backgroundColor="green";
+document.getElementsByClassName('color')[3].style.backgroundColor="blue";
