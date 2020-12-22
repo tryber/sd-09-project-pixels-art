f57ca072699fd0be12714de6926c994022651978
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2020-12-22 14:33:24 -0300

    Corrections and fixes - CodeClimate

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function addPixels(size) {
   for (let index = 1; index <= size; index += 1) {
-    let divPrincipal = document.createElement('div');
-    let pixelBoard = document.getElementById('pixel-board')
+    const divPrincipal = document.createElement('div');
+    const pixelBoard = document.getElementById('pixel-board');
     divPrincipal.className = 'pixel';
     pixelBoard.appendChild(divPrincipal);
     for (let j = 1; j <= size; j += 1) {
-      let divFilha = document.createElement('div');
-      divFilha.className = 'pixel'
+      const divFilha = document.createElement('div');
+      divFilha.className = 'pixel';
       divPrincipal.appendChild(divFilha);
     }
   }
@@ -15,8 +15,8 @@ function addPixels(size) {
 addPixels(5);
 
 function defineBoardDimensions(size) {
-  document.getElementById('pixel-board').style.width = '' + size * 40 + ' px';
-  document.getElementById('pixel-board').style.height = '' + size * 40 + ' px';
+  document.getElementById('pixel-board').style.width = `${size * 40} px`;
+  document.getElementById('pixel-board').style.height = `${size * 40} px`;
 }
 
 defineBoardDimensions(5);
