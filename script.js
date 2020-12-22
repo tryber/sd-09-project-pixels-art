6595e0eea0e8482e9cf09c7c99f8b19836772848
Bernardo Smith <bernardostech@gmail.com>
2020-12-22 01:46:05 -0500

    ajustes13

--- script.js
+++ script.js
@@ -1,44 +1,41 @@
-
- 
-function opcoesCores(){
+function opcoesCores() {
   let linha = ['black', 'red', 'blue', 'yellow'];
   let colorPallette = document.querySelector('.tableColor');
   for (let key = 0; key < linha.length; key += 1) {
-    
+
     let linhaItens = linha[key];
     const colunaPallete = document.createElement('th');
     colunaPallete.className = 'color';
     colunaPallete.id = linhaItens;
-    colorPallette.appendChild(colunaPallete);    
+    colorPallette.appendChild(colunaPallete);
   }
 }
 opcoesCores();
 
 
 
-function criarTR(){
+function criarTR() {
   let paletta = document.querySelector('.pixel-board');
-  for(let i=0; i<5; i+=1){    
+  for (let i = 0; i < 5; i += 1) {
     let linhaPaleta = document.createElement('tr');
     linhaPaleta.id = 'pixel-board';
     paletta.appendChild(linhaPaleta);
-    for (let j=0; j<5; j+=1) {
+    for (let j = 0; j < 5; j += 1) {
       //let linhasPixel= document.getElementById('pixelBoard'); 
       let pixelIndivid = document.createElement('td');
       pixelIndivid.className = 'pixel';
-     
+
       linhaPaleta.appendChild(pixelIndivid);
-      
+
     }
   }
-}  
+}
 criarTR();
-  
+
 
 windows.onload = colorDest;
 
-function colorDest(){
+function colorDest() {
 
   var colorSelected = 'black';
-} 
-
+}
