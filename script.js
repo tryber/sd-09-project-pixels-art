1fe52b88e99d84f898679b5e4ee934c6dfda384c
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2020-12-22 14:26:35 -0300

    Corrections and fixes - requirements 4 & 5

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 function addPixels(size) {
   for (let index = 1; index <= size; index += 1) {
-    const pixelBoard = document.getElementById('pixel-board');
-    const div = document.createElement('div');
-    div.className = 'pixel';
-    pixelBoard.appendChild(div)
+    let divPrincipal = document.createElement('div');
+    let pixelBoard = document.getElementById('pixel-board')
+    divPrincipal.className = 'pixel';
+    pixelBoard.appendChild(divPrincipal);
+    for (let j = 1; j <= size; j += 1) {
+      let divFilha = document.createElement('div');
+      divFilha.className = 'pixel'
+      divPrincipal.appendChild(divFilha);
+    }
   }
 }
 
-addPixels(25);
+addPixels(5);
+
+function defineBoardDimensions(size) {
+  document.getElementById('pixel-board').style.width = '' + size * 40 + ' px';
+  document.getElementById('pixel-board').style.height = '' + size * 40 + ' px';
+}
+
+defineBoardDimensions(5);
