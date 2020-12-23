e9495e8025a5e5285694ea306e975effcc673157
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2020-12-23 12:52:41 -0300

    CodeClimate fixes

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 window.onload = function() {
-  let classPixel = document.getElementsByClassName('pixel');
+  const classPixel = document.getElementsByClassName('pixel');
 
   for (let index = 0; index < classPixel.length; index += 1) {
     classPixel[index].style.backgroundColor = 'white';
   }
 
-  let colorBlack = document.getElementsByClassName('color black')[0];
+  const colorBlack = document.getElementsByClassName('color black')[0];
   colorBlack.className = 'color black selected';
-}
+
+};
 
 function addPixels(size) {
   for (let index = 1; index <= size; index += 1) {
     const divPrincipal = document.createElement('div');
-    const pixelBoard = document.getElementById('pixel-board');
+    const pixelBoard = document.getElementsByClassName('pixel-board')[0];
     pixelBoard.appendChild(divPrincipal);
     for (let j = 1; j <= size; j += 1) {
       const divFilha = document.createElement('div');
