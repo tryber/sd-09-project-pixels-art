80e4cc5eccd7657acfaa4b87cdf39fbb725e0fd6
Micaela Alves <micaela.alves93@gmail.com>
2020-12-24 08:33:25 -0300

    Requisito 6

---
+++ script.js
@@ -0,0 +1,11 @@
+/*### **6 - Ao carregar a página, a cor preta da paleta já deve estar selecionada para pintar os pixels.**
+
+- **As seguintes verificações serão feitas:**
+    - [ ]  O elemento da cor preta deve possuir, inicialmente, a `classe` `selected`;
+    - [ ]  Note que o elemento que deverá receber a classe `selected` deve ser um dos elementos que possuem a classe `color`, como especificado no **requisito 2**.*/
+
+
+function load() {
+  console.log("Evento de carregamento detectado!");
+}
+window.onload = load;
