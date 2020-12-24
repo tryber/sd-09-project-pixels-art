7740936566541018b5a5753232bfdea02bba327f
Micaela Alves <micaela.alves93@gmail.com>
2020-12-24 12:40:50 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-
+function removeSelect(){
+  let colorPalet = document.querySelectorAll('.color');
+  for (let index in colorPalet){
+    if (colorPalet[index].className === 'color selected'){
+      colorPalet[index].className = 'color';
+    }
+  }
+}
+function setColor (){
+  let colorPalet = document.querySelectorAll('.color');
+  for (let index in colorPalet){
+    colorPalet[index].addEventListener('click', function(event) {
+      removeSelect();
+      event.target.className = 'color selected';
+    });
+  }
+}
+setColor();
