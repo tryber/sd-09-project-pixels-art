f7a950fa5ef7ebba45368ab2690c837404fd45e9
Bernardo Smith <bernardostech@gmail.com>
2020-12-23 23:46:28 -0500

    criacao da mudanca de cores da paletta

--- script.js
+++ script.js
@@ -27,7 +27,8 @@ function criarTR(param1, param2) {
     }
   }
 }
-criarTR(5, 5);
+
+criarTR(5,5);
 
 let limpar = document.getElementById("clear-board");
 limpar.addEventListener('click', clearBoard)
@@ -41,10 +42,34 @@ function clearBoard() {
   }
 }
 
+let tamanho=document.getElementById("generate-board");
+tamanho.addEventListener('click',mudarTamanho);
+
+function mudarTamanho(){
+  let coluna = document.getElementById("alteraPalettaColuna");
+  let linha = document.getElementById("board-size");
+  let c =Num(coluna.value);
+  let l =  Num(linha.value);
+  alert(c);
+  alert(l);   
+  criarTR(`${c}`,`${l}`);
+}
+
 function selectColor() {
   let select = document.querySelector('.color');
   if (select.id == 'black'){
     select.className = ' color selected';
   }
 }
+let mudaCor = document.getElementById('alteraCor');
+mudaCor = addEventListener('click',function(){
+  let vermelho = document.getElementById('red');
+  let azul = document.getElementById('blue');
+  let amarelo = document.getElementById('yellow');
+  vermelho.style.backgroundColor = 'rgb(' + Math.floor(Math.random(255) * (255+1)) + ',' + Math.floor(Math.random(255) * (255+1)) + ',' + Math.floor(Math.random(255) * (255+1)) + ')' ;
+  azul.style.backgroundColor = 'rgb(' + Math.floor(Math.random(255) * (255+1)) + ',' + Math.floor(Math.random(255) * (255+1)) + ',' + Math.floor(Math.random(255) * (255+1)) + ')' ;
+  amarelo.style.backgroundColor = 'rgb(' + Math.floor(Math.random(255) * (255+1)) + ',' + Math.floor(Math.random(255) * (255+1)) + ',' + Math.floor(Math.random(255) * (255+1)) + ')' ;
+
+})
+
 window.onload = selectColor();
