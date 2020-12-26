5817bdf444f17527a0464183a1fad0ff40dd0af7
Mathaus <mathausannibal@gmail.com>
2020-12-26 14:25:19 -0300

    adiciona item 9 botao de limpar

--- script.js
+++ script.js
@@ -6,8 +6,15 @@ document.addEventListener('click', function (event) {
         document.getElementsByClassName('selected').item(0).classList.remove('selected');
         event.target.classList.toggle('selected')
     }
-    
+
     if (event.target.classList.contains('pixel')) {
-        event.target.classList.toggle(corAtual);
+        event.target.style.backgroundColor = corAtual;
     }
 }, false);
+
+function limparTabela(){
+    var limpar = document.querySelectorAll('.pixel')
+    limpar.forEach(element => {
+        element.style.backgroundColor = 'white'
+    });
+}
