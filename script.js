401345d47699dab09071012e7ad1971b2be5d339
Micaela Alves <micaela.alves93@gmail.com>
2020-12-26 09:45:56 -0300

    Exercicio 8 - codeClimate

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-function generateColorPallete (){
-  let palette = document.getElementsByClassName('color');
+function generateColorPallete() {
+  const palette = document.getElementsByClassName('color');
   palette[0].style.backgroundColor = 'rgb(0, 0, 0)';
-  for(let index = 1; index < palette.length; index += 1) {
-    let number1 = Math.floor(Math.random() * 256)
-    let number2 = Math.floor(Math.random() * 256)
-    let number3 = Math.floor(Math.random() * 256)
-    palette[index].style.backgroundColor = 'rgb(' + number1 + ', ' + number2 + ', ' + number3 +')';
+  for (let index = 1; index < palette.length; index += 1) {
+    const number1 = Math.floor(Math.random() * 256);
+    const number2 = Math.floor(Math.random() * 256);
+    const number3 = Math.floor(Math.random() * 256);
+    palette[index].style.backgroundColor = `rgb(${number1}, ${number2}, ${number3})`;
   }
 }
-generateColorPallete()
+generateColorPallete();
 function removeSelect() {
   const colorPalet = document.querySelectorAll('.color');
   for (let index = 0; index < colorPalet.length; index += 1) {
@@ -17,27 +17,25 @@ function removeSelect() {
     }
   }
 }
-var colored = 0
 function setColor() {
   const colorPalet = document.querySelectorAll('.color');
   for (let index = 0; index < colorPalet.length; index += 1) {
     colorPalet[index].addEventListener('click', function (event) {
       removeSelect();
       event.target.classList.add('selected');
-      colored = event.target.style.color;
     });
   }
 }
 setColor();
 
 function applyColor() {
-  let pix = document.getElementsByClassName('pixel');
-  let color = document.getElementsByClassName('selected')[0].style.backgroundColor
+  const pix = document.getElementsByClassName('pixel');
+  let color = document.getElementsByClassName('selected')[0].style.backgroundColor;
   for (let index = 0; index < pix.length; index += 1) {
     pix[index].addEventListener('click', function (event) {
-      color = document.getElementsByClassName('selected')[0].style.backgroundColor
-      pix[index].style.backgroundColor = color
+      color = document.getElementsByClassName('selected')[0].style.backgroundColor;
+      pix[index].style.backgroundColor = color;
     });
   }
 }
-applyColor()
+applyColor();
