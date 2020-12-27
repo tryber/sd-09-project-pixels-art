2964cf5fc1c3b8afbca4d12029772229073f83b8
Luciano_Lodi <lrlodi@gmail.com>
2020-12-27 14:25:13 -0300

    Requisito 11. Adicionar a paleta customizavel

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-
-window.onload = function () {
-	let pixelRow = document.getElementById('pixel-board');
-	let canvasHeight = 5;
-  let canvasWidth = 5;   
-  let chosenColor = 'black';
-  let clearPixel = 'white';
-
-	function createRows() {
-    for(let i = 0; i < canvasHeight; i += 1) {
+	function generateBoard(x) {
+    for(let i = 0; i < x; i += 1) {
       let row = document.createElement('div');
       row.className = 'line';
-      for(let j = 0; j < canvasWidth; j += 1) {
+      for(let j = 0; j < x; j += 1) {
         let pixel = document.createElement('div');
         pixel.className = 'pixel';
         pixel.addEventListener('click', paintPixel);
@@ -19,8 +11,6 @@ window.onload = function () {
       pixelRow.appendChild(row);
     }
   }
-  createRows();
-  startPalette();
   
   function paintPixel(event) {
     event.target.style.backgroundColor = chosenColor; //refefine a cor de fundo do pixel clicado
@@ -41,10 +31,49 @@ window.onload = function () {
     actualColor.classList.add('selected');
   }
 
-  document.querySelector('#clear-board').onclick = function() {
+  function clearBoard () {
     let clearPixels = document.querySelectorAll('.pixel');
     for (let i = 0; i < clearPixels.length; i += 1) {
       clearPixels[i].style.backgroundColor = clearPixel;
     }
   }
+
+  function deleteCanvas() {
+    let canvas = document.querySelector('#pixel-board');
+    let canvasRows = document.querySelectorAll('.line');
+    for (let i = 0; i < canvasRows.length; i += 1) {
+      canvas.removeChild(canvasRows[i]);
+    }
+  }
+
+  function genCustomBoard(size) {
+    let userInput = document.querySelector('#board-size').value;
+    size = userInput;
+    if(!userInput) {
+      alert('Board inválido!');
+    }
+    if(userInput < 5) {
+      userInput = 5;
+    }
+    if(userInput > 50) {
+      userInput = 50;
+    }
+    if(userInput) {
+      console.log(userInput)
+      deleteCanvas();
+      generateBoard(userInput);
+    }
+  }
+
+let pixelRow = document.getElementById('pixel-board');
+let chosenColor = 'black';
+let clearPixel = 'white';
+let genBoardButton = document.querySelector('#generate-board');
+genBoardButton.addEventListener('click', genCustomBoard);
+let clearButton = document.querySelector('#clear-board');
+    clearButton.addEventListener('click', clearBoard);
+
+window.onload = function() {
+  startPalette();
+  generateBoard(5);
 }
