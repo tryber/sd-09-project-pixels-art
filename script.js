73d38f40dd9d85743871b4e03d8e11c058933579
Fabio Venancio Almeida <fvalmeida.@gmail.com>
2020-12-28 11:33:21 -0300

    3 requisitos com dificuldades

--- script.js
+++ script.js
@@ -3,9 +3,14 @@ window.addEventListener('load', start);
 function start(){
 
 const colorPainel = document.getElementsByClassName('color');
-palletaCores();
+
+
 colorPainel[0].classList.add('selected');
-//teste commit
+palletaCores();
+const corSelecionada = 'black';
+
+criaBotao();
+cleanSquare();
 }
 
 function palletaCores(){
@@ -16,9 +21,45 @@ function palletaCores(){
   for (let i = 0; i < lengthPainel; i +=1){
     colorPainel[i].classList.add(colorSelect[i]);
   }
+
 }
 
-function selecionaCor(){
-  const painelDeCores = document.getElementsByClassName('pixel');
+function criaBotao(){
+  const painelColor = document.getElementById('color-palette');
+
+  painelColor.style.marginBottom = '10px';
+
+  let tagDiv = document.createElement('div');
+  tagDiv.setAttribute('id','clear-board');
+
+  painelColor.appendChild(tagDiv);
+
+  let buttomTag = document.createElement('button');
+  buttomTag.setAttribute('id', 'botao');
+  buttomTag.setAttribute('type', 'submit');
+  buttomTag.style.marginTop = '3px';
+  buttomTag.textContent = 'Limpar';
+
+  tagDiv.appendChild(buttomTag);
 }
 
+function limparPixel(){
+  const buttonLimpa = document.getElementById('botao');
+  console.log('click');
+  buttonLimpa.addEventListener('click',cleanSquare);
+  console.log(buttonLimpa.targe);
+
+}
+
+function clickPixel(eventoOrigem){
+  const pixelSelected = document.querySelectorAll('pixel');
+  let tamanhoPixel = pixelSelected.length;
+
+  for (let i = 0; i < tamanhoPixel; i +=0){
+    pixelSelected[i].style.backgroundColor = 'yellow';
+  }
+}
+
+function cleanSquare(event){
+  clickPixel();
+}
