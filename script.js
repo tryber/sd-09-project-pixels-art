7e7e8a1f3a27b5553b1ebb7a126ad1da55dae3e5
edison-ps <edison@osite.com.br>
2020-12-28 20:42:42 -0300

    Estrutura basica V1.14

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 
+const selectColor = document.querySelector('#color-palette');
+selectColor.addEventListener('click', function (colorBase) {
+  let prevSelectColor = document.querySelector('.selected');
+  prevSelectColor.className = 'color';
+  colorBase.target.className += ' selected';
+});
+
+const selectPixel = document.querySelector('#pixel-board');
+selectPixel.addEventListener('click', function (pixelSelected) {
+  const  currentColor = document.querySelector('.selected');
+  pixelSelected.target.style.backgroundColor = currentColor.id;
+});
