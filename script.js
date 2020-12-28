b7b70f676a641f13ad5e064926b31b22a5d330fe
Bernardo Smith <bernardostech@gmail.com>
2020-12-27 21:43:27 -0500

    mudanca geral 4

--- script.js
+++ script.js
@@ -7,17 +7,20 @@ function opcoesCores(param) {
     colunaPallete.className = 'color';
     if ([key] == 0) {
       colunaPallete.style.backgroundColor = 'black';
-      colunaPallete.style.border = 'black, 1px, solid';
+      colunaPallete.style.border = 'black, solid, 1px';
       colunaPallete.style.width = '40px';
       colunaPallete.style.height = '40px';
       colunaPallete.style.padding = '1px';
+      colunaPallete.style.margin = '1px';
 
       colunaPallete.className = 'color selected'
     } else {
       colunaPallete.style.width = '40px';
       colunaPallete.style.height = '40px';
       colunaPallete.style.padding = '1px';
-      colunaPallete.style.border = 'black, 1px, solid';
+      colunaPallete.style.border = 'black, solid, 1px';
+      colunaPallete.style.margin = '1px';
+
       let cor = 'rgb(' + Math.floor(Math.random(255) * (255 + 1)) + ',' + Math.floor(Math.random(255) * (255 + 1)) + ',' + Math.floor(Math.random(255) * (255 + 1)) + ')';
       if (cor !== 'rgb(255,255,255') {
         colunaPallete.style.backgroundColor = `${cor}`;
@@ -36,20 +39,30 @@ function criarTR(param1, param2) {
     let paletta = document.querySelector('#pixel-board');
 
     let linhaPaleta = document.createElement('tr');
+    linhaPaleta.style.padding = '-1 px';
     paletta.appendChild(linhaPaleta);
     for (let i = 0; i < param1; i += 1) {
       let linhaPaleta1 = document.createElement('button');
-      linhaPaleta1.id = 'pixel';
+      linhaPaleta1.className = 'pixel';
       linhaPaleta1.style.border = '1px solid black';
       linhaPaleta1.style.height = '40px';
       linhaPaleta1.style.width = '40px';
       linhaPaleta1.style.padding = '-2px';
-      linhaPaleta1.style.backgroundColor = 'white';
+      linhaPaleta1.style.backgroundColor = 'rgb(255,255,0)';
+
       paletta.appendChild(linhaPaleta1);
 
     }
   }
 }
-
-
 criarTR(5, 5);
+
+let pixelClear = document.getElementById('clear-board');
+let pixels = document.querySelectorAll('.pixel');
+pixelClear.addEventListener('click', clear);
+
+function clear(event){
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].style.backgroundColor = 'rgb(255,255,255)';
+  }
+}
