57347ff0c7e7f736fb91264aa133135b969aa30c
LeoMajevski <leonardo.majevski@gmail.com>
2020-12-29 13:39:34 -0300

    mandatory requirements 9 and 10

--- script.js
+++ script.js
@@ -42,32 +42,12 @@ function createPixelBoard(board, amount) {
   const line = Math.sqrt(amount);
   for (let index = 0; index < column; index += 1) {
     const squarecolumn = board.appendChild(newDiv());
+    squarecolumn.className = 'table-row';
     for (let index2 = 0; index2 < line; index2 += 1) {
       const squareLine = squarecolumn.appendChild(newDiv());
       squareLine.className = 'pixel';
       squareLine.style.backgroundColor = 'rgb(255, 255, 255)';
     }
-  }
-}
-
-const paletteColor = document.querySelector('#color-palette');
-const pixelBoard = document.querySelector('#pixel-board');
-const amountColors = 4;
-const amountSquares = 25;
-newPaletteColorSquares(paletteColor, amountColors);
-createPixelBoard(pixelBoard, amountSquares);
-paletteColor.addEventListener('click', selectColor);
-pixelBoard.addEventListener('click', paintPixel);
-document.querySelector('#generate-board').addEventListener('click', resizeBoard);
-
-
-
-
-function paintPixel(event) {
-  const selectedSquare = event.target;
-  if (selectedSquare !== document.querySelector('#pixel-board')) {
-    const currentColor = document.querySelector('.selected');
-    selectedSquare.style.backgroundColor = currentColor.style.backgroundColor;
   }
 }
 
@@ -87,3 +67,85 @@ function selectColor(event) {
   deselectColor();
   selectedColor.className += ' selected';
 }
+
+function paintPixel(event) {
+  const selectedSquare = event.target;
+  if (selectedSquare !== document.querySelector('#pixel-board')) {
+    const currentColor = document.querySelector('.selected');
+    selectedSquare.style.backgroundColor = currentColor.style.backgroundColor;
+  }
+}
+
+function createButton(section) {
+  const newButton = document.createElement('button');
+  newButton.type = 'button';
+  section.appendChild(newButton);
+  return newButton;
+}
+
+function createClearButton(section) {
+  const clearButton = createButton(section);
+  clearButton.innerText = 'Limpar';
+  clearButton.id = 'clear-board';
+  clearButton.className = 'clear-button';
+}
+
+function createForm(section) {
+  const newForm = document.createElement('form');
+  const newInput = document.createElement('input');
+  section.appendChild(newForm);
+  newForm.appendChild(newInput);
+  return newForm;
+}
+
+function clearBoard() {
+  const squares = document.querySelectorAll('.pixel');
+  for (let index = 0; index < squares.length; index += 1) {
+    squares[index].style.backgroundColor = 'white';
+  }
+}
+
+function inputsForm(section) {
+  const form = createForm(section);
+  const inputButton = createButton(form);
+  const formInput = form.firstChild;
+  form.id = 'board-form';
+  formInput.id = 'board-size';
+  formInput.type = 'number';
+  formInput.min = 1;
+  formInput.max = 50;
+  inputButton.innerText = 'VQV';
+  inputButton.id = 'generate-board';
+  inputButton.className = 'resize-button';
+  inputButton.type = 'button';
+}
+
+function resizeBoard() {
+  const board = document.querySelector('#pixel-board');
+  const inputBoardSize = document.querySelector('#board-size');
+  if (inputBoardSize.value === '') {
+    return alert('Board inválido!');
+  } else if (inputBoardSize.value < 5) {
+    inputBoardSize.value = 5;
+  } else if (inputBoardSize.value > 50) {
+    inputBoardSize.value = 50;
+  }
+  deletePixelBoard(board);
+  createPixelBoard(board, (inputBoardSize.value * inputBoardSize.value));
+  return true;
+}
+
+  const paletteColor = document.querySelector('#color-palette');
+  const pixelBoard = document.querySelector('#pixel-board');
+  const amountColors = 4;
+  const amountSquares = 25;
+  const buttonSection = document.querySelector('.section-form');
+  newPaletteColorSquares(paletteColor, amountColors);
+  createPixelBoard(pixelBoard, amountSquares);
+  createClearButton(buttonSection);
+  inputsForm(buttonSection);
+  paletteColor.addEventListener('click', selectColor);
+  pixelBoard.addEventListener('click', paintPixel);
+  document.querySelector('#generate-board').addEventListener('click', resizeBoard);
+  document.querySelector('#clear-board').addEventListener('click', clearBoard);
+  document.querySelector('#board-form').addEventListener('submit', preventSubmit);
