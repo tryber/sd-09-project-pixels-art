71392f48b1b23530828cf5a02e9f340e4d599ccd
Henrique-Moura <henrique_hgm1996@hotmail.com>
2020-12-29 19:13:26 -0300

    Ao clicar em um pixel dentro do quadro após selecionar uma cor na paleta, o pixel deve ser preenchido com esta cor.

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 
-function box(){
-    let numberOfColumns = 5;
-    let numberOfLine = 5;
-    for(let index = 0; index < numberOfLine; index+=1){
-        let pixelBoard = document.querySelector('#pixel-board');
-        let createLine = document.createElement('div');        
-        for(indexTwo = 0; indexTwo < numberOfColumns; indexTwo +=1){
+function box() {
+    const numberOfColumns = 5;
+    const numberOfLine = 5;
+    for (let index = 0; index < numberOfLine; index+=1){
+        let pixelBoard = document.querySelector('.pixel-board');
+        let createLine = document.createElement('div');
+        for(indexTwo = 0; indexTwo < numberOfColumns; indexTwo += 1){
             let createColumn = document.createElement('div');
             createColumn.className = 'pixel';
             createLine.appendChild(createColumn);
         }
         pixelBoard.appendChild(createLine);
-    }    
+    }
 }
 box()
 
 function firstSelected(){
-    let firstSelected = document.querySelector('#black');
+    let firstSelected = document.querySelector('.black');
     firstSelected.classList.add('selected');
 }
 firstSelected();
@@ -24,12 +24,23 @@ firstSelected();
 
 function colorSelected(){
     let classColor = document.querySelectorAll('.color');
-    for(let i=0; i<classColor.length; i+=1){
+    for(let i=0; i<classColor.length; i +=1 ){
         classColor[i].addEventListener('click', function(){
             let classSelected = document.querySelectorAll('.selected')[0];
             classSelected.classList.remove('selected');
-            classColor[i].classList.add('selected');
+            classColor[i].classList.add('selected');            
         });
     }
 }
 colorSelected();
+
+function colorPixel(){
+    let classPixel = document.querySelectorAll('.pixel');
+    for(let indexPixel = 0; indexPixel < classPixel.length; indexPixel += 1){
+        classPixel[indexPixel].addEventListener('click', function(){
+            let selectedBackground = window.getComputedStyle(document.querySelector('.selected')).backgroundColor;
+            classPixel[indexPixel].style.backgroundColor = selectedBackground;            
+        });
+    }
+}
+colorPixel();
