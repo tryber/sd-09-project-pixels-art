74561794bb691d2724809c9c2059bc3949261717
Bernardo Smith <bernardostech@gmail.com>
2020-12-28 20:37:36 -0500

    mudanca geral 10

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ function opcoesCores(param) {
     colunaPallete.className = 'color';
     if ([key] == 0) {
       colunaPallete.style.backgroundColor = 'black';
-     
+
       colunaPallete.id = 'black';
     } else {
       let cor = 'rgb(' + Math.floor(Math.random(255) * (255 + 1)) + ',' + Math.floor(Math.random(255) * (255 + 1)) + ',' + Math.floor(Math.random(255) * (255 + 1)) + ')';
       if (cor !== 'rgb(255,255,255') {
         colunaPallete.style.backgroundColor = `${cor}`;
-        colunaPallete.id =  `${cor}`;
+        colunaPallete.id = `${cor}`;
       }
     }
-    
+
     colorPallette.appendChild(colunaPallete);
   }
 }
@@ -23,23 +23,19 @@ opcoesCores(4);
 
 
 //cria os pixels
-function criarTR(tamanho=5) {
+function criarTR(tamanho = 5) {
   let paletta = document.querySelector('#pixel-board');
-  
-  if (tamanho > 20 || tamanho > 20){
-    paletta.style.marginLeft = '5px';
-    
-  }
-  
+
+
   for (let i = 0; i < tamanho; i += 1) {
-    
+
     let linhaPaleta = document.createElement('tr');
     linhaPaleta.className = 'linha';
     paletta.appendChild(linhaPaleta);
     for (let i = 0; i < tamanho; i += 1) {
       let linhaPaleta1 = document.createElement('button');
       linhaPaleta1.className = 'pixel';
-     
+
       linhaPaleta1.style.backgroundColor = 'green';
       paletta.appendChild(linhaPaleta1);
     }
@@ -52,7 +48,7 @@ let pixelClear = document.getElementById('clear-board');
 let pixels = document.querySelectorAll('.pixel');
 pixelClear.addEventListener('click', clear);
 
-function clear(){
+function clear() {
   for (let i = 0; i < pixels.length; i += 1) {
     pixels[i].style.backgroundColor = 'white';
   }
@@ -65,45 +61,44 @@ let linha = document.querySelectorAll('.linha');
 let pixel = document.querySelectorAll('.pixel');
 newBoard.addEventListener('click', refazer);
 
-function refazer(){
+function refazer() {
   let tamanho;
   for (let i = 0; i < pixel.length; i += 1) {
     linha[i].remove();
     pixel[i].remove();
 
-  if (tamanhoLinha.value < 5){
-    tamanho = 5;  
-    
-    criarTR(tamanho);
-  } else if (tamanhoLinha.value >50){
-    tamanho = 50; 
-    criarTR(tamanho);
-  }else{
-    tamanho = tamanhoLinha.value;
-   
-    criarTR(tamanho);
-  } 
-} 
+    if (tamanhoLinha.value < 5) {
+      tamanho = 5;
+
+      criarTR(tamanho);
+    } else if (tamanhoLinha.value > 50) {
+      tamanho = 50;
+      criarTR(tamanho);
+    } else {
+      tamanho = tamanhoLinha.value;
+
+      criarTR(tamanho);
+    }
+  }
 }
 
 
 let selecaoCor = document.querySelector('.tableColor');
 selecaoCor.addEventListener('click', select);
-function select(event){  
-  if(event.target.className !== 'selected'){
-  event.target.classList.add('selected');
-  }else {
+
+function select(event) {
+  if (event.target.className !== 'selected') {
+    event.target.classList.add('selected');
+  } else {
     event.target.classList.remove('selected');
-    
+
   }
 }
-/*
 
-  let pixel = document.querySelector('#pixel-board');
-  pixel.addEventListener('click', pintar);
 
-  function pintar(event){
-    event.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
-  }
-*/
+let col = document.querySelector('.pixel');
+col.addEventListener('click', pintar);
 
+function pintar(event) {
+  event.target.style.backgroundColor = document.querySelector('.selected').style.backgroundColor;
+}
