4d73c1a483ad680462ba7cf4d45265b1de6ca0a6
Henrique-Moura <henrique_hgm1996@hotmail.com>
2020-12-29 19:21:11 -0300

    Corrigindo requisito 4

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function box() {
     const numberOfColumns = 5;
     const numberOfLine = 5;
     for (let index = 0; index < numberOfLine; index+=1){
-        let pixelBoard = document.querySelector('.pixel-board');
+        let pixelBoard = document.querySelector('#pixel-board');
         let createLine = document.createElement('div');
         for(indexTwo = 0; indexTwo < numberOfColumns; indexTwo += 1){
             let createColumn = document.createElement('div');
