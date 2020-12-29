3226baca04b63ee15e34e5bdcb4c5475533ae421
Henrique-Moura <henrique_hgm1996@hotmail.com>
2020-12-28 22:01:21 -0300

    Delimitando largura e altura dos quadrados

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 
 function box(){
-    let contador = 5;
-    for(let index = 0; index < contador; index+=1){
+    let numberOfColumns = 5;
+    let numberOfLine = 5;
+    for(let index = 0; index < numberOfLine; index+=1){
         let pixelBoard = document.querySelector('#pixel-board');
-        let createDiv = document.createElement('div');        
-        for(indexTwo = 0; indexTwo < contador; indexTwo +=1){
-            let createLine = document.createElement('div');
-            createLine.className = 'pixel';
-            createDiv.appendChild(createLine);
+        let createLine = document.createElement('div');        
+        for(indexTwo = 0; indexTwo < numberOfColumns; indexTwo +=1){
+            let createColumn = document.createElement('div');
+            createColumn.className = 'pixel';
+            createLine.appendChild(createColumn);
         }
-        pixelBoard.appendChild(createDiv);   
+        pixelBoard.appendChild(createLine);
     }    
 }
 box()
