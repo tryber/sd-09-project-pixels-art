e62dc0f5cca0ce13a2bc23d0a6380ec06e473afb
Jhonatan Arguello <jhonatan.arguello@gmail.com>
2020-12-31 20:11:10 -0300

    Implantaçao inicial com paleta e tabela feitas por javascript. Foram criadas as funçoes de selecionar cor, pintar e retirar a cor dos pixels pintados.

--- script.js
+++ script.js
@@ -1,9 +1,100 @@
-function createTable() {
-  
+
+function createPalette() {
+  const tablePaletteLocation = document.querySelector('#color-palette')
+
+
+  for (let index = 0; index < 4; index += 1) {
+ 
+    let green = ['rgb(0, 0, 0)', 'rgb(255, 0, 0)', 'rgb(0, 0, 255)', 'rgb(222, 184, 135)']
+
+    const createSquareColorPalette = document.createElement('td')
+    createSquareColorPalette.className = 'colorPalette'
+    createSquareColorPalette.style.backgroundColor = green[index]
+
+    tablePaletteLocation.appendChild(createSquareColorPalette)
+  }
+
 }
+
+createPalette()
+
+function createTable() {
+  const tablePixelLocation = document.querySelector('#pixel-board')
+
+  for (let index = 0; index < 23; index += 1) {
+    const createPixelLine = document.createElement('tr')
+    createPixelLine.className = 'line'
+  
+    tablePixelLocation.appendChild(createPixelLine)
+
+    for (let index = 0; index < 23; index += 1) {
+      const createPixelColumn = document.createElement('td')
+      createPixelColumn.className = 'pixel'
+
+      createPixelLine.appendChild(createPixelColumn)
+    }
+  }
+}
+
+createTable()
+
+function selectColor() {
+  const paletteListener = document.querySelector('#color-palette')
+
+  paletteListener.addEventListener('click', function(event) {
+    
+    const colorSelectedOld = document.querySelector('.selected')
+    
+    if (event.target.classList.contains('colorPalette') === true) {
+
+      if (document.querySelector('.selected') !== null) {
+        colorSelectedOld.classList.remove('selected')
+      }
+
+      const colorSelected = event.target
+      colorSelected.classList.add('selected')
+    }
+  })
+}
+
+selectColor()
+
+
+
+
+function assignColor() {
+  const selectedPixelListener = document.querySelector('#pixel-board')
+  
+  selectedPixelListener.addEventListener('click', function(event) {
+    const selectedColorOnPalette = document.querySelector('.selected')
+    const selectedPixel = event.target
+
+    if(selectedPixel.classList[1] === 'painted') {
+      selectedPixel.className = 'pixel'
+      selectedPixel.style.backgroundColor = ''
+    } else {
+      if(selectedColorOnPalette !== null && selectedPixel.classList[0] === 'pixel') {
+        selectedPixel.style.backgroundColor = selectedColorOnPalette.style.backgroundColor
+        selectedPixel.classList.add('painted')
+      }
+    }
+  })
+}
+
+assignColor()
+
 
 
 /*
+
+for (let index = 0; index < 4; index += 1) {
+    const createSquareColorPalette = document.createElement('td')
+
+    createSquareColorPalette.classList('colorPalette')
+
+    let insertChild = tableLocation.appendChild(createSquareColorPalette)
+  }
+
 
 // Selecionar Cor
 function colorSelected() {
