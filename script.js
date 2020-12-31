8202f719f5f2e6a94f88cfb323df0ffd55a5ef99
Ricardo Sbeghen <ricardo.sbeghen@gmail.com>
2020-12-31 16:42:02 -0300

    First Step

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 
-let body = document.body;
-let frameSquare = document.createElement('div');
-frameSquare.className = 'pixel-board'
+// let body = document.body;
+// let frameSquare = document.createElement('div');
+// frameSquare.className = 'pixel-board'
 
-body.appendChild(frameSquare);
+// body.appendChild(frameSquare);
 
-for (let i=0 ; i < 5 ; i+=1) {
-    let lines = document.createElement('div');
-    frameSquare.appendChild(lines)
+// for (let i=0 ; i < 5 ; i+=1) {
+//     let lines = document.createElement('div');
+//     frameSquare.appendChild(lines)
 
-    for (let i=0 ; i < 5 ; i+=1) {
-        let squareInLine = document.createElement('div');
-        squareInLine.className='pixels';
-        lines.appendChild(squareInLine);
-    }
-}
+//     for (let i=0 ; i < 5 ; i+=1) {
+//         let squareInLine = document.createElement('div');
+//         squareInLine.className='pixels';
+//         lines.appendChild(squareInLine);
+//     }
+// }
