b4f97d2dd8e8e2f0d9fd8c78f42bb225a6ee19c4
PeryclesF <perycles_reis@hotmail.com>
2021-01-01 18:51:50 -0300

    atualizando projeto

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ window.onload = function() {
   let pixel = document.querySelectorAll('.pixel');
   let linhas = 5;
 
-  fillPixel(pixel);
-
+  
   function fillLine(divPixel) {
     for (let index = 0; index < linhas; index += 1) {
       let box = createBox('box');
@@ -11,8 +10,9 @@ window.onload = function() {
       box.addEventListener('click', paintPixel)
     }
   }
+  fillPixel(pixel);
 
-  function createBox(className) {
+  function createBox() {
     let box = document.createElement("div");
     box.className = 'box';
     return box;
@@ -22,15 +22,6 @@ window.onload = function() {
     for(let index = 0; index < pixel.length; index += 1) {
       fillLine(pixel[index]);
     }
-  }
-
-  function painting() {
-    let pixelColor = document.querySelector('div.color.red');
-    pixelColor.addEventListener('click', getColor);
-  }
-
-  function getColor() {
-    let pixelColor = document.querySelector('div.color.red');
   }
 }
 
