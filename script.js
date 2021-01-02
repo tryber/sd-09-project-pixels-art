e850ccf4b7f2b33ae43b605673146a8c0ae7fcb8
smylle <jjsilveira007@gmail.com>
2021-01-02 16:13:25 -0300

    Requisit 7 - Palette Selection

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
-let selectBlack = document.querySelector(".color");
-selectBlack.className = "color selected";
+const addClassSelected = document.querySelector(".color");
+addClassSelected.className = "color selected";
+
+const alteraNomeClasse = document.getElementById("color-palette");
+
+function colorSelected (event) {
+    document.getElementById("black").className = "color";
+    document.getElementById("blue").className = "color";
+    document.getElementById("yellow").className = "color";
+    document.getElementById("green").className = "color";
+    
+    let targetEvent = event.target.id;
+    document.getElementById(targetEvent).className = "color selected";
+}
+
+alteraNomeClasse.addEventListener("click", colorSelected);
