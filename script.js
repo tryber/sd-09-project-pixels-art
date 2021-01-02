a6a7fc33fdb14f69045ebb1846e160b2cc753403
Felipe Pimentel <lipe.pimentel89@gmail.com>
2021-01-02 20:50:14 -0300

    requisitos 1-3

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+  let boardSize = 5;
+  let colors = document.querySelectorAll('.color');
+
+  colors[0].style.backgroundColor = 'black';
+  colors[1].style.backgroundColor = 'red';
+  colors[2].style.backgroundColor = 'green';
+  colors[3].style.backgroundColor = 'blue';
+
+}
