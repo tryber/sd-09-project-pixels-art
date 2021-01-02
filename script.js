7fe5c2a32eb92eb91b98a22b0d3166284bdcd5c8
Carol-Bitencourt <carolmedeirosbitencourt@gmail.com>
2021-01-02 19:21:43 -0300

    Requisito 6

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-//Iniciando o projeto!
-if (condition) {
-    
-} else {
-    
-}
+window.onload = function () {
+  function selectedSquareColor() {
+    let square = document.getElementsByClassName("color")[0];
+    square.style.border = "1px solid pink";
+    square.classList.add("selected");
+    console.log("square", square)
+  }
+  selectedSquareColor();
+};
