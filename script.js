d774293fd760e9dcf6d273dc7621b4f9b581613b
leonardo <rpgleo.js@gmail.com>
2021-01-02 10:49:53 -0300

    7 requirement - attempt  1

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ window.onload  = function(){
 
 }
 
+let clickSelection = document.getElementById('color-palette');
+clickSelection.addEventListener("click", changeColor);
+
+function changeColor(eventoDeOrigem){
+    let clickID = eventoDeOrigem.target.id;
+    document.getElementById('black').className ='color';
+    document.getElementById('green').className ='color';
+    document.getElementById('blue').className ='color';
+    document.getElementById('cyan').className ='color';
+    
+    if (clickID === 'black' || clickID === 'green' || clickID === 'blue' || clickID === 'cyan'){
+        document.getElementById(clickID).className = 'color selected';
+    }
+}
