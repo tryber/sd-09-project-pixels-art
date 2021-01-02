df0d44ba94d6dca0cff975845fdb5b9afda8f810
Carol-Bitencourt <carolmedeirosbitencourt@gmail.com>
2021-01-02 19:26:03 -0300

    Requisito 6, Code Climate

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 window.onload = function () {
   function selectedSquareColor() {
-    let square = document.getElementsByClassName("color")[0];
-    square.style.border = "1px solid pink";
-    square.classList.add("selected");
-    console.log("square", square)
+    const square = document.getElementsByClassName('color')[0];
+    square.style.border = '1px solid pink';
+    square.classList.add('selected');
   }
   selectedSquareColor();
 };
