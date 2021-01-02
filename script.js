072e673df9e8ef8a59c6ac0011e28ac1f19b70f9
Ricardo Sbeghen <ricardo.sbeghen@gmail.com>
2021-01-02 20:45:54 -0300

    up to number 8

--- script.js
+++ script.js
@@ -1,35 +1,39 @@
-
-// let body = document.body;
-// let frameSquare = document.createElement('div');
-// frameSquare.className = 'pixel-board'
-
-// body.appendChild(frameSquare);
-
-// for (let i=0 ; i < 5 ; i+=1) {
-//     let lines = document.createElement('div');
-//     frameSquare.appendChild(lines)
-
-//     for (let i=0 ; i < 5 ; i+=1) {
-//         let squareInLine = document.createElement('div');
-//         squareInLine.className='pixels';
-//         lines.appendChild(squareInLine);
-//     }
-// }
-
+window.onload = function() {
+    selectedColor()
+    paintPixel ()
+};
+ 
 function selectedColor () {
     let cColor = document.getElementsByClassName('color');
     let cSelectedColor = document.getElementsByClassName('selected color');
 
-    for ( let count = 0 ; count < cColor.length ; count +=1 ) {
-        cColor[count].addEventListener ('click' , function(event) { 
-            for ( let cnt = 0 ; cnt < cSelectedColor.length ; cnt +=1 ) {
-                cSelectedColor[cnt].classList.remove ('selected');
-            }
+ for ( let count = 0 ; count < cColor.length ; count +=1) {
+    cColor[count].addEventListener ('click' , function(event) {
+        for ( let cnt = 0 ; cnt < cSelectedColor.length ; cnt += 1 ) {
+            cSelectedColor[cnt].classList.remove('selected');
+            console.log('Oi');
+        }
 
-            event.target.classList.add ('selected')
-
-        })
-    }
+        event.target.classList.add ('selected');
+    });
+ }
 }
 
-selectedColor()
+function paintPixel () {
+     let cPixel = document.getElementsByClassName('pixel');
+
+     for ( let count = 0 ; count < cPixel.length ; count +=1 ) {
+         cPixel[count].addEventListener ('click' , function (event) {
+            let styleSelectedColor = window.getComputedStyle(document.querySelector('.selected.color'));
+            let selectedColorBGC = styleSelectedColor.backgroundColor;
+
+             event.target.style.backgroundColor = selectedColorBGC;
+
+         })
+     }
+ }
+
+function clearBoardButton {
+    
+
+}
