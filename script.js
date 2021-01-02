fee3da366e081edea305d0989f278a1197894214
Carol-Bitencourt <carolmedeirosbitencourt@gmail.com>
2021-01-02 19:33:48 -0300

    Requisito 6, Tentativa 2

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 window.onload = function () {
   function selectedSquareColor() {
     const square = document.getElementsByClassName('color')[0];
-    square.style.border = '1px solid pink';
     square.classList.add('selected');
   }
   selectedSquareColor();
