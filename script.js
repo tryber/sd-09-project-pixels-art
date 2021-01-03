39bf3f3eec7544d69bd7e071b05622d6cac74d6a
Matheus Schreiner <matheusrubisch@gmail.com>
2021-01-03 19:09:48 -0300

    Fazendo projeto

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
-const palette = document.querySelectorAll('.color');
-palette[0].style.backgroundColor = 'black';
-palette[1].style.backgroundColor = 'blue';
-palette[2].style.backgroundColor = 'red';
-palette[3].style.backgroundColor = 'yellow';
+const paletteBoard = document.querySelector('#color-palette');
+const cor1 = document.getElementById('color1');
+const cor2 = document.getElementById('color2');
+const cor3 = document.getElementById('color3');
+const cor4 = document.getElementById('color4');
+
+cor1.style.backgroundColor = 'black';
+cor2.style.backgroundColor = 'blue';
+cor3.style.backgroundColor = 'red';
+cor4.style.backgroundColor = 'yellow';
+
+
+
+const pixelBoard = document.getElementById('pixel-board');
+
+function createLinePixel(number, pixelLine) {
+  for (let i = 0; i < number; i+= 1) {
+    const pixel = document.createElement('div');
+    pixel.className = 'pixel';
+    pixelLine.appendChild(pixel);
+  }
+}
+
+function createColumPixel(number) {
+  for (let i = 0; i < number; i+= 1) {
+    const pixelLineBoard = document.createElement('div');
+    createLinePixel(number, pixelLineBoard);
+    pixelBoard.appendChild(pixelLineBoard);
+  }
+}
+
+function selectedColor() {
+  paletteBoard.addEventListener('click', function(event) {
+    cor1.className = 'color';
+    cor2.className = 'color';
+    cor3.className = 'color';
+    cor4.className = 'color';
+    event.target.className = 'color selected';
+  })
+}
+
+createColumPixel(5);
+selectedColor();
