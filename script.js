2121ce90c59cc8d0043dc94a4e95a23bcd04884c
Carol-Bitencourt <carolmedeirosbitencourt@gmail.com>
2021-01-04 01:21:27 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-window.onload = function () {
-  function selectedSquareColor() {
-    const square = document.getElementsByClassName('color')[0];
-    square.classList.add('selected');
+function selectedSquareColorInitial() {
+  const square = document.getElementsByClassName("color")[0];
+  square.classList.add("selected");
+}
+
+function selectedSquareColor() {
+  const square = document.getElementsByClassName("color");
+  
+  for (let index = 0; index < square.length; index += 1) {
+    square[index].addEventListener("click", function (event) {
+      const selected = document.getElementsByClassName('selected');
+      let color = square[index].attributes[0].value.split(" ", 2)[1];
+      selected[0].classList.remove("selected");
+      square[index].classList.add("selected");
+      console.log("square", square)
+    });
   }
+}
+
+window.onload = function () {
+  selectedSquareColorInitial();
   selectedSquareColor();
 };
