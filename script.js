b7b957a9b4930d77512d2ac1b7a95922a539f7ea
Rodrigo Farias <deltadigo@gmail.com>
2021-01-04 19:38:53 -0300

    Requisito 8 - Logica para pintar pixels

--- script.js
+++ script.js
@@ -2,9 +2,13 @@ const colorBlack = document.querySelector("#color-palette ul li.black");
 const colorRed = document.querySelector("#color-palette ul li.red");
 const colorGreen = document.querySelector("#color-palette ul li.green");
 const colorBlue = document.querySelector("#color-palette ul li.blue");
+const table = document.querySelector("#table");
+
+let color;
 
 window.addEventListener("load", function(event) {
   colorBlack.classList.add("selected");
+  color = "black";
 })
 
 colorBlack.addEventListener("click", function(event) {
@@ -12,6 +16,7 @@ colorBlack.addEventListener("click", function(event) {
   colorRed.classList.remove("selected");
   colorGreen.classList.remove("selected");
   colorBlue.classList.remove("selected");
+  color = "black";
 })
 
 colorRed.addEventListener("click", function(event) {
@@ -19,6 +24,7 @@ colorRed.addEventListener("click", function(event) {
   colorRed.classList.add("selected");
   colorGreen.classList.remove("selected");
   colorBlue.classList.remove("selected");
+  color = "red";
 })
 
 colorGreen.addEventListener("click", function(event) {
@@ -26,6 +32,7 @@ colorGreen.addEventListener("click", function(event) {
   colorRed.classList.remove("selected");
   colorGreen.classList.add("selected");
   colorBlue.classList.remove("selected");
+  color = "green";
 })
 
 colorBlue.addEventListener("click", function(event) {
@@ -33,4 +40,9 @@ colorBlue.addEventListener("click", function(event) {
   colorRed.classList.remove("selected");
   colorGreen.classList.remove("selected");
   colorBlue.classList.add("selected");
+  color = "blue";
+})
+
+table.addEventListener("click", function(e) {
+  console.log(e.target.style.backgroundColor = color);
 })
