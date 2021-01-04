df8e777c408b4fb8008f46ab9c04de6e905a1d15
Pedro Castro <pa.coelho.castro@gmail.com>
2021-01-04 03:33:26 -0300

    Final Adjustments

--- script.js
+++ script.js
@@ -8,28 +8,28 @@ colors.forEach((color) => {
   color.addEventListener('click', (e) => {
   // Deixando classe selected apenas a cor selecionada
     colors.forEach((clr) => {
-    clr.classList.remove('selected');
-    })
+      clr.classList.remove('selected');
+    });
     selectedColor = e.target.classList[1];
     e.target.classList.add('selected');
   });
 });
 
 // Selecionando o pixel
-let pixels = document.querySelectorAll('.pixel');
+const pixels = document.querySelectorAll('.pixel');
 
 pixels.forEach((pixel) => {
-    pixel.addEventListener('click', (e) => {
-        selectedPixel = pixels[e.target.id - 1];
-        // Adicionando a classe ao pixel selecionado
-        selectedPixel.classList.remove('black', 'green', 'red', 'yellow');
-        selectedPixel.classList.add(selectedColor);
-    });
+  pixel.addEventListener('click', (e) => {
+    selectedPixel = pixels[e.target.id - 1];
+    // Adicionando a classe ao pixel selecionado
+    selectedPixel.classList.remove('black', 'green', 'red', 'yellow');
+    selectedPixel.classList.add(selectedColor);
+  });
 });
 
 // Botão para limpar
 document.querySelector('#clear-board').addEventListener('click', () => {
-    pixels.forEach(pixel => {
-        pixel.classList.remove('black', 'green', 'red', 'yellow');
+    pixels.forEach((pixel) => {
+      pixel.classList.remove('black', 'green', 'red', 'yellow');
     });
 });
