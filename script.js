51bde48ad9a621f9b32b944d65db3be1fc86cff0
Felipe Pimentel <lipe.pimentel89@gmail.com>
2021-01-04 16:19:57 -0300

    correcoes codeclimate

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 window.onload = function() {
-  let boardSize = 5;
-  let colors = document.querySelectorAll('.color');
+  // let boardSize = 5;
+  const colors = document.querySelectorAll('.color');
 
   colors[0].style.backgroundColor = 'black';
   colors[1].style.backgroundColor = 'red';
   colors[2].style.backgroundColor = 'green';
   colors[3].style.backgroundColor = 'blue';
 
-}
+};
