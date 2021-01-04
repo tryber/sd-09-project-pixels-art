5c4fe7579f23ee0e880641b43f0593748070798e
jorgeramires <jorgerl123@gmail.com>
2021-01-04 12:14:09 -0300

    resolvendo quarto exercicio

--- script.js
+++ script.js
@@ -1,30 +1,16 @@
-const container = document.getElementById("container");
-let rows = document.getElementsByClassName("gridRow");
-let cells = document.getElementsByClassName("cell");
 
-// Creates a default grid sized 16x16
-function defaultGrid() {
-    makeRows(16);
-    makeColumns(16);
+function box() {
+    const numberOfColumns = 5;
+    const numberOfLine = 5;
+    for (let index = 0; index < numberOfLine; index+=1){
+        let pixelBoard = document.querySelector('#pixel-board');
+        let createLine = document.createElement('div');
+        for(indexTwo = 0; indexTwo < numberOfColumns; indexTwo += 1){
+            let createColumn = document.createElement('div');
+            createColumn.className = 'pixel';
+            createLine.appendChild(createColumn);
+        }
+        pixelBoard.appendChild(createLine);
+    }
 }
-
-// Takes (rows, columns) input and makes a grid
-function makeRows(rowNum) {
-
-    // Creates rows
-    for (r = 0; r < rowNum; r++) {
-        let row = document.createElement("div");
-        container.appendChild(row).className = "gridRow";
-    };
-};
-
-// Creates columns
-function makeColumns(cellNum) {
-    for (i = 0; i < rows.length; i++) {
-        for (j = 0; j < cellNum; j++) {
-            let newCell = document.createElement("div");
-            rows[j].appendChild(newCell).className = "cell";
-        };
-
-    };
-};
+box()
