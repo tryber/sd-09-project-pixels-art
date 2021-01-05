25b7b63d0344b61d323bf9ff0769e9000c2718a0
Pablo Iago <evangelista.pabloiago@gmail.com>
2021-01-04 21:52:00 -0300

    mais um requisito

--- script.js
+++ script.js
@@ -1,17 +1,52 @@
-let pixels = document.querySelectorAll(".pixel");
-let palette = document.querySelectorAll(".color");
+let pickedColor = 'black';
 
-let paletteColors = ["black", "purple", "blue", "green"];
-for (let index = 0; index < palette.length; index++) {
-    palette[index].style.backgroundColor = paletteColors[index];
+document.getElementById('first-color').classList.add('selected');
+
+let colorPalette = document.getElementById('color-palette');
+colorPalette.addEventListener('click', pickPaint);
+
+
+let pixelBoard = document.getElementById('pixel-board');
+pixelBoard.addEventListener('click', paintPixel);
+
+function drawBoard(rows, cols) {
+  pixelBoard.style.setProperty('--rows', rows);
+  pixelBoard.style.setProperty('--cols', cols);
+  for (let i = 0; i < (rows * cols); i += 1) {
+    let pixel = document.createElement('div');
+    pixel.className = 'pixel';
+    pixelBoard.appendChild(pixel);
+  }
 }
-for( let i = 0; i < palette.length; i++) {
-    palette[i].addEventListener("click", function() {
-    let color = sessionStorage.setItem("color", event.target.innerHTML);
-    for ( let e = 0; e < pixels.length; e++) {
-        pixels[e].addEventListener("click", function(){
-            pixels[e].style.backgroundColor = sessionStorage.color;
-        })
-    }
-})
-} 
+
+drawBoard(5, 5);
+
+
+function pickPaint(event) {
+  if (event.target.id === 'color-palette') {
+    return pickedColor;
+  } else {
+    let oldPaint = document.querySelector('.selected');
+    let currentPaint = event.target;
+    oldPaint.classList.remove('selected');
+    currentPaint.classList.add('selected');
+
+    pickedColor = currentPaint.style.backgroundColor;
+    console.log(currentPaint.style.backgroundColor)
+  }
+}
+
+function paintPixel(event) {
+  event.target.style.backgroundColor = pickedColor;
+}
+
+let clearBtn = document.getElementById('clear-board');
+clearBtn.addEventListener('click', clearBoard);
+
+function clearBoard() {
+  let pixels = document.querySelectorAll('.pixel');
+
+  for(let index in pixels) {
+    pixels[index].style.backgroundColor = 'white';
+  }
+}
