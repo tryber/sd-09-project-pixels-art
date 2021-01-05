c077b07242178c2512f6915cbbe63ecaa8671556
Emanoel Leão <emanuelleaomarinho@gmail.com>
2021-01-05 04:59:41 -0300

    Criando a função que colore o pixel

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 //selecionado.classList.remove('selected')
 
 const paleta = document.getElementsByClassName('color');
+let pixelBoard = document.querySelectorAll('.pixel');
 
 //console.log(pixel);
 
-function addListener(objeto){
-    let objFunc = objeto;
+function addListener(objeto, func) {
+  let objFunc = objeto;
 
-    for(let index = 0; index < objeto.length; index += 1){
-        objFunc[index].addEventListener("click", changeClassSelected);
-    }
+  for (let index = 0; index < objeto.length; index += 1) {
+    objFunc[index].addEventListener("click", func);
+  }
 }
 
-function changeClassSelected(event){
-    let selected = document.querySelector('.selected');
-    selected.classList.remove('selected');
-    event.target.classList.add('selected');
+function changeClassSelected(event) {
+  let selected = document.querySelector('.selected');
+  selected.classList.remove('selected');
+  event.target.classList.add('selected');
+  console.log(selected);
 }
 
-addListener(paleta);
+function paintPixel(event) {
+  let classToAdd = document.querySelector('.selected').classList[1];
+  let pixelCLicked = event.target;
+
+  pixelCLicked.classList.add(classToAdd);
+}
+
+addListener(paleta, changeClassSelected);
+addListener(pixelBoard, paintPixel);
