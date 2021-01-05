2ae1f6d25bbec9978b1c532d596f57eb7d830c1f
pauloeliezerg <pauloeliezerg@gmail.com>
2021-01-05 19:58:37 -0300

    Refactored JS

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ boxColor[1].style.backgroundColor = 'cyan';
 boxColor[2].style.backgroundColor = 'magenta';
 boxColor[3].style.backgroundColor = 'yellow';
 
-const pixelBoard = document.querySelector('#pixel-board')
+const pixelBoard = document.querySelector('#pixel-board');
 
 for (let indexColumn = 0; indexColumn < 5; indexColumn += 1) {
   for (let indexLine = 0; indexLine < 5; indexLine += 1) {
     const pixelCell = document.createElement('div');
     pixelCell.className = 'pixel';
-    pixelBoard.appendChild(pixelCell);  
-    }
+    pixelBoard.appendChild(pixelCell);
+  }
   const pixelBreak = document.createElement('br');
   pixelBoard.appendChild(pixelBreak);
 }
