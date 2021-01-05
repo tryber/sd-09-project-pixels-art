eaf2bd3e9172cad3b4ee14ce13a88cefed569b25
Carol-Bitencourt <carolmedeirosbitencourt@gmail.com>
2021-01-04 21:45:27 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
 function selectedSquareColorInitial() {
-  const square = document.getElementsByClassName("color")[0];
-  square.classList.add("selected");
+  const squareColorDefault = document.getElementsByClassName("color")[0];
+  squareColorDefault.classList.add("selected");
+  let color = squareColorDefault.attributes[0].value.split(" ", 2)[1];
+  const colorEnd = '"' + color + '"';
+  return colorEnd;
 }
 
 function selectedSquareColor() {
   const square = document.getElementsByClassName("color");
-  
+
   for (let index = 0; index < square.length; index += 1) {
-    square[index].addEventListener("click", function (event) {
-      const selected = document.getElementsByClassName('selected');
+    return square[index].addEventListener("click", function (event) {
+      const selected = document.getElementsByClassName("selected");
       let color = square[index].attributes[0].value.split(" ", 2)[1];
+
       selected[0].classList.remove("selected");
       square[index].classList.add("selected");
-      console.log("square", square)
+      const colorEnd = '"' + color + '"';
+      return colorEnd;
     });
   }
+}
+
+function paintSquareColor() {
+  const table = document.querySelectorAll("td");
+  for (let index = 0; index < table.length; index += 1) {
+    table[index].addEventListener("click", function () {
+      table[index].style.backgroundColor = selectedSquareColor();
+    });
+  }
+}
+
+function clearSquare() {
+  var btnClear = document.getElementById("clear-board");
+  btnClear.addEventListener("click", function clickMouseLocation() {
+    window.location.reload();
+  });
 }
 
 window.onload = function () {
   selectedSquareColorInitial();
   selectedSquareColor();
+  paintSquareColor();
+  clearSquare();
 };
