186684543ba28b4339cc73182b6d19f58dca0814
Felipe Pimentel <lipe.pimentel89@gmail.com>
2021-01-04 21:03:53 -0300

    pixel-board

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-  // let boardSize = 5;
+  const boardSize = 5;
   const colors = document.querySelectorAll('.color');
 
   colors[0].style.backgroundColor = 'black';
@@ -7,8 +7,20 @@ window.onload = function () {
   colors[2].style.backgroundColor = 'green';
   colors[3].style.backgroundColor = 'blue';
 
-  // function createBoard() {
-  // }
+  createBoard(boardSize);
+
+  function createBoard(entry) {
+    for (let index = 1; index <= entry; index += 1) {
+      let line = document.createElement('div');
+      line.className = 'line';
+      document.getElementById('pixel-board').appendChild(line);
+      for (let indexPixel = 1; indexPixel <= entry; indexPixel += 1) {
+        let box = document.createElement('div');
+        box.className = 'pixel';
+        document.getElementById('pixel-board').lastChild.appendChild(box);
+      }
+    }
+  }
 };
 
 function clearBoard() {
