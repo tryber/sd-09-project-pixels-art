fcc7efcea24245a309db23487e2b2652f31dc41b
Felipe Pimentel <lipe.pimentel89@gmail.com>
2021-01-04 22:41:25 -0300

    select color

--- script.js
+++ script.js
@@ -7,22 +7,31 @@ window.onload = function () {
   colors[2].style.backgroundColor = 'green';
   colors[3].style.backgroundColor = 'blue';
 
-  createBoard(boardSize);
+  let colorPalette = document.querySelector('#color-palette');
+  let selectedColor = document.querySelector('.selected').style.backgroundColor;
+
+  colorPalette.addEventListener('click', function(event) {
+    selectedColor = event.target.style.backgroundColor;
+    document.querySelector('.selected').classList.remove('selected');
+    event.target.classList.add('selected');
+  });
 
   function createBoard(entry) {
     for (let index = 1; index <= entry; index += 1) {
-      let line = document.createElement('div');
+      const line = document.createElement('div');
       line.className = 'line';
       document.getElementById('pixel-board').appendChild(line);
       for (let indexPixel = 1; indexPixel <= entry; indexPixel += 1) {
-        let box = document.createElement('div');
+        const box = document.createElement('div');
         box.className = 'pixel';
         document.getElementById('pixel-board').lastChild.appendChild(box);
       }
     }
   }
+
+  createBoard(boardSize);
 };
 
 function clearBoard() {
   document.getElementsByClassName('pixel').style.backgroundColor = 'white';
-}
+};
