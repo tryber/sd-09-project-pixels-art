ce625c5a3600e113ea5e648e85d37573bd310a55
pauloeliezerg <pauloeliezerg@gmail.com>
2021-01-05 13:39:55 -0300

    CSS and JS refactor

---
+++ script.js
@@ -0,0 +1,5 @@
+let boxColor = document.getElementsByClassName("color");
+boxColor[0].style.backgroundColor = "black";
+boxColor[1].style.backgroundColor = "cyan";
+boxColor[2].style.backgroundColor = "magenta";
+boxColor[3].style.backgroundColor = "yellow";
