573abb3539b34261f8a16a1947a9d1d93acc16c9
jorgeramires <jorgerl123@gmail.com>
2021-01-05 16:22:06 -0300

    resolvendo setimo exercicio

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
-function box() {
+function board() {
     const numberOfColumns = 5;
     const numberOfLine = 5;
     for (let index = 0; index < numberOfLine; index+=1){
         let pixelBoard = document.querySelector('#pixel-board');
         let createLine = document.createElement('div');
-        for(indexTwo = 0; indexTwo < numberOfColumns; indexTwo += 1){
+        for(i = 0; i < numberOfColumns; i += 1){
             let createColumn = document.createElement('div');
             createColumn.className = 'pixel';
             createLine.appendChild(createColumn);
@@ -13,4 +13,31 @@ function box() {
         pixelBoard.appendChild(createLine);
     }
 }
-box()
+board()
+function initialColor(){
+    let initialColor = document.querySelector('#black');
+    initialColor.classList.add('selected');
+}
+initialColor();
+function choseCollor (){
+    let pallet  = document.querySelectorAll('.color');
+    for(let i=0; i<pallet .length; i +=1 ){
+        pallet[i].addEventListener('click', function(){
+            let colorOn  = document.querySelectorAll('.selected')[0];
+            colorOn .classList.remove('selected');
+            pallet[i].classList.add('selected');            
+        });
+    }
+}
+choseCollor ();
+
+function changeColor(){
+    let pixelSR = document.querySelectorAll('.pixel');
+    for(let index = 0; index < pixelSR.length; index += 1){
+        pixelSR[index].addEventListener('click', function(){
+            let selectedBg = window.getComputedStyle(document.querySelector('.selected')).backgroundColor;
+            pixelSR[index].style.backgroundColor = selectedBg;            
+        });
+    }
+}
+changeColor(); 
