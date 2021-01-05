0cce76cff17e402fc6a89ea526531bdb03398a7d
Sabrina A <sasa2605.alves@gmail.com>
2021-01-05 11:51:07 -0300

    Organizaçao do requisito 8

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 window.onload = function () {
   let divFather = document.getElementById("pixel-board");
   for (let index = 0; index < 5; index += 1) {
@@ -7,29 +8,23 @@ window.onload = function () {
       divFather.appendChild(pixel);
     }
   }
-  function colorBlack (event) {
-    let initialColor = document.getElementsByClassName('pixel')[0].style.backgroundColor;
-    console.log(initialColor);
-
-    event.target.initialColor = 'black';
-    console.log(initialColor);
-  } 
-
-  function appColorBlack () {
-    let pixelBoard = document.getElementById('pixel-board');
-    pixelBoard.addEventListener('click', colorBlack);
-  }
-  appColorBlack()
 }
-
 
 function selected(event) {
   let selectedItem = document.getElementsByClassName("selected")[0];
   selectedItem.className = "color";
   event.target.className = "color selected";
+  storeColor = event.target.style.backgroundColor;
 }
 function clickColorPalette() {
   const colorPalette = document.getElementById("color-palette");
   colorPalette.addEventListener("click", selected);
 }
 clickColorPalette();
+
+let pixList = document.querySelector('#pixel-board');
+
+function changePixels(evento) {
+  evento.target.style.backgroundColor = storeColor;
+}
+changePixels();
