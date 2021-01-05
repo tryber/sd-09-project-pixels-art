ea9532531fcbc27879191e0993051db4ed1ddad3
Pablo Iago <evangelista.pabloiago@gmail.com>
2021-01-04 21:05:26 -0300

    mais um requisito completo

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ window.onload= function(){
      })
   }
   valor.addEventListener("click", apagaTabela)
+
+  function gerarTabela(){
+    if(tamanho.value<5 || tamanho.value>50){
+       window.alert("OPS, insira um valor entre 5 e 50")
+    }else{
+       for (let i=0; i<tamanho.value;i++){
+          let sec = document.createElement("section");
+          sec.classList.add("tab")
+          document.getElementsByClassName("tabela")[0].appendChild(sec)
+          for(let  g=0; g<tamanho.value;g++){
+             let linhaCreada= document.getElementsByClassName("tab")[i]
+             let linha=document.createElement("div")
+             linha.addEventListener("click", function(){
+                linha.style.backgroundColor=caneta
+             })
+             linha.classList.add("pixel")
+             linhaCreada.appendChild(linha)
+          }
+       }
+    }
+ }
+ valor.addEventListener("click", gerarTabela)
