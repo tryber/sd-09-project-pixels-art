ac3fae3846d7d10c00a18a8b30250f929f5ae059
otacisos <otacisods@gmail.com>
2021-01-06 20:31:54 -0300

    Requisito 4, 6, 9, 10

--- script.js
+++ script.js
@@ -1,63 +1,62 @@
 function box() {
   const columns = 5;
   const line = 5;
-  for (let index = 0; index < line; index+=1){
-      let pixelBoard = document.querySelector('#pixel-board');
-      let createLine = document.createElement('div');
-      for(indexTwo = 0; indexTwo < columns; indexTwo += 1){
-          let createColumn = document.createElement('div');
-          createColumn.className = 'pixel';
-          createLine.appendChild(createColumn);
-      }
-      pixelBoard.appendChild(createLine);
+  for (let index = 0; index < line; index += 1) {
+    let pixelBoard = document.querySelector("#pixel-board");
+    let createLine = document.createElement("div");
+    for (indexTwo = 0; indexTwo < columns; indexTwo += 1) {
+      let createColumn = document.createElement("div");
+      createColumn.className = "pixel";
+      createLine.appendChild(createColumn);
+    }
+    pixelBoard.appendChild(createLine);
   }
 }
-box()
+box();
 
-function black(){
-  let black = document.querySelector('.black');
-  black.classList.add('selected');
+function black() {
+  let black = document.querySelector(".black");
+  black.classList.add("selected");
 }
 black();
 
-
-function colorSelected(){
-  let classColor = document.querySelectorAll('.color');
-  for(let i=0; i<classColor.length; i +=1 ){
-      classColor[i].addEventListener('click', function(){
-          let classSelected = document.querySelectorAll('.selected')[0];
-          classSelected.classList.remove('selected');
-          classColor[i].classList.add('selected');            
-      });
+function colorSelected() {
+  let classColor = document.querySelectorAll(".color");
+  for (let i = 0; i < classColor.length; i += 1) {
+    classColor[i].addEventListener("click", function () {
+      let classSelected = document.querySelectorAll(".selected")[0];
+      classSelected.classList.remove("selected");
+      classColor[i].classList.add("selected");
+    });
   }
 }
 colorSelected();
 
-function colorPixel(){
-  let classPixel = document.querySelectorAll('.pixel');
-  for(let indexPixel = 0; indexPixel < classPixel.length; indexPixel += 1){
-      classPixel[indexPixel].addEventListener('click', function(){
-          let selectedBackground = window.getComputedStyle(document.querySelector('.selected')).backgroundColor;
-          classPixel[indexPixel].style.backgroundColor = selectedBackground;            
-      });
+function colorPixel() {
+  let classPixel = document.querySelectorAll(".pixel");
+  for (let indexPixel = 0; indexPixel < classPixel.length; indexPixel += 1) {
+    classPixel[indexPixel].addEventListener("click", function () {
+      let selectedBackground = window.getComputedStyle(document.querySelector(".selected")).backgroundColor;
+      classPixel[indexPixel].style.backgroundColor = selectedBackground;
+    });
   }
 }
 colorPixel();
 
-function buttonClear(){
-  let createButtonClean = document.createElement('button');
-  createButtonClean.id = 'clear-board';
-  createButtonClean.innerText = 'Limpar';
-  const body = document.querySelector('body');
-  const pixelBoard = document.querySelector('#pixel-board');
+function buttonClear() {
+  let createButtonClean = document.createElement("button");
+  createButtonClean.id = "clear-board";
+  createButtonClean.innerText = "Limpar";
+  const body = document.querySelector("body");
+  const pixelBoard = document.querySelector("#pixel-board");
   body.insertBefore(createButtonClean, pixelBoard);
 
-  const buttonClearBoard = document.querySelector('#clear-board');
-  buttonClearBoard.addEventListener('click', function(){
-      const pixelBoardClear = document.querySelectorAll('.pixel');
-      for (let indexClear = 0; indexClear < pixelBoardClear.length; indexClear +=1){
-          pixelBoardClear[indexClear].style.backgroundColor = 'rgb(255, 255, 255)';        
-      }
-  })    
+  const buttonClearBoard = document.querySelector("#clear-board");
+  buttonClearBoard.addEventListener("click", function () {
+    const pixelBoardClear = document.querySelectorAll(".pixel");
+    for (let indexClear = 0; indexClear < pixelBoardClear.length; indexClear += 1) {
+      pixelBoardClear[indexClear].style.backgroundColor = "rgb(255, 255, 255)";
+    }
+  });
 }
 buttonClear();
