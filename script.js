a4c64099a0bd084f32d93351d5e0c12a82273c38
Carol-Bitencourt <carolmedeirosbitencourt@gmail.com>
2021-01-05 22:59:28 -0300

    requisito 7, novamente

--- script.js
+++ script.js
@@ -1,44 +1,47 @@
 function selectedSquareColorInitial() {
-  const squareColorDefault = document.getElementsByClassName('color')[0];
-  squareColorDefault.classList.add('selected');
-  const color = squareColorDefault.attributes[0].value.split(' ', 2)[1];
-  const colorEnd = `'${color}'`;
+  const squareColorDefault = document.getElementsByClassName("color")[0];
+  squareColorDefault.classList.add("selected");
+  let color = squareColorDefault.attributes[0].value.split(" ", 2)[1];
+  const colorEnd = '"' + color + '"';
   return colorEnd;
 }
 
 function selectedSquareColor() {
-  const square = document.getElementsByClassName('color');
+  const square = document.getElementsByClassName("color");
 
   for (let index = 0; index < square.length; index += 1) {
-    square[index].addEventListener('click', function () {
-      const selected = document.getElementsByClassName('selected');
-      const color = square[index].attributes[0].value.split(' ', 2)[1];
+    square[index].addEventListener("click", function (event) {
+      const selected = document.getElementsByClassName("selected");
+      let color = square[index].attributes[0].value.split(" ", 2)[1];
 
-      selected[0].classList.remove('selected');
-      square[index].classList.add('selected');
-      const colorEnd = `'${color}'`;
+      selected[0].classList.remove("selected");
+      square[index].classList.add("selected");
+      const colorEnd = '"' + color + '"';
       return colorEnd;
     });
   }
 }
 
 function paintSquareColor() {
-  const table = document.querySelectorAll('td');
+  const table = document.querySelectorAll("td");
   for (let index = 0; index < table.length; index += 1) {
-    table[index].addEventListener('click', function () {
-      table[index].style.backgroundColor = selectedSquareColor();
+    table[index].addEventListener("click", function () {
+      table[index].style.backgroundColor = "black";
     });
   }
 }
 
 function clearSquare() {
   const square = document.getElementsByTagName('td');
-  for (let index = 0; index < square.length; index+=1) {
-    square[index].style.background = 'white';
+  for (i = 0; i < square.length; i++) {
+    square[i].style.background = "white";
   }
 }
 
+
+
 window.onload = function () {
   selectedSquareColorInitial();
+  selectedSquareColor();
   paintSquareColor();
 };
