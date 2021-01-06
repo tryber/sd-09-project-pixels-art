55cab335c4412dbbb595f933e57b80ce525c1c36
otacisos <otacisods@gmail.com>
2021-01-06 16:16:09 -0300

    Requisito 9 fix 7

--- script.js
+++ script.js
@@ -3,17 +3,16 @@ function clearPixel () {
   eventClear.id = 'clear-board';
 
   const body = document.querySelector('body');
-  const board = document.querySelector('.pixel-board ');
+  const board = document.querySelector('#pixel-board');
   body.insertBefore(eventClear, board);
 
   const buttonClear = document.querySelector('#clear-board');
   buttonClear.addEventListener('click', function(){
-    const board = document.querySelector(".pixel");
+    const board = document.querySelector('.pixel');
 
       for (index = 0; index < board.length; index += 1) {
-        board[index].style.backgroundColor = "white";
+        board[index].style.backgroundColor = 'rgb(255, 255, 255)';
       }
   })
 }
-clearPixel();
-
+clearPixel ();
