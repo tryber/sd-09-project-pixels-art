8b1e7071df98b5d5c61b548982c769ca0f8f0884
Luiz Vicentin <luizpavanello@gmail.com>
2021-01-06 08:51:28 -0300

    Exercise 02 completed

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 function selectColor(event) {
-    const deselect = document.querySelector('.color.selected');
-    deselect.className = 'color';
-    event.target.className = 'color selected';
-  }
+  const deselect = document.querySelector(".color.selected");
+  deselect.className = "color";
+  event.target.className = "color selected";
+}
 
-  function paintSquare(event) {
-    const paletteColor = document.querySelector('.color.selected');
-    event.target.style.backgroundColor = paletteColor.style.backgroundColor;
+function paintSquare(event) {
+  const paletteColor = document.querySelector(".color.selected");
+  event.target.style.backgroundColor = paletteColor.style.backgroundColor;
+}
+
+function clearPixelBoard() {
+  const pixelSquare = document.querySelectorAll(".pixel");
+  for (let index = 0; index < pixelSquare.length; index += 1) {
+    pixelSquare[index].style.backgroundColor = "white";
   }
-  
+}
