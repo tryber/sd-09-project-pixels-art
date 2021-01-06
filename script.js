47aa451d8fa8efaf6945e869838de1e3962f82e1
otacisos <otacisods@gmail.com>
2021-01-06 15:44:13 -0300

    Requisito 9 fix 2

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function clearPixel () {
-  let board = document.querySelector(".pixel").childNodes;
+  let eventClear = document.createElement('button');
+  eventClear.id = 'clear-board';
+  const body = document.querySelector('body');
+  const board = document.querySelector('#pixel-board');
+  body.insertBefore(eventClear, board);
+  const buttonClear = document.querySelector('#clear-board');
+    buttonClear.addEventListener('click', function(){
+  const board = document.querySelector(".pixel");
   for (index = 0; index < board.length; index += 1) {
     board[index].style.backgroundColor = "white";
   }
