ef7f117b1e0af962fef99198ff9a15c2f0a55267
Carol-Bitencourt <carolmedeirosbitencourt@gmail.com>
2021-01-05 23:11:18 -0300

    requisito 7, code climate

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 function selectedSquareColorInitial() {
-  const squareColorDefault = document.getElementsByClassName("color")[0];
-  squareColorDefault.classList.add("selected");
-  let color = squareColorDefault.attributes[0].value.split(" ", 2)[1];
+  const squareColorDefault = document.getElementsByClassName('color')[0];
+  squareColorDefault.classList.add('selected');
+  let color = squareColorDefault.attributes[0].value.split(' ', 2)[1];
   const colorEnd = '"' + color + '"';
   return colorEnd;
 }
 
 function selectedSquareColor() {
-  const square = document.getElementsByClassName("color");
+  const square = document.getElementsByClassName('color');
 
   for (let index = 0; index < square.length; index += 1) {
-    square[index].addEventListener("click", function (event) {
-      const selected = document.getElementsByClassName("selected");
-      let color = square[index].attributes[0].value.split(" ", 2)[1];
+    square[index].addEventListener('click', function () {
+      const selected = document.getElementsByClassName('selected');
+      let color = square[index].attributes[0].value.split(' ', 2)[1];
 
-      selected[0].classList.remove("selected");
-      square[index].classList.add("selected");
+      selected[0].classList.remove('selected');
+      square[index].classList.add('selected');
       const colorEnd = '"' + color + '"';
       return colorEnd;
     });
@@ -23,10 +23,10 @@ function selectedSquareColor() {
 }
 
 function paintSquareColor() {
-  const table = document.querySelectorAll("td");
+  const table = document.querySelectorAll('td');
   for (let index = 0; index < table.length; index += 1) {
-    table[index].addEventListener("click", function () {
-      table[index].style.backgroundColor = "black";
+    table[index].addEventListener('click', function () {
+      table[index].style.backgroundColor = 'black';
     });
   }
 }
@@ -34,7 +34,7 @@ function paintSquareColor() {
 function clearSquare() {
   const square = document.getElementsByTagName('td');
   for (i = 0; i < square.length; i++) {
-    square[i].style.background = "white";
+    square[i].style.background = 'white';
   }
 }
 
