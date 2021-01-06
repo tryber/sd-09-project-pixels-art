6460d8c125a769da198b157a8c0d4a43e883669b
Cristiano Roberto <cristianoroberto@outlook.com>
2021-01-06 08:57:26 -0300

    Inserido EventListener e function do requisito 7

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 window.onload = function() {
-    let corSelecionada = document.getElementById("black");
-    corSelecionada.className += " selected";
+    let corPreSelecionada = document.getElementById("black");
+    corPreSelecionada.className += " selected";
+}
+
+let corClique = document.getElementById("color-palette");
+corClique.addEventListener("click", corSelecionada);
+
+function corSelecionada(eventOrigem) {
+    let IdClique = eventOrigem.taret.id;
+    document.getElementById("black").className ="color"; 
+    document.getElementById("red").className ="color";
+    document.getElementById("blue").className ="color";
+    document.getElementById("green").className ="color";
+
+    if (IdClique == "black" || IdClique == "red" || 
+    IdClique == "blue" || IdClique == "green") {
+        document.getElementById(IdClique).className ="color selected";
+    }
 }
