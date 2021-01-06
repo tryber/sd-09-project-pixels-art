dd9bd0c7e05f4d51a8f1d46e9e33c4031c793d47
otacisos <otacisods@gmail.com>
2021-01-06 18:09:44 -0300

    Requisito 12

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function clearPixel () {
+function clearPixel() {
   const buttonContainer = document.querySelector('.button-container');
   const button = document.createElement('button');
   button.id = 'clear-board';
@@ -7,20 +7,27 @@ function clearPixel () {
 
   button.addEventListener('click', function () {
     const allPixel = document.querySelectorAll('.pixel');
-    for (let index = 0; index < allPixel.length; index += 1){
+    for (let index = 0; index < allPixel.length; index += 1) {
       allPixel[index].style.backgroundColor = '';
     }
   });
 }
 
+function createButton (container) {
+  const button = document.createElement('button');
+  button.id = 'generate-board';
+  button.innerText = 'Enter';
+  container.appendChild(button);
+
   const size = document.getElementById('board-size');
   if (size.value <= 5) {
-    size.value = 5;
-  } else if (size.value >= 50) {
-    size.value = 50;
+  size.value = 5;
+  } else (size.value >= 50) {
+  size.value = 50;
   }
-
+}
 
 window.onload = function () {
   clearPixel();
-}
+  createButton();
+};
