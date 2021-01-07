a39752bb904bcf815cbabc9e98d79bd7f9b20d25
FredericoCanoeira <fcanoeira@gmail.com>
2021-01-07 03:03:45 +0100

    corrige codeclimate

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const pixelBoard = document.querySelector('#pixel-board');
-//black color pre selection
+// black color pre selection
 let sselectedColor = 'black';
 
 function allColors() {
   let singColor = '(';
-  singColor = `yellow`;
-  singColor = `blue`;
-  singColor = `red`;
+  singColor = 'yellow';
+  singColor = 'blue';
+  singColor = 'red';
   
   return singColor;
 }
@@ -14,9 +14,9 @@ function allColors() {
 // change background color of all color palette squares
 function palletColor() {
   document.querySelector('#black').style.backgroundColor = 'black';
-  document.querySelector('#YellowColor').style.backgroundColor = `yellow`
-  document.querySelector('#blueColor').style.backgroundColor = `blue`
-  document.querySelector('#RedColor').style.backgroundColor = `red`
+  document.querySelector('#YellowColor').style.backgroundColor = 'yellow';
+  document.querySelector('#blueColor').style.backgroundColor = 'blue';
+  document.querySelector('#RedColor').style.backgroundColor = 'red';
 }
 
 function selectedClass(event) {
@@ -25,7 +25,7 @@ function selectedClass(event) {
   event.target.className = 'color selected';
   sselectedColor = event.target.style.backgroundColor;
 }
-//monitors the palette selection click
+// monitors the palette selection click
 function clickOnColor() {
   const clickEv = document.querySelector('#color-palette');
   clickEv.addEventListener('click', selectedClass);
@@ -34,12 +34,13 @@ function clickOnColor() {
 function backgroundColorChange(event) {
   event.target.style.backgroundColor = sselectedColor;
 }
-//paint a square with a click
+// paint a square with a click
 function paintSquare() {
   pixelBoard.addEventListener('click', backgroundColorChange);
 }
 
-//notwithstanding the size of painted pictures, it is clean, that is, it makes the white background standard when the function is activated
+// size of painted pictures, it is clean, that is, 
+// it makes the white background standard when the function is activated
 function clearPixel() {
   const px = document.querySelectorAll('.pixel');
   for (let fill = 0; fill < px.length; fill += 1) {
@@ -47,7 +48,7 @@ function clearPixel() {
   }
 }
 
-//Button connected to the function "clearPixel" calls it and clears everything.
+// Button connected to the function "clearPixel" calls it and clears everything.
 function clearButton() {
   const buttonCreate = document.createElement('button');
   const buttoncontainer = document.querySelector('#button-container');
@@ -56,7 +57,7 @@ function clearButton() {
   buttonCreate.addEventListener('click', clearPixel);
   buttoncontainer.appendChild(buttonCreate);
 }
-//Functions to be called as soon as the page is open or updated
+// Functions to be called as soon as the page is open or updated
 window.onload = function () {
   palletColor();
   clickOnColor();
