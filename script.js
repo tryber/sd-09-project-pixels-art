94084659eb645d3b4cda8029adf94d26770308f7
João Pedro Barros <joaopedrobarros5@gmail.com>
2021-01-06 21:39:56 -0300

    refatorando codigo

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-const boardPixel = document.querySelector('#pixel-board')
-let qtPixels = 0;
-
-function colorselected(event) {
-  const selectColor = document.querySelectorAll('.selected');
-  selectColor.classList.remove('.selected');
-  event.target.classList.add('.selected');
+function generatePixelBoard(size) {
+  const boardPixel = document.querySelector("#pixel-board")
+  for (let i = 0 ; i < size ; i += 1) {
+    const row = document.createElement("div")
+    row.className = 'row'
+    boardPixel.appendChild(row)
+    for (let ii = 0 ; ii < size ; ii += 1) {
+      const elementPixel = document.createElement("div")
+      elementPixel.className = 'pixel'
+      row.appendChild(elementPixel)
+    }
+  }
 }
 
-function coloringBoard(event) {
-  const selectedColor = document.querySelector('.selected');
-  const fillColor = selectedColor.style.backgroundColor;
-  event.target.style.backgroundColor = fillColor;
+generatePixelBoard(5)
+
+function createPallets(qty) {
+  const colorpalet = document.querySelector("#color-palette")
+  console.log(colorpalet)
+  for (let i = 0 ; i < qty.length ; i += 1) {
+    const color = document.createElement("div")
+    color.className = 'color'
+    color.style.backgroundColor = qty[i]
+    colorpalet.appendChild(color)
+  }
 }
 
-
+createPallets(['black','red','blue','green','yellow'])
