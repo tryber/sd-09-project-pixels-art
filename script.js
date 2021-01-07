d6c79a43896e1dc942fa3be4257d95143fd5360d
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-01-07 06:58:21 -0300

    Adicionando a funcao selecionar cor

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-const color1 = document.getElementsByClassName('first-color')[0].classList.add('selected');
+const color1 = document.getElementsByClassName('first-color')[0];
 const color2 = document.getElementsByClassName('second-color')[0];
 const color3 = document.getElementsByClassName('third-color')[0];
 const color4 = document.getElementsByClassName('fourth-color')[0];
 
+const paletaCores = [color1, color2, color3, color4];
 
-/* color1.addEventListener("click",selectingColor)
-color2.addEventListener("click",selectingColor)
-color3.addEventListener("click",selectingColor)
-color4.addEventListener("click",selectingColor) */
+function selectingColor(event) {
+  for (let index = 0; index < paletaCores.length; index += 1) {
+    if (paletaCores[index].classList.contains('selected')){
+      paletaCores[index].classList.remove('selected');
+    }
+  }
+  event.target.classList.add('selected');
+}
+
+color1.addEventListener('click', selectingColor);
+color2.addEventListener('click', selectingColor);
+color3.addEventListener('click', selectingColor);
+color4.addEventListener('click', selectingColor);
 
 // let selectedColor = selectingColor();
 // color1.classList.add("selected");
