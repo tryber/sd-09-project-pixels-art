12c118b2e89a98fbeda70f78f61ed15b2ca8bf3e
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-01-08 19:04:26 -0300

    funcao limpa pixels

--- script.js
+++ script.js
@@ -22,35 +22,24 @@ color4.addEventListener('click', selectingColor);
 let arrayDePixels = document.getElementsByClassName('pixel');
 
 function paintingPixel(event) {
-  let corAtual = document.querySelector('.selected');
+  const corAtual = document.querySelector('.selected');
   console.log(corAtual.style.backgroundColor);
   console.log(getComputedStyle(corAtual).backgroundColor);
-  event.target.style.backgroundColor = getComputedStyle(corAtual).backgroundColor;  
-  }
+  event.target.style.backgroundColor = getComputedStyle(corAtual).backgroundColor;
+}
 
   for (let index = 0; index < arrayDePixels.length; index += 1) {
     arrayDePixels[index].addEventListener('click', paintingPixel);
   }
-  //event.target.style.backgroundColor = corAtual.style.backgroundColor;
-  //for (let index = 0; index < paletaCores.length; index += 1) {
-  //  if (paletaCores[index].classList.contains('selected')) {
-  //    elementoX.style.backgroundColor = paletaCores[index].style.backgroundColor;
-  //  }
-  //}
-
+  
 // resolver esta funcao
 function clearBoard() {
   const pixels = document.getElementsByClassName('pixel');
    for (let index = 0; index < pixels.length; index +=1) {
-     pixels[index].classList.add("color-white");
+     pixels[index].style.backgroundColor = 'rgb(255, 255, 255)';
+     
   }
 }
 
-function testEvent() {
-  const textToPrint = document.getElementById('teste');
-  textToPrint.innerText = 'Hora da verdade. Acho que funcionou!';
-}
-
 const clickButton = document.getElementById('clear-board');
-clickButton.addEventListener('click', testEvent);
 clickButton.addEventListener('click', clearBoard);
