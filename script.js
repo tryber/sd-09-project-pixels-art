daa618001810f4c05b0c04aba2feed4ada7ba12e
Arlesson Moura <arlessonmss@gmail.com>
2021-01-10 12:45:39 -0300

    First JS constructions, Clear Button and paint brush.

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-let colorClass = document.getElementsByClassName
+// Botão para limpar todos os pixels
+
+let btnClear = document.getElementById('clear-board');
+btnClear.addEventListener('click', clearEcran);
+
+function clearEcran(paintedPixel) {
+    paintedPixel = document.getElementsByClassName('pixel');
+    for (let index = 0; index < paintedPixel.length; index += 1) {
+        paintedPixel[index].style.backgroundColor = 'white';        
+    }
+}
+
+// Ação de pintar
+ 
+let pixelEcran = document.getElementsByClassName('pixel');
+for (let index = 0; index < pixelEcran.length; index += 1) {    
+    pixelEcran[index].addEventListener('click', singlePixel)
+    }
+
+function singlePixel (clickPixel) {
+    let selectedPixel = clickPixel.target;
+    return selectedPixel.style.backgroundColor = 'black'
+    }
+
+// Ação selecionar cor
+
+let colorSelected;
+colorSelected.style.backgroundColor = 'black'
+
+
+
+
+
+
+let pixelSelected = document.getElementsByClassName('selected')
