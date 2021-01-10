4d2282f931b0faf29adbb8b74638866cca3310b6
Arlesson Moura <arlessonmss@gmail.com>
2021-01-10 16:37:44 -0300

    JS functions: Choose color and paint

--- script.js
+++ script.js
@@ -10,26 +10,44 @@ function clearEcran(paintedPixel) {
     }
 }
 
-// Ação de pintar
- 
+// 1° ação de pintar 
 let pixelEcran = document.getElementsByClassName('pixel');
+
 for (let index = 0; index < pixelEcran.length; index += 1) {    
     pixelEcran[index].addEventListener('click', singlePixel)
     }
 
 function singlePixel (clickPixel) {
     let selectedPixel = clickPixel.target;
-    return selectedPixel.style.backgroundColor = 'black'
+    return selectedPixel.style.backgroundColor = colorSelected
     }
 
-// Ação selecionar cor
+/// 2° Ação de escolher a cor
+let colorPallete = document.getElementsByClassName('color');
+let colorSelected = 'black'
+for (let index = 0; index < colorPallete.length; index++) {    
+    colorPallete[index].addEventListener('click', getColor)              
+}
 
-let colorSelected;
-colorSelected.style.backgroundColor = 'black'
+function getColor (clickButton) {
+    let buttonPressed = clickButton.target;
+    
+    if (buttonPressed === colorPallete[0]) {
+        colorSelected = 'black'
+    }
+    else if (buttonPressed === colorPallete[1]) {
+        colorSelected = 'red'
+    }
+    else if (buttonPressed === colorPallete[2]) {
+        colorSelected = 'green'
+    }
+    else if(buttonPressed === colorPallete[3]) {
+        colorSelected = 'blue'
+    }
+}
 
 
 
 
 
 
-let pixelSelected = document.getElementsByClassName('selected')
