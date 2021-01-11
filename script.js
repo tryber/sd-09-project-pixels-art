fa84830236bc7ed62944eec3c11ca18380d8f645
Arlesson Moura <arlessonmss@gmail.com>
2021-01-11 17:54:05 -0300

    back to the rails.

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
 let colorPallete = document.getElementsByClassName('color');
 let pixelEcran = document.getElementsByClassName('pixel');
-
-
-//Ação de pintar
-// for (let index = 0; index < pixelEcran.length; index += 1) {    
-//     pixelEcran[index].addEventListener('click', singlePixel)
-// }
-//     function singlePixel(clickPixel) {
-//         let selectedPixel = clickPixel.target;
-//         document.querySelector('.selected').classList.add(.classList[1])        
-//     }
-
-// Mudança na cor do pincel -- 1° desabilita seletor anterio 2° gera novo seletor
+let chargedBrush = document.querySelector('.selected').classList[1];
+console.log(chargedBrush)
+// Mudança na cor do pincel -- 1° desabilita seletor anterior 2° gera novo seletor
 
 for (let index = 0; index < colorPallete.length; index += 1) {    
     colorPallete[index].addEventListener('click', disableColor)    
-    colorPallete[index].addEventListener('click', getColor)               
-    }
+    colorPallete[index].addEventListener('click', getColor)              
+}
     
-    function disableColor() {
-            document.querySelector('.selected').classList.remove('selected')
-        }
+function disableColor() {
+    document.querySelector('.selected').classList.remove('selected')
+}
 
-    function getColor(clickButton) {
+function getColor(clickButton) {
     let buttonPressed = clickButton.target;     
 
     if (buttonPressed === colorPallete[0]) {
@@ -34,12 +25,19 @@ for (let index = 0; index < colorPallete.length; index += 1) {
     else if (buttonPressed === colorPallete[2]) {
         colorPallete[2].classList.add('selected')
     }
-    else if(buttonPressed === colorPallete[3]) {
+    else {
         colorPallete[3].classList.add('selected')
     }
+}   
+
+// Ação de pintar
+for (let index = 0; index < pixelEcran.length; index += 1) {    
+    pixelEcran[index].addEventListener('click', singlePixel)
+}
+    function singlePixel(clickPixel) {
+        let selectedPixel = clickPixel.target;
+        selectedPixel.classList.add(chargedBrush);                      
     }
-
-
 
 // Botão para limpar todos os pixels
 
