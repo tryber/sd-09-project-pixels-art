db13372a462a48ab577e4a4aef5e581e40727594
Arlesson Moura <arlessonmss@gmail.com>
2021-01-11 11:21:40 -0300

    Desperate changes

--- script.js
+++ script.js
@@ -1,48 +1,51 @@
-// Botão para limpar todos os pixels
+let colorPallete = document.getElementsByClassName('color');
+let colorSelected = document.querySelector('.selected').classList[1]
 
-let btnClear = document.getElementById('clear-board');
-btnClear.addEventListener('click', clearEcran);
-
-function clearEcran(paintedPixel) {
-    paintedPixel = document.getElementsByClassName('pixel');
-    for (let index = 0; index < paintedPixel.length; index += 1) {
-        paintedPixel[index].style.backgroundColor = 'white';        
-    }
-}
-
-// 1° ação de pintar 
+function colors() {
 let pixelEcran = document.getElementsByClassName('pixel');
 
 for (let index = 0; index < pixelEcran.length; index += 1) {    
     pixelEcran[index].addEventListener('click', singlePixel)
     }
 
-function singlePixel (clickPixel) {
-    let selectedPixel = clickPixel.target;
-    return selectedPixel.style.backgroundColor = colorSelected
+function singlePixel(clickPixel) {
+     let selectedPixel = clickPixel.target;
+     selectedPixel.classList.add(colorSelected)
     }
-
-/// 2° Ação de escolher a cor
-let colorPallete = document.getElementsByClassName('color');
-let colorSelected = 'black'
-for (let index = 0; index < colorPallete.length; index++) {    
-    colorPallete[index].addEventListener('click', getColor)              
 }
 
-function getColor (clickButton) {
-    let buttonPressed = clickButton.target;
     
-    if (buttonPressed === colorPallete[0]) {
-        colorSelected = 'black'
+
+
+function changeSelector() {
+    for (let index = 0; index < colorPallete.length; index++) {    
+    colorPallete[index].addEventListener('click', disableColor)           
+}
+function disableColor() {
+    document.querySelector('.selected').classList.remove('selected')
+}
+
+///////////
+function brush() {
+    for (let index = 0; index < colorPallete.length; index++) {    
+        colorPallete[index].addEventListener('click', getColor)               
     }
-    else if (buttonPressed === colorPallete[1]) {
-        colorSelected = 'red'
-    }
-    else if (buttonPressed === colorPallete[2]) {
-        colorSelected = 'green'
-    }
-    else if(buttonPressed === colorPallete[3]) {
-        colorSelected = 'blue'
+            function getColor(clickButton) {
+            let buttonPressed = clickButton.target;     
+        
+            if (buttonPressed === colorPallete[0]) {
+                colorPallete[0].classList.add('selected')
+            }
+            else if (buttonPressed === colorPallete[1]) {
+                colorPallete[1].classList.add('selected')
+            }
+            else if (buttonPressed === colorPallete[2]) {
+                colorPallete[2].classList.add('selected')
+            }
+            else if(buttonPressed === colorPallete[3]) {
+                colorPallete[3].classList.add('selected')
+            }
+        }
     }
 }
 
@@ -50,4 +53,16 @@ function getColor (clickButton) {
 
 
 
+
+// Botão para limpar todos os pixels
+
+let btnClear = document.getElementById('clear-board');
+btnClear.addEventListener('click', clearEcran);
+
+function clearEcran() {
+    for (let index = 0; index < pixelEcran.length; index += 1) {
+        pixelEcran[index].style.backgroundColor = 'white';        
+    }
+}
+///2° Ação de escolher a cor (muito mais bonita que a exigida pelo avaliador)
 
