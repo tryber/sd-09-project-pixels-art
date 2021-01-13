c3d9ff8b893b8bebe6837cba8d3d6e47a11d2283
MarceloPelegrino <pelegrinocorrea@gmail>
2021-01-13 10:36:05 -0300

    Corrigindo codeclimate Dia 13-01-2021 10:35

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
 // Seleciona a cor black ao carregar a pagina.
 window.onload = function () {
   // Seleciona a cor preta ao iniciar e adiciona ao elemento.
-  let selectedBlack = document.querySelector('.color.black');
+  const selectedBlack = document.querySelector('.color.black');
   selectedBlack.classList.add('selected');
 
   // Adicionando a cor selecionada ao elemento pixel
-  let pixelBoard = document.querySelector('#pixel-board');
-  pixelBoard.addEventListener('click',function(event) {
-      let selectedElement = document.querySelector('.selected');
-      event.target.style.backgroundColor = getComputedStyle(selectedElement).backgroundColor;
-  })
+  const pixelBoard = document.querySelector('#pixel-board');
+  pixelBoard.addEventListener('click', function (event) {
+    const selectedElement = document.querySelector('.selected');
+    event.target.style.backgroundColor = getComputedStyle(selectedElement).backgroundColor;
+  });
 
   // Seleciona uma cor da paleta e adiciona ao elemento pixel.
-  let colorPalette = document.querySelector('#color-palette');
-  colorPalette.addEventListener('click',function(event) {
-      let erasingClass = document.querySelector('.selected');
-      erasingClass.classList.remove('selected');
-      event.target.classList.add('selected');
-      // Adicionando a cor selecionada ao elemento pixel
-      let pixelBoard = document.querySelector('#pixel-board');
-      pixelBoard.addEventListener('click',function(event) {
-          let selectedElement = document.querySelector('.selected');
-          event.target.style.backgroundColor = getComputedStyle(selectedElement).backgroundColor;
-      })
-  })
+  const colorPalette = document.querySelector('#color-palette');
+  colorPalette.addEventListener('click', function (event) {
+    const erasingClass = document.querySelector('.selected');
+    erasingClass.classList.remove('selected');
+    event.target.classList.add('selected');
+    // Adicionando a cor selecionada ao elemento pixel
+    //??? const pixelBoard = document.querySelector('#pixel-board');
+    //??? pixelBoard.addEventListener('click', function (event) {
+        const selectedElement = document.querySelector('.selected');
+        event.target.style.backgroundColor = getComputedStyle(selectedElement).backgroundColor;
+    //??? });
+  });
 
   // Botão limpar
-  let clearBoard = document.querySelector('#clear-board');
-  clearBoard.addEventListener('click', function(event) {
-      let erasingClass = document.querySelector('.selected');
-      erasingClass.classList.remove('selected');
-      let erasingPixels = document.querySelectorAll('.pixel');
-      for (let index = 0; index < erasingPixels.length; index += 1) {
-          erasingPixels[index].style.backgroundColor = 'white';           
+  const clearBoard = document.querySelector('#clear-board');
+  clearBoard.addEventListener('click', function (event) {
+    const erasingClass = document.querySelector('.selected');
+    erasingClass.classList.remove('selected');
+    const erasingPixels = document.querySelectorAll('.pixel');
+    for (let index = 0; index < erasingPixels.length; index += 1) {
+        erasingPixels[index].style.backgroundColor = 'white';
       }
-  })
+  });
 
   //Botão VQV(input)
-}
+};
 
 // // Seleciona uma cor da paleta e adiciona ao elemento.
 // let colorPalette = document.querySelector('#color-palette')
