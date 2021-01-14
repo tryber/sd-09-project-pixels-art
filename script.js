46c6caf3897212cc79979fd4a17c36edbf1d51f1
MarceloPelegrino <pelegrinocorrea@gmail>
2021-01-14 18:41:15 -0300

    Dia 14-01-2021 18:41

--- script.js
+++ script.js
@@ -37,32 +37,36 @@ window.onload = function () {
   });
 
   //Botão VQV(input)
+  const generateBoard = document.querySelector('#generate-board');
+  generateBoard.addEventListener('click', function () {
+    const boardSize = document.querySelector('#board-size');
+    if (boardSize.value === "") {
+      alert('Board inválido!');
+    } else if (boardSize.value < 5) {
+      boardSize.value = 5;
+    } else if (boardSize.value > 50) {
+      boardSize.value = 50;
+    }
+    // Construindo um quadrado de pixels
+    if (boardSize.value > 1) {
+      const pixelBoard = document.querySelector('#pixel-board')
+      while (pixelBoard.firstChild) {
+        pixelBoard.removeChild(pixelBoard.firstChild)
+      }
+    }
+    //??? const removingPixelSquare = pixelBoard.childNodes;
+    //??? document.pixelBoard.removeChild(removingPixelSquare);
+    for (let index = 0; index < boardSize.value; index += 1) {
+      const creatingDiv = document.createElement('div')
+      pixelBoard.appendChild(creatingDiv);
+      const lastLineDiv = pixelBoard.lastChild;
+      lastLineDiv.className = 'line-board';
+      for (let index = 0; index < boardSize.value; index += 1) {
+        const creatingPixel = document.createElement('div');
+        creatingDiv.appendChild(creatingPixel);
+        const lastPixelDiv = creatingDiv.lastChild;
+        lastPixelDiv.className = 'pixel';
+      }
+    }
+  });
 };
-
-// // Seleciona uma cor da paleta e adiciona ao elemento.
-// let colorPalette = document.querySelector('#color-palette')
-// let pixelBoard = document.querySelector('#pixel-board');
-// pixelBoard.addEventListener('click',function(event) {
-//     event.target.style.backgroundColor = getComputedStyle(colorPalette).backgroundColor;
-// })
-
-// Seleciona a cor ao clicar.
-// let selected = document.querySelector('#color-palette');
-// selected.addEventListener('click', addColor);
-
-// Função adicionar classe
-// function addClass () {
-//     selected.addClass = selected;
-// }
-
-// Função adicionar cor.
-// function addColor (event) {
-//     event.target.style.backgroundColor = selected.style.backgroundColor;
-// }
-
-// Adiciona a cor selecionada em line-board.
-// let pixelBoard = document.querySelectorAll('.pixel');
-// pixelBoard.addEventListener('click', addColor);
-// for (let index = 0; index < pixelBoard.length; index += 1) {
-//     selected.style.backgroundColor = pixelBoard[index];
-// }
