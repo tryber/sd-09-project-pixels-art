20f7fc394d108b51d60be5dedcd8e7d55a3aa11c
Lucas Henrique Brito <lucas.silva.brito@hotmail.com.br>
2021-01-18 18:47:28 -0300

    Refatoração

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ window.onload = function () {
   const father = document.getElementById('pixel-board');
   for (let i = 0; i < 5; i += 1) {
     const son = document.createElement('div');
+    son.className = 'line';
     father.appendChild(son);
     for (let j = 0; j < 5; j += 1) {
       const grandSon = document.createElement('div');
