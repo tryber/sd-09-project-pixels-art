7a1da4142abc5845429e6417dae6cd714ba17a39
Lucas Henrique Brito <lucas.silva.brito@hotmail.com.br>
2021-01-18 17:01:12 -0300

    Requisito 4

---
+++ script.js
@@ -0,0 +1,11 @@
+const board = document.getElementById('pixel-board');
+
+function pixelBoard () {
+  for(let i = 0; i < 25; i += 1) {
+    let pixel = document.createElement('div');
+    pixel.className = 'pixel';
+    board.appendChild(pixel);
+  }
+}
+
+pixelBoard();
