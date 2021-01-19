3fc74faa4e4af3a3aa9ae71300f35e6a3490815e
Lucas Henrique Brito <lucas.silva.brito@hotmail.com.br>
2021-01-19 14:37:33 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const black = document.getElementsByTagName('div')[0];
+const red = document.getElementsByTagName('div')[1];
+const green = document.getElementsByTagName('div')[2];
+const blue = document.getElementsByTagName('div')[3];
+
+black.style.backgroundColor = 'black';
+red.style.backgroundColor = 'red';
+green.style.backgroundColor = 'green';
+blue.style.backgroundColor = 'blue';
+
 window.onload = function () {
   const board = document.getElementById('pixel-board');
   for (let i = 0; i < 5; i += 1) {
@@ -9,8 +19,11 @@ window.onload = function () {
   }
 };
 
-function selectedColor() {
-  const colorPalette = document.getElementById('color-palette');
+function selectedColor(color) {
+  let previousSelected = document.querySelector('.selected');
+  let newSelected = color.target;
+  previousSelected.classList.remove('selected');
+  newSelected.classList.add('selected');
 }
 
-selectedColor();
+document.getElementById('color-palette').addEventListener('click', selectedColor);
