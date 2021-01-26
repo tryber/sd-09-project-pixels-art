4439ef36bddb4aeb83574049ead74765b3cd33d0
Roberto Natividade <robertonnv@gmail.com>
2021-01-26 14:38:34 -0300

    Alteracao

---
+++ script.js
@@ -0,0 +1,11 @@
+//Click
+let clickP = document.getElementById('black');
+
+//Adicionando o evento de click
+clickP.addEventListener('click', recebeClick);
+
+// Criando a função recebe click
+function recebeClick(eventoDeOrigem) {
+    // objeto evento
+    console.log('Eu sou vasco');
+}
