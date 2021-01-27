625db56f25b35170a3e0984a4160fd15fd032761
Roberto Natividade <robertonnv@gmail.com>
2021-01-26 23:20:37 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 //Click
-let clickP = document.getElementById('black');
-
-//Adicionando o evento de click
+let clickP = document.getElementById('color-palette');
 clickP.addEventListener('click', recebeClick);
-
 // Criando a função recebe click
 function recebeClick(eventoDeOrigem) {
     // objeto evento
+    console.log('Eu sou preto');
+}
+//Criando  evento para os 25 pixels
+let clickQ = document.getElementById('pixel-board');
+clickQ.addEventListener('click', recebeClicks);
+
+function recebeClicks(Evento) {
     console.log('Eu sou vasco');
 }
+
