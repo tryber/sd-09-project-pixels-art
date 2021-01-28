1f3d5e600a999c2c3abc401d6bb98666dece80e3
Roberto Natividade <robertonnv@gmail.com>
2021-01-27 23:47:44 -0300

    Requisitos removed selected

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-//Click
-let clickP = document.getElementById('color-palette');
-clickP.addEventListener('click', recebeClick);
-// Criando a função recebe click
-function recebeClick(eventoDeOrigem) {
-    // objeto evento
-    console.log('Eu sou preto');
+function removeSelectedClass() {
+    let clickP = document.querySelectorAll('.color')
+
+    for (let i = 0; i < clickP.length; i+= 1) {
+        clickP[i].classList.remove('selected')
+    }
 }
+
+
+function recebeClick(event) {
+    // objeto evento
+    event.target.classList.add('selected')
+}
+
+
+//adicionando evento para o pai
+function listenToColors(){
+    let clickP = document.querySelector('#color-palette');
+    clickP.addEventListener('click', removeSelectedClass)
+    clickP.addEventListener('click', recebeClick);
+}
+listenToColors();
+// Criando a função recebe click
 //Criando  evento para os 25 pixels
 let clickQ = document.getElementById('pixel-board');
 clickQ.addEventListener('click', recebeClicks);
