42dbcef168f079f7febc4463a7988a11371ce939
Roberto Natividade <robertonnv@gmail.com>
2021-01-28 22:55:10 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,31 +1,43 @@
+//Removendo o selected do anterior e passando para o proximo
 function removeSelectedClass() {
-    let clickP = document.querySelectorAll('.color')
+    //Recebe todos os elementos com a classe .color
+    let recuperandoAClasseColor = document.querySelectorAll('.color')
 
-    for (let i = 0; i < clickP.length; i+= 1) {
-        clickP[i].classList.remove('selected')
+    for (let i = 0; i < recuperandoAClasseColor.length; i+= 1) {
+        recuperandoAClasseColor[i].classList.remove('selected')
     }
 }
-
-
-function recebeClick(event) {
-    // objeto evento
+// Adicionando selected
+function adicionandoSelected(event) {
     event.target.classList.add('selected')
+    
+    
 }
 
-
-//adicionando evento para o pai
+//adicionando evento de adicionar selected  e remover para o pai
 function listenToColors(){
-    let clickP = document.querySelector('#color-palette');
-    clickP.addEventListener('click', removeSelectedClass)
-    clickP.addEventListener('click', recebeClick);
+    let adicionandoEventoBoxPai = document.querySelector('#color-palette');
+    adicionandoEventoBoxPai.addEventListener('click', removeSelectedClass);
+    adicionandoEventoBoxPai.addEventListener('click', adicionandoSelected);
 }
 listenToColors();
-// Criando a função recebe click
-//Criando  evento para os 25 pixels
-let clickQ = document.getElementById('pixel-board');
-clickQ.addEventListener('click', recebeClicks);
 
-function recebeClicks(Evento) {
-    console.log('Eu sou vasco');
+// 2 parte do problema
+function paintPixel(event) {
+  const selectedElement = document.querySelector('.selected');
+  const cor = selectedElement.style.backgroundColor;
+  event.target.style.backgroundColor = cor;
+   
 }
+
+
+// Evento para a tabela
+function eventoDaTabela(){
+
+    let clickQ = document.querySelector('#pixel-board')
+    clickQ.addEventListener('click', paintPixel)
+}
+
+eventoDaTabela();
+
 
