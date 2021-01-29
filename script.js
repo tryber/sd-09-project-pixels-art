c1c18cbf19b7b1fe1e18f8a4e6afa245575ace7a
Caio <caioqs.info@gmail.com>
2021-01-29 13:04:14 -0300

    Concluido projeto

--- script.js
+++ script.js
@@ -1,83 +1,119 @@
-const colorBlack = document.getElementById('black');
-const colorYellow = document.getElementById('yellow');
-const colorRed = document.getElementById('red');
-const colorGreen = document.getElementById('green');
+const colors = document.querySelectorAll('.color');
+const pixelBoard = document.querySelector('#pixel-board');
+const colorPalette = document.querySelector('#color-palette');
+const btnClear = document.querySelector('#clear-board');
+const inBoardSize = document.querySelector('#board-size');
+const btnGenerateBoard = document.querySelector('#generate-board');
 
+// cor inicial dos pixels de referência
+colors[0].style.backgroundColor = 'black';
+colors[1].style.backgroundColor = 'red';
+colors[2].style.backgroundColor = 'blue';
+colors[3].style.backgroundColor = 'green';
+
+// Cria uma cor aleatória
 function rgbRandom() {
-    const r = Math.ceil(Math.random() * 256);
-    const g = Math.ceil(Math.random() * 256);
-    const b = Math.ceil(Math.random() * 256);
-  
-    const rgb = `rgb(${r}, ${g}, ${b})`;
-  
-    return rgb;
+  const r = Math.ceil(Math.random() * 256);
+  const g = Math.ceil(Math.random() * 256);
+  const b = Math.ceil(Math.random() * 256);
+
+  const rgb = `rgb(${r}, ${g}, ${b})`;
+
+  return rgb;
 }
 
-colorBlack.style.backgroundColor = 'rgb(0, 0, 0)';
-colorYellow.style.backgroundColor = rgbRandom();
-colorRed.style.backgroundColor = rgbRandom();
-colorGreen.style.backgroundColor = rgbRandom();
+// Colore as caixas referência de cores
+function colorizePalette() {
+  for (let index = 0; index < colors.length; index += 1) {
+    colors[index].style.backgroundColor = rgbRandom();
+    colors[0].style.backgroundColor = 'black';
+  }
+}
+colorizePalette();
 
-
-function selectYellow(event) {
-
-  colorBlack.classList.remove('selected');
-  colorRed.classList.remove('selected');
-  colorGreen.classList.remove('selected');
-  event.target.classList.add('selected');
+// Cria pixel
+function createPixel() {
+  const lineOfPixels = document.createElement('div');
+  lineOfPixels.className = 'pixel';
+  pixelBoard.appendChild(lineOfPixels);
 }
 
-function selectRed(event) {
-    colorBlack.classList.remove('selected');
-    colorYellow.classList.remove('selected');
-    colorGreen.classList.remove('selected');
-    event.target.classList.add('selected');
-}
-
-function selectGreen(event) {
-    colorBlack.classList.remove('selected');
-    colorRed.classList.remove('selected');
-    colorYellow.classList.remove('selected');
-    event.target.classList.add('selected');
-}
-
-function selectBlack(event) {
-    colorYellow.classList.remove('selected');
-    colorRed.classList.remove('selected');
-    colorGreen.classList.remove('selected');
-    event.target.classList.add('selected');
-}
-
-
-const buttonYellow = document.getElementById('yellow');
-buttonYellow.addEventListener('click', selectYellow);
-
-const buttonRed = document.getElementById('red');
-buttonRed.addEventListener('click', selectRed);
-
-const buttonGreen = document.getElementById('green');
-buttonGreen.addEventListener('click', selectYellow);
-
-const buttonBlack = document.getElementById('black');
-buttonBlack.addEventListener('click', selectBlack);
-
-function paintPixels(event) {
-    const selectedColor = document.querySelector('.selected').style.backgroundColor;
-    const pixel = event.target;
-    pixel.style.backgroundColor = selectedColor;
-}
-
-const pixels = document.getElementById('pixel-board');
-pixels.addEventListener('click', paintPixels);
-
-function clearPixels() {
-    const pixels = document.getElementsByClassName('pixel');
-    for (let index = 0; index < pixels.length; index += 1) {
-        pixels[index].style.backgroundColor = 'white';
+// Apaga a tabela atual
+function deleteBoard() {
+  const pixel = document.querySelectorAll('.pixel');
+  for (let item = 0; item < pixel.length; item += 1) {
+    if (pixel[item].parentNode) {
+      pixel[item].parentNode.removeChild(pixel[item]);
     }
+  }
 }
 
-const buttonClean = document.getElementById('clear-board');
-buttonClean.addEventListener('click', clearPixels);
+// Salva a cor do pixel que estiver com a classe 'selected'
+let savedColor = 'black';
+pixelBoard.addEventListener('click', function (event) {
+  event.target.style.backgroundColor = savedColor;
+});
 
+// Cria a tabela com os pixels e adiciona a classe 'selected' ao primeiro pixel
+function createBoard() {
+  let userInput = Number(inBoardSize.value);
+  colors[0].classList.add('selected');
+  deleteBoard();
 
+  if (userInput < 5) {
+    userInput = 5;
+  } else if (userInput > 50) {
+    userInput = 50;
+  }
+
+  for (let pixelRow = 0; pixelRow < userInput; pixelRow += 1) {
+    for (let pixelColumn = 0; pixelColumn < userInput; pixelColumn += 1) {
+      createPixel();
+    }
+  }
+  pixelBoard.style.width = `${userInput * colors[0].offsetWidth}px`;
+  pixelBoard.style.height = `${userInput * colors[0].offsetWidth}px`;
+  pixelBoard.style.margin = 'auto';
+}
+createBoard();
+
+// Remove a classe 'selected'
+function removeClassSelected() {
+  for (let color = 0; color < colors.length; color += 1) {
+    if (colors[color].className === 'color selected') {
+      colors[color].classList.remove('selected');
+    }
+  }
+}
+
+// Adiciona a classe 'selected' ao pixel e guarda a cor em uma variável
+colorPalette.addEventListener('click', function (event) {
+  removeClassSelected();
+  if (event.target.className === 'color') {
+    event.target.classList.add('selected');
+    savedColor = event.target.style.backgroundColor;
+  }
+});
+
+// Preenche os pixels com a cor branca
+btnClear.addEventListener('click', () => {
+  const pixel = document.querySelectorAll('#pixel-board div');
+  for (let item = 0; item < pixel.length; item += 1) {
+    pixel[item].style.backgroundColor = '#fff';
+  }
+});
+
+// Evento 'VQV', altera o tamanho da tabela
+btnGenerateBoard.addEventListener('click', () => {
+  if (inBoardSize.value === '') {
+    alert('Board inválido!');
+  } else {
+    createBoard();
+  }
+});
+
+inBoardSize.addEventListener('keypress', function (tecla) {
+  if (tecla.keyCode === 13) {
+    createBoard();
+  }
+});
