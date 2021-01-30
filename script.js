69455984717100614c5ee863d873984e2ce3706d
thosijulio <thosijulio@gmail.com>
2021-01-30 01:08:17 -0300

    correcao exercicio 4

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ function criarQuadro () {
     for (index = 0 ; index < 5 ; index += 1) {
         let linha = document.createElement('tr');
         linha.className = 'pixel';
-        let pixelBoard = document.getElementById('pixels-board');
+        let pixelBoard = document.getElementById('pixel-board');
         pixelBoard.appendChild(linha);
         for (index2 = 0; index2 < 5; index2 += 1) {
             let coluna = document.createElement('td');
             coluna.className = 'pixel';
-            document.getElementById('pixels-board').appendChild(coluna);
+            document.getElementById('pixel-board').appendChild(coluna);
         }
 
     }
