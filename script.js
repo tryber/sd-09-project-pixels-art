c2f23d8e2acf760a5143c35c2439b36064db00c8
thosijulio <thosijulio@gmail.com>
2021-01-30 01:52:35 -0300

    resolução exercicio 7

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 window.onload = function () {
-    let corPreta = document.getElementById('color1');
-    corPreta.className += ' selected';
+  let corPreta = document.getElementById('color1');
+  corPreta.className += ' selected';
 }
 
 function criarQuadro () {
-    for (index = 0 ; index < 5 ; index += 1) {
-        let linha = document.createElement('tr');
-        let pixelBoard = document.getElementById('pixel-board');
-        pixelBoard.appendChild(linha);
-        for (index2 = 0; index2 < 5; index2 += 1) {
-            let coluna = document.createElement('td');
-            coluna.className = 'pixel';
-            document.getElementById('pixel-board').appendChild(coluna);
-        }
-
+  for (index = 0 ; index < 5 ; index += 1) {
+    let linha = document.createElement('tr');
+    let pixelBoard = document.getElementById('pixel-board');
+    pixelBoard.appendChild(linha);
+    for (index2 = 0; index2 < 5; index2 += 1) {
+      let coluna = document.createElement('td');
+      coluna.className = 'pixel';
+      document.getElementById('pixel-board').appendChild(coluna);
     }
-    
+  }
 }
 
+function selectedColorChoise () {
+  let paletaDeCores = document.getElementById('color-palette');
+  paletaDeCores.addEventListener ('click', function (event) {
+    let divsCores = document.getElementsByClassName('color');
+    for (index = 0; index < divsCores.length; index += 1) {
+      divsCores[index].classList.remove('selected');
+    }
+    event.target.className += ' selected';
+  })
+}
+
+selectedColorChoise();
 criarQuadro();
